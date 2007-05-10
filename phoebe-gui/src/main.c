2fe5526b6e7ced34db8e90dee4511e473885f5fd
oliveralatkovic <oliveralatkovic@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2007-05-10 12:20:53 +0000

    Removed auxiliary files generated by Glade, and added basic handling of various ways to quit the GUI.

--- phoebe-gui/src/main.c
+++ phoebe-gui/src/main.c
@@ -1,6 +1,27 @@
+#ifdef HAVE_CONFIG_H
+#  include <phoebe_gui_build_config.h>
+#endif
+
 #include <phoebe/phoebe.h>
 
-int main ()
+#include "phoebe_gui_callbacks.h"
+
+int main (int argc, char *argv[])
 {
+    GtkWidget *phoebe_window;
+    GladeXML *phoebe_gui;
+    
+    gtk_set_locale();
+    gtk_init(&argc, &argv);
+    glade_init();
+    
+    phoebe_gui = glade_xml_new("phoebe.glade", "phoebe_window", NULL);
+    glade_xml_signal_autoconnect (phoebe_gui);
+    
+    phoebe_window = glade_xml_get_widget(phoebe_gui, "phoebe_window");
+    gtk_widget_show(phoebe_window);
+    
+    gtk_main();
+    
 	return SUCCESS;
 }
