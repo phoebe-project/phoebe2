3c3c08c21daa5074a6bf182f55905feef17ca1d9
andrej_prsa <andrej_prsa@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2007-05-15 15:33:39 +0000

    Same thing, plot_rv_using_gnuplot () command.

--- devel/phoebe-scripter/src/phoebe_scripter_commands.c
+++ devel/phoebe-scripter/src/phoebe_scripter_commands.c
@@ -1592,6 +1592,11 @@ scripter_ast_value scripter_plot_rv_using_gnuplot (scripter_ast_list *args)
 		PHOEBE_output_dep   odep;
 
 		phoebe_get_parameter_value ("phoebe_rv_filename", vals[0].value.i-1, &filename);
+		if (!filename_exists (filename)) {
+			phoebe_scripter_output ("observed data file cannot be found, aborting.\n");
+			scripter_ast_value_array_free (vals, 3);
+			return out;
+		}
 
 		phoebe_get_parameter_value ("phoebe_rv_indep", vals[0].value.i-1, &readout_str);
 		get_input_independent_variable (readout_str, &iindep);
