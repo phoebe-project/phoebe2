bb8f303afc315eec79957d7345f8e93c1f0a4c21
gal_matijevic <gal_matijevic@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2007-06-15 11:41:15 +0000

    Widget hash table and accompanying wrappers were addad to the phoebe_gui_types source files.

--- phoebe-gui/src/phoebe_gui_types.h
+++ phoebe-gui/src/phoebe_gui_types.h
@@ -1,5 +1,5 @@
 #ifndef PHOEBE_GUI_TYPES
-#define PHOEBE_GUI_TYPES 1
+	#define PHOEBE_GUI_TYPES 1
 
 #include <gtk/gtk.h>
 #include <phoebe/phoebe.h>
@@ -19,10 +19,39 @@ typedef struct GUI_widget {
 	GtkWidget        *gtk;
 } GUI_widget;
 
+GUI_widget 	  *gui_widget_new 		();
+int 				gui_widget_add 		(char *name, GtkWidget *gtk, GUI_widget_type type, PHOEBE_parameter *par);
+unsigned int	gui_widget_hash 		(char *name);
+int 				gui_widget_hookup 	(GUI_widget *widget, GtkWidget *gtk, GUI_widget_type type, char *name, PHOEBE_parameter *par);
+int 				gui_widget_commit 	(GUI_widget *widget);
+int 				gui_widget_free 		(GUI_widget *widget);
+
+int 				gui_free_widgets 		();
+
 /* 
  * Create a hashed GUI_widget_table here (see phoebe_parameters.c for
  * reference) and, instead of a complicated table structure, just something
  * like GUI_widget_table *GUI_wt.
  */
+
+/***************************   WIDGET TABLE   ********************************/
+
+enum {
+	/* Do we need more than 103 buckets? */
+
+	GUI_WT_HASH_MULTIPLIER = 31, /*113*/
+	GUI_WT_HASH_BUCKETS    = 103 /*337*/
+};
+
+typedef struct GUI_wt_bucket {
+	GUI_widget				*widget;
+	struct GUI_wt_bucket *next;
+} GUI_wt_bucket;
+
+typedef struct GUI_widget_table {
+	GUI_wt_bucket *elem[GUI_WT_HASH_BUCKETS];
+} GUI_widget_table;
+
+GUI_widget_table *GUI_wt;
 
 #endif
