b072c670377896c236e98e8b6e2c3555f33b98e1
gal_matijevic <gal_matijevic@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2007-06-15 11:41:15 +0000

    Widget hash table and accompanying wrappers were addad to the phoebe_gui_types source files.

--- devel/phoebe-gui/src/phoebe_gui_types.c
+++ devel/phoebe-gui/src/phoebe_gui_types.c
@@ -3,6 +3,11 @@
 
 #include "phoebe_gui_types.h"
 
+int gui_init_widgets ()
+{
+	return SUCCESS;
+}
+
 GUI_widget *gui_widget_new ()
 {
 	GUI_widget *widget = phoebe_malloc (sizeof (*widget));
@@ -58,3 +63,83 @@ int gui_widget_hookup (GUI_widget *widget, GtkWidget *gtk, GUI_widget_type type,
 //
 // You could do some error handling if you wish, i.e. if gtk or par are null etc.
 //
+
+int gui_widget_add (char *name, GtkWidget *gtk, GUI_widget_type type, PHOEBE_parameter *par)
+{
+	GUI_widget *widget = gui_widget_new();	
+	
+	if (!gtk)
+		return -1;
+
+	if (!par)
+		return -1;
+
+	gui_widget_hookup (widget, gtk, type, name, par);
+	gui_widget_commit(widget);
+
+	return SUCCESS;
+}
+
+unsigned int gui_widget_hash (char *name)
+{
+	/*
+	 * This is the hashing function for storing widgets into the widget
+	 * table.
+	 */
+
+	unsigned int h = 0;
+	unsigned char *w;
+
+	for (w = (unsigned char *) name; *w != '\0'; w++)
+		h = GUI_WT_HASH_MULTIPLIER * h + *w;
+
+	return h % GUI_WT_HASH_BUCKETS;
+}
+
+int gui_widget_commit (GUI_widget *widget)
+{
+	int hash = gui_widget_hash (widget->name);
+	GUI_wt_bucket *elem = GUI_wt->elem[hash];
+
+	while (elem) {
+		if (strcmp (elem->widget->name, widget->name) == 0) break;
+		elem = elem->next;
+	}
+
+	if (elem) {
+		/* gui_error(widget already commited...) */
+		return SUCCESS;
+	}
+
+	else{
+		elem = phoebe_malloc (sizeof (*elem));
+
+		elem->widget = widget;
+		elem->next	 = GUI_wt->elem[hash];
+		GUI_wt->elem[hash] = elem;
+	}
+
+	return SUCCESS;
+}
+
+int gui_free_widgets ()
+{
+	/*
+	 * This function frees all widgets from the widget table.
+	 */
+
+	int i;
+	GUI_wt_bucket *elem;
+
+	for (i = 0; i < GUI_WT_HASH_BUCKETS; i++) {
+		while (GUI_wt->elem[i]) {
+			elem = GUI_wt->elem[i];
+			GUI_wt->elem[i] = elem->next;
+			gui_widget_free (elem->widget);
+			free (elem);
+		}
+	}
+	free (GUI_wt);
+
+	return SUCCESS;
+}
