091ba352d360575f652351fd1cc4266a87651b25
gal_matijevic <gal_matijevic@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2007-06-19 12:15:26 +0000

    Added some calls to gui_widget_add inside gui_init_widgets and function phoebe_gui_quit to the phoebe_gui_base source file.

--- devel/phoebe-gui/src/phoebe_gui_types.h
+++ devel/phoebe-gui/src/phoebe_gui_types.h
@@ -19,20 +19,15 @@ typedef struct GUI_widget {
 	GtkWidget        *gtk;
 } GUI_widget;
 
-GUI_widget 	       *gui_widget_new 		();
+GUI_widget 	  *gui_widget_new 		();
 int 				gui_widget_add 		(char *name, GtkWidget *gtk, GUI_widget_type type, PHOEBE_parameter *par);
-unsigned int	    gui_widget_hash 	(char *name);
+unsigned int	gui_widget_hash 		(char *name);
 int 				gui_widget_hookup 	(GUI_widget *widget, GtkWidget *gtk, GUI_widget_type type, char *name, PHOEBE_parameter *par);
 int 				gui_widget_commit 	(GUI_widget *widget);
-int 				gui_widget_free     (GUI_widget *widget);
+int 				gui_widget_free     	(GUI_widget *widget);
 
-int 				gui_free_widgets 	();
-
-/* 
- * Create a hashed GUI_widget_table here (see phoebe_parameters.c for
- * reference) and, instead of a complicated table structure, just something
- * like GUI_widget_table *GUI_wt.
- */
+int				gui_init_widgets		();
+int 				gui_free_widgets 		();
 
 /***************************   WIDGET TABLE   ********************************/
 
