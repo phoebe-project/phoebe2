9c5c854aa21dc9292e96328e5d5cd9fdd76d892f
andrej_prsa <aprsa09@gmail.com>
2007-06-22 15:45:32 +0000

    A stale GLADE define statement removed from phoebe_error_handling.

--- phoebe-lib/libphoebe/phoebe_error_handling.c
+++ phoebe-lib/libphoebe/phoebe_error_handling.c
@@ -5,13 +5,6 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-#define GLADE_HOOKUP_OBJECT(component,widget,name) \
-  g_object_set_data_full (G_OBJECT (component), name, \
-    gtk_widget_ref (widget), (GDestroyNotify) gtk_widget_unref)
-
-#define GLADE_HOOKUP_OBJECT_NO_REF(component,widget,name) \
-  g_object_set_data (G_OBJECT (component), name, widget)
-
 #include "phoebe_build_config.h"
 #include "phoebe_error_handling.h"
 #include "phoebe_global.h"
