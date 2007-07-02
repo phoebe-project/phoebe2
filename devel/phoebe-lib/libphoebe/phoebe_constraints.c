497a65e40c82443978d7787c01da1b74795d94be
andrej_prsa <andrej_prsa@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2007-07-02 20:11:34 +0000

    PHOEBE constraints are shaping up... :) The old idea with hard-coded constraints such as a \sin i = const and similar is now completely abandoned and has been replaced by a scary yet effective parser. Yep, that means that flex and bison are now required to build the library and tests have been added to configure. These tests are temporary, though, because they shouldn't stop the build if the parser sources are already present.
    So what's the idea: the library now implements a complete infrastructure 
    for abstract syntax trees (AST). The constraint has to be contained in a 
    parsable string that consists of mathematical operators (+, -, *, /, ^ 
    are currently supported), numeric values (i.e. 1, 20.3, 1.4e-2 etc), 
    built-in functions (sin, cos, tan, asin, acos, atan, exp, ln, log, sqrt 
    are currently supported) and qualifiers (i.e. phoebe_sma, phoebe_rm 
    etc). These constraints must be expressed explicitly. For example, 
    a constraint for phoebe_sma could be: 
    
    	"10/sin(phoebe_incl*3.14/180.0)"
    
    This constraint would keep a*sin(i) = 10.0 whenever phoebe_incl is 
    changed.
    
    Back to the implementation: the lexer and the parser are in place and 
    can already parse such strings, however the connection to parameters is 
    not yet done. All build files have been updated and are now generating 
    parser source code automatically by using flex and bison. For the 
    curious, sources phoebe_constraints.c and phoebe_constraints.h are most 
    informative... ;)

--- devel/phoebe-lib/libphoebe/phoebe_constraints.c
+++ devel/phoebe-lib/libphoebe/phoebe_constraints.c
@@ -2,20 +2,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "phoebe_constraints.h"
 #include "phoebe_error_handling.h"
 #include "phoebe_parameters.h"
 
-int apply_asini_constraint (double *a, double *i, double asini, int direction)
+PHOEBE_ast *phoebe_ast_add_numval (double numval)
 {
-switch (direction) {
-	case +1:
-		/* This means that i is constant and a should be computed:                */
-		*a = asini / sin(*i/180.0*M_PI);
-	break;
-	case -1:
-		/* This means that a is constant and i should be computed:                */
-		*i = 180.0/M_PI*asin(asini/*a);
-	break;
+	PHOEBE_ast *ast = phoebe_malloc (sizeof (*ast));
+	ast->type       = PHOEBE_AST_NUMVAL;
+	ast->val.numval = numval;
+
+	return ast;
 }
 
+PHOEBE_ast *phoebe_ast_add_builtin (char *builtin)
+{
+	PHOEBE_ast *ast    = phoebe_malloc (sizeof (*ast));
+	ast->type          = PHOEBE_AST_STRING;
+	ast->val.str       = strdup (builtin);
+
+	return ast;
+}
+
+PHOEBE_ast *phoebe_ast_add_parameter (PHOEBE_parameter *par)
+{
+	PHOEBE_ast *ast    = phoebe_malloc (sizeof (*ast));
+	ast->type          = PHOEBE_AST_PARAMETER;
+	ast->val.par       = par;
+
+	return ast;
+}
+
+PHOEBE_ast *phoebe_ast_add_node (PHOEBE_node_type type, PHOEBE_ast_list *args)
+{
+	PHOEBE_ast *ast    = phoebe_malloc (sizeof (*ast));
+	ast->type          = PHOEBE_AST_NODE;
+	ast->val.node.type = type;
+	ast->val.node.args = args;
+
+	return ast;
+}
+
+int phoebe_ast_print (int depth, PHOEBE_ast *in)
+{
+	int i;
+	PHOEBE_ast_list *args;
+
+	for (i = 0; i < depth; i++)
+		printf ("| ");
+
+	switch (in->type) {
+		case PHOEBE_AST_NUMVAL:
+			printf ("| %lf\n", in->val.numval);
+		break;
+		case PHOEBE_AST_STRING:
+			printf ("| \"%s\"\n", in->val.str);
+		break;
+		case PHOEBE_AST_PARAMETER:
+			printf ("| %s\n", in->val.par->qualifier);
+		break;
+		case PHOEBE_AST_NODE:
+			depth += 1;
+			printf ("| %d\n", in->val.node.type);
+			args = in->val.node.args;
+			while (args) {
+				phoebe_ast_print (depth, args->elem);
+				args = args->next;
+			}
+		break;
+		default:
+			printf ("exception handler invoked in scripter_ast_print (), please report this.\n");
+		break;
+	}
+
+	return SUCCESS;
+}
+
+PHOEBE_ast_list *phoebe_ast_construct_list (PHOEBE_ast *ast, PHOEBE_ast_list *list)
+{
+	PHOEBE_ast_list *out = phoebe_malloc (sizeof (*out));
+	out->elem = ast;
+	out->next = list;
+	return out;
 }
--- devel/phoebe-lib/libphoebe/phoebe_constraints.h
+++ devel/phoebe-lib/libphoebe/phoebe_constraints.h
@@ -1,6 +1,52 @@
 #ifndef _PHOEBE_CONSTRAINTS_H
-#define _PHOEBE_CONSTRAINTS_H
+#define _PHOEBE_CONSTRAINTS_H 1
 
+#include "phoebe_parameters.h"
 
+typedef enum {
+	PHOEBE_NODE_TYPE_ADD,
+	PHOEBE_NODE_TYPE_SUB,
+	PHOEBE_NODE_TYPE_MUL,
+	PHOEBE_NODE_TYPE_DIV,
+	PHOEBE_NODE_TYPE_POT,
+	PHOEBE_NODE_TYPE_BUILTIN
+} PHOEBE_node_type;
+
+typedef struct PHOEBE_ast {
+	enum {
+		PHOEBE_AST_NUMVAL,
+		PHOEBE_AST_STRING,
+		PHOEBE_AST_PARAMETER,
+		PHOEBE_AST_NODE
+	} type;
+	union {
+		double numval;
+		char  *str;
+		PHOEBE_parameter *par;
+		struct {
+			PHOEBE_node_type        type;
+			struct PHOEBE_ast_list *args;
+		} node;
+	} val;
+} PHOEBE_ast;
+
+typedef struct PHOEBE_ast_list {
+	PHOEBE_ast *elem;
+	struct PHOEBE_ast_list *next;
+} PHOEBE_ast_list;
+
+PHOEBE_ast_list *phoebe_ast_construct_list (PHOEBE_ast *ast, PHOEBE_ast_list *list);
+
+PHOEBE_ast *phoebe_ast_add_numval    (double numval);
+PHOEBE_ast *phoebe_ast_add_builtin   (char *builtin);
+PHOEBE_ast *phoebe_ast_add_parameter (PHOEBE_parameter *par);
+PHOEBE_ast *phoebe_ast_add_node      (PHOEBE_node_type type, PHOEBE_ast_list *args);
+
+int phoebe_ast_print (int depth, PHOEBE_ast *in);
+
+typedef struct PHOEBE_constraint {
+	PHOEBE_ast *func;
+	struct PHOEBE_constraint *next;
+} PHOEBE_constraint;
 
 #endif /* _PHOEBE_CONSTRAINTS_H */
