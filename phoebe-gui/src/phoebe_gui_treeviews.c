0ab8ffd28b3773cb5d18914096889b33cbb980f7
oliveralatkovic <oliveralatkovic@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2007-07-03 14:13:03 +0000

    The one-in-all TreeViewModel for RV related treeviews (one in the data tab, and one in the LD tab) is in place. I replaced the TextEntry and the Button in the phoebe_load_rv.glade with a FilechooserButton, so it is now possible to add and remove a RV file.
    I also updated the gui_init_widgets function with the remaining 
    TreeViewColumns, so the passband depentant parameters should all be 
    hooked up now.

--- phoebe-gui/src/phoebe_gui_treeviews.c
+++ phoebe-gui/src/phoebe_gui_treeviews.c
@@ -5,6 +5,7 @@
 int gui_init_treeviews(GladeXML *phoebe_window)
 {
     gui_init_lc_treeviews(phoebe_window);
+    gui_init_rv_treeviews(phoebe_window);
 
     return SUCCESS;
 }
@@ -30,6 +31,8 @@ int gui_init_lc_treeviews(GladeXML *phoebe_window)
     column      = gtk_tree_view_column_new_with_attributes("Filename", renderer, "text", LC_COL_FILENAME, NULL);
     gtk_tree_view_insert_column ((GtkTreeView*)phoebe_data_lc_treeview, column, LC_COL_FILENAME);
 
+    /* we don't need the filename in every treeview
+
     renderer    = gtk_cell_renderer_text_new ();
     column      = gtk_tree_view_column_new_with_attributes("Filename", renderer, "text", LC_COL_FILENAME, NULL);
     gtk_tree_view_insert_column ((GtkTreeView*)phoebe_para_lc_el3_treeview, column, LC_COL_FILENAME);
@@ -45,6 +48,8 @@ int gui_init_lc_treeviews(GladeXML *phoebe_window)
     renderer    = gtk_cell_renderer_text_new ();
     column      = gtk_tree_view_column_new_with_attributes("Filename", renderer, "text", LC_COL_FILENAME, NULL);
     gtk_tree_view_insert_column ((GtkTreeView*)phoebe_para_lc_ld_treeview, column, LC_COL_FILENAME);
+
+    */
 
     renderer    = gtk_cell_renderer_text_new ();
     column      = gtk_tree_view_column_new_with_attributes("Filter", renderer, "text", LC_COL_FILTER, NULL);
@@ -107,26 +112,90 @@ int gui_init_lc_treeviews(GladeXML *phoebe_window)
     gtk_tree_view_insert_column((GtkTreeView*)phoebe_para_lc_el3_treeview, column, LC_COL_EXTINCTION);
 
     renderer    = gtk_cell_renderer_text_new ();
-    column      = gtk_tree_view_column_new_with_attributes("Primary LD coefficient x", renderer, "text", LC_COL_LCX1, NULL);
-    gtk_tree_view_insert_column((GtkTreeView*)phoebe_para_lc_ld_treeview, column, LC_COL_LCX1);
+    column      = gtk_tree_view_column_new_with_attributes("X1", renderer, "text", LC_COL_X1, NULL);
+    gtk_tree_view_insert_column((GtkTreeView*)phoebe_para_lc_ld_treeview, column, LC_COL_X1);
 
     renderer    = gtk_cell_renderer_text_new ();
-    column      = gtk_tree_view_column_new_with_attributes("Secondary LD coefficient x", renderer, "text", LC_COL_LCX2, NULL);
-    gtk_tree_view_insert_column((GtkTreeView*)phoebe_para_lc_ld_treeview, column, LC_COL_LCX2);
+    column      = gtk_tree_view_column_new_with_attributes("X2", renderer, "text", LC_COL_X2, NULL);
+    gtk_tree_view_insert_column((GtkTreeView*)phoebe_para_lc_ld_treeview, column, LC_COL_X2);
 
     renderer    = gtk_cell_renderer_text_new ();
-    column      = gtk_tree_view_column_new_with_attributes("Primary LD coefficient y", renderer, "text", LC_COL_LCY1, NULL);
-    gtk_tree_view_insert_column((GtkTreeView*)phoebe_para_lc_ld_treeview, column, LC_COL_LCY1);
+    column      = gtk_tree_view_column_new_with_attributes("Y1", renderer, "text", LC_COL_Y1, NULL);
+    gtk_tree_view_insert_column((GtkTreeView*)phoebe_para_lc_ld_treeview, column, LC_COL_Y1);
 
     renderer    = gtk_cell_renderer_text_new ();
-    column      = gtk_tree_view_column_new_with_attributes("Secondary LD coefficient y", renderer, "text", LC_COL_LCY2, NULL);
-    gtk_tree_view_insert_column((GtkTreeView*)phoebe_para_lc_ld_treeview, column, LC_COL_LCY2);
+    column      = gtk_tree_view_column_new_with_attributes("Y2", renderer, "text", LC_COL_Y2, NULL);
+    gtk_tree_view_insert_column((GtkTreeView*)phoebe_para_lc_ld_treeview, column, LC_COL_Y2);
 
     gtk_tree_view_set_model ((GtkTreeView*)phoebe_data_lc_treeview,            lc_model);
     gtk_tree_view_set_model ((GtkTreeView*)phoebe_para_lc_el3_treeview,        lc_model);
     gtk_tree_view_set_model ((GtkTreeView*)phoebe_para_lc_levels_treeview,     lc_model);
     gtk_tree_view_set_model ((GtkTreeView*)phoebe_para_lc_levweight_treeview,  lc_model);
     gtk_tree_view_set_model ((GtkTreeView*)phoebe_para_lc_ld_treeview,         lc_model);
+
+    return SUCCESS;
+}
+
+int gui_init_rv_treeviews(GladeXML *phoebe_window)
+{
+    phoebe_data_rv_treeview             = glade_xml_get_widget (phoebe_window, "phoebe_data_rv_treeview");
+    phoebe_para_rv_ld_treeview          = glade_xml_get_widget (phoebe_window, "phoebe_params_ld_rvcoefs_treeview");
+
+    GtkTreeModel *rv_model = rv_model_create();
+
+    GtkCellRenderer     *renderer;
+    GtkTreeViewColumn   *column;
+
+    renderer    = gtk_cell_renderer_toggle_new ();
+    column      = gtk_tree_view_column_new_with_attributes("Active", renderer, "active", RV_COL_ACTIVE, NULL);
+    gtk_tree_view_insert_column ((GtkTreeView*)phoebe_data_rv_treeview, column, RV_COL_ACTIVE);
+
+    renderer    = gtk_cell_renderer_text_new ();
+    column      = gtk_tree_view_column_new_with_attributes("Filename", renderer, "text", RV_COL_FILENAME, NULL);
+    gtk_tree_view_insert_column ((GtkTreeView*)phoebe_data_rv_treeview, column, RV_COL_FILENAME);
+
+    renderer    = gtk_cell_renderer_text_new ();
+    column      = gtk_tree_view_column_new_with_attributes("Filter", renderer, "text", RV_COL_FILTER, NULL);
+    gtk_tree_view_insert_column ((GtkTreeView*)phoebe_data_rv_treeview, column, RV_COL_FILTER);
+
+    renderer    = gtk_cell_renderer_text_new ();
+    column      = gtk_tree_view_column_new_with_attributes("Filter", renderer, "text", RV_COL_FILTER, NULL);
+    gtk_tree_view_insert_column ((GtkTreeView*)phoebe_para_rv_ld_treeview, column, RV_COL_FILTER);
+
+    renderer    = gtk_cell_renderer_text_new ();
+    column      = gtk_tree_view_column_new_with_attributes("Col. 1", renderer, "text", RV_COL_ITYPE, NULL);
+    gtk_tree_view_insert_column ((GtkTreeView*)phoebe_data_rv_treeview, column, RV_COL_ITYPE);
+
+    renderer    = gtk_cell_renderer_text_new ();
+    column      = gtk_tree_view_column_new_with_attributes("Col. 2", renderer, "text", RV_COL_DTYPE, NULL);
+    gtk_tree_view_insert_column ((GtkTreeView*)phoebe_data_rv_treeview, column, RV_COL_DTYPE);
+
+    renderer    = gtk_cell_renderer_text_new ();
+    column      = gtk_tree_view_column_new_with_attributes("Col. 3", renderer, "text", RV_COL_WTYPE, NULL);
+    gtk_tree_view_insert_column ((GtkTreeView*)phoebe_data_rv_treeview, column, RV_COL_WTYPE);
+
+    renderer    = gtk_cell_renderer_text_new ();
+    column      = gtk_tree_view_column_new_with_attributes("Sigma", renderer, "text", RV_COL_SIGMA, NULL);
+    gtk_tree_view_insert_column ((GtkTreeView*)phoebe_data_rv_treeview, column, RV_COL_SIGMA);
+
+    renderer    = gtk_cell_renderer_text_new ();
+    column      = gtk_tree_view_column_new_with_attributes("X1", renderer, "text", RV_COL_X1, NULL);
+    gtk_tree_view_insert_column((GtkTreeView*)phoebe_para_rv_ld_treeview, column, RV_COL_X1);
+
+    renderer    = gtk_cell_renderer_text_new ();
+    column      = gtk_tree_view_column_new_with_attributes("X2", renderer, "text", RV_COL_X2, NULL);
+    gtk_tree_view_insert_column((GtkTreeView*)phoebe_para_rv_ld_treeview, column, RV_COL_X2);
+
+    renderer    = gtk_cell_renderer_text_new ();
+    column      = gtk_tree_view_column_new_with_attributes("Y1", renderer, "text", RV_COL_Y1, NULL);
+    gtk_tree_view_insert_column((GtkTreeView*)phoebe_para_rv_ld_treeview, column, RV_COL_Y1);
+
+    renderer    = gtk_cell_renderer_text_new ();
+    column      = gtk_tree_view_column_new_with_attributes("Y2", renderer, "text", RV_COL_Y2, NULL);
+    gtk_tree_view_insert_column((GtkTreeView*)phoebe_para_rv_ld_treeview, column, RV_COL_Y2);
+
+    gtk_tree_view_set_model ((GtkTreeView*)phoebe_data_rv_treeview,            rv_model);
+    gtk_tree_view_set_model ((GtkTreeView*)phoebe_para_rv_ld_treeview,         rv_model);
 
     return SUCCESS;
 }
@@ -152,5 +221,23 @@ GtkTreeModel *lc_model_create()
                                              G_TYPE_DOUBLE,         /* lcx2                 */
                                              G_TYPE_DOUBLE,         /* lcy1                 */
                                              G_TYPE_DOUBLE);        /* lcy2                 */
+    return (GtkTreeModel*)model;
+}
+
+GtkTreeModel *rv_model_create()
+{
+    /* Creating the model:                                                                  */
+    GtkListStore *model = gtk_list_store_new(RV_COL_COUNT,          /* number of columns    */
+                                             G_TYPE_BOOLEAN,        /* active               */
+                                             G_TYPE_STRING,         /* filename             */
+                                             G_TYPE_STRING,         /* passband             */
+                                             G_TYPE_STRING,         /* itype                */
+                                             G_TYPE_STRING,         /* dtype                */
+                                             G_TYPE_STRING,         /* wtype                */
+                                             G_TYPE_DOUBLE,         /* sigma                */
+                                             G_TYPE_DOUBLE,         /* rvx1                 */
+                                             G_TYPE_DOUBLE,         /* rvx2                 */
+                                             G_TYPE_DOUBLE,         /* rvy1                 */
+                                             G_TYPE_DOUBLE);        /* rvy2                 */
     return (GtkTreeModel*)model;
 }
