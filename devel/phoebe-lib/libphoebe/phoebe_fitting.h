8a8d74c4251958657bb98110f35c1e2c200e496b
andrej_prsa <andrej_prsa@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2007-07-19 17:32:53 +0000

    NMS is up and running! Computing passband luminosities and systemic velocity from data is still offline, but everything else, including constraints, should now work. Code should be polished a bit, since there are many remnants from the old implementation.

--- devel/phoebe-lib/libphoebe/phoebe_fitting.h
+++ devel/phoebe-lib/libphoebe/phoebe_fitting.h
@@ -15,7 +15,7 @@
 #define wd_dc(atmtab,pltab,corrs,errors,chi2s,cfval) dc_(atmtab,pltab,corrs,errors,chi2s,cfval,strlen(atmtab),strlen(pltab))
 
 int phoebe_minimize_using_dc  (FILE *dc_output, PHOEBE_minimizer_feedback *feedback);
-int phoebe_minimize_using_nms (double accuracy, int iter_no, FILE *nms_output, PHOEBE_minimizer_feedback *feedback);
+int phoebe_minimize_using_nms (double accuracy, int iter_max, FILE *nms_output, PHOEBE_minimizer_feedback *feedback);
 
 int kick_parameters (double sigma);
 
