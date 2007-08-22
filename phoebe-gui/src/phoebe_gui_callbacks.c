ef0763a5966f5eb39ea5a432801ba89d3a62559a
gal_matijevic <gal_matijevic@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2007-08-22 07:45:37 +0000

    Added phoebe_gui_accessories source files. These should hold all the helper functions that don't fitt anywhere else.

--- phoebe-gui/src/phoebe_gui_callbacks.c
+++ phoebe-gui/src/phoebe_gui_callbacks.c
@@ -3840,29 +3840,6 @@ on_phoebe_sidesheet_detach_button_clicked
 	}
 }
 
-void set_text_view_from_file (GtkWidget *text_view, gchar *filename)
-{
-	GtkTextBuffer *text_buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (text_view));
-	GtkTextIter iter;
-
-	FILE *file = fopen(filename, "r");
-	char line[255];
-	int i=1;
-	int maxlines=50;
-
-	fgets (line, 255, file);
-	gtk_text_buffer_set_text (text_buffer, line, -1);	
-
-	gtk_text_buffer_get_iter_at_line (text_buffer, &iter, i);
-		while(!feof (file) && i<maxlines){
-			fgets (line, 255, file);	
-			gtk_text_buffer_insert (text_buffer, &iter, line, -1);
-			i++;
-		}
-
-	fclose(file);
-}
-
 void on_phoebe_load_lc_filechooserbutton_selection_changed (GtkFileChooserButton *filechooserbutton, gpointer user_data)
 {
 	set_text_view_from_file ((GtkWidget *) user_data, gtk_file_chooser_get_filename ((GtkFileChooser*)filechooserbutton));
