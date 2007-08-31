bda9f6ea7bec363d0677fa2b3a60330e0f6c279a
gal_matijevic <gal_matijevic@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2007-08-31 07:48:08 +0000

    Added very basic LC plotting funcionality. Everything is still very much unstable so phoebe will most likely segfult if something wrong is pressed. It may also cause some problems with the different gnuplot setup.

--- phoebe-gui/src/phoebe_gui_callbacks.c
+++ phoebe-gui/src/phoebe_gui_callbacks.c
@@ -104,14 +104,14 @@ on_phoebe_data_lc_add_button_clicked   (GtkButton       *button,
 
 			GtkTreeIter filter_iter;
 			gint 		filter_number;
-			gchar 		filter_selected[255] = "Undefined";
+			gchar 		filter_selected[255] = "Johnson:V";
 
 			GtkWidget *phoebe_data_lc_treeview = gui_widget_lookup("phoebe_data_lc_treeview")->gtk;
             model = gtk_tree_view_get_model((GtkTreeView*)phoebe_data_lc_treeview);
 
 			if (gtk_combo_box_get_active_iter (GTK_COMBO_BOX (phoebe_load_lc_filter_combobox), &filter_iter)) {
 				gtk_tree_model_get (gtk_combo_box_get_model(GTK_COMBO_BOX(phoebe_load_lc_filter_combobox)), &filter_iter, 1, &filter_number, -1);
-				sprintf (filter_selected, "%s  (%.0lfnm)", PHOEBE_passbands[filter_number]->name, PHOEBE_passbands[filter_number]->effwl/10.);
+				sprintf (filter_selected, "%s:%s", PHOEBE_passbands[filter_number]->set, PHOEBE_passbands[filter_number]->name);
 			}
 
             PHOEBE_parameter *indep     = phoebe_parameter_lookup ("phoebe_lc_indep");
@@ -197,7 +197,7 @@ on_phoebe_data_lc_edit_button_clicked  (GtkButton       *button,
 		gchar *filter;
         gdouble sigma;
 
-        gchar filter_selected[255] = "Undefined";
+        gchar filter_selected[255] = "Johnson:V";
 		gint filter_number;
 		GtkTreeIter filter_iter;
 
@@ -242,7 +242,7 @@ on_phoebe_data_lc_edit_button_clicked  (GtkButton       *button,
 
 				if (gtk_combo_box_get_active_iter (GTK_COMBO_BOX (phoebe_load_lc_filter_combobox), &filter_iter)) {
 					gtk_tree_model_get (gtk_combo_box_get_model(GTK_COMBO_BOX(phoebe_load_lc_filter_combobox)), &filter_iter, 1, &filter_number, -1);
-					sprintf (filter_selected, "%s  (%.0lfnm)", PHOEBE_passbands[filter_number]->name, PHOEBE_passbands[filter_number]->effwl/10.);
+					sprintf (filter_selected, "%s:%s", PHOEBE_passbands[filter_number]->set, PHOEBE_passbands[filter_number]->name);
 				}
 
                 PHOEBE_parameter *indep     = phoebe_parameter_lookup("phoebe_lc_indep");
@@ -1093,7 +1093,7 @@ on_phoebe_lc_plot_toolbutton_clicked  (GtkToolButton   *toolbutton,
 	GUI_widget *box = gui_widget_lookup ("phoebe_lc_plot_table");
 	GUI_widget *parent = gui_widget_lookup ("phoebe_lc_plot_parent_table");
 
-	detach_box_from_parent (box->gtk, parent->gtk, &PHOEBE_WINDOW_LC_PLOT_IS_DETACHED, "PHOEBE - LC Plot", 700, 550);
+	detach_box_from_parent (box->gtk, parent->gtk, &PHOEBE_WINDOW_LC_PLOT_IS_DETACHED, "PHOEBE - LC Plot", 750, 550);
 }
 
 
@@ -1104,7 +1104,7 @@ on_phoebe_rv_plot_toolbutton_clicked  (GtkToolButton   *toolbutton,
 	GUI_widget *box = gui_widget_lookup ("phoebe_rv_plot_table");
 	GUI_widget *parent = gui_widget_lookup ("phoebe_rv_plot_parent_table");
 
-	detach_box_from_parent (box->gtk, parent->gtk, &PHOEBE_WINDOW_RV_PLOT_IS_DETACHED, "PHOEBE - RV Plot", 700, 550);
+	detach_box_from_parent (box->gtk, parent->gtk, &PHOEBE_WINDOW_RV_PLOT_IS_DETACHED, "PHOEBE - RV Plot", 750, 550);
 }
 
 
@@ -4003,7 +4003,7 @@ on_phoebe_lc_plot_detach_button_clicked
 	GUI_widget *box = gui_widget_lookup ("phoebe_lc_plot_table");
 	GUI_widget *parent = gui_widget_lookup ("phoebe_lc_plot_parent_table");
 
-	detach_box_from_parent (box->gtk, parent->gtk, &PHOEBE_WINDOW_LC_PLOT_IS_DETACHED, "PHOEBE - LC Plot", 700, 550);
+	detach_box_from_parent (box->gtk, parent->gtk, &PHOEBE_WINDOW_LC_PLOT_IS_DETACHED, "PHOEBE - LC Plot", 750, 550);
 }
 
 void
@@ -4014,7 +4014,7 @@ on_phoebe_rv_plot_detach_button_clicked
 	GUI_widget *box = gui_widget_lookup ("phoebe_rv_plot_table");
 	GUI_widget *parent = gui_widget_lookup ("phoebe_rv_plot_parent_table");
 
-	detach_box_from_parent (box->gtk, parent->gtk, &PHOEBE_WINDOW_RV_PLOT_IS_DETACHED, "PHOEBE - RV Plot", 700, 550);
+	detach_box_from_parent (box->gtk, parent->gtk, &PHOEBE_WINDOW_RV_PLOT_IS_DETACHED, "PHOEBE - RV Plot", 750, 550);
 }
 
 void
@@ -4052,4 +4052,157 @@ on_phoebe_rv_plot_options_obs_checkbutton_toggled
 	GUI_widget *combobox = gui_widget_lookup ("phoebe_rv_plot_options_obs_combobox");
 	gtk_widget_set_sensitive (combobox->gtk, gtk_toggle_button_get_active(togglebutton));
 	if(gtk_combo_box_get_active(GTK_COMBO_BOX(combobox->gtk))==-1) gtk_combo_box_set_active(GTK_COMBO_BOX(combobox->gtk),0);
+}
+
+
+
+int gui_plot_lc_using_gnuplot()
+{
+	PHOEBE_curve *obs;
+	PHOEBE_curve *syn;
+
+	PHOEBE_vector *indep;
+
+	gchar oname[255];	/* observed curve filename  */
+	gchar sname[255]; 	/* synthetic curve filename */
+	gchar cname[255];	/* gnuplot command filename */
+	gchar pname[255]; 	/* plot filename			*/
+	gchar  line[255];  	/* buffer line				*/
+
+	gint ofd, sfd, cfd, pfd;	/* file descriptors */
+
+	gint i;
+	gint status;
+
+	gboolean plot_obs;
+	gboolean plot_syn;
+	gint vertices = 100;
+	gint index = 0;				
+
+	GUI_widget *plot_image				= gui_widget_lookup ("phoebe_lc_plot_image");
+	GUI_widget *syn_checkbutton 		= gui_widget_lookup ("phoebe_lc_plot_options_syn_checkbutton");
+	GUI_widget *obs_checkbutton 		= gui_widget_lookup ("phoebe_lc_plot_options_obs_checkbutton");
+	GUI_widget *vertices_no_spinbutton 	= gui_widget_lookup ("phoebe_lc_plot_options_vertices_no_spinbutton");
+	GUI_widget *obs_combobox 			= gui_widget_lookup ("phoebe_lc_plot_options_obs_combobox");
+	GUI_widget *x_combobox 				= gui_widget_lookup ("phoebe_lc_plot_options_x_combobox");
+	GUI_widget *y_combobox				= gui_widget_lookup ("phoebe_lc_plot_options_y_combobox");
+
+	plot_obs = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(obs_checkbutton->gtk));
+	plot_syn = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(syn_checkbutton->gtk));		
+	
+	if(1){
+		printf("%d\n",index);
+		/*index = gtk_combo_box_get_active(GTK_COMBO_BOX(obs_combobox->gtk))-1; problems with function w->p*/
+		printf("%d\n",index);
+
+		obs = phoebe_curve_new_from_pars (PHOEBE_CURVE_LC, index);
+		syn = phoebe_curve_new ();
+		syn->type = PHOEBE_CURVE_LC;
+
+		phoebe_curve_transform (obs, PHOEBE_COLUMN_PHASE, PHOEBE_COLUMN_FLUX, PHOEBE_COLUMN_WEIGHT);
+
+		/* Write first curve data to the temporary file */
+		sprintf(oname, "%s/phoebe-lc-XXXXXX", PHOEBE_TEMP_DIR);
+		ofd = mkstemp (oname);
+
+		for (i=0;i<obs->indep->dim;i++) {
+			sprintf(line, "%lf\t%lf\t%lf\n", obs->indep->val[i], obs->dep->val[i], obs->weight->val[i]) ;
+			write(ofd, line, strlen(line));
+		}
+		close(ofd) ;
+
+		/* Write first curve data to the temporary file */
+		/* Create phase vector */
+		indep = phoebe_vector_new ();
+		phoebe_vector_alloc (indep, vertices);
+		for (i = 0; i < vertices; i++) indep->val[i] = -0.6 + 1.2 * (double) i/(vertices-1);
+
+		status = phoebe_curve_compute (syn, indep, index, PHOEBE_COLUMN_PHASE, PHOEBE_COLUMN_FLUX);
+		phoebe_vector_free (indep);
+
+		sprintf(sname, "%s/phoebe-lc-XXXXXX", PHOEBE_TEMP_DIR);
+		sfd = mkstemp (sname);
+
+		for (i=0;i<syn->indep->dim;i++) {
+			sprintf(line, "%lf\t%lf\n", syn->indep->val[i], syn->dep->val[i]) ;
+			write(sfd, line, strlen(line));
+		}
+		close(sfd) ;
+	
+
+	//----------------
+
+	sprintf(pname, "%s/phoebe-lc-plot-XXXXXX", PHOEBE_TEMP_DIR);
+	pfd = mkstemp (pname);
+
+	sprintf(cname, "%s/phoebe-com-XXXXXX", PHOEBE_TEMP_DIR);
+	cfd = mkstemp (cname);	
+
+	sprintf(line, "set terminal png truecolor nocrop enhanced small size 614,336\n");
+	write(cfd, line, strlen(line));
+	sprintf(line, "set output '%s'\n", pname);
+	write(cfd, line, strlen(line));
+	sprintf(line, "set offsets 0.05, 0.05, 0, 0\n");
+	write(cfd, line, strlen(line));
+	
+	sprintf(line, "set mxtics\n");
+	write(cfd, line, strlen(line));
+	sprintf(line, "set mytics\n");
+	write(cfd, line, strlen(line));
+
+	sprintf(line, "set grid xtics ytics\n");
+	write(cfd, line, strlen(line));
+	sprintf(line, "set xrange [-0.7:0.7]\n");
+	write(cfd, line, strlen(line));
+	sprintf(line, "set xlabel 'Phase'\n");
+	write(cfd, line, strlen(line));
+	sprintf(line, "set ylabel 'Flux'\n");
+	write(cfd, line, strlen(line));
+	sprintf(line, "set lmargin 5\n");
+	write(cfd, line, strlen(line));
+
+	sprintf(line, "plot  '%s' w l notitle,'%s' w p notitle\n", sname, oname);
+	write(cfd, line, strlen(line));
+
+	close(cfd);
+
+	sprintf(line,"gnuplot \"%s\"",cname);
+
+	system(line);
+
+	gtk_image_set_from_pixbuf(GTK_IMAGE(plot_image->gtk), gdk_pixbuf_new_from_file(pname, NULL));	
+
+
+	remove(oname);
+	remove(sname);
+	remove(cname);
+	remove(pname);
+
+
+	//----------------
+
+	phoebe_curve_free(obs);
+	phoebe_curve_free(syn);
+	}
+
+	return SUCCESS;
+}
+
+
+void
+on_phoebe_lc_plot_plot_button_clicked
+                                        (GtkButton       *button,
+                                        gpointer         user_data)
+{
+	int lcno;	
+
+	PHOEBE_parameter *par = phoebe_parameter_lookup("phoebe_lcno");
+	phoebe_parameter_get_value(par, &lcno);
+
+	if(lcno > 0){
+		gui_get_values_from_widgets();	
+		gui_plot_lc_using_gnuplot();
+	}
+
+	else printf("Nothing to plot...\n");	
 }
