33580d0528c90d932486361c66a40ad45bcacf70
andrej_prsa <andrej_prsa@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2007-11-09 20:29:06 +0000

    Error handling infrastructure added to PHOEBE GUI. There are three output functions defined:
    	phoebe_gui_output ():   general output (unconditional)
    	phoebe_gui_debug ():    debug output   (conditional)
    	phoebe_gui_fatal ():    catch crashes  (exits on execution)
    
    The debug mode is controlled by gui_verbosity_mode parameter.
    
    Next to that, PHOEBE_gui_error_code enum is defined that should hold 
    symbolic names of all exceptions. Right now I put only a generic symbol
    that should be changed with a real exception. A printing function 
    phoebe_gui_error () has also been added.

---
+++ devel/phoebe-gui/src/phoebe_gui_error_handling.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+#include <phoebe/phoebe.h>
+
+#include "phoebe_gui_error_handling.h"
+
+char *phoebe_gui_error (int code)
+{
+	/*
+	 * This function takes the error code and translates it to a human-readable
+	 * string.
+	 */
+
+	switch (code) {
+		case ERROR_GUI_GENERIC_ERROR:
+			return "this is an example of an error.\n";
+		default:
+			return phoebe_error (code);
+	}
+}
+
+int phoebe_gui_fatal (const char *fmt, ...)
+{
+	va_list ap;
+
+	printf ("PHOEBE GUI fatal: ");
+	va_start (ap, fmt);
+	vprintf (fmt, ap);
+	va_end (ap);
+
+	exit (EXIT_FAILURE);
+}
+
+int phoebe_gui_debug (const char *fmt, ...)
+{
+	va_list ap;
+	int r;
+	int verbosity;
+
+	phoebe_parameter_get_value (phoebe_parameter_lookup ("gui_verbosity_level"), &verbosity);
+	if (verbosity == 0) return 0;
+
+	printf ("GUI debug: ");
+	va_start (ap, fmt);
+	r = vprintf (fmt, ap);
+	va_end (ap);
+
+	return r;
+}
+
+int phoebe_gui_output (const char *fmt, ...)
+{
+	va_list ap;
+
+	printf ("GUI: ");
+	va_start (ap, fmt);
+	vprintf (fmt, ap);
+	va_end (ap);
+
+	return SUCCESS;
+}
