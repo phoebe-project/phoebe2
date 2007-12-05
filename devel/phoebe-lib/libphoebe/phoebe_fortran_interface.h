465d66f4267113a628d0b84e25c11a3a7514b493
andrej_prsa <andrej_prsa@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2007-12-05 18:04:55 +0000

    Build files updated to work without f2c, flex or bison if the source files are present (i.e. from a distribution tarball, but not from SVN). Hopefully this commit resolves all remaining problems with the missing f2c.

--- devel/phoebe-lib/libphoebe/phoebe_fortran_interface.h
+++ devel/phoebe-lib/libphoebe/phoebe_fortran_interface.h
@@ -1,9 +1,9 @@
 #ifndef PHOEBE_FORTRAN_INTERFACE_H
 	#define PHOEBE_FORTRAN_INTERFACE_H 1
 
-#include "../libwd/f2c.h"
 #include <string.h>
 
+#include "f2c.h"
 #include "phoebe_global.h"
 
 #define wd_wrlci(fn,mpage,nref,mref,ifsmv1,ifsmv2,icor1,icor2,ld,jdphs,hjd0,period,dpdt,pshift,stddev,noise,seed,jdstrt,jdend,jdinc,phstrt,phend,phinc,phnorm,mode,ipb,ifat1,ifat2,n1,n2,perr0,dperdt,the,vunit,e,sma,f1,f2,vga,xincl,gr1,gr2,abunin,tavh,tavc,alb1,alb2,phsv,pcsv,rm,xbol1,xbol2,ybol1,ybol2,iband,hla,cla,x1a,x2a,y1a,y2a,el3,opsf,mzero,factor,wla,nsp1,xlat1,xlong1,radsp1,temsp1,nsp2,xlat2,xlong2,radsp2,temsp2) \
