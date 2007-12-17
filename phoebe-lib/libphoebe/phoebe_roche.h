839b7863794b2b324e441ea1a34d6205c595a900
andrej_prsa <aprsa09@gmail.com>
2007-12-17 08:24:35 +0000

    Initial implementation of star surface rasterization done. The old (obsolete) implementation removed from phoebe_roche sources, and sources have been removed too. Now everything is defined in phoebe_model sources. Please take a look to see the logic of the implementation.
    To use these functions, the easiest is something like this:
    
      PHOEBE_star_surface *surface = phoebe_star_surface_rasterize (60);
    
    Then, i.e. to compute local radii, one would use:
    
      phoebe_star_surface_compute_radii (surface, 3.75, 1.0, 1.0, 1.0);
    
    To print out the xz plane shapes:
    
      for (i = 0; i < surface->elemno; i++) {
        printf ("%3d\t% 8.3lf% 8.3lf% 8.3lf% 8.3lf% 8.3lf\n", i, 
            surface->theta[i], surface->phi[i], surface->rho[i], 
            surface->rho[i]*surface->sinth[i]*surface->cosphi[i], 
            surface->rho[i]*surface->costh[i]);
      }
    
    This allows for point sampling visualization as discussed on PHOEBE 
    telecon on Wed, Dec 12.

--- phoebe-lib/libphoebe/phoebe_roche.h
+++
@@ -1,7 +0,0 @@
-#ifndef PHOEBE_ROCHE_H
-  #define PHOEBE_ROCHE_H 1
-
-double phoebe_compute_polar_radius (double Omega, double D, double q);
-double phoebe_get_radius (double rp, double q, double D, double F, double lambda, double nu);
-
-#endif
