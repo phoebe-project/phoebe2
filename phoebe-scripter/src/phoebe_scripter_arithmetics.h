3b2cd0e5d450a50a764ea4acb96889f2c0f0e439
andrej_prsa <aprsa09@gmail.com>
2008-04-08 15:16:14 +0000

    The remaining wrappers for comparison parameters written and implemented in the arithmetics source.

--- phoebe-scripter/src/phoebe_scripter_arithmetics.h
+++ phoebe-scripter/src/phoebe_scripter_arithmetics.h
@@ -9,8 +9,11 @@ int scripter_ast_values_multiply (scripter_ast_value *out, scripter_ast_value va
 int scripter_ast_values_divide   (scripter_ast_value *out, scripter_ast_value val1, scripter_ast_value val2);
 int scripter_ast_values_raise    (scripter_ast_value *out, scripter_ast_value val1, scripter_ast_value val2);
 int scripter_ast_values_equal    (scripter_ast_value *out, scripter_ast_value val1, scripter_ast_value val2);
+int scripter_ast_values_nequal   (scripter_ast_value *out, scripter_ast_value val1, scripter_ast_value val2);
 int scripter_ast_values_lequal   (scripter_ast_value *out, scripter_ast_value val1, scripter_ast_value val2);
 int scripter_ast_values_gequal   (scripter_ast_value *out, scripter_ast_value val1, scripter_ast_value val2);
+int scripter_ast_values_greater  (scripter_ast_value *out, scripter_ast_value val1, scripter_ast_value val2);
+int scripter_ast_values_less     (scripter_ast_value *out, scripter_ast_value val1, scripter_ast_value val2);
 
 #endif
 
