5bb0a5e294a242cf3536d49f170e53daa60c57ae
andrej_prsa <andrej_prsa@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2008-04-08 06:47:47 +0000

    Wrappers for the >= and <= operators written and implemented. These comparison operators can now mix vectors and scalars, i.e. {1,2,3} < 4 is valid.

--- devel/phoebe-scripter/src/phoebe_scripter_arithmetics.h
+++ devel/phoebe-scripter/src/phoebe_scripter_arithmetics.h
@@ -9,6 +9,8 @@ int scripter_ast_values_multiply (scripter_ast_value *out, scripter_ast_value va
 int scripter_ast_values_divide   (scripter_ast_value *out, scripter_ast_value val1, scripter_ast_value val2);
 int scripter_ast_values_raise    (scripter_ast_value *out, scripter_ast_value val1, scripter_ast_value val2);
 int scripter_ast_values_equal    (scripter_ast_value *out, scripter_ast_value val1, scripter_ast_value val2);
+int scripter_ast_values_lequal   (scripter_ast_value *out, scripter_ast_value val1, scripter_ast_value val2);
+int scripter_ast_values_gequal   (scripter_ast_value *out, scripter_ast_value val1, scripter_ast_value val2);
 
 #endif
 
