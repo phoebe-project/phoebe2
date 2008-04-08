2d3a6a5448e5a7e66c23c0c4bcc72083c898fb08
andrej_prsa <aprsa09@gmail.com>
2008-04-08 06:47:47 +0000

    Wrappers for the >= and <= operators written and implemented. These comparison operators can now mix vectors and scalars, i.e. {1,2,3} < 4 is valid.

--- phoebe-scripter/src/phoebe_scripter_ast.c
+++ phoebe-scripter/src/phoebe_scripter_ast.c
@@ -580,9 +580,8 @@ scripter_ast_value scripter_ast_evaluate (scripter_ast *in)
 				 * dedicated directive or command.
 				 */
 
-				double x, y;
-				phoebe_ld_get_coefficients (LD_LAW_SQRT, phoebe_passband_lookup ("Johnson:V"), 0.5, 6500, 4, &x, &y);
-				printf ("coeffs: %lf, %lf\n", x, y);
+				double L1, L2;
+				phoebe_calculate_critical_potentials (0.2, 1.0, 0.0, &L1, &L2);
 				return out;
 			}
 			if (in->value.node.kind == kind_statement) {
@@ -2221,106 +2220,32 @@ scripter_ast_value scripter_ast_evaluate (scripter_ast *in)
 				out.type = type_void;
 				scripter_ast_value_free (val1);
 				scripter_ast_value_free (val2);
-						return out;
+				return out;
 			}
 
-#warning ADD_WRAPPER_FOR_KIND_LEQUAL
 			if (in->value.node.kind == kind_lequal) {
 				scripter_ast_value val1 = scripter_ast_evaluate (in->value.node.args->elem);
 				scripter_ast_value val2 = scripter_ast_evaluate (in->value.node.args->next->elem);
 
-				if (val1.type == type_int && val2.type == type_int) {
-					out.type = type_bool;
-					out.value.b = (val1.value.i <= val2.value.i);
-					scripter_ast_value_free (val1);
-					scripter_ast_value_free (val2);
-					return out;
+				status = scripter_ast_values_lequal (&out, val1, val2);
+				if (status != SUCCESS) {
+					phoebe_scripter_output ("%s", phoebe_error (status));
+					out.type = type_void;
 				}
-				if (val1.type == type_int && val2.type == type_double) {
-					out.type = type_bool;
-					out.value.b = (val1.value.i <= val2.value.d);
-					scripter_ast_value_free (val1);
-					scripter_ast_value_free (val2);
-					return out;
-				}
-				if (val1.type == type_double && val2.type == type_int) {
-					out.type = type_bool;
-					out.value.b = (val1.value.d <= val2.value.i);
-					scripter_ast_value_free (val1);
-					scripter_ast_value_free (val2);
-					return out;
-				}
-				if (val1.type == type_double && val2.type == type_double) {
-					out.type = type_bool;
-					out.value.b = (val1.value.d <= val2.value.d);
-					scripter_ast_value_free (val1);
-					scripter_ast_value_free (val2);
-					return out;
-				}
-				if (val1.type == type_vector && val2.type == type_vector) {
-					out.type = type_bool;
-					status = phoebe_vector_leq_than (&out.value.b, val1.value.vec, val2.value.vec);
-					if (status != SUCCESS) {
-						phoebe_scripter_output ("%s", phoebe_scripter_error (status));
-						out.type = type_void;
-					}
-					scripter_ast_value_free (val1);
-					scripter_ast_value_free (val2);
-					return out;
-				}
-				phoebe_scripter_output ("exception handler invoked in kind_lequal, please report this!\n");
-				out.type = type_void;
 				scripter_ast_value_free (val1);
 				scripter_ast_value_free (val2);
 				return out;
 			}
 
-#warning ADD_WRAPPER_FOR_KIND_GEQUAL
 			if (in->value.node.kind == kind_gequal) {
 				scripter_ast_value val1 = scripter_ast_evaluate (in->value.node.args->elem);
 				scripter_ast_value val2 = scripter_ast_evaluate (in->value.node.args->next->elem);
 
-				if (val1.type == type_int && val2.type == type_int) {
-					out.type = type_bool;
-					out.value.b = (val1.value.i >= val2.value.i);
-					scripter_ast_value_free (val1);
-					scripter_ast_value_free (val2);
-					return out;
+				status = scripter_ast_values_gequal (&out, val1, val2);
+				if (status != SUCCESS) {
+					phoebe_scripter_output ("%s", phoebe_error (status));
+					out.type = type_void;
 				}
-				if (val1.type == type_int && val2.type == type_double) {
-					out.type = type_bool;
-					out.value.b = (val1.value.i >= val2.value.d);
-					scripter_ast_value_free (val1);
-					scripter_ast_value_free (val2);
-					return out;
-				}
-				if (val1.type == type_double && val2.type == type_int) {
-					out.type = type_bool;
-					out.value.b = (val1.value.d >= val2.value.i);
-					scripter_ast_value_free (val1);
-					scripter_ast_value_free (val2);
-					return out;
-				}
-				if (val1.type == type_double && val2.type == type_double) {
-					out.type = type_bool;
-					out.value.b = (val1.value.d >= val2.value.d);
-					scripter_ast_value_free (val1);
-					scripter_ast_value_free (val2);
-					return out;
-				}
-				if (val1.type == type_vector && val2.type == type_vector) {
-					out.type = type_bool;
-					status = phoebe_vector_geq_than (&out.value.b, val1.value.vec, val2.value.vec);
-					if (status != SUCCESS) {
-						phoebe_scripter_output ("%s", phoebe_scripter_error (status));
-						out.type = type_void;
-					}
-					scripter_ast_value_free (val1);
-					scripter_ast_value_free (val2);
-					return out;
-				}
-				phoebe_scripter_output ("exception handler invoked in kind_gequal, please report this!\n");
-				out.type = type_void;
 				scripter_ast_value_free (val1);
 				scripter_ast_value_free (val2);
 				return out;
