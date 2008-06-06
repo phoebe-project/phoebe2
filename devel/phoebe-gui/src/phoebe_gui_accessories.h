f52c31280a957fcb71420f28dd67c4f0ccc32458
patrickwils <patrickwils@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2008-06-06 15:07:19 +0000

    Gtk on Windows does not keep track of the active window when there are detached windows.  Especially when plotting, the detached plotting window loses focus.  To circumvent this, a temporary window is shown when the plot is being created in a detached window.

--- devel/phoebe-gui/src/phoebe_gui_accessories.h
+++ devel/phoebe-gui/src/phoebe_gui_accessories.h
@@ -4,7 +4,9 @@
 #include "phoebe_gui_types.h"
 
 void gui_set_text_view_from_file (GtkWidget *text_view, gchar *filename);
-void gui_detach_box_from_parent (GtkWidget *box, GtkWidget *parent, gboolean *flag, gchar *window_title, gint x, gint y);
+GtkWidget *gui_detach_box_from_parent (GtkWidget *box, GtkWidget *parent, gboolean *flag, gchar *window_title, gint x, gint y);
+GtkWidget *gui_show_temp_window();
+void gui_hide_temp_window(GtkWidget *temp_window, GtkWidget *new_active_window);
 
 int gui_open_parameter_file();
 int gui_save_parameter_file();
