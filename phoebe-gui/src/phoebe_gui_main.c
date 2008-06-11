7fb7750cdaef7b34d80d5c6bd8e5222a3b1a3243
patrickwils <patrickwils@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2008-06-11 23:05:26 +0000

    With a new version of the libglade dll on Windows, the specific code to define GtkFileChooserButton and GtkAboutDialog is no longer needed.

--- phoebe-gui/src/phoebe_gui_main.c
+++ phoebe-gui/src/phoebe_gui_main.c
@@ -14,99 +14,6 @@
 #include "phoebe_gui_treeviews.h"
 #include "phoebe_gui_types.h"
 
-#ifdef __MINGW32__
-#include <glade/glade-build.h>
-#include <libgen.h>
-
-gchar *PROGRAM_NAME = "";
-
-// GtkFileChooserButton is not defined in libglade for MinGW/MSYS, so it has to be defined here
-GtkWidget *gui_GtkFileChooserButton(GladeXML *xml, GType widget_type, GladeWidgetInfo *info)
-{
-	gint width_chars = 0;
-	GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN;
-	gchar *title = NULL;
-	gint i;
-
-	for (i = 0; i < info->n_properties; i++) {
-		if (!strcmp (info->properties[i].name, "width_chars")) {
-			width_chars = atoi(info->properties[i].value);
-		}
-		else if (!strcmp (info->properties[i].name, "action")) {
-			if (!strcmp (info->properties[i].value, "GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER"))
-				action = GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER;
-		}
-		else if (!strcmp (info->properties[i].name, "title")) {
-			title = info->properties[i].value;
-		}
-	}
-	GtkWidget *filechooserbutton = gtk_file_chooser_button_new (title, action);
-	gtk_widget_show(filechooserbutton);
-	if (width_chars > 0)
-		gtk_file_chooser_button_set_width_chars(GTK_FILE_CHOOSER_BUTTON(filechooserbutton), width_chars);
-
-	return filechooserbutton;
-}
-
-// GtkAboutDialog is not defined in libglade for MinGW/MSYS, so it has to be defined here
-GtkWidget *gui_GtkAboutDialog(GladeXML *xml, GType widget_type, GladeWidgetInfo *info)
-{
-	gchar *program_name = "";
-	gchar *version = "";
-	gchar *comments = "";
-	gchar *license = "";
-	gchar *website = "";
-	gchar *website_label = "";
-	gchar **authors = malloc(30 * sizeof(gchar *));
-	gchar **documenters = malloc(30 * sizeof(gchar *));
-	gint i;
-
-	for (i = 0; i < info->n_properties; i++) {
-		if (!strcmp (info->properties[i].name, "program_name")) {
-			program_name = info->properties[i].value;
-		}
-		else if (!strcmp (info->properties[i].name, "version")) {
-			version = info->properties[i].value;
-		}
-		else if (!strcmp (info->properties[i].name, "comments")) {
-			comments = info->properties[i].value;
-		}
-		else if (!strcmp (info->properties[i].name, "license")) {
-			license = info->properties[i].value;
-		}
-		else if (!strcmp (info->properties[i].name, "website")) {
-			website = info->properties[i].value;
-		}
-		else if (!strcmp (info->properties[i].name, "website_label")) {
-			website_label = info->properties[i].value;
-		}
-		else if (!strcmp (info->properties[i].name, "authors")) {
-			int j = 0;
-			authors[j++] = strtok (info->properties[i].value, "\n");
-			while ((authors[j++] = strtok (NULL, ",")));
-		}
-		else if (!strcmp (info->properties[i].name, "documenters")) {
-			int j = 0;
-			documenters[j++] = strtok (info->properties[i].value, "\n");
-			while ((documenters[j++] = strtok (NULL, ",")));
-		}
-	}
-	GtkWidget *aboutdialog = gtk_about_dialog_new ();
-	gtk_about_dialog_set_program_name(GTK_ABOUT_DIALOG(aboutdialog), (PROGRAM_NAME) ? PROGRAM_NAME : program_name);
-	gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(aboutdialog), version);
-	gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(aboutdialog), comments);
-	gtk_about_dialog_set_license(GTK_ABOUT_DIALOG(aboutdialog), license);
-	gtk_about_dialog_set_website(GTK_ABOUT_DIALOG(aboutdialog), website);
-	gtk_about_dialog_set_website_label(GTK_ABOUT_DIALOG(aboutdialog), website_label);
-	gtk_about_dialog_set_authors(GTK_ABOUT_DIALOG(aboutdialog), (const gchar **)authors);
-	gtk_about_dialog_set_documenters(GTK_ABOUT_DIALOG(aboutdialog), (const gchar **)documenters);
-
-	free(documenters);
-
-	return aboutdialog;
-}
-#endif
-
 int parse_startup_line (int argc, char *argv[])
 {
 	/*
@@ -162,14 +69,6 @@ int main (int argc, char *argv[])
 	gtk_set_locale ();
 	gtk_init (&argc, &argv);
 	glade_init ();
-#ifdef __MINGW32__
-	PROGRAM_NAME = basename(argv[0]);
-	char *exe = strstr(PROGRAM_NAME, ".exe");
-	if (exe)
-		*exe = '\0';
-	glade_register_widget (GTK_TYPE_FILE_CHOOSER_BUTTON, gui_GtkFileChooserButton, NULL, NULL);
-	glade_register_widget (GTK_TYPE_ABOUT_DIALOG, gui_GtkAboutDialog, NULL, NULL);
-#endif
 
 	status = phoebe_init ();
 	if (status != SUCCESS) {
