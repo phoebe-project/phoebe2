7dd61f0d3d33b6bc4938410040c697540e5239de
andrej_prsa <aprsa09@gmail.com>
2008-07-05 23:58:49 +0000

    PHOEBE now passes data arrays to WD directly rather than via dci file. This was done to avoid flux rounding to 4 significant digits and, more importantly, weight rounding to 1 significant digit.
    The transformation from weights to sigmas now no longer rescales the 
    weights for the DCI input file; the weight values are now consistent 
    between what DC returns and what phoebe_cf_compute() returns.

--- phoebe-lib/libphoebe/phoebe_fortran_interface.h
+++ phoebe-lib/libphoebe/phoebe_fortran_interface.h
@@ -8,8 +8,8 @@
 #define wd_wrlci(fn,mpage,nref,mref,ifsmv1,ifsmv2,icor1,icor2,ld,jdphs,hjd0,period,dpdt,pshift,stddev,noise,seed,jdstrt,jdend,jdinc,phstrt,phend,phinc,phnorm,mode,ipb,ifat1,ifat2,n1,n2,perr0,dperdt,the,vunit,e,sma,f1,f2,vga,xincl,gr1,gr2,abunin,tavh,tavc,alb1,alb2,phsv,pcsv,rm,xbol1,xbol2,ybol1,ybol2,iband,hla,cla,x1a,x2a,y1a,y2a,el3,opsf,mzero,factor,wla,nsp1,xlat1,xlong1,radsp1,temsp1,nsp2,xlat2,xlong2,radsp2,temsp2) \
            wrlci_(fn,&mpage,&nref,&mref,&ifsmv1,&ifsmv2,&icor1,&icor2,&ld,&jdphs,&hjd0,&period,&dpdt,&pshift,&stddev,&noise,&seed,&jdstrt,&jdend,&jdinc,&phstrt,&phend,&phinc,&phnorm,&mode,&ipb,&ifat1,&ifat2,&n1,&n2,&perr0,&dperdt,&the,&vunit,&e,&sma,&f1,&f2,&vga,&xincl,&gr1,&gr2,&abunin,&tavh,&tavc,&alb1,&alb2,&phsv,&pcsv,&rm,&xbol1,&xbol2,&ybol1,&ybol2,&iband,&hla,&cla,&x1a,&x2a,&y1a,&y2a,&el3,&opsf,&mzero,&factor,&wla,&nsp1,xlat1,xlong1,radsp1,temsp1,&nsp2,xlat2,xlong2,radsp2,temsp2,strlen(fn))
 
-#define wd_wrdci(fn,del,kep,ifder,ifm,ifr,xlamda,kspa,nspa,kspb,nspb,ifvc1,ifvc2,nlc,k0,kdisk,isym,nppl,nref,mref,ifsmv1,ifsmv2,icor1,icor2,ld,jdphs,hjd0,period,dpdt,pshift,mode,ipb,ifat1,ifat2,n1,n2,n1l,n2l,perr0,dperdt,the,vunit,e,a,f1,f2,vga,xincl,gr1,gr2,abunin,tavh,tavc,alb1,alb2,phsv,pcsv,rm,xbol1,xbol2,ybol1,ybol2,iband,hla,cla,x1a,x2a,y1a,y2a,el3,opsf,noise,sigma,wla,nsp1,xlat1,xlong1,radsp1,temsp1,nsp2,xlat2,xlong2,radsp2,temsp2,vertno,indep,dep,weight) \
-           wrdci_(fn,del,kep,&ifder,&ifm,&ifr,&xlamda,&kspa,&nspa,&kspb,&nspb,&ifvc1,&ifvc2,&nlc,&k0,&kdisk,&isym,&nppl,&nref,&mref,&ifsmv1,&ifsmv2,&icor1,&icor2,&ld,&jdphs,&hjd0,&period,&dpdt,&pshift,&mode,&ipb,&ifat1,&ifat2,&n1,&n2,&n1l,&n2l,&perr0,&dperdt,&the,&vunit,&e,&a,&f1,&f2,&vga,&xincl,&gr1,&gr2,&abunin,&tavh,&tavc,&alb1,&alb2,&phsv,&pcsv,&rm,&xbol1,&xbol2,&ybol1,&ybol2,iband,hla,cla,x1a,x2a,y1a,y2a,el3,opsf,noise,sigma,wla,&nsp1,xlat1,xlong1,radsp1,temsp1,&nsp2,xlat2,xlong2,radsp2,temsp2,&vertno,indep,dep,weight,strlen(fn))
+#define wd_wrdci(fn,del,kep,ifder,ifm,ifr,xlamda,kspa,nspa,kspb,nspb,ifvc1,ifvc2,nlc,k0,kdisk,isym,nppl,nref,mref,ifsmv1,ifsmv2,icor1,icor2,ld,jdphs,hjd0,period,dpdt,pshift,mode,ipb,ifat1,ifat2,n1,n2,n1l,n2l,perr0,dperdt,the,vunit,e,a,f1,f2,vga,xincl,gr1,gr2,abunin,tavh,tavc,alb1,alb2,phsv,pcsv,rm,xbol1,xbol2,ybol1,ybol2,iband,hla,cla,x1a,x2a,y1a,y2a,el3,opsf,noise,sigma,wla,nsp1,xlat1,xlong1,radsp1,temsp1,nsp2,xlat2,xlong2,radsp2,temsp2,knobs,indep,dep,weight) \
+           wrdci_(fn,del,kep,&ifder,&ifm,&ifr,&xlamda,&kspa,&nspa,&kspb,&nspb,&ifvc1,&ifvc2,&nlc,&k0,&kdisk,&isym,&nppl,&nref,&mref,&ifsmv1,&ifsmv2,&icor1,&icor2,&ld,&jdphs,&hjd0,&period,&dpdt,&pshift,&mode,&ipb,&ifat1,&ifat2,&n1,&n2,&n1l,&n2l,&perr0,&dperdt,&the,&vunit,&e,&a,&f1,&f2,&vga,&xincl,&gr1,&gr2,&abunin,&tavh,&tavc,&alb1,&alb2,&phsv,&pcsv,&rm,&xbol1,&xbol2,&ybol1,&ybol2,iband,hla,cla,x1a,x2a,y1a,y2a,el3,opsf,noise,sigma,wla,&nsp1,xlat1,xlong1,radsp1,temsp1,&nsp2,xlat2,xlong2,radsp2,temsp2,knobs,indep,dep,weight,strlen(fn))
 
 typedef struct WD_LCI_parameters {
 	int    MPAGE;
@@ -177,7 +177,10 @@ typedef struct WD_DCI_parameters {
 	double *spot2long;
 	double *spot2rad;
 	double *spot2temp;
-	PHOEBE_curve **obs;
+	int    *knobs;
+	double *indeps;
+	double *fluxes;
+	double *weights;
 } WD_DCI_parameters;
 
 int create_lci_file (char *filename, WD_LCI_parameters *param);
