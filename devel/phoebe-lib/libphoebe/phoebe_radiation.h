5306165d31c14b7c5e47e9c8342b134ec47a4573
andrej_prsa <andrej_prsa@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2009-06-05 21:21:47 +0000

    Function compute_passband_intensity() prefixed with phoebe_ and documented. Its source, phoebe_radiation.c, was missing from phoebe.h, causing implicit declaration to be reported at link time. Fixed.

--- devel/phoebe-lib/libphoebe/phoebe_radiation.h
+++ devel/phoebe-lib/libphoebe/phoebe_radiation.h
@@ -1,3 +1,3 @@
 #include "phoebe_types.h"
 
-int compute_passband_intensity (double *intensity, PHOEBE_hist *SED, PHOEBE_hist *PTF);
+int phoebe_compute_passband_intensity (double *intensity, PHOEBE_hist *SED, PHOEBE_hist *PTF);
