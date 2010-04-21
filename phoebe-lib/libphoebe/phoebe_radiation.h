0075d34b887ef548ac9ee5db3f2435db4850dd95
andrej_prsa <aprsa09@gmail.com>
2010-04-21 02:04:50 +0000

    Intensity integration now supports energy weighting and photon flux weighting. The latter is more appropriate for CCD observations and the differences are notable for broad passbands.

--- phoebe-lib/libphoebe/phoebe_radiation.h
+++ phoebe-lib/libphoebe/phoebe_radiation.h
@@ -1,3 +1,3 @@
 #include "phoebe_types.h"
 
-int phoebe_compute_passband_intensity (double *intensity, PHOEBE_hist *SED, PHOEBE_hist *PTF);
+int phoebe_compute_passband_intensity (double *intensity, PHOEBE_hist *SED, PHOEBE_hist *PTF, int mode);
