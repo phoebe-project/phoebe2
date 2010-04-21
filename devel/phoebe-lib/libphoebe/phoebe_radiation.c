d536148c7c7a387d82f58f7b7eabc128cde694cb
andrej_prsa <andrej_prsa@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2010-04-21 02:04:50 +0000

    Intensity integration now supports energy weighting and photon flux weighting. The latter is more appropriate for CCD observations and the differences are notable for broad passbands.

--- devel/phoebe-lib/libphoebe/phoebe_radiation.c
+++ devel/phoebe-lib/libphoebe/phoebe_radiation.c
@@ -8,13 +8,14 @@
 #include <gsl/gsl_spline.h>
 #endif
 
-int phoebe_compute_passband_intensity (double *intensity, PHOEBE_hist *SED, PHOEBE_hist *PTF)
+int phoebe_compute_passband_intensity (double *intensity, PHOEBE_hist *SED, PHOEBE_hist *PTF, int mode)
 {
 	/**
 	 * phoebe_compute_passband_intensity:
 	 * @intensity: placeholder for the computed value
 	 * @SED: spectral energy distribution to be integrated
 	 * @PTF: passband transmission function to be integrated
+	 * @mode: 1 for energy-weighted and 2 for photon flux-weighted integration
 	 *
 	 * This function computes passband intensity of the passed spectral
 	 * energy distribution function and the passed passband transmission
@@ -38,6 +39,13 @@ int phoebe_compute_passband_intensity (double *intensity, PHOEBE_hist *SED, PHOE
 	/* Error handling: */
 	if (!SED) return ERROR_HIST_NOT_INITIALIZED;
 	if (!PTF) return ERROR_HIST_NOT_INITIALIZED;
+
+	/* If we are weighting the integrals with photon flux, we need to multiply
+	 * the response function with \lambda:
+	 */
+	if (mode == 2)
+		for (i = 0; i < PTF->bins; i++)
+			PTF->val[i] *= 0.5*(PTF->range[i]+PTF->range[i+1]);
 	
 	/* Compute the integral over the passband transmission function: */
 	status = phoebe_hist_integrate (&intPTF, PTF, PTF->range[0], PTF->range[PTF->bins]);
