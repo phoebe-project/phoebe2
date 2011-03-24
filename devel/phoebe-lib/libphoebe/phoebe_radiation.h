e57d892636448e6b5c98b5777a7aafe3b16d61ba
andrej_prsa <andrej_prsa@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2011-03-24 15:51:12 +0000

    #ifndef ... #endif added for seamless compiling on macs.

--- devel/phoebe-lib/libphoebe/phoebe_radiation.h
+++ devel/phoebe-lib/libphoebe/phoebe_radiation.h
@@ -1,3 +1,9 @@
+#ifndef PHOEBE_RADIATION_H
+	#define PHOEBE_RADIATION_H 1
+
 #include "phoebe_types.h"
 
 int phoebe_compute_passband_intensity (double *intensity, PHOEBE_hist *SED, PHOEBE_hist *PTF, int mode);
+
+#endif
+
