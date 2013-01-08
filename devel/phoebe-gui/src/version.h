6841fdf9045d16467456b87a1e3854e850446a03
andrej_prsa <andrej_prsa@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2013-01-08 01:02:56 +0000

    Moved the 0.3x branch from devel/ to stable/.

--- devel/phoebe-gui/src/version.h
+++
@@ -1,2 +0,0 @@
-#define SVN_DATE "$Date$"
-#define SVN_REV "$Rev$"
--- devel/phoebe-scripter/src/phoebe_scripter_configuration.h
+++
@@ -1,6 +0,0 @@
-#ifndef PHOEBE_SCRIPTER_CONFIGURATION
-	#define PHOEBE_SCRIPTER_CONFIGURATION 1
-
-int scripter_create_config_file ();
-
-#endif
