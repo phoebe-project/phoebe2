df2d35d293f8ae1e94accc39e4121e0d63a5b5ed
pieterdegroote <pieterdegroote@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2013-03-07 20:28:09 +0000

    added datasets.parse_headser

--- devel/phoebe-testlib/datafiles/parse_rv.py
+++ devel/phoebe-testlib/datafiles/parse_rv.py
@@ -2,18 +2,18 @@ import phoebe
 
 logger = phoebe.get_basic_logger()
 
-output = phoebe.parse_rv('example0.rv')
-print output['componentA']
-print output['componentA'][0][-1]
-print output['componentA'][1][-1]
-print '==================='
+#output = phoebe.parse_rv('example0.rv')
+#print output['componentA']
+#print output['componentA'][0][-1]
+#print output['componentA'][1][-1]
+#print '==================='
 
-output = phoebe.parse_rv('example1.rv')
-print output[0][-1]
-print output[1][-1]
-print '==================='
+#output = phoebe.parse_rv('example1.rv')
+#print output[0][-1]
+#print output[1][-1]
+#print '==================='
 
-output = phoebe.parse_rv('example3.rv')
-print output['starA'][0][-1]
-print output['starB'][0][-1]
+output = phoebe.parse_rv('example3.rv',components=['A',None,'A','B','B'])
+print output['A'][0][-1]
+print output['B'][0][-1]
 print '==================='
