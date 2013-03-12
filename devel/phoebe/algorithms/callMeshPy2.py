40c011765e45357592b58b58bb9f8f9b4eb8e9b7
giammarc <giammarc@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2013-03-12 23:34:19 +0000

    Added Rotated Roche to the marching algorithm Fixed the memory leak, at least one of them

---
+++ devel/phoebe/algorithms/callMeshPy2.py
@@ -0,0 +1,58 @@
+
+from marching2FLib import getMesh
+
+
+from math import sqrt, sin, cos, acos, atan2, trunc, pi
+import numpy as np
+
+try:
+    from enthought.mayavi.mlab import *
+except ImportError:
+    from mayavi.mlab import *
+
+
+SPHERE, BINARY_ROCHE, MISALIGNED_BINARY_ROCHE, ROTATE_ROCHE, TORUS = range(5)
+# for BINARY_ROCHE
+#numParams = 5
+#D = 0.75
+#q = 0.6
+#F = 1.5
+#Omega = 3.8
+
+#sphere
+#numParams = 2
+#R = 4.0
+
+# Roateta Roche
+numParams = 3
+Omega = 0.9
+RPole = 1.0
+
+
+# first works well roche, second sphere
+#delta = 0.02
+#delta = 0.2
+delta = 0.05
+
+#tableA = getMesh(BINARY_ROCHE, numParams, D, q, F, Omega, delta)
+#tableA = getMesh(SPHERE, numParams, R, delta)
+tableA = getMesh(ROTATE_ROCHE, numParams, Omega, RPole, delta)
+#print "N = ",len(table)
+#print "table = ",tableA
+#table = np.zeros((len(Ts), 16))
+print "length of tableA = ", len(tableA[0])
+print "table[0] = ",tableA[0]
+#table1 = np.zeros((10, 2))
+#print "table1 = ",table1
+table = tableA[0]
+N = len(table)
+print "the number of triangles = ", N
+x = np.hstack([table[:,i+4] for i in range(0,9,3)])
+y = np.hstack([table[:,i+4] for i in range(1,9,3)])
+z = np.hstack([table[:,i+4] for i in range(2,9,3)])
+triangles = [(i,N+i,2*N+i) for i in range(N)]
+triangular_mesh(x,y,z,triangles)
+show()
+
+
+#print "q = ", table
