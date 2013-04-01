66d0f0eeb4f9118156ab93d0cc8e153fead2071d
pieterdegroote <pieterdegroote@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2013-04-01 17:49:42 +0000

    added ascii.py, which can hopefully be replaced by a future numpy version if loadtxt is more flexible to work with input/output of comments

---
+++ devel/phoebe/io/ascii.py
@@ -0,0 +1,112 @@
+"""
+Read and write ASCII files.
+"""
+import gzip
+import logging
+import os
+import re
+import numpy as np
+
+logger = logging.getLogger("IO.ASCII")
+logger.addHandler(logging.NullHandler())
+
+def write_array(data, filename, **kwargs):
+    """
+    Save a numpy array to an ASCII file.
+    
+    Add comments via keyword comments (a list of strings denoting every comment
+    line). By default, the comment lines will be preceded by the C{commentchar}.
+    If you want to override this behaviour, set C{commentchar=''}.
+    
+    If you give a record array, you can simply set C{header} to C{True} to write
+    the header, instead of specifying a list of strings.
+    
+    @keyword header: optional header for column names
+    @type header: list of str (or boolean for record arrays)
+    @keyword comments: comment lines
+    @type comments: list of str
+    @keyword commentchar: comment character
+    @type commentchar: str
+    @keyword sep: separator for the columns and header names
+    @type sep: str
+    @keyword axis0: string denoting the orientation of the matrix. If you gave
+    a list of columns, set C{axis0='cols'}, otherwise C{axis='rows'} (default).
+    @type axis0: str, one of C{cols}, C{rows}.
+    @keyword mode: file mode (a for appending, w for (over)writing...)
+    @type mode: char (one of 'a','w'...)
+    @keyword auto_width: automatically determine the width of the columns
+    @type auto_width: bool
+    @keyword formats: formats to use to write each column
+    @type formats: list of string formatters
+    """
+    header = kwargs.get('header',None)
+    comments = kwargs.get('comments',None)
+    commentchar = kwargs.get('commentchar','#')
+    sep = kwargs.get('sep',' ')
+    axis0 = kwargs.get('axis0','rows')
+    mode = kwargs.get('mode','w')
+    auto_width = kwargs.get('auto_width',False)
+    formats = kwargs.get('formats',None)
+    # use '%g' or '%f' or '%e' for writing floats automatically from record arrays with auto width
+    use_float = kwargs.get('use_float','%f') 
+    
+    #-- switch to rows first if a list of columns is given
+    if not isinstance(data,np.ndarray):
+        data = np.array(data)
+    if not 'row' in axis0.lower():
+        data = data.T
+    
+    if formats is None:
+        try:
+            formats = [('S' in str(data[col].dtype) and '%s' or use_float) for col in data.dtype.names]
+        except TypeError:
+            formats = [('S' in str(col.dtype) and '%s' or '%s') for col in data.T]
+    #-- determine width of columns: also take the header label into account
+    col_widths = []
+    #-- for record arrays
+    if auto_width is True and header==True:
+        for fmt,head in zip(formats,data.dtype.names):
+            col_widths.append(max([len('%s'%(fmt)%(el)) for el in data[head]]+[len(head)]))
+    #-- for normal arrays and specified header
+    elif auto_width is True and header is not None:
+        for i,head in enumerate(header):
+            col_widths.append(max([len('%s'%(formats[i])%(el)) for el in data[:,i]]+[len(head)]))
+    #-- for normal arrays without header
+    elif auto_width is True and header is not None:
+        for i in range(data.shape[1]):
+            col_widths.append(max([len('%s'%(formats[i])%(el)) for el in data[:,i]]))
+    
+    if header is True:
+        col_fmts = [str(data.dtype[i]) for i in range(len(data.dtype))]
+        header = data.dtype.names
+    else:
+        col_fmts = None
+    
+    ff = open(filename,mode)
+    if comments is not None:
+        ff.write('\n'.join(comments)+'\n')
+    
+    #-- WRITE HEADER
+    #-- when header is desired and automatic width
+    if header is not None and col_widths:
+        ff.write('#'+sep.join(['%%%s%ss'%(('s' in fmt and '-' or ''),cw)%(head) for fmt,head,cw in zip(formats,header,col_widths)])+'\n')
+    #-- when header is desired
+    elif header is not None:
+        ff.write('#'+sep.join(header)+'\n')
+    
+    #-- WRITE COLUMN FORMATS
+    if col_fmts is not None and col_widths:
+        ff.write('#'+sep.join(['%%%s%ss'%(('s' in fmt and '-' or ''),cw)%(colfmt) for fmt,colfmt,cw in zip(formats,col_fmts,col_widths)])+'\n')
+    elif col_fmts is not None:
+        ff.write('#'+sep.join(['%%%ss'%('s' in fmt and '-' or '')%(colfmt) for fmt,colfmt in zip(formats,col_fmts)])+'\n')
+    
+    #-- WRITE DATA
+    #-- with automatic width
+    if col_widths:
+        for row in data:
+            ff.write(' '+sep.join(['%%%s%s%s'%(('s' in fmt and '-' or ''),cw,fmt[1:])%(col) for col,cw,fmt in zip(row,col_widths,formats)])+'\n')
+    #-- without automatic width
+    else:
+        for row in data:
+            ff.write(sep.join(['%s'%(col) for col in row])+'\n')
+    ff.close()
