(1) Fixed issues with magnetic field strength and orientation. There still seems to be a bug in the calculation of the Zeeman splitting though (2) Added interface to MINUIT fitting package (optional) (3) Improvements in interface to Stokes profiles, and their ParameterSets (3) Improvements in structure of documentation

devel/phoebe/atmospheres/roche.py
@@ -2,6 +2,39 @@
 Derive parameters in the Roche approximation.
 
 Most of these parameters are local atmospheric parameters.
+
+**Binaries**
+
+.. autosummary::
+
+    approximate_lagrangian_points
+    calculate_critical_potentials
+    calculate_critical_radius
+    potential2radius
+    radius2potential
+    change_component
+    binary_surface_gravity
+    binary_potential
+    binary_potential_gradient
+    improve_potential
+
+**Single stars**
+
+.. autosummary::
+
+    critical_angular_frequency
+    critical_velocity
+    fast_rotation_radius
+    diffrotlaw_to_internal
+    
+
+**Phoebe specific:**
+
+.. autosummary::
+
+    temperature_zeipel
+    temperature_espinosa
+
 """
 import logging
 import numpy as np
@@ -145,11 +178,13 @@ def approximate_lagrangian_points(q,d=1.,sma=1.):
     (0.990028699155567, 1.010038030137821, -0.9999982474945123)
     
     The Sun/Earth L1 is located about 1.5 million km from Earth:
+    
     >>> L1,L2,L3 = approximate_lagrangian_points(constants.Mearth/constants.Msol,sma=constants.au)
     >>> (constants.au-L1)/1000.
     1491685.3744362793
     
     The Earth/Moon L1 is located about 60.000 km from the Moon:
+    
     >>> dlun = 384499. # km
     >>> L1,L2,L3 = approximate_lagrangian_points(constants.Mlun/constants.Mearth,sma=dlun)
     >>> (dlun-L1)
@@ -440,17 +475,19 @@ def binary_surface_gravity(x,y,z,d,omega,mass1,mass2,normalize=False):
         return g_pole
 
 def binary_potential(r,theta,phi,Phi,q,d,F,component=1):
-    """
+    r"""
     Unitless eccentric asynchronous Roche potential in spherical coordinates.
     
-    See Wilson, 1979.
+    See [Wilson1979]_.
     
     The  synchronicity parameter F is 1 for synchronised circular orbits. For
-    pseudo-synchronous eccentrical orbits, it is equal to (Hut, 1981)
+    pseudo-synchronous eccentrical orbits, it is equal to [Hut1981]_
     
-    F = sqrt( (1+e)/ (1-e)^3)
+    .. math::
     
-    Periastron is reached when d = 1-e.
+        F = \sqrt{ \frac{(1+e)}{(1-e)^3}}
+    
+    Periastron is reached when :math:`d = 1-e`.
         
     @param r: radius of Roche volume at potential Phi (in units of semi-major axis)
     @type r: float
@@ -524,28 +561,36 @@ def binary_potential_gradient(x,y,z,q,d,F,component=1,normalize=False):
         return dOmega
 
 def improve_potential(V,V0,pot0,rpole0,d,q,F,sma=1.):
-    """
+    r"""
     Return an approximate value for the potential in eccentric orbit.
     
-    Given the volume V0 at a reference time t0, this function returns
-    the approximate value for a new potential at another time at t1, to
-    conserve the volume. At t1, the volume at the potential V(pot=pot(t0))
-    equals V.
+    Given the volume :math:`V_0` at a reference time :math:`t_0`, this
+    function returns the approximate value for a new potential :math:`P(t_1)`
+    at another time at :math:`t_1`, to conserve the volume. At :math:`t_1`,
+    the volume at the potential :math:`V(P=P(t_0))` equals :math:`V_0`.
     
-    The value is calculated as follows::
+    The value is calculated as follows:
     
-        Pot(t) = DeltaPot + Pot(t0)
-        Pot(t) = DeltaPot/DeltaR * DeltaR + Pot(t0)
+    .. math::
     
-    We know that::
-    
-        DeltaV/DeltaR = 4*pi*R**2
-        DeltaR = DeltaV/4*pi*R**2
+        P(t) & = \Delta P + P(t_0)
         
-    We compute DeltaPot/DeltaR (the gradient of the potential) at the
-    pole with L{marching.dBinaryRochedz}. Thus::
+        P(t) & = \left(\frac{\Delta P}{\Delta R}\right) \Delta R + P(t_0)
     
-        Pot(t) = DeltaPot/DeltaR * DeltaR + Pot(t0)
+    We know that:
+    
+    .. math::
+    
+        \frac{\Delta V}{\Delta R} & = 4\pi R^2
+        
+        \Delta R & = \frac{\Delta V}{4\pi R^2}
+        
+    We compute :math:`\Delta P / \Delta R` (the gradient of the potential) at the
+    pole with L{marching.dBinaryRochedz}. Thus:
+    
+    .. math::
+    
+        P(t) = \left(\frac{\Delta P}{\Delta R}\right) \Delta R + P(t_0)
     
     If you give all values (V,V0,rpole0 and d) in Rsol, you need to give
     sma in Rsol also.
@@ -578,12 +623,16 @@ def improve_potential(V,V0,pot0,rpole0,d,q,F,sma=1.):
 #{ Rotating stars
 
 def critical_angular_frequency(M,R_pole):
-    """
+    r"""
     Compute the critical angular frequency (rad/s).
     
-    Definition taken from Cranmer and Owocki, 1995 and equal to
+    Definition taken from [Cranmer1995]_ and equal to
     
-    Omega_crit = sqrt( 8GM / 27Rp**3 )
+    .. math::
+    
+        \Omega_\mathrm{crit} = \sqrt{\frac{8GM}{27R_p^3}}
+    
+    with :math:`R_p` the polar radius of the star.
     
     Example usage (includes conversion to period in days):
     
@@ -605,16 +654,20 @@ def critical_angular_frequency(M,R_pole):
     return omega_crit
 
 def critical_velocity(M,R_pole):
-    """
+    r"""
     Compute the critical velocity (km/s)
     
-    Definition 1 from Cranmer and Owocki, 1995:
+    Definition 1 from [Cranmer1995]_:
     
-    v_c = 2 pi R_eq(omega_c) * omega_c
+    .. math::
     
-    Definition 2 from Townsend 2004:
+        v_c = 2 \pi R_\mathrm{eq}(\omega_c) \omega_c
     
-    v_c = sqrt ( 2GM/3Rp )
+    Definition 2 from [Townsend2004]_:
+    
+    .. math::
+    
+        v_c = \sqrt{\frac{2GM}{3R_p}}
     
     which both amount to the same value.
     
@@ -632,6 +685,9 @@ def critical_velocity(M,R_pole):
     return veq
 
 def fast_rotation_radius(colat,r_pole,omega):
+    """
+    Compute the radius of a fast rotating star.
+    """
     Rstar = 3*r_pole/(omega*sin(colat)) * cos((np.pi + np.arccos(omega*sin(colat)))/3.)
     #-- solve singularities
     if np.isinf(Rstar) or sin(colat)<1e-10:
@@ -644,7 +700,9 @@ def diffrotlaw_to_internal(omega_pole,omega_eq):
     
     Assuming a differential rotation law of the form
     
-    oemga = b1 + b2*s**2
+    .. math::
+    
+        \omega = b_1 + b_2 s^2
     
     with s the distance to the rotational axis, compute the value of b2 given
     the polar and equatorial rotation period in units of critical angular

devel/phoebe/parameters/definitions.py
@@ -232,18 +232,30 @@ defs += [dict(qualifier='ld_func', description='Limb darkening model',repr='%s',
          dict(qualifier='rv',   description='Radial velocities',repr='%s',cast_type=np.array,value=[],frame=["phoebe"],context='rvobs'),
          dict(qualifier='rv',   description='Radial velocities',repr='%s',value=[],unit='Rsol/d',frame=["phoebe"],context='rvsyn'),
          dict(qualifier='columns',  description='Data columns',repr='%s',value=['time','rv'],frame=["phoebe"],context=['rvsyn']),
-         dict(qualifier='columns',  description='Data columns',repr='%s',value=['wavelength','time','flux','continuum'],unit='nm',cast_type='return_list_of_strings',frame=["phoebe"],context=['spobs','spsyn']),
+         dict(qualifier='columns',  description='Data columns',repr='%s',value=['wavelength','time','flux','continuum'],cast_type='return_list_of_strings',frame=["phoebe"],context=['spobs','spsyn']),
+         dict(qualifier='columns',  description='Data columns',repr='%s',value=['wavelength','time','I','V','Q','U','continuum'],cast_type='return_list_of_strings',frame=["phoebe"],context=['plobs','plsyn']),
         ]
 
 defs += [dict(qualifier='wavelength',description='Wavelengths of calculated spectrum',repr='%s',value=[],unit='nm',frame=["phoebe"],context='spobs'),
          dict(qualifier='continuum',  description='Continuum intensity of the spectrum',repr='%s',value=[],frame=["phoebe"],context='spobs'),
          dict(qualifier='flux',  description='Flux of the spectrum',repr='%s',value=[],frame=["phoebe"],context='spobs'),
          dict(qualifier='sigma',  description='Noise in the spectrum',repr='%s',value=[],frame=["phoebe"],context='spobs'),
-         dict(qualifier='l3',       description='Third light',repr='%f',cast_type=float,value=0.,adjust=False,frame=["phoebe"],context=['lcobs','spobs','ifobs']),
-         dict(qualifier='pblum',    description='Passband luminosity',repr='%f',cast_type=float,value=1.0,adjust=False,frame=["phoebe"],context=['lcobs','spobs','ifobs']),
-         dict(qualifier='statweight',    description='Statistical weight in overall fitting',repr='%f',cast_type=float,value=1.0,adjust=False,frame=["phoebe"],context=['lcobs','spobs','ifobs']),
+         dict(qualifier='l3',       description='Third light',repr='%f',cast_type=float,value=0.,adjust=False,frame=["phoebe"],context=['lcobs','spobs','ifobs','plobs']),
+         dict(qualifier='pblum',    description='Passband luminosity',repr='%f',cast_type=float,value=1.0,adjust=False,frame=["phoebe"],context=['lcobs','spobs','ifobs','plobs']),
+         dict(qualifier='statweight',    description='Statistical weight in overall fitting',repr='%f',cast_type=float,value=1.0,adjust=False,frame=["phoebe"],context=['lcobs','spobs','ifobs','plobs']),
          ]        
 
+defs += [dict(qualifier='wavelength',description='Wavelengths of calculated spectrum',repr='%s',value=[],unit='nm',frame=["phoebe"],context='plobs'),
+         dict(qualifier='continuum',  description='Continuum intensity of the spectrum',repr='%s',value=[],frame=["phoebe"],context='plobs'),
+         dict(qualifier='I',  description='Stokes I profile',repr='%s',value=[],frame=["phoebe"],context='plobs'),
+         dict(qualifier='sigma_I',  description='Noise in the Stokes I profile',repr='%s',value=[],frame=["phoebe"],context='plobs'),
+         dict(qualifier='V',  description='Stokes V profile',repr='%s',value=[],frame=["phoebe"],context='plobs'),
+         dict(qualifier='sigma_V',  description='Noise in the Stokes V profile',repr='%s',value=[],frame=["phoebe"],context='plobs'),
+         dict(qualifier='Q',  description='Stokes Q profile',repr='%s',value=[],frame=["phoebe"],context='plobs'),
+         dict(qualifier='sigma_Q',  description='Noise in the Stokes Q profile',repr='%s',value=[],frame=["phoebe"],context='plobs'),
+         dict(qualifier='U',  description='Stokes U profile',repr='%s',value=[],frame=["phoebe"],context='plobs'),
+         dict(qualifier='sigma_U',  description='Noise in the Stokes U profile',repr='%s',value=[],frame=["phoebe"],context='plobs'),
+         ]
         
 defs += [dict(qualifier='ld_coeffs',description='Limb darkening coefficients',repr='%s',cast_type='return_string_or_list',value=[1.],frame=["phoebe"],context='rvdep'),
          dict(qualifier='passband', description='Photometric passband',repr='%s',value='JOHNSON.V',cast_type='make_upper',frame=["phoebe"],context='rvdep'),
@@ -258,7 +270,7 @@ defs += [dict(qualifier='ld_func', description='Limb darkening model',repr='%s',
          dict(qualifier='glande',   description='Lande factor',repr='%f',cast_type=float,value=1.2,frame=["phoebe"],context=['pldep']),
          dict(qualifier='clambda',   description='Central wavelength',repr='%s',unit='nm',cast_type=float,value=457.2,frame=["phoebe"],context=['spdep','pldep']),
          dict(qualifier='max_velo', description='Maximum velocity in wavelength array',repr='%s',unit='km/s',cast_type=float,value=350,frame=["phoebe"],context=['spdep','pldep']),
-         dict(qualifier='R',        description='Resolving power lambda/Dlambda (or c/Deltav)',repr='%s',cast_type=float,value=400000.,frame=["phoebe"],context=['spdep','pldep','spobs']),
+         dict(qualifier='R',        description='Resolving power lambda/Dlambda (or c/Deltav)',repr='%s',cast_type=float,value=400000.,frame=["phoebe"],context=['spdep','pldep','spobs','plobs']),
          dict(qualifier='vmacro',   description='Analytical macroturbulent velocity',repr='%s',unit='km/s',cast_type=float,value=0.,frame=["phoebe"],context=['spobs']),
          dict(qualifier='profile',  description='Line profile source (gridname or "gauss")',repr='%s',cast_type=str,value='gauss',frame=["phoebe"],context=['spdep','pldep']),
          dict(qualifier='time',     description='Timepoint',repr='%s',value=[],frame=["phoebe"],context=['spsyn','plsyn']),
@@ -361,6 +373,11 @@ defs += [dict(qualifier='method',    description='Nonlinear fitting method',repr
          dict(qualifier='compute_ci',description='Compute detailed confidence intervals',repr='%s',cast_type=bool,value=True,frame=["phoebe"],context='fitting:lmfit'),
          dict(qualifier='bounded',   description='Include boundaries in fit',repr='%s',cast_type=bool,value=True,frame=["phoebe"],context='fitting:lmfit'),
          dict(qualifier='feedback',  description='Results from fitting procedure',repr='%s',cast_type=dict,value={},frame=["phoebe"],context='fitting:lmfit'),
+        ]
+
+defs += [dict(qualifier='feedback',  description='Results from MINUIT',repr='%s',cast_type=dict,value={},frame=["phoebe"],context='fitting:minuit'),
+         dict(qualifier='label',     description='Fit run name',repr='%s',cast_type=str,value='',frame=["phoebe"],context='fitting:minuit'),
+         dict(qualifier='bounded',   description='Include boundaries in fit',repr='%s',cast_type=bool,value=True,frame=["phoebe"],context='fitting:minuit'),
         ]
         
 defs += [dict(qualifier='feedback',  description='Results from gridding procedure',repr='%s',cast_type=dict,value={},frame=["phoebe"],context='fitting:grid'),