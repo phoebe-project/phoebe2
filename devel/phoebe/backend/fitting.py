(1) Fixed issues with magnetic field strength and orientation. There still seems to be a bug in the calculation of the Zeeman splitting though (2) Added interface to MINUIT fitting package (optional) (3) Improvements in interface to Stokes profiles, and their ParameterSets (3) Improvements in structure of documentation

devel/phoebe/backend/fitting.py
@@ -1,6 +1,9 @@
 """
 Fitting routines, minimization, optimization.
 
+Section 1. Summary
+==================
+
 The top level function you should use is L{run}.
 
 **Main functions**
@@ -19,6 +22,7 @@ The top level function you should use is L{run}.
    run_emcee
    run_lmfit
    run_grid
+   run_minuit
    
 **Wilson Devinney**
 
@@ -26,6 +30,31 @@ The top level function you should use is L{run}.
    
    run_wd_emcee
    
+Section 2. Details
+==================
+
+The basic idea behind fitting Phoebe is the following chain of events, once
+you defined a model and attached observations:
+
+1. Given a set of parameters, compute a model.
+2. Compute observables to mimic the observations attached to that model.
+3. **Evaluate the goodness-of-fit**.
+4. If **some criterion** is satisfied, then ``STOP``
+5. **Change the values** of the adjustable parameters and return to step 1.
+
+Three remarks are in place to explain the statements in bold:
+
+* **some criterion** generally means that you can either stop computing if you
+  found a (what you think) optimal set of parameters, or if you computed enough
+  models to satisfactory sample the posterior distributions of the adjustable
+  parameters.
+* **evaluate the goodness-of-fit** means...
+* **change the values** means you have to pick a certain algorithm. You can
+  use algorithms that try to descent into a minimum of the parameter space,
+  algorithms that just try out a whole predefined set of parameters, or
+  algorithms that try to walk through the parameter space to sample the
+  posterior distributions, rather than finding an optimal value.
+
 
 """
 import os
@@ -57,6 +86,11 @@ try:
     import lmfit
 except ImportError:
     print("Unable to load nonlinear fitting routines from lmfit: restricted fitting facilities")
+try:
+    import iminuit
+except ImportError:
+    print("Unable to load MINUIT fitting routines from iminuit: restricted fitting facilities")
+
     
 logger = logging.getLogger("FITTING")
 
@@ -71,6 +105,7 @@ def run(system,params=None,fitparams=None,mpi=None,accept=False):
         1. :ref:`fitting:pymc <parlabel-phoebe-fitting:pymc>`: Metropolis-Hastings MCMC via pymc
         2. :ref:`fitting:emcee <parlabel-phoebe-fitting:emcee>`: Affine Invariant MCMC via emcee
         3. :ref:`fitting:lmfit <parlabel-phoebe-fitting:lmfit>`: nonlinear optimizers via lmfit
+        3. :ref:`fitting:minuit <parlabel-phoebe-fitting:minuit>`: nonlinear optimizers via MINUIT
     
     The parameters determining how the system is computed are defined by the
     ParameterSet :ref:`params <parlabel-phoebe-compute>`.
@@ -113,6 +148,8 @@ def run(system,params=None,fitparams=None,mpi=None,accept=False):
         solver = run_emcee
     elif fitparams.context=='fitting:lmfit':
         solver = run_lmfit
+    elif fitparams.context=='fitting:minuit':
+        solver = run_minuit
     else:
         raise NotImplementedError("Fitting context {} is not understood".format(fitparams.context))
     fitparams = solver(system,params=params,mpi=mpi,fitparams=fitparams)
@@ -299,8 +336,15 @@ def run_emcee(system,params=None,mpi=None,fitparams=None,pool=None):
     * To get the original chains per walker back, and make a histogram of the
       final states, do
     
-    >>> chains = fitparams['feedback']['traces'][0].reshape((-1,nwalkers))
-    >>> freqs,bins = np.hist(chains[:,-1])
+    >>> chains = fitparams['feedback']['traces'][0].reshape((walkers,-1))
+    
+    Then you should be able to recover a sampling of the prior via
+    
+    >>> freqs,bins = np.hist(chains[0])
+    
+    And (hopefully) of the posterior via
+    
+    >>> freqs,bins = np.hist(chains[-1])
     
     Reference: [Foreman-Mackey2012]_
     
@@ -817,6 +861,130 @@ def _run_lmfit(system,params=None,mpi=None,fitparams=None):
                     Ndata=Nmodel['Nd'],Npars=Nmodel['Np'])
     fitparams['feedback'] = feedback
     return fitparams
+
+
+class MinuitMinimizer(object):
+    def __init__(self,system,params=None,mpi=None):
+        self.system = system
+        self.params = params # computational parameters
+        self.mpi = mpi
+        
+        #-- evaluate the system, get the results and return a probability
+        had = []
+        #-- walk through all the parameterSets available:
+        walk = utils.traverse(system,list_types=(universe.BodyBag,universe.Body,list,tuple),dict_types=(dict,))
+        self.param_names = []
+        for parset in walk:
+            #-- for each parameterSet, walk to all the parameters
+            for qual in parset:
+                #-- extract those which need to be fitted
+                if parset.get_adjust(qual) and parset.has_prior(qual):
+                    #-- ask a unique ID and update the value of the parameter
+                    this_param = parset.get_parameter(qual)
+                    myid = this_param.get_unique_label().replace('-','_')
+                    if myid in had: continue
+                    id_for_minuit = '{}_{}'.format(qual,myid)
+                    setattr(self,id_for_minuit,this_param.get_value())
+                    had.append(myid)
+                    self.param_names.append(id_for_minuit)
+                    
+        self.func_code = iminuit.util.make_func_code(self.param_names)
+        self.func_defaults = None
+        #-- construct the iminuit Minimizer
+    
+    def __call__(self,*pars):
+        """
+        Define the chi square.
+        """
+        system = self.system
+        had = []
+        #-- walk through all the parameterSets available:
+        walk = utils.traverse(system,list_types=(universe.BodyBag,universe.Body,list,tuple),dict_types=(dict,))
+        for parset in walk:
+            #-- for each parameterSet, walk to all the parameters
+            for qual in parset:
+                #-- extract those which need to be fitted
+                if parset.get_adjust(qual) and parset.has_prior(qual):
+                    #-- ask a unique ID and update the value of the parameter
+                    myid = parset.get_parameter(qual).get_unique_label().replace('-','_')
+                    if myid in had: continue
+                    id_for_minuit = '{}_{}'.format(qual,myid)
+                    index = self.param_names.index(id_for_minuit)
+                    parset[qual] = pars[index]
+                    had.append(myid)
+        #-- evaluate the system, get the results and return a probability
+        system.reset()
+        system.clear_synthetic()
+        observatory.compute(system,params=self.params,mpi=self.mpi)
+        mu,sigma,model = system.get_model()
+        retvalue = (model-mu)/sigma
+        
+        print(", ".join(["{}={}".format(name,pars[i]) for i,name in enumerate(self.param_names)]))
+        print("Current value",sum(retvalue**2))
+        return sum(retvalue**2)
+
+
+def run_minuit(system,params=None,mpi=None,fitparams=None):
+    """
+    Use MINUIT.
+    
+    MINUIT2, is a numerical minimization computer program originally written
+    in the FORTRAN programming language[1] by CERN staff physicist Fred James
+    in the 1970s (from wikipedia).
+    
+    Best to call Minos to check if the parameters are reasonably independent.
+    The tool is MnHesse.
+    """
+    mimi = MinuitMinimizer(system)
+    had = []
+    ppars = [] # Phoebe parameters
+    pars = {}  # Minuit parameters
+    #-- walk through all the parameterSets available:
+    walk = utils.traverse(system,list_types=(universe.BodyBag,universe.Body,list,tuple),dict_types=(dict,))
+    for parset in walk:
+        #-- for each parameterSet, walk to all the parameters
+        for qual in parset:
+            #-- extract those which need to be fitted
+            if parset.get_adjust(qual) and parset.has_prior(qual):
+                #-- ask a unique ID and update the value of the parameter
+                this_param = parset.get_parameter(qual)
+                myid = this_param.get_unique_label().replace('-','_')
+                if myid in had: continue
+                id_for_minuit = '{}_{}'.format(qual,myid)
+                pars[id_for_minuit] = this_param.get_value()
+                if this_param.has_step():
+                    pars['error_{}'.format(id_for_minuit)] = this_param.get_step()
+                else:
+                    #-- this is MINUIT's default, we'll set it otherwise we get a warning
+                    pars['error_{}'.format(id_for_minuit)] = 1.
+                if fitparams['bounded']:
+                    lower,upper = this_param.get_prior().get_limits()
+                    pars['limit_{}'.format(id_for_minuit)] = (lower,upper)
+                had.append(myid)
+                ppars.append(this_param)
+    if fitparams['bounded']:
+        logger.warning("MINUIT: parameters are bounded, this can affect the error estimations")
+    #-- a change of this value should be interpreted as 1 sigma for iminuit:
+    # (in order to calculate it we need to now the number of observations)
+    data,sigma = system.get_data()
+    N = len(data)-len(ppars)
+    errordef = np.sqrt(2.0*(N-2.0))
+    logger.warning("MINUIT's 1sigma = {}".format(errordef))
+    #-- initialize the fit
+    m = iminuit.Minuit(mimi,errordef=errordef,**pars)
+    #-- run the fit
+    m.migrad()
+    #-- collect the feedback:
+    values = [m.values[x] for x in m.parameters]
+    sigmas = [m.error[x] for x in m.parameters]
+    #-- note: m.covariance is also available, as well as m.matrix(correlation=True)
+    #   there is also stuff for contours etc... google for it! But we probably
+    #   want to add the info to the feedback
+    feedback = dict(parameters=ppars,values=values,sigmas=sigmas,redchi=m.fval,
+                    message=m.get_fmin(),success=m.get_fmin().is_valid)
+    fitparams['feedback'] = feedback
+    return fitparams
+
 
 
 #}

devel/phoebe/backend/observatory.py
@@ -651,7 +651,8 @@ def make_spectrum(the_system,wavelengths=None,sigma=2.,depth=0.4,ref=0,rv_grav=T
         
     return wavelengths,total_spectrum,total_continum
 
-def stokes(the_system,wavelengths=None,sigma=2.,depth=0.4,ref=0,glande=None,rv_grav=True):
+def stokes(the_system,wavelengths=None,sigma=2.,depth=0.4,ref=0,
+           rv_grav=True,do_V=True,do_Q=False,do_U=False):
     r"""
     Calculate the stokes profiles.
     
@@ -729,10 +730,23 @@ def stokes(the_system,wavelengths=None,sigma=2.,depth=0.4,ref=0,glande=None,rv_g
     
     .. math::
         I_{\mu,\lambda} \longrightarrow I_{\mu,\lambda,T_\mathrm{eff},\log g,z,v_\mathrm{rad},\ldots}
+    
     """
     
     mesh = the_system.mesh
-    
+    #-- is there data available? then we can steal the wavelength array
+    #   from there
+    iobs,ref_ = the_system.get_parset(ref=ref,context='plobs')
+    if ref_ is not None:
+        if not 'wavelength' in iobs or not len(iobs['wavelength']):
+            iobs.load()
+        wavelengths = iobs['wavelength']
+        R = iobs['R']
+        if 'vmacro' in iobs:
+            vmacro = iobs['vmacro']
+    else:
+        R = None
+        vmacro = 0.
     #-- information on dependable set
     idep,ref = the_system.get_parset(ref=ref,context='pldep')
     ld_model = idep['ld_func']
@@ -780,8 +794,8 @@ def stokes(the_system,wavelengths=None,sigma=2.,depth=0.4,ref=0,glande=None,rv_g
         return wavelengths,np.zeros(len(wavelengths)),0.
     
     #-- magnitude of magnetic field and angle towards the LOS
-    mesh['B_'] = mesh['B_']*1e-4 # convert to Tesla
-    B = coordinates.norm(mesh['B_'][keep],axis=1)
+    #mesh['B_'] = mesh['B_']*1e-4 # convert to Tesla
+    B = coordinates.norm(mesh['B_'][keep],axis=1)*1e-4 # and convert to Tesla 
     cos_theta = coordinates.cos_angle(mesh['B_'][keep],np.array([[0,0.,-1]]),axis=1)
     sin2theta = 1 - cos_theta**2
     cos_chi = coordinates.cos_angle(mesh['B_'][keep],np.array([[1.,0.,0]]),axis=1)
@@ -790,7 +804,9 @@ def stokes(the_system,wavelengths=None,sigma=2.,depth=0.4,ref=0,glande=None,rv_g
     cos22chi = (cos2chi - sin2chi)**2
     sin22chi = 1. - cos22chi
     #-- Zeeman splitting in angstrom
-    delta_nu_zeemans = -glande*constants.qe*B/(4*np.pi*constants.me)
+    #bohr_magneton = 9.27400968e-24
+    #delta_nu_zeemans = -glande*bohr_magneton*B
+    delta_nu_zeemans = -glande*constants.qe*B/(4*np.pi*constants.me) # (qe is negative but somewhere my B field has wrong sign?)
     delta_nu_zeemans2 = delta_nu_zeemans**2
     delta_v_zeemans = (wc*1e-10)*delta_nu_zeemans/1000. # from cy/s to km/s
     #-- radial velocities
@@ -860,7 +876,8 @@ def stokes(the_system,wavelengths=None,sigma=2.,depth=0.4,ref=0,glande=None,rv_g
             spec  = pri*sz*tools.doppler_shift(wavelengths,rv+rv_grav,flux=template)
             specm = pri*sz*tools.doppler_shift(wavelengths,rv+rv_grav-rvz,flux=template)
             specp = pri*sz*tools.doppler_shift(wavelengths,rv+rv_grav+rvz,flux=template)            
-            stokes_V += costh*(specm-specp)/2.
+            if do_V:
+                stokes_V += costh*(specm-specp)/2.
             #-- second version
             #mytemplate = pri*sz*(1.00 - depth*np.exp( -(wavelengths-wc-rad_velosw[i])**2/(2*sigma**2)))
             #stokes_V -= costh*delta_nu_zeemans[i]*utils.deriv(nus,mytemplate)
@@ -869,11 +886,15 @@ def stokes(the_system,wavelengths=None,sigma=2.,depth=0.4,ref=0,glande=None,rv_g
             
             #-- Stokes Q and U
             sec_deriv = utils.deriv(nus,utils.deriv(nus,spec))
-            stokes_Q -= 0.25*sin2th*cos22chi*delta_nu_zeemans2[i]*sec_deriv
-            stokes_U -= 0.25*sin2th*cos22chi*delta_nu_zeemans2[i]*sec_deriv
+            if do_Q:
+                stokes_Q -= 0.25*sin2th*cos22chi*delta_nu_zeemans2[i]*sec_deriv
+            if do_U:
+                stokes_U -= 0.25*sin2th*cos22chi*delta_nu_zeemans2[i]*sec_deriv
             
             stokes_I += spec
             total_continum += pri*sz
+        logger.info("Zeeman splitting: between {} and {} AA".format(min(conversions.convert('Hz','AA',delta_nu_zeemans,wave=(wc,'AA'))),max(conversions.convert('Hz','AA',delta_nu_zeemans,wave=(wc,'AA')))))
+        logger.info("Zeeman splitting: between {} and {} Hz".format(min(delta_nu_zeemans/1e6),max(delta_nu_zeemans/1e6)))
         logger.info("Zeeman splitting: between {} and {} km/s".format(min(delta_v_zeemans),max(delta_v_zeemans)))
     else:
         raise NotImplementedError
@@ -883,7 +904,7 @@ def stokes(the_system,wavelengths=None,sigma=2.,depth=0.4,ref=0,glande=None,rv_g
         stokes_I = stokes_I[keep]
         stokes_V = stokes_V[keep]
         total_continum = total_continum[keep]
-    return wavelengths,stokes_I,stokes_V,total_continum
+    return wavelengths,stokes_I,stokes_V,stokes_Q,stokes_U,total_continum
     
 
 #}

devel/phoebe/backend/universe.py
@@ -1060,8 +1060,36 @@ class Body(object):
         k = Ndata - Npar
         prob = scipy.stats.distributions.chi2.cdf(chi2,k)
         #-- that's it!
-        return prob, chi2, Ndata, Npar
+        return chi2, prob, Ndata, Npar
         
+    
+    def get_data(self):
+        """
+        Return all data in one long chain of data.
+        """
+        mu = []
+        sigma = []
+        for idata in self.params['obs'].values():
+            for observations in idata.values():
+                #-- make sure to have loaded the observations from a file
+                loaded = observations.load(force=False)
+                if observations.context=='spobs':
+                    obser_ = np.ravel(np.array(observations['flux'])/np.array(observations['continuum']))
+                    sigma_ = np.ravel(np.array(observations['sigma']))
+                elif observations.context=='lcobs':
+                    obser_ = np.ravel(np.array(observations['flux']))
+                    sigma_ = np.ravel(np.array(observations['sigma']))
+                elif observations.context=='ifobs':
+                    obser_ = np.ravel(np.array(observations['vis2']))
+                    sigma_ = np.ravel(np.array(observations['sigma_vis2']))
+                else:
+                    raise NotImplementedError('probability')  
+                #-- append to the "whole" model.
+                mu.append(obser_)
+                sigma.append(sigma_)
+                if loaded:
+                    observations.unload()
+        return np.hstack(mu),np.hstack(sigma)
     
     def get_model(self):
         """
@@ -2274,12 +2302,12 @@ class PhysicalBody(Body):
     @decorators.parse_ref
     def sp(self,wavelengths=None,ref='allspdep',sigma=5.,depth=0.4,time=None):
         """
-        Compute projected intensity and add results to the pbdep ParameterSet.
+        Compute spectrum and add results to the pbdep ParameterSet.
         """
         #-- don't bother if we cannot do anything...
         if hasattr(self,'params') and 'pbdep' in self.params:
             if not ('spdep' in self.params['pbdep']): return None
-            #-- compute the projected intensities for all light curves.
+            #-- compute the spectrum for all references
             for lbl in ref:
                 base,lbl = self.get_parset(ref=lbl,type='syn')
                 wavelengths_,specflux,cont = observatory.make_spectrum(self,ref=lbl,wavelengths=wavelengths,sigma=sigma,depth=depth)
@@ -2288,6 +2316,26 @@ class PhysicalBody(Body):
                 base['flux'].append(specflux)
                 base['continuum'].append(cont)
         
+    @decorators.parse_ref
+    def pl(self,wavelengths=None,ref='allpldep',sigma=5.,depth=0.4,time=None):
+        """
+        Compute Stokes profiles and add results to the pbdep ParameterSet.
+        """
+        #-- don't bother if we cannot do anything...
+        if hasattr(self,'params') and 'pbdep' in self.params:
+            if not ('pldep' in self.params['pbdep']): return None
+            #-- compute the Stokes profiles for all references
+            for lbl in ref:
+                base,lbl = self.get_parset(ref=lbl,type='syn')
+                wavelengths_,I,V,Q,U,cont = observatory.stokes(self,ref=lbl,
+                         wavelengths=wavelengths,sigma=sigma,depth=depth)
+                base['time'].append(self.time)
+                base['wavelength'].append(wavelengths_)
+                base['I'].append(I)
+                base['V'].append(V)
+                base['Q'].append(Q)
+                base['U'].append(U)
+                base['continuum'].append(cont)
     
         
     
@@ -3378,7 +3426,7 @@ class Star(PhysicalBody):
         """
         self.mesh['abun'] = self.params.values()[0]['abun']
     
-    def magnetic_field(self):
+    def magnetic_field_best(self):
         """
         Calculate the magnetic field.
         """
@@ -3396,15 +3444,54 @@ class Star(PhysicalBody):
             r = coordinates.norm(r_)
             r_ *= r
         m_ = np.array([np.sin(beta)*np.cos(np.pi/2),np.sin(np.pi/2),np.cos(beta)])
-        m_*= R**3*np.abs(Bpolar)/2.
+        m_*= R**3*np.abs(Bpolar)/2. #---> use this for agreement with first test run of Evelyne
         
         dotprod = np.dot(m_,r_.T).reshape(-1,1)
         B = (3*dotprod*r_/r**5 - m_/r**3)
         nB = coordinates.norm(B,axis=1).max()
         print("Maximum B-field = {}, Bpolar should be = {}".format(nB,Bpolar))
         B = B/nB*Bpolar
+        
+        #-- polar value:
+        #r_polar = np.array([[0.,0.,R]])
+        #dotprod = np.dot(m_,r_polar.T).reshape(-1,1)
+        #B_polar = (3*dotprod*r_polar/r**5 - m_/r**3)
+        #B_polar = coordinates.norm(B_polar,axis=1).max()
+        #print("Bpolar field {}".format(B_polar))
+        #B = B/B_polar*Bpolar
+        
+        
         self.mesh['_o_B_'] = B
         self.mesh['B_'] = self.mesh['_o_B_']
+    
+    def magnetic_field(self):
+        """
+        Calculate the magnetic field.
+        
+        Problem: when the surface is deformed, I need to know the value of
+        the radius at the magnetic pole! Or we could just interpret the
+        polar magnetic field as the magnetic field strength in the direction
+        of the magnetic axes but at a distance of 1 polar radius....
+        """
+        #-- dipolar:
+        parset = self.params['magnetic_field']
+        beta = parset.get_value('beta','rad')
+        Bpolar = parset.get_value('Bpolar')
+        R = self.params.values()[0].get_value('radius')
+        r_ = self.mesh['_o_center']/R
+        trans = np.pi/2.
+        
+        #m_ = np.array([np.sin(beta),0.,np.cos(beta)])
+        m_ = np.array([np.sin(beta)*np.cos(trans)-0*np.sin(trans),
+                       np.sin(beta)*np.sin(trans)+0.*np.cos(trans),
+                       np.cos(beta)])
+        dotprod = np.dot(m_,r_.T).reshape(-1,1)
+        B =     (3*dotprod    *r_ - m_)
+        B = B/2.0*Bpolar
+        self.mesh['_o_B_'] = B
+        self.mesh['B_'] = self.mesh['_o_B_']
+        logger.info("Added magnetic field with Bpolar={}G, beta={} deg".format(Bpolar,beta/np.pi*180))
+        logger.info("Maximum B-field on surface = {}G".format(coordinates.norm(B,axis=1).max()))
     
     @decorators.parse_ref
     def intensity(self,ref='all'):