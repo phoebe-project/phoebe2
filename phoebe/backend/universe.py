af7ccef57786d829fc3525c3a566a172d0fa817d
pieterdegroote <pieterdegroote10@gmail.com>
2013-06-11 06:23:48 +0000

    implemented general __eq__ for Body - see ticket discussion

--- phoebe/backend/universe.py
+++ phoebe/backend/universe.py
@@ -719,6 +719,12 @@ class Body(object):
         self._postprocessing = []
         #self.params['pbdep'] = {}
     
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+            and self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
     
     def __str__(self):
         return self.to_string()
