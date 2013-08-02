ae876346f6c71de3a87e86c90e8da8bdc772bbc3
pieterdegroote <pieterdegroote10@gmail.com>
2013-08-02 14:44:05 +0000

    added weights column to testlib data files, and changed phoebe parameter files so that the data files are read in when parsing

--- phoebe-testlib/synthetic/compare2_0.py
+++ phoebe-testlib/synthetic/compare2_0.py
@@ -24,35 +24,43 @@ from matplotlib import pyplot as plt
 logger = phoebe.get_basic_logger()
 #logger = phoebe.get_basic_logger(clevel=None,filename='detached_1.log')
 
+DELTA = 0.25
+
 def compare(name):
+    basedir = os.path.dirname(__file__)
     
     # Parse the Legacy file
-    system = parsers.legacy_to_phoebe("{}.phoebe".format(name), mesh='marching',
-                                    create_body=True)
+    filename = os.path.join(basedir, "{}.phoebe".format(name))
+    system = parsers.legacy_to_phoebe(filename, mesh='marching',
+                                    create_body=True, root=basedir)
     print(system.list(summary='long'))
     
     # Override the mesh density, we don't want to wait forever
-    system[0].params['mesh']['delta'] = 0.15
-    system[1].params['mesh']['delta'] = 0.15
+    for body in system.bodies:
+        body.params['mesh']['delta'] = DELTA
     
-    # Get the references
-    ref_lc = system.get_refs(category='lc')[0]
-    ref_rv1 = system.get_refs(category='rv')[0]
-    ref_rv2 = system.get_refs(category='rv')[1]
-
     # Add Phoebe 1.0 results as observations, but only if they exist
-    lc_file = "{}.lc.1.0.data".format(name)
+    lc_file = os.path.join(basedir, "{}.lc.1.0.data".format(name))
 
     system_obs = []
 
     if os.path.isfile(lc_file):
+        ref_lc = system.get_refs(category='lc')[0]
         lcobs, lcdep = datasets.parse_lc(lc_file, ref=ref_lc)
         system_obs.append(lcobs)
 
     system.add_obs(system_obs)
 
     # Compute Phoebe 2.0 results
-    phoebe.compute(system, eclipse_alg='convex', mpi=True)
+    if 'overcontact' in name:
+        system[0].params['pbdep']['lcdep'].values()[0]['pblum'] = \
+            system[0].params['pbdep']['lcdep'].values()[0]['pblum'] +\
+            system[1].params['pbdep']['lcdep'].values()[0]['pblum']
+                                
+        system.bodies = system.bodies[:1]
+        phoebe.compute(system, eclipse_alg='full', mpi=True)
+    else:    
+        phoebe.compute(system, eclipse_alg='convex', mpi=True)
 
 
     results = []
@@ -66,23 +74,25 @@ def compare(name):
         pass
     
     try:
+        ref_rv1 = system.get_refs(category='rv')[0]
         diffs = plot_rv(system, 0, ref_rv1)
         results.append([np.median(diffs), diffs.std(), diffs.min(), diffs.max()])
     except:
         pass
     
     try:
+        ref_rv2 = system.get_refs(category='rv')[1]
         diffs = plot_rv(system, 1, ref_rv2)
         results.append([np.median(diffs), diffs.std(), diffs.min(), diffs.max()])
     except:
         pass
     
     
-    print(("{:20s}"*4).format('MEDIAN','STDDEV','MIN',"MAX"))
+    print(("{:>20s}"*4).format('MEDIAN','STDDEV','MIN',"MAX"))
     for result in results:
         print(("{:20.8f}"*4).format(*result))
     
-    return results
+    return np.array(results)
 
 
 def snapshot(name, time):
@@ -90,23 +100,21 @@ def snapshot(name, time):
     # Parse the Legacy file
     system = parsers.legacy_to_phoebe("{}.phoebe".format(name), mesh='marching',
                                     create_body=True)
-    print(system.list(summary='long'))
+    system[0].params['orbit']['q'] = 0.1
+    system[0].params['component']['pot'] = 1.9
+    print(system.list(summary='physical'))
     
     # Override the mesh density, we don't want to wait forever
-    system[0].params['mesh']['delta'] = 0.15
-    system[1].params['mesh']['delta'] = 0.15
+    for body in system.bodies:
+        body.params['mesh']['delta'] = DELTA
     
-    # Get the references
-    ref_lc = system.get_refs(category='lc')[0]
-    ref_rv1 = system.get_refs(category='rv')[0]
-    ref_rv2 = system.get_refs(category='rv')[1]
-
     # Add Phoebe 1.0 results as observations, but only if they exist
     lc_file = "{}.lc.1.0.data".format(name)
 
     system_obs = []
 
     if os.path.isfile(lc_file):
+        ref_lc = system.get_refs(category='lc')[0]
         lcobs, lcdep = datasets.parse_lc(lc_file, ref=ref_lc)
         system_obs.append(lcobs)
 
@@ -115,7 +123,7 @@ def snapshot(name, time):
     # Compute Phoebe 2.0 results
     params = phoebe.ParameterSet(context='compute')
     phoebe.observatory.extract_times_and_refs(system, params)
-    for i, j, k in zip(params['time'], params['refs'], params['types']):
+    for nr,(i, j, k) in enumerate(zip(params['time'], params['refs'], params['types'])):
         if np.abs(i-time)>0.001:
             new_list = params['time']
             new_list.remove(i)
@@ -127,13 +135,19 @@ def snapshot(name, time):
             new_list.remove(k)
             params['types'] = new_list
     
-    phoebe.compute(system, params=params, eclipse_alg='convex')
+    if 'overcontact' in name:
+        system[0].params['pbdep']['lcdep'].values()[0]['pblum'] = \
+            system[0].params['pbdep']['lcdep'].values()[0]['pblum'] +\
+            system[1].params['pbdep']['lcdep'].values()[0]['pblum']
+                                
+        system.bodies = system.bodies[:1]
+        phoebe.compute(system, params=params, eclipse_alg='full')
+    else:
+        phoebe.compute(system, params=params, eclipse_alg='convex')
     
     ps, lc, rv = parsers.phoebe_to_wd(system)
-    lc2 = lc.copy()
-    lc2['indep'] = [1.31]
-    curve, params = phoebe.wd.lc(ps, request='image', light_curve=lc2, rv_curve=rv)
-    lc['indep'] = lc['indep'][np.abs(lc['indep']-time)<=0.001]
+    index = np.searchsorted(lc['indep'],time)
+    lc['indep'] = lc['indep'][index:index+1]
     curve, params = phoebe.wd.lc(ps, request='image', light_curve=lc, rv_curve=rv)
     
     plt.figure()
@@ -217,6 +231,36 @@ def plot_rv(system, comp, ref_rv):
     
     return diffs
     
+
+def as_test(name):
+    """
+    Run the comparison as a test
+    """
+    calibration = read_testlib()
+    diffs = compare(name)
+    assert(np.all(np.abs(diffs-calibration[name])<=1e-8))
+
+
+def read_testlib():
+    """
+    Read in the testlib results
+    """
+    calibration = {}
+    current = None
+    
+    filename = os.path.join(os.path.dirname(__file__), 'calibration.testlib')
+    with open(filename,'r') as ff:
+        for line in ff.readlines():
+            if line and line[0]=='*':
+                if current is not None:
+                    calibration[current] = np.array(calibration[current])
+                current = line[1:].strip()
+                calibration[current] = []
+            elif line:
+                calibration[current].append([float(i) for i in line.strip().split()])
+    
+    return calibration
+
 
 if __name__=="__main__":
     if sys.argv[1:]:
