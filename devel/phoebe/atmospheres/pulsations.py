- now mass, radius and rotation period are parsed correctly in PulsatingBinaryRocheStar - fixed ZeroDivisionError when rotation period is infinite in BinaryRocheStar (or when syncpar=0) - attempt to translate stellar inclination and longitude orientation to orbital   inclination and longitude of AN when they are the same in both stars (otherwise   a warning is issued) - Orientation of pulsation for m!=0 modes in PulsatingBinaryRocheStar is now OK   (there were troubles with the mesh, since the mesh in BinaryRocheStar rotates   along with the orbital phase and is thus independent of the rotation, in   contrast to BinaryStar).

devel/phoebe/atmospheres/pulsations.py
@@ -395,7 +395,7 @@ def longitudinal(theta,phi,l,m,freq,phase,t,spin,k):
     term3 = -norm_atlm1(l,m,spin,k) * dsph_harm_dtheta(theta,phi,l-1,m)*exp(1j*2*pi*(freq*t+phase-0.25))
     return term1 + term2 + term3
 
-def surface(radius,theta,phi,t,l,m,freq,phases,spin,k,asl):
+def surface(radius,theta,phi,t,l,m,freq,phases,spin,k,asl, mesh_phase=0):
     """
     Compute surface displacements.
     
@@ -412,17 +412,22 @@ def surface(radius,theta,phi,t,l,m,freq,phases,spin,k,asl):
     velo_phi = 0.
     zero = np.zeros_like(theta,complex)
     
+    # Take care of meshes that rotate themselves (i.e. the mesh rotates
+    # independently from the rotation period, as happens e.g. in
+    # BinaryRocheStars)
+    phi_ = phi - mesh_phase
+    
     for il,im,ifreq,iphase,ispin,ik,iasl in zip(l,m,freq,phases,spin,k,asl):
         #-- radial perturbation
-        ksi_r_ = iasl*radius*sqrt(4*pi)*radial(theta,phi,il,im,ifreq,iphase,t)
+        ksi_r_ = iasl*radius*sqrt(4*pi)*radial(theta,phi_,il,im,ifreq,iphase,t)
         #-- add to the total perturbation of the radius and velocity
         ksi_r += ksi_r_
         velo_r += 1j*2*pi*ifreq*ksi_r_
         #-- colatitudinal and longitudonal perturbation when l>0
         norm = sqrt(4*pi)
         if il>0:
-            ksi_theta_ = iasl*norm*colatitudinal(theta,phi,il,im,ifreq,iphase,t,ispin,ik)
-            ksi_phi_   = iasl*norm* longitudinal(theta,phi,il,im,ifreq,iphase,t,ispin,ik)
+            ksi_theta_ = iasl*norm*colatitudinal(theta,phi_,il,im,ifreq,iphase,t,ispin,ik)
+            ksi_phi_   = iasl*norm* longitudinal(theta,phi_,il,im,ifreq,iphase,t,ispin,ik)
             ksi_theta += ksi_theta_
             ksi_phi += ksi_phi_
             velo_theta += 1j*2*pi*ifreq*ksi_theta_
@@ -438,7 +443,10 @@ def surface(radius,theta,phi,t,l,m,freq,phases,spin,k,asl):
            (phi + ksi_phi.real),\
            velo_r.real,velo_theta.real,velo_phi.real
 
-def observables(radius,theta,phi,teff,logg,t,l,m,freq,phases,spin,k,asl,delta_T,delta_g):
+def observables(radius, theta, phi, teff, logg,
+                t, l, m, freq, phases,
+                spin, k, asl, delta_T, delta_g,
+                mesh_phase=0.0):
     """
     Good defaults:
     
@@ -460,15 +468,20 @@ def observables(radius,theta,phi,teff,logg,t,l,m,freq,phases,spin,k,asl,delta_T,
     ksi_teff = 0.
     zero = np.zeros_like(phi,complex)
     
+    # Take care of meshes that rotate themselves (i.e. the mesh rotates
+    # independently from the rotation period, as happens e.g. in
+    # BinaryRocheStars)
+    phi_ = phi - mesh_phase
+    
     for il,im,ifreq,iphase,ispin,ik,iasl,idelta_T,idelta_g in \
        zip(l,m,freq,phases,spin,k,asl,delta_T,delta_g):
-        rad_part = radial(theta,phi,il,im,ifreq,iphase,t)
+        rad_part = radial(theta,phi_,il,im,ifreq,iphase,t)
         ksi_r_ = iasl*sqrt(4*pi)*rad_part#radial(theta,phi,il,im,ifreq,t)
         ksi_r += ksi_r_*radius
         velo_r += 1j*2*pi*ifreq*ksi_r_*radius
         if il>0:
-            ksi_theta_ = iasl*sqrt(4*pi)*colatitudinal(theta,phi,il,im,ifreq,iphase,t,ispin,ik)
-            ksi_phi_ = iasl*sqrt(4*pi)*longitudinal(theta,phi,il,im,ifreq,iphase,t,ispin,ik)
+            ksi_theta_ = iasl*sqrt(4*pi)*colatitudinal(theta,phi_,il,im,ifreq,iphase,t,ispin,ik)
+            ksi_phi_ = iasl*sqrt(4*pi)*longitudinal(theta,phi_,il,im,ifreq,iphase,t,ispin,ik)
             ksi_theta += ksi_theta_
             ksi_phi += ksi_phi_
             velo_theta += 1j*2*pi*ifreq*ksi_theta_
@@ -497,20 +510,39 @@ def observables(radius,theta,phi,teff,logg,t,l,m,freq,phases,spin,k,asl,delta_T,
 
 #{ Phoebe specific interface
 
-def add_pulsations(self,time=None):
+def add_pulsations(self,time=None, mass=None, radius=None, rotperiod=None,
+                   mesh_phase=0.0):
+    """
+    Add pulsations to a Body.
+    
+    Stellar parameters are only used to estimate parameters such as horizontal
+    over vertical displacement.
+    
+    @param mass: object's mass (kg)
+    @type mass: float or None
+    @param radius: object's radius (m)
+    @type radius: float or None
+    @param rotperiod: object's rotation period (d)
+    @type rotperiod: float or None
+    """
     if time is None:
         time = self.time
     
-    #-- relevant stellar parameters
-    try:
-        rotfreq = 1./self.params['star'].request_value('rotperiod','d')
+    # Relevant stellar parameters
+    if mass is None:
+        M = self.params['star'].request_value('mass','kg')
+    else:
+        M = mass
+    
+    if radius is None:
         R = self.params['star'].request_value('radius','m')
-        M = self.params['star'].request_value('mass','kg')    
-    except:
-        logger.critical('Cannot figure out stellar parameters')
-        rotfreq = 20.
-        R = constants.Rsol
-        M = constants.Msol
+    else:
+        R = radius
+        
+    if rotperiod is None:
+        rotfreq = 1./self.params['star'].request_value('rotperiod','d')
+    else:
+        rotfreq = 1./rotperiod
     
     #-- prepare extraction of pulsation parameters
     freqs = []
@@ -595,12 +627,12 @@ def add_pulsations(self,time=None):
     r2,phi2,theta2 = coordinates.cart2spher_coord(*self.mesh['_o_triangle'][:,3:6].T[index])
     r3,phi3,theta3 = coordinates.cart2spher_coord(*self.mesh['_o_triangle'][:,6:9].T[index])
     r4,phi4,theta4 = coordinates.cart2spher_coord(*self.mesh['_o_center'].T[index])
-    r1,theta1,phi1,vr1,vth1,vphi1 = surface(r1,theta1,phi1,time,ls,ms,freqs,phases,spinpars,ks,ampls)        
-    r2,theta2,phi2,vr2,vth2,vphi2 = surface(r2,theta2,phi2,time,ls,ms,freqs,phases,spinpars,ks,ampls)
-    r3,theta3,phi3,vr3,vth3,vphi3 = surface(r3,theta3,phi3,time,ls,ms,freqs,phases,spinpars,ks,ampls)
+    r1,theta1,phi1,vr1,vth1,vphi1 = surface(r1,theta1,phi1,time,ls,ms,freqs,phases,spinpars,ks,ampls, mesh_phase=mesh_phase)        
+    r2,theta2,phi2,vr2,vth2,vphi2 = surface(r2,theta2,phi2,time,ls,ms,freqs,phases,spinpars,ks,ampls, mesh_phase=mesh_phase)
+    r3,theta3,phi3,vr3,vth3,vphi3 = surface(r3,theta3,phi3,time,ls,ms,freqs,phases,spinpars,ks,ampls, mesh_phase=mesh_phase)
     r4,theta4,phi4,vr4,vth4,vphi4,teff,logg = observables(r4,theta4,phi4,
                  self.mesh['teff'],self.mesh['logg'],time,ls,ms,freqs,phases,
-                 spinpars,ks,ampls,deltaTs,deltags)
+                 spinpars,ks,ampls,deltaTs,deltags, mesh_phase=mesh_phase)
     self.mesh['triangle'][:,0:3] = np.array(coordinates.spher2cart_coord(r1,phi1,theta1))[index_inv].T
     self.mesh['triangle'][:,3:6] = np.array(coordinates.spher2cart_coord(r2,phi2,theta2))[index_inv].T
     self.mesh['triangle'][:,6:9] = np.array(coordinates.spher2cart_coord(r3,phi3,theta3))[index_inv].T

devel/phoebe/backend/observatory.py
@@ -69,7 +69,7 @@ logger.addHandler(logging.NullHandler())
 def image(the_system, ref='__bol', context='lcdep',
             cmap=None, select='proj', background=None, vmin=None, vmax=None,
             size=800, ax=None, savefig=False, nr=0, zorder=1, dpi=80,
-            fourier=False, with_partial_as_half=True):
+            fourier=False, with_partial_as_half=True, coords='cartesian'):
     """
     Compute images of a system or make a 2D plot.
     
@@ -263,7 +263,27 @@ def image(the_system, ref='__bol', context='lcdep',
     # the right order.
     mesh = the_system.mesh
     mesh = mesh[np.argsort(mesh['center'][:, 2])]
+    
+    # Now transform the coordinates to spherical coordinates
+    # scales) if needed
+    if coords == 'angular':
+        globals = the_system.get_globals()
+        if globals is None:
+            raise ValueError('There are no global variables defined in this system: cannot compute angular coordinates.')
+        distance = globals.request_value('distance', 'Rsol')
+        origin = globals['ra'], globals['dec']
+        mesh['center'] = np.array(keplerorbit.truecoords_to_spherical(mesh['center'],
+                                         distance=distance, origin=origin, units='mas')).T
+        mesh['triangle'][:,0:3] = np.array(keplerorbit.truecoords_to_spherical(mesh['triangle'][:,0:3],
+                                         distance=distance, origin=origin, units='mas')).T
+        mesh['triangle'][:,3:6] = np.array(keplerorbit.truecoords_to_spherical(mesh['triangle'][:,3:6],
+                                         distance=distance, origin=origin, units='mas')).T
+        mesh['triangle'][:,6:9] = np.array(keplerorbit.truecoords_to_spherical(mesh['triangle'][:,6:9],
+                                         distance=distance, origin=origin, units='mas')).T
+    
     x, y = mesh['center'][:, 0],mesh['center'][:, 1]
+    
+        
     
     # Initiate the figure: if a Fourier transform needs to be computed, we'll
     # create a small figure. Else we let the user decide. If the user supplied

devel/phoebe/backend/universe.py
@@ -5204,7 +5204,9 @@ class Star(PhysicalBody):
             raise ValueError(("Maximum number of triangles reached ({}). "
                               "Consider raising the value of the parameter "
                               "'maxpoints' in the mesh ParameterSet, or "
-                              "decrease the mesh density").format(N))
+                              "decrease the mesh density. It is also "
+                              "possible that the equipotential surface is "
+                              "not closed.").format(N))
         ld_law = 5
         ldbol_law = 5
         if not 'logg' in self.mesh.dtype.names:
@@ -5339,12 +5341,15 @@ class BinaryRocheStar(PhysicalBody):
     Body representing a binary Roche surface.
     """
     
-    def __init__(self,component,orbit=None,mesh=None,reddening=None,pbdep=None,obs=None,**kwargs):
+    def __init__(self, component, orbit=None, mesh=None, reddening=None,
+                 pbdep=None, obs=None, **kwargs):
         """
         Component: 0 is primary 1 is secondary
         """
         super(BinaryRocheStar,self).__init__(dim=3)
         #-- remember the values given, but check their contexts!
+        
+        # Perform some checks on "component"
         if not component.get_context() == 'component':
             raise ValueError(("First argument to BinaryRocheStar needs to be of "
                               "context 'component', not '{}'. You can use the "
@@ -5353,6 +5358,7 @@ class BinaryRocheStar(PhysicalBody):
                               "helps.").format(component.get_context()))
         self.params['component'] = component
         
+        # Perform some checks on "orbit"
         if not orbit.get_context() == 'orbit':
             raise ValueError(("ParameterSet of context '{}' given to keyword "
                               "argument 'orbit'. Should be of context "
@@ -5467,10 +5473,12 @@ class BinaryRocheStar(PhysicalBody):
         #   important: the polar surface gravity of a rotating star is equal
         #   to that of a nonrotating star!
         omega_rot = F * 2*pi/P # rotation frequency
+        omega_orb = 2*pi/P
         r_pole = marching.projectOntoPotential((0,0,1e-5),'BinaryRoche',d,q,F,Phi).r
         r_pole_= np.linalg.norm(r_pole)
         r_pole = r_pole_*a
-        g_pole = roche.binary_surface_gravity(0,0,r_pole,d*a,omega_rot/F,M1,M2,normalize=True)
+            
+        g_pole = roche.binary_surface_gravity(0,0,r_pole,d*a,omega_orb,M1,M2,normalize=True)
         self.params['component'].add_constraint('{{r_pole}} = {0:.16g}'.format(r_pole))
         self.params['component'].add_constraint('{{g_pole}} = {0:.16g}'.format(g_pole))
         self.params['orbit'].add_constraint('{{d}} = {0:.16g}'.format(d*a))
@@ -5503,7 +5511,9 @@ class BinaryRocheStar(PhysicalBody):
             raise ValueError(("Maximum number of triangles reached ({}). "
                               "Consider raising the value of the parameter "
                               "'maxpoints' in the mesh ParameterSet, or "
-                              "decrease the mesh density").format(N))
+                              "decrease the mesh density. It is also "
+                              "possible that the equipotential surface is "
+                              "not closed.").format(N))
         ld_law = 5
         ldbol_law = 5
         new_dtypes = []
@@ -5591,6 +5601,7 @@ class BinaryRocheStar(PhysicalBody):
         pos1,pos2,d = get_binary_orbit(self,time)
         d_ = d*sma
         omega_rot = F * 2*pi/P # rotation frequency
+        omega_orb = 2*pi/P
         
         #-- keep track of the potential vs volume function to compute
         #   derivatives numerically
@@ -5644,7 +5655,7 @@ class BinaryRocheStar(PhysicalBody):
                 oldpot = grad*(V1-volumes[-2])+potentials[-2]
         
         #-- keep parameters up-to-date
-        g_pole = roche.binary_surface_gravity(0,0,R*constants.Rsol,d_*constants.Rsol,omega_rot/F,M1,M2,normalize=True)
+        g_pole = roche.binary_surface_gravity(0,0,R*constants.Rsol,d_*constants.Rsol,omega_orb,M1,M2,normalize=True)
         self.params['component'].add_constraint('{{r_pole}} = {0:.16g}'.format(R*constants.Rsol),do_run_constraints=False)
         self.params['component'].add_constraint('{{g_pole}} = {0:.16g}'.format(g_pole),do_run_constraints=False)
         self.params['orbit'].add_constraint('{{d}} = {0:.16g}'.format(d_*constants.Rsol))
@@ -5840,16 +5851,19 @@ class BinaryRocheStar(PhysicalBody):
                 #   time. Then we compute the mesh at that time and remember
                 #   the value
                 if conserve_volume and e>0:
-                    cvol_index = ['periastron','sup_conj','inf_conj','asc_node','desc_node'].index(conserve_phase)
-                    per0 = self.params['orbit'].request_value('per0') / 180. *np.pi#,'rad')
+                    cvol_index = ['periastron', 'sup_conj', 'inf_conj',
+                                  'asc_node', 'desc_node'].index(conserve_phase)
+                    per0 = self.params['orbit'].request_value('per0')/180.*np.pi
                     P = self.params['orbit']['period']
                     t0 = self.params['orbit']['t0']
                     phshift = self.params['orbit']['phshift']
                     t0type = self.params['orbit']['t0type']
                     crit_times = tools.critical_times(self.params['orbit'])
-                    logger.info("t0 = {}, t_conserve = {}, {}".format(t0,crit_times[cvol_index], conserve_phase))
-                    self.compute_mesh(crit_times[cvol_index],conserve_volume=True)
-                    #print("CONSERVING VOLUME AT {}".format(crit_times[cvol_index]))
+                    logger.info("t0 = {}, t_conserve = {}, {}".format(t0,
+                                        crit_times[cvol_index], conserve_phase))
+                    self.compute_mesh(crit_times[cvol_index],
+                                      conserve_volume=True)
+                    # Calculated the basic properties at this time
                     self.surface_gravity()
                     self.temperature()
                     self.intensity(ref=ref)
@@ -5910,7 +5924,31 @@ class PulsatingBinaryRocheStar(BinaryRocheStar):
             self.params['puls'] = to_add
             
     def add_pulsations(self,time=None):
-        pulsations.add_pulsations(self, time=time)
+        component = self.get_component()
+        mass = self.params['orbit']['mass{}'.format(component+1)]
+        radius = self.params['component']['r_pole']
+        F = self.params['component']['syncpar'] 
+        orbperiod = self.params['orbit']['period']
+        if F>0:
+            rotperiod = orbperiod / F
+        else:
+            rotperiod = np.inf
+        
+        # The mesh of a PulsatingBinaryRocheStar rotates along the orbit, and
+        # it is independent of the rotation of the star. Thus, we need to
+        # specifically specify in which phase the mesh is. It has an "orbital"
+        # phase, and a "rotational" phase. We add 90deg so that it has the
+        # same orientation as a single star at phase 0.
+        
+        # to match coordinate system of Star:
+        mesh_phase = np.pi / 2.0
+        # to correct for orbital phase:
+        mesh_phase+= (time % orbperiod)/orbperiod * 2*np.pi
+        # to correct for rotational phase:
+        mesh_phase-= (time % rotperiod)/rotperiod * 2*np.pi
+        
+        pulsations.add_pulsations(self, time=time, mass=mass, radius=radius,
+                                  rotperiod=rotperiod, mesh_phase=mesh_phase)
 
 
 class MisalignedBinaryRocheStar(BinaryRocheStar):
@@ -6082,7 +6120,9 @@ class MisalignedBinaryRocheStar(BinaryRocheStar):
             raise ValueError(("Maximum number of triangles reached ({}). "
                               "Consider raising the value of the parameter "
                               "'maxpoints' in the mesh ParameterSet, or "
-                              "decrease the mesh density").format(N))
+                              "decrease the mesh density. It is also "
+                              "possible that the equipotential surface is "
+                              "not closed.").format(N))
         ld_law = 5
         ldbol_law = 5
         new_dtypes = []

devel/phoebe/parameters/tools.py
@@ -835,6 +835,7 @@ def to_supconj(orbit):
             
     logger.info('Set t0type to time of superior conjunction')
 
+
 def critical_times(orbit_in, time_range=None):
     """
     Compute critical times in an orbit.
@@ -878,9 +879,39 @@ def critical_times(orbit_in, time_range=None):
         crit_times = crit_times[keep]
         
         
-        
+def make_synchronous(orbit, comp1=None, comp2=None):
+    r"""
+    Set the synchronicity parameter to match synchronous or pseudosynchronous rotation.
     
-    return crit_times
+    Synchronous rotation in the circular (:math:`e=0`) case means:
+    
+    .. math::
+    
+        P_\mathrm{rot} = P_\mathrm{orbit}
+    
+    and Pseudosynchronous rotation means that the star has an agnular rotation
+    rate equal to the orbit angular velocity at periastron. In general, the
+    orbital angular velocity is given by [Hut1981]_:
+    
+    .. math::
+        
+        \omega(t) = \frac{1}{\delta^2}\left(\frac{2\pi}{P_\mathrm{orbit}}\right)\sqrt{1-e^2}
+        
+    At periastron, :math:`\delta=1-e`, so this gives for the synchronicity
+    parameter:
+    
+    .. math::
+    
+        F = \sqrt{\frac{1+e}{(1-e)^3}}
+        
+    """
+    ecc = orbit['ecc']
+    F = np.sqrt( (1+ecc) / (1-ecc)**3)
+    if comp1 is not None:
+        comp1['syncpar'] = F
+    if comp2 is not None:
+        comp2['syncpar'] = F
+    
 #}
 
 #{ Pulsation constraints