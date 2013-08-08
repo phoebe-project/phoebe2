63798a72120ca428027cd7d601593bc0f969d55f
Kyle Conroy <kyleconroy@gmail.com>
2013-08-08 15:32:57 +0000

    added parse_etv and etv plotting functions

--- phoebe/__init__.py
+++ phoebe/__init__.py
@@ -62,7 +62,7 @@ from .backend.universe import Star,BinaryRocheStar,MisalignedBinaryRocheStar,\
 from .parameters.parameters import load as load_ps
 from .backend.universe import load as load_body
 from .parameters.datasets import parse_lc,parse_phot,parse_rv,\
-                                 parse_spec_as_lprof,parse_vis2
+                                 parse_etv,parse_spec_as_lprof,parse_vis2
 from .parameters import create
 from .utils.utils import get_basic_logger
 from phoebe.io import parsers
