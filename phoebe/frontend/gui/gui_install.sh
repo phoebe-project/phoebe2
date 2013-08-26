252fc139a3d189a8940d933592e2c0800484126b
Kyle Conroy <kyleconroy@gmail.com>
2013-08-26 15:50:32 +0000

    changed to install gui to /usr/local/bin

--- phoebe/frontend/gui/gui_install.sh
+++ phoebe/frontend/gui/gui_install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-sudo cp phoebe_gui.py /usr/bin/phoebe_gui
-sudo chmod +x /usr/bin/phoebe_gui
+sudo cp phoebe_gui.py /usr/local/bin/phoebe_gui
+sudo chmod +x /usr/local/bin/phoebe_gui
 
 desktop_file=/usr/share/applications/phoebe_gui.desktop
 
-echo "[Desktop Entry]" > $desktop_file
-echo "Name=PHOEBE 2.0" >> $desktop_file
-echo "Comment=PHysics Of Eclipsing BinariEs" >> $desktop_file
-echo "Exec=/usr/bin/phoebe_gui" >> $desktop_file
-echo "Terminal=false" >> $desktop_file
-echo "Type=Application" >> $desktop_file
-echo "Icon=phoebe-gui" >> $desktop_file
-echo "Categories=Education;Science;Astronomy" >> $desktop_file
+sudo echo "[Desktop Entry]" > $desktop_file
+sudo echo "Name=PHOEBE 2.0" >> $desktop_file
+sudo echo "Comment=PHysics Of Eclipsing BinariEs" >> $desktop_file
+sudo echo "Exec=/usr/bin/phoebe_gui" >> $desktop_file
+sudo echo "Terminal=false" >> $desktop_file
+sudo echo "Type=Application" >> $desktop_file
+sudo echo "Icon=phoebe-gui" >> $desktop_file
+sudo echo "Categories=Education;Science;Astronomy" >> $desktop_file
 
