(1) Fixed bug in computation spectra, introduced after one of my optimization sprees: interp_nDgrid returns lists instead of arrays, since array creation was one of the bottlenecks. Since the spectra need arrays, they are cast to arrays inside atmospheres.spectra. (2) added fast_rotator test_suite to testlib

devel/phoebe/backend/observatory.py
@@ -371,8 +371,7 @@ def image(the_system, ref='__bol', context='lcdep',
             cmap_ = cmap
             cmap = plotlib.blackbody_cmap()
             vmin_, vmax_ = 2000, 20000
-            colors = (values-vmin_) / (vmax_-vmin_)
-            
+            colors = (values-vmin_) / (vmax_-vmin_)        
     # Check for nans or all zeros, that usually means the user did something
     # wrong (OK, there's also a tiny chance that there's a bug somewhere)
     if np.all(values == 0):
@@ -2125,6 +2124,9 @@ def compute(system, params=None, extra_func=None, extra_func_kwargs=None,
                 circular = False
                 logger.info("... but at least one of the components is misaligned")
                 break
+            elif hasattr(body,'bodies'):
+                circular = False
+                logger.info("... but at least one component is a BodyBag")
     else:
         circular = False
         logger.info("Cannot figure out if system is circular or not, leaving at circular={}".format(circular))
@@ -2219,6 +2221,7 @@ def compute(system, params=None, extra_func=None, extra_func_kwargs=None,
     # If the system is circular, we're not recomputing stuff. Make sure to
     # have computed everything as least once:
     if circular:
+        logger.info('System is cicular, pre-computing system')
         system.set_time(0., ref='all')
     
     logger.info("Number of subdivision stages: {}".format(params['subdiv_num']))
@@ -2335,7 +2338,6 @@ def observe(system,times, lc=False, rv=False, sp=False, pl=False, mpi=None,
     params['time'] = times
     params['refs'] = [refs] * len(times)
     params['types'] = [typs] * len(times)  
-    
     # And run compute
     compute(system, params=params, mpi=mpi, extra_func=extra_func,
             extra_func_kwargs=extra_func_kwargs, animate=animate)

devel/phoebe/backend/universe.py
@@ -764,7 +764,10 @@ def _parse_obs(body, data):
         # Check if the reference is present in the Body. There should be a
         # corresponding pbdep somewhere!
         if not context in pbdep_refs:
-            raise ValueError("You can't add observations without adding pbdeps. Add a pbdep of type {} first".format(context))
+            raise ValueError(("You can't add observations of type {} (with "
+                              "reference {}) without adding pbdeps. Add a "
+                              "pbdep of type {} with that reference "
+                              "first").format(context, ref, context))
         if not ref in pbdep_refs[context]:
             logger.warning(("Adding obs with ref='{}', but no corresponding "
                             "pbdeps found. Attempting fix.").format(ref))
@@ -4319,22 +4322,32 @@ class BodyBag(Body):
         """
         # Make sure the mesh is initialised
         self.init_mesh()
-        #-- here, we check which columns are missing from each Body's
-        #   mesh. If they are missing, we simply add them and copy
-        #   the contents from the original mesh.
         
+        # here, we check which columns are missing from each Body's mesh. If
+        # they are missing, we simply add them and copy the contents from the
+        # original mesh.
         logger.info("Preparing mesh")
+        
+        # Get a flattened list of all the bodies
         bodies = self.get_bodies()
+        
+        # Retrieve all the column names and column types from the individual
+        # meshes. Start with the first Body
         names = list(bodies[0].mesh.dtype.names)
         descrs = bodies[0].mesh.dtype.descr
+        
+        # and then append the rest
         for b in bodies[1:]:
             descrs_ = b.mesh.dtype.descr
             for descr in descrs_:
                 if descr[0] in names: continue
                 descrs.append(descr)                    
                 names.append(descr[0])
+        
+        # For each Body, now reinitialize the mesh with a new mesh containing
+        # all the columns from all the components
         dtypes = np.dtype(descrs)
-        for b in self.bodies:
+        for b in bodies:
             N = len(b.mesh)
             new_mesh = np.zeros(N,dtype=dtypes)
             if N:
@@ -4343,6 +4356,7 @@ class BodyBag(Body):
                     new_mesh[col] = b.mesh[col]
                 #new_mesh[cols_to_copy] = b.mesh[cols_to_copy]
             b.mesh = new_mesh
+            
         # We need to make sure to reset the body, otherwise we could be fooled
         # into thinking that everything is still calculated! Some bodies do not
         # recalculate anything when the time is already set (because they are
@@ -4350,9 +4364,11 @@ class BodyBag(Body):
         # columns to zero!
         self.reset()
     
+    
     def remove_mesh(self):
         for body in self.bodies:
             body.remove_mesh()
+        
         
     def get_bodies(self):
         """
@@ -4451,7 +4467,7 @@ class BodyBag(Body):
             # for ltt's, we can only ask for one of it's members to return their
             # proper time. But if you do this you assume the proper time of the
             # members of the bodybag is the same anyway. So this should be fine!
-            time = self.get_proper_time()[0]
+            #time = self.get_proper_time(time)[0]
             #-- once we have the mesh, we need to place it into orbit
             #keplerorbit.place_in_binary_orbit(self, time)
             n_comp = self.get_component()