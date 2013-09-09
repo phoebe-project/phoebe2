(1) Fixed issue with pblum and l3 scaling, bug introduced after adding oversampling (2) Updated V380_circular lc and rv synthetic data files to match the synthetic parameters (3) added V380_circular to testlib, so that pblum and l3 scaling is also tested,     as well as rv grouping for systemic velocities

devel/phoebe/algorithms/interp_nDgrid.py
@@ -12,7 +12,7 @@ except ImportError:
     print("Soft warning: pyfits could not be found on your system, you can only use black body atmospheres and Gaussian synthetic spectral profiles")    
 
 
-def create_pixeltypegrid(grid_pars,grid_data):
+def create_pixeltypegrid(grid_pars, grid_data):
     """
     Creates pixelgrid and arrays of axis values.
     
@@ -58,7 +58,7 @@ def create_pixeltypegrid(grid_pars,grid_data):
     return axis_values, pixelgrid
 
 
-def interpolate(p, axis_values, pixelgrid):
+def interpolate(p, axis_values, pixelgrid, order=1):
     """
     Interpolates in a grid prepared by create_pixeltypegrid().
     
@@ -76,7 +76,11 @@ def interpolate(p, axis_values, pixelgrid):
     p_coord = (p-lowervals_stepsize[:,0])/lowervals_stepsize[:,1] + p_-1
 
     # interpolate
-    return [ndimage.map_coordinates(pixelgrid[...,i],p_coord, order=1, prefilter=False) \
+    if order > 1:
+        prefilter = True
+    else:
+        prefilter = False
+    return [ndimage.map_coordinates(pixelgrid[...,i],p_coord, order=order, prefilter=prefilter) \
                 for i in range(np.shape(pixelgrid)[-1])]
     
 

devel/phoebe/atmospheres/tables/build_grids.py
@@ -65,7 +65,10 @@ if __name__=="__main__":
     #build_grid(filetag='phoenix', passbands=None, ld_func='claret', fitmethod='equidist_r_leastsq',
     #           redlaw='fitzpatrick2004', Rv=3.1, z='p00', vmic=1, ebvs=None,
     #           vgamma=None)
-    build_grid(filetag='kurucz', passbands=('JOHNSON.V',), ld_func='linear', fitmethod='equidist_r_leastsq',
-               z='p00', ebvs=None, redlaw=None, 
-               vgamma=np.linspace(-500,500,21))
+    #build_grid(filetag='kurucz', passbands=('JOHNSON.V',), ld_func='linear', fitmethod='equidist_r_leastsq',
+               #z='p00', ebvs=None, redlaw=None, 
+               #vgamma=np.linspace(-500,500,21))
+    build_grid(filetag='kurucz', passbands=('JOHNSON.V','KEPLER.V'), ld_func='quadratic', fitmethod='equidist_r_leastsq',
+               z='m01', ebvs=None, redlaw=None)#, 
+#               vgamma=np.linspace(-50,50,21))
     

devel/phoebe/parameters/datasets.py
@@ -271,7 +271,8 @@ class DataSet(parameters.ParameterSet):
         result = self.copy()
         columns = result.get_value('columns')
         for col in columns:
-            if col in ['time','weights','flag','wavelength']: continue
+            if col in ['time','weights','flag','wavelength','samprate']:
+                continue
             this_col = np.array(result[col])
             other_col = np.array(other[col])
             if col in ['sigma']:
@@ -341,10 +342,22 @@ class DataSet(parameters.ParameterSet):
             return self_copy
     
     def take(self, index):
+        """
+        Take elements from the data arrays.
+        
+        This function does the same thing as fancy indexing (indexing arrays
+        using arrays).
+        """
         for col in self['columns']:
             self[col] = self[col][index]
     
-    def hostile_alien_takeover(self, other_ds):
+    def overwrite_values_from(self, other_ds):
+        """
+        Overwrite parameters values in this dataset with those from another.
+        
+        @param other_ds: other dataset to take the values from
+        @type other_ds: DataSet
+        """
         for key in other_ds:
             self[key] = other_ds[key]
     
@@ -1193,7 +1206,6 @@ def process_file(filename, default_column_order, ext, columns, components,\
     parsed = parse_header(filename, ext=ext)
     (columns_in_file, components_in_file, units_in_file, dtypes_in_file, ncol),\
                    (pb, ds) = parsed
-    
     # Remember user-supplied arguments and keyword arguments for this parse
     # function
     # add columns, components, dtypes, units, full_output    
@@ -1557,7 +1569,6 @@ def parse_rv(filename, columns=None, components=None,
         (pb, ds) = process_file(filename, default_column_order, 'rv', columns, \
                                             components, dtypes, units, **kwargs)
     
-    
     # Add sigma if not available:
     myds = output.values()[0][0][-1]
     if not 'sigma' in myds['columns']:
@@ -1567,6 +1578,10 @@ def parse_rv(filename, columns=None, components=None,
     # Convert to right units
     for col in units:
         if col == 'rv':
+            # if sigma units and normal units are not the same, we need to
+            # probably first convert rv to sigma units, then rv&sigma to correct
+            # units, just to be safe (probably more important for mag-flux
+            # conversions)
             f, e_f = conversions.convert(units[col],
                                          myds.get_parameter(col).get_unit(), 
                                          myds['rv'], myds['sigma'])