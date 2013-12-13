e5cc33adce8ca34937176a303a40655c9fbf2bc0
pieterdegroote <pieterdegroote@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2013-12-13 08:44:48 +0000

    some additions to bundle_new (phoebeBackend interface and set_time). added get_logp function to Parameter, which returns the probability of the current parameter value given the pprior (if no prior, the probability is 1)

--- devel/phoebe/backend/observatory.py
+++ devel/phoebe/backend/observatory.py
@@ -2440,6 +2440,7 @@ def compute(system, params=None, extra_func=None, extra_func_kwargs=None,
         except:
             logger.warning("Cannot compute pblum or l3. I can think of three reasons why this would fail: (1) you're in MPI (2) you have previous results attached to the body (3) you did not give any actual observations, so there is nothing to scale the computations to.")
         
+        
 def observe(system,times, lc=False, rv=False, sp=False, pl=False, mpi=None,
             extra_func=[], extra_func_kwargs=[{}], animate=None, **kwargs):
     """
