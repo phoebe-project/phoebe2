be6853e98308904e0655faf5a00568e114bef22c
pieterdegroote <pieterdegroote@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2014-02-19 12:15:43 +0000

    updated setup.py script for new extensions

--- devel/phoebe-testlib/test_scaling/test_v380cyg.py
+++ devel/phoebe-testlib/test_scaling/test_v380cyg.py
@@ -12,7 +12,7 @@ logger = phoebe.get_basic_logger()
 
 exdir = os.path.dirname(__file__)
 
-def test_pblums_l3():
+def test_pblums_l3(debug=False):
     """
     Pblum and l3 scaling: synthetic V380
     """
@@ -43,7 +43,6 @@ def test_pblums_l3():
             components=[None,'V380Cyg','V380Cyg'],
             ref='mylc')
 
-
     # create bodies
     starA = universe.BinaryRocheStar(Apars, mesh=meshpars, orbit=orbitpars,
                                     pbdep=[pbdep_lc,pbdep_rv_a], obs=[obs_rv_a])
@@ -68,14 +67,27 @@ def test_pblums_l3():
     rvobs2 = system[1].get_obs(category='rv', ref='myrv_comp2').asarray()
     rvsyn2 = system[1].get_synthetic(category='rv', ref='myrv_comp2').asarray()
 
-
-    assert(np.mean((lcobs['flux']-(lcsyn['flux']*lcobs['pblum'] - lcobs['l3']))**2/lcobs['sigma']**2)<0.00017)
-    assert(np.mean((rvobs1['rv']-(rvsyn1['rv'] - rvobs1['vgamma_offset']))**2/rvobs1['sigma']**2)<2.14)
-    assert(np.mean((rvobs2['rv']-(rvsyn2['rv'] - rvobs2['vgamma_offset']))**2/rvobs2['sigma']**2)<2.41)
+    if not debug:
+        assert(np.mean((lcobs['flux']-(lcsyn['flux']*lcobs['pblum'] - lcobs['l3']))**2/lcobs['sigma']**2)<0.00017)
+        assert(np.mean((rvobs1['rv']-(rvsyn1['rv'] - rvobs1['vgamma_offset']))**2/rvobs1['sigma']**2)<2.14)
+        assert(np.mean((rvobs2['rv']-(rvsyn2['rv'] - rvobs2['vgamma_offset']))**2/rvobs2['sigma']**2)<2.41)
+    else:
+        print rvobs1['rv']
+        print rvsyn1['rv']
+        print rvobs1['vgamma_offset']
+        diagnostics = np.mean((lcobs['flux']-(lcsyn['flux']*lcobs['pblum'] - lcobs['l3']))**2/lcobs['sigma']**2),\
+                      np.mean((rvobs1['rv']-(rvsyn1['rv'] - rvobs1['vgamma_offset']))**2/rvobs1['sigma']**2),\
+                      np.mean((rvobs2['rv']-(rvsyn2['rv'] - rvobs2['vgamma_offset']))**2/rvobs2['sigma']**2)
+        return system, diagnostics
 
 
 
 if __name__=='__main__':
+    
+    system, diag = test_pblums_l3(debug=True)
+    print diag
+    starA = system[0]
+    starB = system[1]
     # plot
     plt.cla()
     plotting.plot_lcobs(system, ref='mylc', color='k', marker='o', errorbars=False)
@@ -83,8 +95,8 @@ if __name__=='__main__':
     plt.show()
 
     plt.cla()
-    plotting.plot_rvobs(starA, ref='myrv_comp1', color='b', marker='o', errorbars=False)
-    plotting.plot_rvobs(starB, ref='myrv_comp2', color='r', marker='o', errorbars=False)
+    plotting.plot_rvobs(starA, ref='myrv_comp1', color='b', marker='o', linestyle='', errorbars=False)
+    plotting.plot_rvobs(starB, ref='myrv_comp2', color='r', marker='o', linestyle='', errorbars=False)
     plotting.plot_rvsyn(starA, ref='myrv_comp1', color='b')
     plotting.plot_rvsyn(starB, ref='myrv_comp2', color='r')
     plt.show()
