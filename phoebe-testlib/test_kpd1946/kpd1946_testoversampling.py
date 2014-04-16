86233e663ca0c71fe379016b9e0e71597f624a32
sbloemen <s.bloemen@astro.ru.nl>
2014-04-16 08:49:45 +0000

    Update KPD1946 nosetest

--- phoebe-testlib/test_kpd1946/kpd1946_testoversampling.py
+++ phoebe-testlib/test_kpd1946/kpd1946_testoversampling.py
@@ -12,20 +12,22 @@ def test_oversampling():
     assert(np.allclose(b1dat['time'][0],0.))
 
 def test_beaming():
+    phases = [0.75]
     bundle1 = phoebe.Bundle('KPD1946+4340')
-    bundle1.create_data(phase=[0.25],passband='KEPLER.V', dataref='keplc')
+    bundle1.create_data(phase=phases,passband='KEPLER.V', dataref='keplc')
     bundle1.run_compute(beaming_alg='simple')
 
     bundle2 = phoebe.Bundle('KPD1946+4340')
 
-    bundle2.create_data(phase=[0.25],passband='KEPLER.V', dataref='keplc')
+    bundle2.create_data(phase=phases,passband='KEPLER.V', dataref='keplc')
     bundle2.run_compute(beaming_alg='none')
     b1dat = bundle1.get_syn('keplc', objref='KPD1946+4340')
     b2dat = bundle2.get_syn('keplc', objref='KPD1946+4340')
-    assert(b1dat['flux'][0]/b2dat['flux'][0]>1.0004 and b1dat['flux'][0]/b2dat['flux'][0]<1.0006)
+        
+    assert(b1dat['flux'][0]/b2dat['flux'][0]>1.0007 and b1dat['flux'][0]/b2dat['flux'][0]<1.0008)
 
 
 if __name__ == "__main__":
     logger = phoebe.get_basic_logger()
     test_oversampling()
-    #test_beaming()
+    test_beaming()
