b1d5f9bcb299082791faaecd5073da5f38faaa92
Kyle Conroy <kyleconroy@gmail.com>
2014-04-26 21:52:14 +0000

    fixed json format to reload times for datasets without filenames with update to testlib

--- phoebe-testlib/test_bundle/test_io.py
+++ phoebe-testlib/test_bundle/test_io.py
@@ -9,33 +9,58 @@ def test_json():
     """
     mybundle = phoebe.Bundle()
     
+    # attach a non-standard  parameter set
+    mybundle.attach_ps('new_system', phoebe.PS('reddening:interstellar'))
+    mybundle.set_value('passband@reddening', 'JOHNSON.K')
+    
+    # change a value
     mybundle.set_value('period',200)
+    
+    # change adjust
     mybundle.set_adjust('period',True)
+
+    # create a synthetic dataset
+    mybundle.rv_fromarrays(phase=np.linspace(0,1,100),objref=['primary','secondary'],dataref='myrv_phase')
+    mybundle.rv_fromarrays(time=np.linspace(0,1,50),objref='primary',dataref='myrv_time')
+    mybundle.lc_fromarrays(time=np.linspace(0,1,10),dataref='mylc_time')
+    
+    #~ print "\n", mybundle.twigs('myrv_phase')
+    #~ print "\n", mybundle.twigs('myrv_time')
+    #~ print "\n", mybundle.twigs('mylc_time')
+    
+    assert(len(mybundle.twigs('myrv_phase'))==6)
+    assert(len(mybundle.twigs('myrv_time'))==3)
+    assert(len(mybundle.twigs('mylc_time'))==4)
+    assert(len(mybundle['phase@myrv_phase@rvobs@primary'])==100)
+    assert(len(mybundle['time@myrv_time@rvobs@primary'])==50)
+    assert(len(mybundle['time@mylc_time@lcobs@new_system'])==10)
+    
 
     mybundle.save('test_io.json')
 
     mybundle = phoebe.Bundle('test_io.json')
     
-    assert(mybundle.get_value('period')==200)
-    assert(mybundle.get_adjust('period')==True)
-
-#def test_json2():
-    #"""
-    #Testing customizing Bundle with json input and output
-    #"""
-    ## What happens with non standard parameterSets?
-    #mybundle = phoebe.Bundle()
-    #mybundle.attach_ps('new_system', phoebe.PS('reddening:interstellar'))
+    # make sure the non-standard PS was restored with the changed value
+    assert(mybundle.get_value('passband@reddening') == 'JOHNSON.K')
     
-    #mybundle.set_value('passband@reddening', 'JOHNSON.K')
-    #mybundle.save('test_io2.json')
+    # make sure the value change was loaded
+    assert(mybundle.get_value('period')==200)
+    
+    # make sure adjustments are loaded
+    assert(mybundle.get_adjust('period')==True)
+    
+    # make sure data_fromarrays all loaded
+    #~ print "\n", mybundle.twigs('myrv_phase')
+    #~ print "\n", mybundle.twigs('myrv_time')
+    #~ print "\n", mybundle.twigs('mylc_time')
+    
+    assert(len(mybundle.twigs('myrv_phase'))==6)
+    assert(len(mybundle.twigs('myrv_time'))==3)
+    assert(len(mybundle.twigs('mylc_time'))==4)
+    assert(len(mybundle['phase@myrv_phase@rvobs@primary'])==100)
+    assert(len(mybundle['time@myrv_time@rvobs@primary'])==50)
+    assert(len(mybundle['time@mylc_time@lcobs@new_system'])==10)
 
-    #mybundle = phoebe.Bundle('test_io.json')
-    #assert(mybundle.get_value('passband@reddening') == 'JOHNSON.K')
-    #assert(mybundle.get_value('period')==200)
-    #assert(mybundle.get_adjust('period')==True)
 
 if __name__ == "__main__":
     test_json()
-    #test_json2()
-    
