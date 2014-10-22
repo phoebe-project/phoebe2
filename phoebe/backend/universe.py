fixed *critical* bug in volume computation of non-spherical stars

phoebe/backend/universe.py
@@ -7204,8 +7204,12 @@ class Star(PhysicalBody):
         """
         Compute volume of the convex mesh of the Star.
         """
-        norm = coordinates.norm(self.mesh['_o_center'],axis=1)
-        return np.sum(self.mesh['_o_size']*norm/3.)
+        #~ norm = coordinates.norm(self.mesh['_o_center'],axis=1)
+        #~ return np.sum(self.mesh['_o_size']*norm/3.)
+
+        # the volume of a slanted triangular cone is A_triangle * (r_vec dot norm_vec) / 3.
+        norm_along_normal = np.array([np.dot(c,n) for c,n in zip(self.mesh['_o_center'], self.mesh['_o_normal_'])])
+        return np.sum(self.mesh['_o_size']*norm_along_normal/3.)
     
     def surface_gravity(self):
         """
@@ -8321,8 +8325,12 @@ class BinaryRocheStar(PhysicalBody):
         """
         Compute volume of a BinaryRocheStar.
         """
-        norm = coordinates.norm(self.mesh['_o_center'],axis=1)
-        return np.sum(self.mesh['_o_size']*norm/3.)
+        #~ norm = coordinates.norm(self.mesh['_o_center'],axis=1)
+        #~ return np.sum(self.mesh['_o_size']*norm/3.)
+        
+        # the volume of a slanted triangular cone is A_triangle * (r_vec dot norm_vec) / 3.
+        norm_along_normal = np.array([np.dot(c,n) for c,n in zip(self.mesh['_o_center'], self.mesh['_o_normal_'])])
+        return np.sum(self.mesh['_o_size']*norm_along_normal/3.)
         
     
     @decorators.parse_ref