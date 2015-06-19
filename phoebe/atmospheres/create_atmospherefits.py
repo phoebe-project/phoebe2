be5b3aceae9975ba8817afcec56c74d05950b7e6
sbloemen <s.bloemen@astro.ru.nl>
2015-06-19 15:03:04 +0000

    Typofix

--- phoebe/atmospheres/create_atmospherefits.py
+++ phoebe/atmospheres/create_atmospherefits.py
@@ -547,7 +547,7 @@ def compute_grid_ld_coeffs(atm_files,atm_pars=('teff', 'logg'),\
             
             # transform fluxes and wavelength
             # we first smooth somewhat to avoid numerical issues with metal lines. We go for gaussian smoothing of 10 angstrom in case we have a regular grid here.
-            logger.debug("Smoothing over %f points before computing beaming factor"10/np.median(np.diff(wave)))
+            logger.debug("Smoothing over %f points before computing beaming factor"%(10/np.median(np.diff(wave))))
             flux_smooth = scipy.ndimage.filters.gaussian_filter1d(flux, 10/np.median(np.diff(wave)))
             wavl_smooth = scipy.ndimage.filters.gaussian_filter1d(wave, 10/np.median(np.diff(wave)))
 
