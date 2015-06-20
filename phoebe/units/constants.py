f6c5ef04d38f0e09ec0c72d7cce664e2afdb0435
andrej_prsa <aprsa09@gmail.com>
2015-06-20 14:22:05 +0000

    Constants updated to the latest IAU resolution draft values.

--- phoebe/units/constants.py
+++ phoebe/units/constants.py
@@ -40,10 +40,10 @@ _current_frequency = 'rad'
 #===============================================================================
 cc     = 299792458.        # speed of light              m/s
 cc_air = cc/1.0002773      # speed of light in air       m/s           T=15C, standard atmospheric pressure
-Msol   = 1.988547e30       # solar mass                  kg            Harmanec & Prsa 2011
-Rsol   = 6.95508e8         # solar radius                m             Harmanec & Prsa 2011
-Lsol   = 3.846e26          # solar luminosity            W             Harmanec & Prsa 2011
-Tsol   = 5779.5747         # solar effective temperature K             Harmanec & Prsa 2011
+Msol   = 1.988547e30       # solar mass                  kg            IAU resolution draft, June 2015
+Rsol   = 6.9566e8          # solar radius                m             IAU resolution draft, June 2015
+Lsol   = 3.828e26          # solar luminosity            W             IAU resolution draft, June 2015
+Tsol   = 5772.0            # solar effective temperature K             IAU resolution draft, June 2015
 agesol = 1.44221e+17       # solar age                   s             Bahcall 2005
 Mearth = 5.9742e24         # earth mass                  kg            google
 Rearth = 6371.e3            # earth radius                m             wikipedia
@@ -60,7 +60,7 @@ kB     = 1.380650424e-23   # Boltzmann constant          J/K
 NA     = 6.0221417930e23   # Avogadro constant           1/mol
 sigma  = 5.67040040e-8     # Stefan-Boltzmann constant   W/m2/K4       Harmanec & Prsa 2011
 GG     = 6.67384e-11       # gravitational constant      m3/kg/s2      Harmanec & Prsa 2011
-GGMsol = 1.32712442099e20  # grav. constant x solar mass m3/s2         Harmanec & Prsa 2011
+GGMsol = 1.3271244e20      # grav. constant x solar mass m3/s2         IAU resolution draft, June 2015
 RR     = 8.31447215        # (ideal) gas constant        J/K/mol
 aa     = 7.5657e-16        # radiation constant          J/m3/K4
 a0     = 52.9177e-12       # Bohr radius of hydrogen     m
@@ -120,9 +120,9 @@ F0_ST_units  = 'W m-3'
 # value                        name                          unit           reference
 #====================================================================================
 cc_cgs     = 29979245800.      # speed of light              cm/s
-Msol_cgs   = 1.988547e33       # solar mass                  g              Harmanec & Prsa 2011
-Rsol_cgs   = 6.95508e10        # solar radius                cm             Harmanec & Prsa 2011
-Lsol_cgs   = 3.846e33          # solar luminosity            erg/s          Harmanec & Prsa 2011
+Msol_cgs   = 1.988547e33       # solar mass                  g              IAU resolution draft, June 2015
+Rsol_cgs   = 6.9566e10         # solar radius                cm             IAU resolution draft, June 2015
+Lsol_cgs   = 3.828e33          # solar luminosity            erg/s          IAU resolution draft, June 2015
 Mearth_cgs = 5.9742e27         # earth mass                  g              google
 Rearth_cgs = 6371e5            # earth radius                cm             wikipedia
 Mjup_cgs   = 1.8986e30         # Jupiter mass                g              wikipedia
@@ -135,8 +135,8 @@ hh_cgs     = 6.6260689633e-27  # Planck constant             erg/s
 hhbar_cgs  = 1.05457162853e-27 # reduced Planck constant     erg/s
 kB_cgs     = 1.380650424e-16   # Boltzmann constant          erg/K
 sigma_cgs  = 5.67040040e-5     # Stefan-Boltzmann constant   erg/cm2/s/K4   Harmanec & Prsa 2011
-GG_cgs     = 6.67384e-8        # gravitational constant      cm3/g/s2       Harmanec & Prsa 2011
-GGMsol_cgs = 1.32712442099e26  # grav. constant x solar mass cm3/s2         Harmanec & Prsa 2011
+GG_cgs     = 6.67384e-8        # gravitational constant      cm3/g/s2       IAU resolution draft, June 2015
+GGMsol_cgs = 1.3271244e26      # grav. constant x solar mass cm3/s2         IAU resolution draft, June 2015
 RR_cgs     = 8.31447215e7      # (ideal) gas constant        erg/K/mol
 aa_cgs     = 7.5657e-15        # radiation constant          erg/cm2/K4
 a0_cgs     = 52.9177e-10       # Bohr radius of hydrogen     cm
