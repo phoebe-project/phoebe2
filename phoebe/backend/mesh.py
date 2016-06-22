- fixed bug in rotation velocities - observables (in addition to physical quantities) are now computed at vertices and then averaged for each element - updated legacy example script to compare legacy to both wd and marching meshes

phoebe/backend/mesh.py
@@ -68,22 +68,28 @@ def transform_position_array(array, pos, euler, is_normal):
     else:
         return np.dot(np.asarray(array), trans_matrix.T) + np.asarray(pos)
 
-def transform_velocity_array(array, vel, euler, rotation_vel=(0,0,0)):
+def transform_velocity_array(array, pos_array, vel, euler, rotation_vel=(0,0,0)):
     """
     TODO: add documentation
 
+    array are the velocities wrt the mesh (probably zeros, with same shape as centers or vertices)
+    pos_array are the position of the elements wrt the star (either centers or vertices)
     vel is center of mass velocitiy (in new system frame)
-    rotation_vel is vector of the rotation velocity of the star (in original frame?)
+    rotation_vel is vector of the rotation velocity of the star (in original/proto frame)
     """
 
     # TODO: check this cross product - the star doesn't seem to be rotating correctly
     # (could also be a units or convention issue)
-    trans_matrix = np.cross(euler_trans_matrix(*euler), np.asarray(rotation_vel))
+    # trans_matrix = np.cross(euler_trans_matrix(*euler), np.array([0, 0, 1]))
+    trans_matrix = euler_trans_matrix(*euler)
+
+    rotation_component = np.cross(pos_array, rotation_vel, axisa=1)
+    orbital_component = np.asarray(vel)
 
     if isinstance(array, ComputedColumn):
         array = array.for_computations
 
-    new_vel = np.dot(np.asarray(array), trans_matrix.T) + np.asarray(vel)
+    new_vel = np.dot(np.asarray(array)+rotation_component, trans_matrix.T) + orbital_component
 
     return new_vel
 
@@ -202,13 +208,18 @@ class ComputedColumn(object):
     @property
     def for_observations(self):
         if self.mesh._compute_at_vertices:
-            return self.averages
-
-            # TODO: switch to the following once weights are properly returned from eclipse detection
+            # TODO: make this an option at some point?
+            # return self.averages
             return self.weighted_averages
         else:
             return self.centers
 
+
+    def set_for_computations(self, value):
+        if self.mesh._compute_at_vertices:
+            self._vertices = value
+        else:
+            self._centers = value
 
 
 class ProtoMesh(object):
@@ -305,10 +316,7 @@ class ProtoMesh(object):
             col = getattr(self, hkey)
 
             if isinstance(col, ComputedColumn) and not isinstance(value, ComputedColumn):
-                if self._compute_at_vertices:
-                    col._vertices = value
-                else:
-                    col._centers = value
+                col.set_for_computations(value)
             else:
                 setattr(self, hkey, value)
 
@@ -318,7 +326,11 @@ class ProtoMesh(object):
             # applicable only for Mesh, not ProtoMesh
             # even if it doesn't exist, we'll make a new entry in observables]
 
-            self._observables[key] = value
+            if key not in self._observables.keys():
+                self._observables[key] = ComputedColumn(self)
+
+            self._observables[key].set_for_computations(value)
+
 
         elif hasattr(self, key) and not hasattr(self, hkey):
             pass
@@ -636,7 +648,7 @@ class Mesh(ScaledProtoMesh):
         # or itself be ComputedColumns??
         self._observables       = {}    # Nx1 (each)
 
-        keys = ['mus', 'visibilities', 'weights', 'observables']
+        keys = ['mus', 'vmus', 'visibilities', 'weights', 'observables']
         keys = keys + kwargs.pop('keys', [])
 
         super(Mesh, self).__init__(keys=keys, **kwargs)
@@ -689,13 +701,19 @@ class Mesh(ScaledProtoMesh):
         norm_ks = ['vnormals', 'tnormals'] #, 'cnormals']
         vel_ks = ['velocities']
 
+        # TODO: we need to copy here to use the non-transformed position arrays
+        # for determining rotation... but instead maybe we can just change the
+        # order or store a copy of the positions wrt COM of the star.  Will
+        # also need to keep this in mind if we try to support incremental
+        # transformations.
+        pos_array = self.vertices if self._compute_at_vertices else self.centers
+        self.update_columns_dict({k: transform_velocity_array(self[k], pos_array, vel, euler, rotation_vel) for k in vel_ks if self[k] is not None})
+        # TODO: handle velocity from mesh reprojection during volume conservation
 
         # handle rotation/displacement
         # NOTE: mus will automatically be updated when updating normals
         self.update_columns_dict({k: transform_position_array(self[k], pos, euler, False) for k in pos_ks if self[k] is not None})
         self.update_columns_dict({k: transform_position_array(self[k], pos, euler, True) for k in norm_ks if self[k] is not None})
-        self.update_columns_dict({k: transform_velocity_array(self[k], vel, euler, rotation_vel) for k in vel_ks if self[k] is not None})
-        # TODO: handle velocity from mesh reprojection during volume conservation
 
 
     @property
@@ -709,6 +727,17 @@ class Mesh(ScaledProtoMesh):
         # this requires tnormals to be NORMALIZED (currently we don't do any
         # checks - this is just assumed)
         return self.tnormals[:,2]
+
+    @property
+    def mus_for_computations(self):
+        """
+        TODO: add documentation
+        """
+        if self._compute_at_vertices:
+            return self.vnormals[:,2]
+        else:
+            return self.mus
+
 
 
     @property
@@ -765,7 +794,7 @@ class Mesh(ScaledProtoMesh):
             # self._compute_mus()
         if kwargs.get('triangles', None) is not None:
             # reset visibilities and velocities so that they are reset
-            # when nexted queried
+            # when next queried
             self.update_columns(visibilities=None, velocities=None)
 
 

phoebe/backend/universe.py
@@ -200,6 +200,7 @@ class System(object):
                 body.populate_observable(time, method, dataset, **kwargs)
 
 
+
     def handle_eclipses(self, **kwargs):
         """
         Detect the triangles at the horizon and the eclipsed triangles, handling
@@ -812,7 +813,7 @@ class Body(object):
 
         # intens_norm_abs are directly out of the passbands module and are
         # emergent normal intensities in this dataset's passband/atm in absolute units
-        intens_norm_abs = self.mesh['intens_norm_abs:{}'.format(dataset)]
+        intens_norm_abs = self.mesh['intens_norm_abs:{}'.format(dataset)].for_observations
 
         # The luminosity will be the integrated NORMAL intensities, but since some
         # limbdarkening laws still darken/brighten at mu=0, we'll compute the
@@ -1335,7 +1336,7 @@ class Star(Body):
         """
         g_rel_to_abs = c.G.si.value*c.M_sun.si.value*self.masses[self.ind_self]/(self.sma*c.R_sun.si.value)**2*100. # 100 for m/s**2 -> cm/s**2
 
-        loggs = np.log10(g_rel_to_abs * self.mesh.normgrads.for_computations)
+        loggs = np.log10(self.mesh.normgrads.for_computations * g_rel_to_abs)
         self.mesh.update_columns(loggs=loggs)
 
         # logger.info("derived surface gravity: %.3f <= log g<= %.3f (g_p=%s and Rp=%s Rsol)"%(loggs.min(), loggs.max(), self._instantaneous_gpole, self._instantaneous_rpole*self._scale))
@@ -1350,7 +1351,7 @@ class Star(Body):
 
         g_rel_to_abs = c.G.si.value*c.M_sun.si.value*self.masses[self.ind_self]/(self.sma*c.R_sun.si.value)**2*100. # 100 for m/s**2 -> cm/s**2
         # TODO: check the division by 100 - is this just to change units back to m?
-        gravs = (g_rel_to_abs * self.mesh.normgrads.for_computations/self._instantaneous_gpole/100.)**self.gravb_bol
+        gravs = ((self.mesh.normgrads.for_computations * g_rel_to_abs)/self._instantaneous_gpole/100.)**self.gravb_bol
 
         # TODO: make sure equivalent to the old way here
         # gravs = abs(10**(self.mesh.loggs.for_computations-2)/self._instantaneous_gpole)**self.gravb_bol
@@ -1502,8 +1503,7 @@ class Star(Body):
         # the change in sign from our right-handed system to RV conventions.
         # These will be weighted by the fluxes when integrating
 
-        # TODO: should this be .for_observations (.weighted_averages) or .averages
-        rvs = -1*self.mesh.velocities.for_observations[:,2]
+        rvs = -1*self.mesh.velocities.for_computations[:,2]
 
         # Gravitational redshift
         if self.do_rv_grav:
@@ -1549,10 +1549,9 @@ class Star(Body):
                 self._pbs[passband] = pb
 
             # intens_norm_abs are the normal emergent passband intensities:
-
-            intens_norm_abs = self._pbs[passband].Inorm(Teff=self.mesh.teffs.for_observations,
-                                                        logg=self.mesh.loggs.for_observations,
-                                                        met=self.mesh.abuns.for_observations,
+            intens_norm_abs = self._pbs[passband].Inorm(Teff=self.mesh.teffs.for_computations,
+                                                        logg=self.mesh.loggs.for_computations,
+                                                        met=self.mesh.abuns.for_computations,
                                                         atm=atm)
 
             # Handle pblum - distance and l3 scaling happens when integrating (in observe)
@@ -1588,8 +1587,7 @@ class Star(Body):
 
             # light speed in Rsol/d
             # TODO: should we mutliply velo__bol_ by -1?
-            # TODO: should this be .for_observations (.weighted_averages) or .averages?
-            ampl_boost = 1.0 + alpha_b * self.mesh.velocities.for_observations[:,2]/37241.94167601236
+            ampl_boost = 1.0 + alpha_b * self.mesh.velocities.for_computations[:,2]/37241.94167601236
 
 
             # Limb-darkening
@@ -1597,7 +1595,7 @@ class Star(Body):
             # TODO: the abs shouldn't be necessary here, but for some reason
             # is causing issues when ld_logarithmic with negative mus (well
             # I mean the reason it fails is obvious)
-            ld = getattr(limbdark, 'ld_{}'.format(ld_func))(np.abs(self.mesh.mus), ld_coeffs)
+            ld = getattr(limbdark, 'ld_{}'.format(ld_func))(np.abs(self.mesh.mus_for_computations), ld_coeffs)
 
             # TODO: FIX AND ENABLE LIMB DARKENING
             # ld = np.ones(self.mesh.mus.shape)
@@ -1958,9 +1956,6 @@ class Envelope(Body):
         """
         [NOT IMPLEMENTED]
         requires _fill_loggs and _fill_gravs to have been called
-
-
-
         """
         self.mesh.update_columns(teffs=0.0)
 

phoebe/frontend/plotting.py
@@ -48,11 +48,11 @@ logger.addHandler(logging.NullHandler())
 
 
 # TODO: this is redundant with parameters.py - figure out a way to import without circular imports (perhaps put these in their own module)
-_meta_fields_twig = ['time', 'qualifier', 'history', 'component', 
-                'dataset', 'constraint', 'compute', 'model', 'fitting', 
+_meta_fields_twig = ['time', 'qualifier', 'history', 'component',
+                'dataset', 'constraint', 'compute', 'model', 'fitting',
                 'feedback', 'plugin', 'method',
                 'context']
-                
+
 _meta_fields_all = _meta_fields_twig + ['twig', 'uniquetwig', 'uniqueid']
 
 def _mpl_append_axes(fig, **kwargs):
@@ -64,7 +64,7 @@ def _mpl_append_axes(fig, **kwargs):
     N = len(fig.axes)
 
     # we'll reset the layout later anyways
-    ax_new = fig.add_subplot(1,N+1,N+1, **kwargs)  
+    ax_new = fig.add_subplot(1,N+1,N+1, **kwargs)
 
     axes = fig.axes
     N = len(axes)
@@ -93,7 +93,7 @@ def mpl(ps, data, plot_inds, do_plot=True, **kwargs):
         # color from mplkwargs, make the plotting call without it, and then
         # overplot a scatter call
 
-        _symmetric_colorkeys = ['rv', 'vz']
+        _symmetric_colorkeys = ['rv', 'vx', 'vy', 'vz']
 
         try:
             float(kwargs[colorkey])
@@ -119,7 +119,7 @@ def mpl(ps, data, plot_inds, do_plot=True, **kwargs):
                 colorarray = np.concatenate([_value(ps.get_value(twig=color, component=c)) for c in ps.components]) if len(ps.components) else _value(ps.get_value(twig=color))
 
             if make_array is not None:
-                # then we need to alter the value in the kwargs dictionary to be a 
+                # then we need to alter the value in the kwargs dictionary to be a
                 # normalized array that can be sent to the cmap
 
                 if colorarray.dtype != np.float:
@@ -163,13 +163,13 @@ def mpl(ps, data, plot_inds, do_plot=True, **kwargs):
         elif colorkey is None:
             return None
         else:
-            return 'rainbow'        
+            return 'rainbow'
 
         # TODO: add support for plt.blackbody_cmap()
 
         if colorkey in ['rv']:
             cmap = 'RdBu_r'
-        elif colorkey in ['vz']:
+        elif colorkey in ['vx', 'vy', 'vz']:
             cmap = 'RdBu'
         elif colorkey in ['teff']:
             cmap = 'afmhot'
@@ -185,7 +185,7 @@ def mpl(ps, data, plot_inds, do_plot=True, **kwargs):
 
         logger.info("defaulting to '{}' colormap for '{}'".format(cmap, colorkey))
         return cmap
-        
+
     return_artists = []
     return_data_per_artist = []
 
@@ -207,7 +207,7 @@ def mpl(ps, data, plot_inds, do_plot=True, **kwargs):
         else:
             # not sure if we want this - the user may have set the aspect ratio already
             if ps.method in ['ORB', 'MESH'] and xunit==yunit:
-                # TODO: for aspect ratio (here and above) can we be smarter and 
+                # TODO: for aspect ratio (here and above) can we be smarter and
                 # check for same units?
                 ax.set_aspect('equal')
 
@@ -220,9 +220,9 @@ def mpl(ps, data, plot_inds, do_plot=True, **kwargs):
 
 
 
-    mplkwargs = {k:v for k,v in kwargs.items() if k not in 
+    mplkwargs = {k:v for k,v in kwargs.items() if k not in
         _meta_fields_all+['x', 'y', 'z', 'xerrors', 'yerrors', 'zerrors',
-            'xlabel', 'ylabel', 'zlabel', 
+            'xlabel', 'ylabel', 'zlabel',
             'xlim', 'ylim', 'zlim',
             'xunit', 'yunit', 'zunit', 'time', 'highlight', 'highlight_ms',
             'highlight_marker', 'highlight_color', 'uncover',
@@ -243,7 +243,7 @@ def mpl(ps, data, plot_inds, do_plot=True, **kwargs):
             pckwargs['edgecolors'] = pckwargs['facecolors']
 
 
-        # pckwargs will now have facecolors and edgecolors - either as strings 
+        # pckwargs will now have facecolors and edgecolors - either as strings
         # or as an array of colors ready for matplotlib.  We do that this way
         # here because edge and face may have different cmaps.  For individual
         # xy plots we leave the normalizing and cmaps up to matplotlib.
@@ -306,7 +306,7 @@ def mpl(ps, data, plot_inds, do_plot=True, **kwargs):
             if axes_3d:
                 ax.set_xlim3d(kwargs.get('xlim', xlim))
             else:
-                ax.set_xlim(kwargs.get('xlim', xlim)) 
+                ax.set_xlim(kwargs.get('xlim', xlim))
             if axes_3d:
                 ax.set_ylim3d(kwargs.get('ylim', ylim))
             else:
@@ -365,7 +365,7 @@ def mpl(ps, data, plot_inds, do_plot=True, **kwargs):
                 else:
                     return_data_per_artist.append((xarray[plot_inds], yarray[plot_inds]))
 
-        # NOTE that these are separate ifs (rather than elifs) because they may 
+        # NOTE that these are separate ifs (rather than elifs) because they may
         # both need to be called if asking for a linestyle and marker
         if colorarray is not None and kwargs['linestyle'] not in [None, 'none', 'None']:
             # then we have to do this the hard way
@@ -380,7 +380,7 @@ def mpl(ps, data, plot_inds, do_plot=True, **kwargs):
                 segments = np.concatenate([points[:-1], points[1:]], axis=1)
 
             # TODO: add support for linewidths (but what keyword would do this - data would need to be markersizes)
-            
+
             # Now we handle the color map for a bunch of line-segments.  Note
             # that we normalize the colormap based on the entire colorarray,
             # so that the map will stay in place if uncover=True
@@ -388,7 +388,7 @@ def mpl(ps, data, plot_inds, do_plot=True, **kwargs):
                 logger.debug("kwargs passed to LineCollection: {}".format(mplkwargs))
 
                 mplkwargs.setdefault('cmap', _default_cmap(ps, mplkwargs.get('color', None)))
-                lc = LineCollection(segments, 
+                lc = LineCollection(segments,
                     norm=plt.Normalize(min(colorarray), max(colorarray)),
                     **{k:v for k,v in mplkwargs.items() if k not in ['markersize', 'marker']})
                 lc.set_array(colorarray[plot_inds])
@@ -407,13 +407,13 @@ def mpl(ps, data, plot_inds, do_plot=True, **kwargs):
                     artist = ax.scatter(xarray[plot_inds], yarray[plot_inds], zarray[plot_inds], c=colorarray[plot_inds],
                         cmap=plt.get_cmap(kwargs.get('cmap', _default_cmap(ps, mplkwargs.get('color', None)))),
                         norm=plt.Normalize(min(colorarray), max(colorarray)),
-                        marker=kwargs['marker'], 
+                        marker=kwargs['marker'],
                         linewidths=0) # linewidths=0 removes the black edge
                 else:
                     artist = ax.scatter(xarray[plot_inds], yarray[plot_inds], c=colorarray[plot_inds],
                         cmap=plt.get_cmap(kwargs.get('cmap', _default_cmap(ps, mplkwargs.get('color', None)))),
                         norm=plt.Normalize(min(colorarray), max(colorarray)),
-                        marker=kwargs['marker'], 
+                        marker=kwargs['marker'],
                         linewidths=0) # linewidths=0 removes the black edge
 
                 return_artists.append(artist)
@@ -486,7 +486,7 @@ def mpl(ps, data, plot_inds, do_plot=True, **kwargs):
             if len(ax.get_zlabel()) and ax.get_zlabel()!=zlabel:
                 logger.warning("zlabel is already set but is not '{}' - ignoring".format(zlabel))
             else:
-                ax.set_zlabel(zlabel) 
+                ax.set_zlabel(zlabel)
 
         # now let's handle setting the axes limits, but only if the user sent values
         # TODO: should we handle ax._phoebe_xlim and friends here?
@@ -494,12 +494,12 @@ def mpl(ps, data, plot_inds, do_plot=True, **kwargs):
             if axes_3d:
                 ax.set_xlim3d(kwargs['xlim'])
             else:
-                ax.set_xlim(kwargs['xlim']) 
+                ax.set_xlim(kwargs['xlim'])
         if kwargs.get('ylim', False):
             if axes_3d:
                 ax.set_ylim3d(kwargs['ylim'])
             else:
-                ax.set_ylim(kwargs['ylim']) 
+                ax.set_ylim(kwargs['ylim'])
         if axes_3d and kwargs.get('zlim', False):
             ax.set_zlim3d(kwargs['zlim'])
 
@@ -511,7 +511,7 @@ def mpl(ps, data, plot_inds, do_plot=True, **kwargs):
             # inspired by The Martian
             at = AnchoredText("CALCULATIONS\n     CORRECT",
                               prop=dict(size=15), frameon=True,
-                              loc=10, 
+                              loc=10,
                               )
             at.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
             # this is not animatable
@@ -553,7 +553,7 @@ def show_mpld3(**kwargs):
     # TODO: needs to just be input for python3
     # raw_input("press enter to continue...")
     sleep(2)
-    
+
     os.remove('_mpld3.html')
 
 
@@ -721,11 +721,11 @@ def show_lightning(**kwargs):
 
     # TODO: needs to just be input for python3
     raw_input("press enter to continue...")
-    
+
     os.remove('_lgn.html')
 
 def save_lightning(fname, **kwargs):
-    
+
     os.rename('_lgn.html', fname)
     return fname
 
@@ -766,5 +766,5 @@ def _template_plotting_function(ps, data, plot_inds, **kwargs):
 # - mpl2plotly
 # - chaco (https://github.com/enthought/chaco)
 # - mayavi (***)
-# - opengl 
+# - opengl
 # - visvis