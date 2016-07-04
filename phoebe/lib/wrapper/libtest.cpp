25f5114d856cc4240e4dc2b601e3bf059d035fe3
Martin Horvat <horvat77@gmail.com>
2016-07-04 22:39:06 +0000

    Fixing some additional memory leaks.

---
+++ phoebe/lib/wrapper/libtest.cpp
@@ -0,0 +1,61 @@
+/*
+  Testing wrapping and memory leaks. 
+
+  Author: Martin Horvat, Jul 2016
+*/ 
+
+#include <iostream>
+
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#include <Python.h>
+#include <numpy/arrayobject.h>
+
+static PyObject *fun1(PyObject *self, PyObject *args) {
+  
+  double x;
+  
+  if (!PyArg_ParseTuple(args, "d", &x)) return NULL;
+  
+  double *a = new double[10];
+  
+  for (int i = 0; i < 10; ++i) a[i] = x + 10;
+  
+  npy_intp dims[1] = {10};
+
+  PyObject *pya = PyArray_SimpleNewFromData(1, dims, NPY_DOUBLE, a);
+  
+  PyArray_ENABLEFLAGS((PyArrayObject *)pya, NPY_ARRAY_OWNDATA);
+  
+  return pya; 
+}
+
+/*
+  Define functions in module
+   
+  Some modification in declarations due to use of keywords
+  Ref:
+  * https://docs.python.org/2.0/ext/parseTupleAndKeywords.html
+*/ 
+static PyMethodDef Methods[] = {
+    
+    { "fun1", 
+      fun1,   
+      METH_VARARGS, 
+      "Testing function 1."},
+
+    {NULL,  NULL, 0, NULL} // terminator record
+};
+
+/* module initialization */
+PyMODINIT_FUNC initlibtest(void)
+{
+  PyObject *backend = Py_InitModule("libtest", Methods);
+  
+  // Added to handle Numpy arrays
+  // Ref: 
+  // * http://docs.scipy.org/doc/numpy-1.10.1/user/c-info.how-to-extend.html
+  import_array();
+  
+  if (!backend) return;
+}
+
