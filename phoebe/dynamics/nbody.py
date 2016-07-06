a1dfabbd98b15892f768be9204d6ca421f1709a2
Kyle Conroy <kyleconroy@gmail.com>
2016-07-06 09:34:28 +0000

    Implemented new nbody dynamics based on the rebound library.  There are currently three choices for dynamics_method parameter that use nbody codes: - 'nbody' - defaults to rebound - 'rebound' - requires the rebound python package to be install (available through pip, soft dependency) - 'bs' - uses the old burlishstoer code adapted from Josh Carter (does not support vgamma or ltte-corrected velocities, currently built via phoebe's setup.py)
    For now I'm leaving burlishstoer for the sake of testing, but these will likely all eventually become 'nbody' once we decide which is preferable.

--- phoebe/dynamics/nbody.py
+++ phoebe/dynamics/nbody.py
@@ -2,11 +2,25 @@
 """
 
 import numpy as np
+from scipy.optimize import newton
+
 
 from phoebe import u, c
 
 # from phoebe.dynamics import burlishstoer as bs
-import phoebe_burlishstoer as bs
+try:
+    import phoebe_burlishstoer as bs
+except ImportError:
+    _can_bs = False
+else:
+    _can_bs = True
+
+try:
+    import rebound
+except ImportError:
+    _can_rebound = False
+else:
+    _can_rebound = True
 
 import logging
 logger = logging.getLogger("DYNAMICS.NBODY")
@@ -17,13 +31,13 @@ def _ensure_tuple(item):
     """
     Simply ensure that the passed item is a tuple.  If it is not, then
     convert it if possible, or raise a NotImplementedError
-    
+
     Args:
         item: the item that needs to become a tuple
-        
+
     Returns:
         the item casted as a tuple
-        
+
     Raises:
         NotImplementedError: if converting the given item to a tuple
             is not implemented.
@@ -36,13 +50,13 @@ def _ensure_tuple(item):
         return tuple(item.tolist())
     else:
         raise NotImplementedError
-        
+
 def dynamics_from_bundle(b, times, stepsize=0.01, orbiterror=1e-16, ltte=False):
     """
     Parse parameters in the bundle and call :func:`dynamics`.
-    
+
     See :func:`dynamics` for more detailed information.
-    
+
     Args:
         b: (Bundle) the bundle with a set hierarchy
         times: (list or array) times at which to run the dynamics
@@ -51,23 +65,169 @@ def dynamics_from_bundle(b, times, stepsize=0.01, orbiterror=1e-16, ltte=False):
         orbiterror: (float, optional) orbiterror for the integration
             [default: 1e-16]
         ltte: (bool, default False) whether to account for light travel time effects.
-        
+
     Returns:
         t, xs, ys, zs, vxs, vys, vzs.  t is a numpy array of all times,
         the remaining are a list of numpy arrays (a numpy array per
         star - in order given by b.hierarchy.get_stars()) for the cartesian
         positions and velocities of each star at those same times.
-            
+
     """
-       
+
     hier = b.hierarchy
-    
+
+    starrefs = hier.get_stars()
+    orbitrefs = [hier.get_parent_of(star) for star in starrefs]
+
+    def mean_anom(t0, t0_perpass, period):
+        # TODO: somehow make this into a constraint where t0 and mean anom
+        # are both in the compute options if dynamic_method==nbody
+        # (one is constrained from the other and the orbit.... nvm, this gets ugly)
+        return 2 * np.pi * (t0 - t0_perpass) / period
+
+    masses = [b.get_value('mass', u.solMass, component=component, context='component') * c.G.to('AU3 / (Msun d2)').value for component in starrefs]  # GM
+    smas = [b.get_value('sma', u.AU, component=component, context='component') for component in orbitrefs]
+    eccs = [b.get_value('ecc', component=component, context='component') for component in orbitrefs]
+    incls = [b.get_value('incl', u.rad, component=component, context='component') for component in orbitrefs]
+    per0s = [b.get_value('per0', u.rad, component=component, context='component') for component in orbitrefs]
+    long_ans = [b.get_value('long_an', u.rad, component=component, context='component') for component in orbitrefs]
+    t0_perpasses = [b.get_value('t0_perpass', u.d, component=component, context='component') for component in orbitrefs]
+    periods = [b.get_value('period', u.d, component=component, context='component') for component in orbitrefs]
+
+    vgamma = b.get_value('vgamma', context='system', unit=u.AU/u.d)
+    t0 = b.get_value('t0', context='system', unit=u.d)
+
+    mean_anoms = [mean_anom(t0, t0_perpass, period) for t0_perpass, period in zip(t0_perpasses, periods)]
+
+    return dynamics(times, masses, smas, eccs, incls, per0s, long_ans, \
+                    mean_anoms, t0, vgamma, stepsize, orbiterror, ltte)
+
+
+def dynamics(times, masses, smas, eccs, incls, per0s, long_ans, mean_anoms,
+        t0=0.0, vgamma=0.0, stepsize=0.01, orbiterror=1e-16, ltte=False):
+
+    if not _can_rebound:
+        raise ImportError("rebound is not installed")
+
+    def particle_ltte(sim, particle_N, t_obs):
+        c_AU_d = c.c.to(u.AU/u.d).value
+
+        def residual(t):
+            # print "*** ltte trying t:", t
+            if sim.t != t:
+                sim.integrate(t)
+            ltte_dt = sim.particles[particle_N].z / c_AU_d
+            t_barycenter = t - ltte_dt
+            # print "***** ", t_barycenter-t_obs
+
+            return t_barycenter - t_obs
+
+        t_barycenter = newton(residual, t_obs)
+
+        if sim.t != t_barycenter:
+            sim.integrate(t_barycenter)
+
+        return sim.particles[particle_N]
+
+
+    times = np.asarray(times)
+    # print "***", times.shape
+
+    # TODO: remove or implement stepsize
+    # TODO: remove or implement orbiterror
+    # TODO: implement LTTE
+    # TODO: implement vgamma
+
+    sim = rebound.Simulation()
+    for mass, sma, ecc, incl, per0, long_an, mean_anom in zip(masses, smas, eccs, incls, per0s, long_ans, mean_anoms):
+        # print "*** adding", mass, sma, ecc, incl, per0, long_an, mean_anom
+
+        N = sim.N
+        sim.add(primary=None if N==0 else sim.particles[-1],
+                m=mass,
+                a=sma,
+                e=ecc,
+                inc=incl,
+                Omega=long_an,
+                omega=per0,
+                M=mean_anom)
+
+    sim.move_to_com()
+
+    # Now handle vgamma by editing the initial vz on each particle
+    for particle in sim.particles:
+        particle.vz += vgamma
+
+    # fig = rebound.OrbitPlot(sim, slices=True)
+    # fig.show()
+
+    xs = [np.zeros(times.shape) for m in masses]
+    ys = [np.zeros(times.shape) for m in masses]
+    zs = [np.zeros(times.shape) for m in masses]
+    vxs = [np.zeros(times.shape) for m in masses]
+    vys = [np.zeros(times.shape) for m in masses]
+    vzs = [np.zeros(times.shape) for m in masses]
+
+    au_to_solrad = (1*u.AU).to(u.solRad).value
+
+    for i,time in enumerate(times):
+
+        sim.integrate(time)
+
+        for j in range(len(masses)):
+
+            if ltte:
+                # then we need to integrate to different times per object
+                particle = particle_ltte(sim, j, time)
+            else:
+                # print "***", time, j, sim.N
+                particle = sim.particles[j]
+
+            # NOTE: x and y are flipped because of different coordinate system
+            # conventions.  If we change our coordinate system to have x point
+            # to the left, this will need to be updated to match as well.
+            xs[j][i] = -1 * particle.x * au_to_solrad # solRad
+            ys[j][i] = -1 * particle.y * au_to_solrad # solRad
+            zs[j][i] = particle.z * au_to_solrad  # solRad
+            vxs[j][i] = -1 * particle.vx * au_to_solrad # solRad/d
+            vys[j][i] = -1 * particle.vy * au_to_solrad # solRad/d
+            vzs[j][i] = particle.vz * au_to_solrad # solRad/d
+
+
+    return times, xs, ys, zs, vxs, vys, vzs
+
+
+def dynamics_from_bundle_bs(b, times, stepsize=0.01, orbiterror=1e-16, ltte=False):
+    """
+    Parse parameters in the bundle and call :func:`dynamics`.
+
+    See :func:`dynamics` for more detailed information.
+
+    Args:
+        b: (Bundle) the bundle with a set hierarchy
+        times: (list or array) times at which to run the dynamics
+        stepsize: (float, optional) stepsize for the integration
+            [default: 0.01]
+        orbiterror: (float, optional) orbiterror for the integration
+            [default: 1e-16]
+        ltte: (bool, default False) whether to account for light travel time effects.
+
+    Returns:
+        t, xs, ys, zs, vxs, vys, vzs.  t is a numpy array of all times,
+        the remaining are a list of numpy arrays (a numpy array per
+        star - in order given by b.hierarchy.get_stars()) for the cartesian
+        positions and velocities of each star at those same times.
+
+    """
+
+    hier = b.hierarchy
+
     starrefs = hier.get_stars()
     orbitrefs = hier.get_orbits()
-    
+
     def mean_anom(t0, t0_perpass, period):
-        # TODO: somehow make this into a constraint where t0 and mean anom 
-        # are both in the compute options if dynamic_method==nbody 
+        # TODO: somehow make this into a constraint where t0 and mean anom
+        # are both in the compute options if dynamic_method==nbody
         # (one is constrained from the other and the orbit.... nvm, this gets ugly)
         return 2 * np.pi * (t0 - t0_perpass) / period
 
@@ -85,34 +245,36 @@ def dynamics_from_bundle(b, times, stepsize=0.01, orbiterror=1e-16, ltte=False):
 
     mean_anoms = [mean_anom(t0, t0_perpass, period) for t0_perpass, period in zip(t0_perpasses, periods)]
 
-    return dynamics(times, masses, smas, eccs, incls, per0s, long_ans, \
+    return dynamics_bs(times, masses, smas, eccs, incls, per0s, long_ans, \
                     mean_anoms, t0, vgamma, stepsize, orbiterror, ltte)
-                        
 
-def dynamics(times, masses, smas, eccs, incls, per0s, long_ans, mean_anoms, 
+
+
+
+def dynamics_bs(times, masses, smas, eccs, incls, per0s, long_ans, mean_anoms,
         t0=0.0, vgamma=0.0, stepsize=0.01, orbiterror=1e-16, ltte=False):
     """
     Burlisch-Stoer integration of orbits to give positions and velocities
     of any given number of stars in hierarchical orbits.  This code
     currently uses the NBody code in Josh Carter's photodynam code
     available here:
-    
+
     [[TODO: include link]]
-    
+
     If using the Nbody mode in PHOEBE, please cite him as well:
-    
+
     [[TODO: include citation]]
-    
+
     See :func:`dynamics_from_bundle` for a wrapper around this function
     which automatically handles passing everything in the correct order
     and in the correct units.
-    
+
     For each iterable input, stars and orbits should be listed in order
-    from primary -> secondary for each nested hierarchy level.  Each 
+    from primary -> secondary for each nested hierarchy level.  Each
     iterable for orbits should have length one less than those for each
     star (ie if 3 masses are provided, then 2 smas, eccs, etc need to
     be provided)
-    
+
     Args:
         times: (iterable) times at which to compute positions and
             velocities for each star
@@ -125,20 +287,23 @@ def dynamics(times, masses, smas, eccs, incls, per0s, long_ans, mean_anoms,
             orbit [rad]
         mean_anoms: (iterable) mean anomalies for each orbit
         t0: (float) time at which to start the integrations
-        stepsize: (float, optional) stepsize of the integrations 
+        stepsize: (float, optional) stepsize of the integrations
             [default: 0.01]
         orbiterror: (float, optional) orbiterror of the integrations
             [default: 1e-16]
         ltte: (bool, default False) whether to account for light travel time effects.
-    
+
     Returns:
         t, xs, ys, zs, vxs, vys, vzs.  t is a numpy array of all times,
         the remaining are a list of numpy arrays (a numpy array per
         star - in order given by b.hierarchy.get_stars()) for the cartesian
         positions and velocities of each star at those same times.
-    
+
     """
-    
+
+    if not _can_bs:
+        raise ImportError("phoebe_burlishstoer is not installed")
+
     times = _ensure_tuple(times)
     masses = _ensure_tuple(masses)
     smas = _ensure_tuple(smas)
@@ -147,20 +312,20 @@ def dynamics(times, masses, smas, eccs, incls, per0s, long_ans, mean_anoms,
     per0s = _ensure_tuple(per0s)
     long_ans = _ensure_tuple(long_ans)
     mean_anoms = _ensure_tuple(mean_anoms)
-    
+
     # TODO: include vgamma!!!!
     d = bs.do_dynamics(times, masses, smas, eccs, incls, per0s, long_ans, mean_anoms, t0, stepsize, orbiterror, ltte)
     # d is in the format: {'t': (...), 'x': ( (1,2,3), (1,2,3), ...), 'y': ..., 'z': ...}
-    
+
     nobjects = len(masses)
     ntimes = len(times)
 
     # TODO: need to return euler angles... if that even makes sense?? Or maybe we
     # need to make a new place in orbit??
-    
+
     # define scale_factor to convert from au to solRad and return values in solRad
     scale_factor = c.au/c.R_sun
-    
+
     return np.array(d['t']), \
         [(-1*np.array([d['x'][ti][oi] for ti in range(ntimes)])*scale_factor*u.solRad) for oi in range(nobjects)], \
         [(-1*np.array([d['y'][ti][oi] for ti in range(ntimes)])*scale_factor*u.solRad) for oi in range(nobjects)], \
