- fixed recently introduced bug in numerical RVs (fix is a bit expensive - may need to be revisited) - fixed other minor bugs introduced in the last week - updated tutorials as necessary

phoebe/backend/backends.py
@@ -503,22 +503,12 @@ def phoebe(b, compute, time=[], as_generator=False, **kwargs):
     # TODO: only do this if we need the mesh for actual computations
     # TODO: move as much of this pblum logic into mesh.py as possible
 
-    def dynamics_at_i(xs, ys, zs, vxs, vys, vzs, ethetas=None, elongans=None, eincls=None, i=0):
-        xi, yi, zi = [x[i].to(u.solRad) for x in xs], [y[i].to(u.solRad) for y in ys], [z[i].to(u.solRad) for z in zs]
-        vxi, vyi, vzi = [vx[i].to(u.solRad/u.d) for vx in vxs], [vy[i].to(u.solRad/u.d) for vy in vys], [vz[i].to(u.solRad/u.d) for vz in vzs]
-        if ethetas is not None:
-            ethetai, elongani, eincli = [etheta[i].to(u.rad) for etheta in ethetas], [elongan[i].to(u.rad) for elongan in elongans], [eincl[i].to(u.rad) for eincl in eincls]
-        else:
-            ethetai, elongani, eincli = None, None, None
-
-        return xi, yi, zi, vxi, vyi, vzi, ethetai, elongani, eincli
-
     methods = b.get_dataset().methods
     if 'LC' in methods or 'RV' in methods:  # TODO this needs to be WAY more general
         # we only need to handle pblum_scale if we have a dataset method which requires
         # intensities
         if len(meshablerefs) > 1 or hier.get_kind_of(meshablerefs[0])=='envelope':
-            x0, y0, z0, vx0, vy0, vz0, etheta0, elongan0, eincl0 = dynamics_at_i(xs0, ys0, zs0, vxs0, vys0, vzs0, ethetas0, elongans0, eincls0, i=0)
+            x0, y0, z0, vx0, vy0, vz0, etheta0, elongan0, eincl0 = dynamics.dynamics_at_i(xs0, ys0, zs0, vxs0, vys0, vzs0, ethetas0, elongans0, eincls0, i=0)
         else:
             x0, y0, z0 = [0.], [0.], [0.]
             vx0, vy0, vz0 = [0.], [0.], [0.]
@@ -584,21 +574,22 @@ def phoebe(b, compute, time=[], as_generator=False, **kwargs):
         # Check to see what we might need to do that requires a mesh
         # TOOD: make sure to use the requested distortion_method
 
-        if True in [info['needs_mesh'] for info in infolist]:
 
+        # we need to extract positions, velocities, and euler angles of ALL bodies at THIS TIME (i)
+        if len(meshablerefs) > 1 or hier.get_kind_of(meshablerefs[0])=='envelope':
+            xi, yi, zi, vxi, vyi, vzi, ethetai, elongani, eincli = dynamics.dynamics_at_i(xs, ys, zs, vxs, vys, vzs, ethetas, elongans, eincls, i=i)
+        else:
+            xi, yi, zi = [0.], [0.], [0.]
+            vxi, vyi, vzi = [0.], [0.], [0.]
+            # TODO: star needs long_an (yaw?)
+
+            ethetai, elongani, eincli = [0.], [0.], [b.get_value('incl', component=meshablerefs[0], unit=u.rad)]
+
+        if True in [info['needs_mesh'] for info in infolist]:
 
             if dynamics_method == 'nbody':
                 raise NotImplementedError('nbody not supported for meshes yet')
 
-            # we need to extract positions, velocities, and euler angles of ALL bodies at THIS TIME (i)
-            if len(meshablerefs) > 1 or hier.get_kind_of(meshablerefs[0])=='envelope':
-                xi, yi, zi, vxi, vyi, vzi, ethetai, elongani, eincli = dynamics_at_i(xs, ys, zs, vxs, vys, vzs, ethetas, elongans, eincls, i=i)
-            else:
-                xi, yi, zi = [0.], [0.], [0.]
-                vxi, vyi, vzi = [0.], [0.], [0.]
-                # TODO: star needs long_an (yaw?)
-
-                ethetai, elongani, eincli = [0.], [0.], [b.get_value('incl', component=hier.get_parent_of(meshablerefs[0]), unit=u.rad)]
 
             # TODO: eventually we can pass instantaneous masses and sma as kwargs if they're time dependent
             # masses = [b.get_value('mass', component=star, context='component', time=time, unit=u.solMass) for star in starrefs]
@@ -666,8 +657,7 @@ def phoebe(b, compute, time=[], as_generator=False, **kwargs):
                     this_syn['rv'].append(rv*u.solRad/u.d)
                 else:
                     # then rv_method == 'dynamical'
-                    # TODO: send in with units
-                    this_syn['rv'].append(-1*vzs[cind][i])
+                    this_syn['rv'].append(-1*vzi[cind]*u.solRad/u.d)
 
             elif method=='LC':
 
@@ -696,12 +686,12 @@ def phoebe(b, compute, time=[], as_generator=False, **kwargs):
                 # ts[i], xs[cind][i], ys[cind][i], zs[cind][i], vxs[cind][i], vys[cind][i], vzs[cind][i]
 
                 ### this_syn['time'].append(ts[i])  # time array was set when initializing the syns
-                this_syn['x'].append(xs[cind][i])
-                this_syn['y'].append(ys[cind][i])
-                this_syn['z'].append(zs[cind][i])
-                this_syn['vx'].append(vxs[cind][i])
-                this_syn['vy'].append(vys[cind][i])
-                this_syn['vz'].append(vzs[cind][i])
+                this_syn['x'].append(xi[cind])
+                this_syn['y'].append(yi[cind])
+                this_syn['z'].append(zi[cind])
+                this_syn['vx'].append(vxi[cind])
+                this_syn['vy'].append(vyi[cind])
+                this_syn['vz'].append(vzi[cind])
 
             elif method=='MESH':
                 # print "*** info['component']", info['component'], " info['dataset']", info['dataset']
@@ -744,12 +734,11 @@ def phoebe(b, compute, time=[], as_generator=False, **kwargs):
 
                 this_syn['visibility'] = body.mesh.visibilities
 
-                # vcs = np.sum(body.mesh.vertices_per_triangle*body.mesh.weights[:,:,np.newaxis], axis=1)
-                # for i,vc in enumerate(vcs):
-                #     if np.all(vc==np.array([0,0,0])):
-                #         vcs[i] = np.full(3, np.nan)
-                # this_syn['visible_centroids'] = vcs
-                this_syn['visible_centroids'] = []
+                vcs = np.sum(body.mesh.vertices_per_triangle*body.mesh.weights[:,:,np.newaxis], axis=1)
+                for i,vc in enumerate(vcs):
+                    if np.all(vc==np.array([0,0,0])):
+                        vcs[i] = np.full(3, np.nan)
+                this_syn['visible_centroids'] = vcs
 
                 indeps = {'RV': ['rv', 'intens_norm_abs', 'intens_norm_rel', 'intens_proj_abs', 'intens_proj_rel', 'ampl_boost', 'ld'], 'LC': ['intens_norm_abs', 'intens_norm_rel', 'intens_proj_abs', 'intens_proj_rel', 'ampl_boost', 'ld'], 'IFM': ['intens_norm_abs', 'intens_norm_rel', 'intens_proj_abs', 'intens_proj_rel']}
                 for infomesh in infolist:

phoebe/backend/etvs.py
@@ -12,7 +12,7 @@ def barycentric():
     raise NotImplementedError
 
 
-def crossing(b, component, time, dynamics_method='keplerian', ltte=True, tol=1e-4):
+def crossing(b, component, time, dynamics_method='keplerian', ltte=True, tol=1e-4, maxiter=1000):
     """
     tol in days
     """
@@ -26,19 +26,22 @@ def crossing(b, component, time, dynamics_method='keplerian', ltte=True, tol=1e-
 
         times = np.array([time])
 
-        if dynamics_method == 'nbody':
+        if dynamics_method in ['nbody', 'rebound']:
             # TODO: make sure that this takes systemic velocity and corrects positions and velocities (including ltte effects if enabled)
-            ts, xs, ys, zs, vxs, vys, vzs = dynamics.nbody.dynamics_from_bundle(b, times, ltte=ltte)
+            ts, xs, ys, zs, vxs, vys, vzs = dynamics.nbody.dynamics_from_bundle(b, times, compute=None, ltte=ltte)
+
+        elif dynamics_method=='bs':
+            ts, xs, ys, zs, vxs, vys, vzs = dynamics.nbody.dynamics_from_bundle_bs(b, times, compute, ltte=ltte)
 
         elif dynamics_method=='keplerian':
             # TODO: make sure that this takes systemic velocity and corrects positions and velocities (including ltte effects if enabled)
-            ts, xs, ys, zs, vxs, vys, vzs = dynamics.keplerian.dynamics_from_bundle(b, times, ltte=ltte, return_euler=False)
+            ts, xs, ys, zs, vxs, vys, vzs = dynamics.keplerian.dynamics_from_bundle(b, times, compute=None, ltte=ltte, return_euler=False)
 
         else:
             raise NotImplementedError
 
 
-        return (xs[cind2][0].value-xs[cind1][0].value)**2 + (ys[cind2][0].value-ys[cind1][0].value)**2
+        return (xs[cind2][0]-xs[cind1][0])**2 + (ys[cind2][0]-ys[cind1][0])**2
 
 
     # TODO: optimize this by allowing to pass cind1 and cind2 directly (and fallback to this if they aren't)
@@ -46,5 +49,5 @@ def crossing(b, component, time, dynamics_method='keplerian', ltte=True, tol=1e-
     cind1 = starrefs.index(component)
     cind2 = starrefs.index(b.hierarchy.get_sibling_of(component))
 
-    # TODO: provide options for tol and maxiter?
-    return newton(projected_separation_sq, x0=time, args=(b, dynamics_method, cind1, cind2, ltte), tol=tol, maxiter=1000)
+    # TODO: provide options for tol and maxiter (in the frontend computeoptionsp)?
+    return newton(projected_separation_sq, x0=time, args=(b, dynamics_method, cind1, cind2, ltte), tol=tol, maxiter=maxiter)

phoebe/dynamics/nbody.py
@@ -58,11 +58,14 @@ def _ensure_tuple(item):
     else:
         raise NotImplementedError
 
-def dynamics_from_bundle(b, times, compute, **kwargs):
+def dynamics_from_bundle(b, times, compute=None, **kwargs):
     """
     Parse parameters in the bundle and call :func:`dynamics`.
 
     See :func:`dynamics` for more detailed information.
+
+    NOTE: you must either provide compute (the label) OR all relevant options
+    as kwargs (ltte, stepsize, gr, integrator)
 
     Args:
         b: (Bundle) the bundle with a set hierarchy
@@ -223,14 +226,18 @@ def dynamics(times, masses, smas, eccs, incls, per0s, long_ans, mean_anoms,
             vzs[j][i] = particle.vz * au_to_solrad # solRad/d
 
 
+    # d, solRad, solRad/d, rad
     return times, xs, ys, zs, vxs, vys, vzs
 
 
-def dynamics_from_bundle_bs(b, times, compute, **kwargs):
+def dynamics_from_bundle_bs(b, times, compute=None, **kwargs):
     """
     Parse parameters in the bundle and call :func:`dynamics`.
 
     See :func:`dynamics` for more detailed information.
+
+    NOTE: you must either provide compute (the label) OR all relevant options
+    as kwargs (ltte)
 
     Args:
         b: (Bundle) the bundle with a set hierarchy
@@ -358,14 +365,14 @@ def dynamics_bs(times, masses, smas, eccs, incls, per0s, long_ans, mean_anoms,
     # TODO: need to return euler angles... if that even makes sense?? Or maybe we
     # need to make a new place in orbit??
 
-    # define scale_factor to convert from au to solRad and return values in solRad
-    scale_factor = c.au/c.R_sun
+    au_to_solrad = (1*u.AU).to(u.solRad).value
 
+    # d, solRad, solRad/d, rad
     return np.array(d['t']), \
-        [(-1*np.array([d['x'][ti][oi] for ti in range(ntimes)])*scale_factor*u.solRad) for oi in range(nobjects)], \
-        [(-1*np.array([d['y'][ti][oi] for ti in range(ntimes)])*scale_factor*u.solRad) for oi in range(nobjects)], \
-        [(np.array([d['z'][ti][oi] for ti in range(ntimes)])*scale_factor*u.solRad) for oi in range(nobjects)], \
-        [(-1*np.array([d['vx'][ti][oi] for ti in range(ntimes)])*scale_factor*u.solRad/u.d) for oi in range(nobjects)], \
-        [(-1*np.array([d['vy'][ti][oi] for ti in range(ntimes)])*scale_factor*u.solRad/u.d) for oi in range(nobjects)], \
-        [(np.array([d['vz'][ti][oi] for ti in range(ntimes)])*scale_factor*u.solRad/u.d) for oi in range(nobjects)]
+        [(-1*np.array([d['x'][ti][oi] for ti in range(ntimes)])*au_to_solrad) for oi in range(nobjects)], \
+        [(-1*np.array([d['y'][ti][oi] for ti in range(ntimes)])*au_to_solrad) for oi in range(nobjects)], \
+        [(np.array([d['z'][ti][oi] for ti in range(ntimes)])*au_to_solrad) for oi in range(nobjects)], \
+        [(-1*np.array([d['vx'][ti][oi] for ti in range(ntimes)])*au_to_solrad) for oi in range(nobjects)], \
+        [(-1*np.array([d['vy'][ti][oi] for ti in range(ntimes)])*au_to_solrad) for oi in range(nobjects)], \
+        [(np.array([d['vz'][ti][oi] for ti in range(ntimes)])*au_to_solrad) for oi in range(nobjects)]
 

phoebe/frontend/io.py
@@ -151,7 +151,7 @@ def load_lc_data(filename, indep, dep, indweight=None, mzero=None):
     """
     load dictionary with lc data
     """
-   
+
     lcdata = np.loadtxt(filename)
     ncol = len(lcdata[0])
     if dep == 'Magnitude':
@@ -166,7 +166,7 @@ def load_lc_data(filename, indep, dep, indweight=None, mzero=None):
         if ncol >= 3:
             d['phoebe_lc_sigmalc'] = lcdata[:,2]
         else:
-            logger.warning('A sigma column was is mentioned in the .phoebe file but is not present in the data file') 
+            logger.warning('A sigma column was is mentioned in the .phoebe file but is not present in the data file')
 
 
 
@@ -210,7 +210,7 @@ def det_dataset(eb, passband, dataid, comp, time):
             rv_dataset = eb.add_dataset('rv', dataset=dataid, time = [])
 
         except ValueError:
-        
+
             logger.warning("The name picked for the lightcurve is forbidden. Applying default name instead")
             rv_dataset = eb.add_dataset('rv', time =[])
 
@@ -225,13 +225,13 @@ def det_dataset(eb, passband, dataid, comp, time):
             comp_o = 'primary'
         for x in rvs:
             test_dataset = eb.get_dataset(x)
-            if len(test_dataset.get_value(qualifier='rv', component=comp)) == 0:                #so at least it has an empty spot now check against filter and length           
+            if len(test_dataset.get_value(qualifier='rv', component=comp)) == 0:                #so at least it has an empty spot now check against filter and length
                 time_o = test_dataset.get_value('time', component=comp_o)
                 passband_o = test_dataset.get_value('passband')
                 if np.all(time_o == time) and (passband == passband_o):
                     rv_dataset = test_dataset
                     found = True
-        
+
         if not found:
             try:
                 eb._check_label(dataid)
@@ -239,7 +239,7 @@ def det_dataset(eb, passband, dataid, comp, time):
                 rv_dataset = eb.add_dataset('rv', dataset=dataid, time = [])
 
             except ValueError:
-        
+
                 logger.warning("The name picked for the lightcurve is forbidden. Applying default name instead")
                 rv_dataset = eb.add_dataset('rv', time =[])
 
@@ -278,7 +278,7 @@ def load_legacy(filename, add_compute_legacy=True, add_compute_phoebe=True):
     rvno = np.int(params[:,1][list(params[:,0]).index('phoebe_rvno')])
     lcno = np.int(params[:,1][list(params[:,0]).index('phoebe_lcno')])
 # delete parameters that have already been accounted for and find lc and rv parameters
-    
+
     params = np.delete(params, [list(params[:,0]).index('phoebe_lcno'), list(params[:,0]).index('phoebe_rvno')], axis=0)
 # create mzero and grab it if it exists
     mzero = None
@@ -306,7 +306,7 @@ def load_legacy(filename, add_compute_legacy=True, add_compute_phoebe=True):
 
 # First LC
     for x in range(1,lcno+1):
-    
+
         #list of parameters related to current dataset
 
         lcint = [list(lcpars[:,0]).index(s) for s in lcpars[:,0] if "["+str(x)+"]" in s]
@@ -347,7 +347,7 @@ def load_legacy(filename, add_compute_legacy=True, add_compute_phoebe=True):
                 lc_dataset = eb.add_dataset('lc', dataset=dataid)
 
             except ValueError:
-        
+
                 logger.warning("The name picked for the lightcurve is forbidden. Applying default name instead")
                 lc_dataset = eb.add_dataset('lc')
 
@@ -358,7 +358,7 @@ def load_legacy(filename, add_compute_legacy=True, add_compute_phoebe=True):
     #enable dataset
         enabled = lc_dict['phoebe_lc_active']
         del lc_dict['phoebe_lc_active']
-    
+
         d ={'qualifier':'enabled', 'dataset':dataid, 'value':enabled}
         eb.set_value_all(check_relevant= False, **d)
 
@@ -372,7 +372,7 @@ def load_legacy(filename, add_compute_legacy=True, add_compute_phoebe=True):
 
         for k in lc_dict:
             pnew, d = ret_dict(k, lc_dict[k], dataid=dataid)
-        # as long as the parameter exists add it 
+        # as long as the parameter exists add it
             if len(d) > 0:
 
                 if d['qualifier'] == 'passband' and d['value'] not in choices:
@@ -407,17 +407,17 @@ def load_legacy(filename, add_compute_legacy=True, add_compute_phoebe=True):
 
         if rv_dict['phoebe_rv_filename'] != 'Undefined':
             data_dict = load_rv_data(filename = rv_dict['phoebe_rv_filename'],  indep=rv_dict['phoebe_rv_indep'], dep=rv_dict['phoebe_rv_dep'], indweight=indweight)
-    
+
             rv_dict.update(data_dict)
             time = rv_dict['phoebe_rv_time']
-        # 
+        #
 
         rv_dataset = det_dataset(eb, passband, dataid, comp, time)
         dataid = rv_dataset.dataset
         #enable dataset
         enabled = rv_dict['phoebe_rv_active']
         del rv_dict['phoebe_rv_active']
-    
+
         d ={'qualifier':'enabled', 'dataset':dataid, 'value':enabled}
         eb.set_value_all(check_relevant= False, **d)
 
@@ -431,14 +431,14 @@ def load_legacy(filename, add_compute_legacy=True, add_compute_phoebe=True):
         del rv_dict['phoebe_rv_filter']
 # now go through parameters and input the results into phoebe2
         for k  in rv_dict:
-            
+
             pnew, d = ret_dict(k, rv_dict[k], rvdep = comp, dataid=dataid)
             if len(d) > 0:
                 eb.set_value_all(check_relevant= False, **d)
 
 
     for x in range(len(params)):
-        
+
         pname = params[:,0][x]
         pname = pname.split('.')[0]
         val = params[:,1][x].strip('"')
@@ -475,11 +475,11 @@ def load_legacy(filename, add_compute_legacy=True, add_compute_phoebe=True):
             if val == 1:
                 d['value'] = 'kurucz'
             logger.warning('If you would like to use phoebe 1 atmospheres, you must add this manually')
-            d['compute'] = 'backend' 
+            d['compute'] = 'backend'
             eb.set_value(check_relevant=False, **d)
             d['compute'] = 'detailed'
             atm_choices = eb.get_compute('detailed').get_parameter('atm', component='primary').choices
-            if d['value'] not in choices:
+            if d['value'] not in atm_choices:
                 #TODO FIND appropriate default
                 d['value'] = 'atmcof'
 
@@ -559,7 +559,7 @@ def par_value(param, index=None):
 
         if d['qualifier'] == 'refl_num':
 
-            val = [param.get_value()-1]   
+            val = [param.get_value()-1]
 
         else:
             val = [param.get_value()]
@@ -809,7 +809,7 @@ def pass_to_legacy(eb, filename='2to1.phoebe'):
             try:
                 pnew = _2to1par[param.qualifier]
                 if param.qualifier in [ 'alb', 'l3', 'ld_func', 'flux', 'sigma', 'time'] or param.component == '_default':
-                    
+
                     param = None
             except:
 
@@ -922,7 +922,7 @@ def pass_to_legacy(eb, filename='2to1.phoebe'):
                     try:
                         pnew = _2to1par[param.qualifier]
                         if param.qualifier in ['ld_func', 'time', 'rv', 'sigma']or param.component == '_default':
-                            
+
                             param = None
 
                     except:
@@ -990,7 +990,7 @@ def pass_to_legacy(eb, filename='2to1.phoebe'):
     comquals = eb.get_compute(method='legacy', check_relevant=False)-eb.get_compute(method='legacy', component='_default')
 
     for param in comquals.to_list():
-            
+
         if param.qualifier == 'heating':
             if param.get_value() == False:
                in1 =  parnames.index('phoebe_alb1.VAL')