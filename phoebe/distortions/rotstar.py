a70097ade3b52c41b0af8b0ffecdf8a9394bbbe2
Kyle Conroy <kyleconroy@gmail.com>
2016-07-12 09:08:48 +0000

    fixed (and by fixed I mean it doesn't crash anymore) single star support

--- phoebe/distortions/rotstar.py
+++ phoebe/distortions/rotstar.py
@@ -9,7 +9,7 @@ def rotfreq_to_omega(rotfreq, scale=c.R_sun.si.value, solar_units=False):
     """
     TODO: add documentation
 
-    NOTE: everything MUST be in SI units
+    NOTE: everything MUST be in consistent units according to solar_units bool
     """
     if solar_units:
         omega = rotfreq / (2*np.pi) / np.sqrt(c.GM_sun.to(u.solRad**3/u.d**2).value/scale**3)
@@ -17,7 +17,7 @@ def rotfreq_to_omega(rotfreq, scale=c.R_sun.si.value, solar_units=False):
         # then SI units
         omega = rotfreq / (2*np.pi) / np.sqrt(c.GM_sun.value/scale**3)
 
-    print "*** rotstar.rotfreq_to_omega", rotfreq, scale, solar_units, omega
+    # print "*** rotstar.rotfreq_to_omega", rotfreq, scale, solar_units, omega
 
     return omega
 
@@ -26,17 +26,21 @@ def rpole2potential(rpole, rotfreq, solar_units=False):
     """
     Transforms polar radius to surface potential
     """
+    if not solar_units:
+        rpole = rpole/c.R_sun.si.value
     rpole_ = np.array([0., 0., rpole])
     omega = rotfreq_to_omega(rotfreq, solar_units=solar_units)
-    print "*** rotstar.rpole2potential", rpole, rotfreq, solar_units, omega
-    return libphoebe.rotstar_Omega(omega, rpole_)
+    pot =  libphoebe.rotstar_Omega(omega, rpole_)
+    # print "*** rotstar.rpole2potential", rpole, rotfreq, solar_units, omega, pot
+    return pot
+
 
 def potential2rpole(pot, rotfreq, solar_units=False):
     """
     Transforms surface potential to polar radius
     """
     omega = rotfreq_to_omega(rotfreq, scale=1.0, solar_units=solar_units)
-    print "*** rotstar.potential2rpole", pot, rotfreq, solar_units, omega
+    # print "*** rotstar.potential2rpole", pot, rotfreq, solar_units, omega
     rpole = libphoebe.rotstar_pole(omega, pot)
     if solar_units:
         return rpole
