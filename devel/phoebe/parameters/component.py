626b630a15a102837d30bea4d1b6c11f82b76f12
kconroy <kconroy@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2016-07-15 10:20:23 +0000

    added mean_anom parameter and constraint

--- devel/phoebe/parameters/component.py
+++ devel/phoebe/parameters/component.py
@@ -37,6 +37,7 @@ def orbit(component, **kwargs):
     params += [FloatParameter(qualifier='deccdt', value=kwargs.get('deccdt', 0.0), default_unit=u.dimensionless_unscaled/u.d, description='Eccentricity change')]
     params += [FloatParameter(qualifier='t0_perpass', value=kwargs.get('t0_perpass', 0.0), default_unit=u.d, description='Zeropoint date at periastron passage')]  # TODO: d vs JD
     params += [FloatParameter(qualifier='t0_supconj', value=kwargs.get('t0_supconj', 0.0), default_unit=u.d, description='Zeropoint date at superior conjunction')]  # TODO: d vs JD
+    params += [FloatParameter(qualifier='mean_anom', value=kwargs.get('mean_anom', 0.0), default_unit=u.deg, description='Mean anomaly')]
     #params += [FloatParameter(qualifier='ph_perpass', value=kwargs.get('ph_perpass', 0.0), default_unit=u.cycle, description='Phase at periastron passage')]
     #params += [FloatParameter(qualifier='ph_supconj', value=kwargs.get('ph_supconj', 0.0), default_unit=u.cycle, description='Phase at superior conjunction')]
     #params += [FloatParameter(qualifier='ph_infconj', value=kwargs.get('ph_infconj', 0.0), default_unit=u.cycle, description='Phase at inferior conjunction')]
@@ -51,6 +52,7 @@ def orbit(component, **kwargs):
     constraints = []
     constraints += [(constraint.asini, component)]
     constraints += [(constraint.t0, component)]
+    constraints += [(constraint.mean_anom, component)]
     #constraints += [(constraint.ph_perpass, component)]
     #constraints += [(constraint.ph_supconj, component)]
     #constraints += [(constraint.ph_infconj, component)]
