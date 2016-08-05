3193491fc5e65e88545b35dffed1424b0ecb935a
horvatm <horvatm@a2c7b9f9-86ad-47c3-932c-941601cf3e97>
2016-08-05 04:17:28 +0000

    Correcting wrapper for limb darkening coefficients.

---
+++ devel/phoebe/lib/ld.py
@@ -0,0 +1,14 @@
+#  Testing the C++ wrapper for calculation of limb darkening 
+#  Author: Martin Horvat, August 2016
+
+
+from libphoebe import *
+
+mu = 0.1
+
+for i in range(1, 400):
+  d = ld_funcD(mu, ("linear", 0.1))
+  g = ld_gradparD(mu, ("linear", 0.1))
+  
+
+print d, " ", g
