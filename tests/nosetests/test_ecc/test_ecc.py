772adaa106c2d04af1747340add4f18dd1531256
Kyle Conroy <kyleconroy@gmail.com>
2016-09-01 17:56:32 -0400

    added eccentricity nosetest (LC only)
    current passes at rtol=1e-3

---
+++ tests/nosetests/test_ecc/test_ecc.py
@@ -0,0 +1,53 @@
+"""
+"""
+
+import phoebe
+from phoebe import u
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def test_binary(plot=False):
+    b = phoebe.Bundle.default_binary()
+
+    b.set_value('ecc', 0.4)
+
+    b.add_dataset('LC', time=np.linspace(0,3,101))
+    b.add_compute('phoebe', compute='phoebe2')
+    b.add_compute('legacy', compute='phoebe1')
+
+    # set matching atmospheres
+    b.set_value_all('atm@phoebe2', 'extern_planckint')
+    b.set_value_all('atm@phoebe1', 'blackbody')
+
+    # set matching limb-darkening, both bolometric and passband
+    b.set_value_all('ld_func_bol', 'logarithmic')
+    b.set_value_all('ld_coeffs_bol', [0.0, 0.0])
+
+    b.set_value_all('ld_func', 'logarithmic')
+    b.set_value_all('ld_coeffs', [0.0, 0.0])
+
+    print "running phoebe2 model..."
+    b.run_compute(compute='phoebe2', model='phoebe2model')
+    print "running phoebe1 model..."
+    b.run_compute(compute='phoebe1', model='phoebe1model')
+
+    phoebe2_val = b.get_value('flux@phoebe2model')
+    phoebe1_val = b.get_value('flux@phoebe1model')
+
+    if plot:
+        b.plot(dataset='lc01')
+        plt.legend()
+        plt.show()
+
+        print "max (rel):", abs((phoebe2_val-phoebe1_val)/phoebe1_val).max()
+
+    assert(np.allclose(phoebe2_val, phoebe1_val, rtol=1e-3, atol=0.))
+
+    return b
+
+if __name__ == '__main__':
+    logger = phoebe.logger(clevel='INFO')
+
+
+    b = test_binary(plot=True)
