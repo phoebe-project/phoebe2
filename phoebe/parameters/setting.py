fada613a2bf94098a030d27b04ed6b77a7bb378f
Kyle Conroy <kyleconroy@gmail.com>
2016-09-11 14:00:57 -0400

    moved all alternate plotting backends to developer mode

--- phoebe/parameters/setting.py
+++ phoebe/parameters/setting.py
@@ -1,23 +1,24 @@
 
 from phoebe.parameters import *
+from phoebe import _devel_enabled
 
 def settings(**kwargs):
     """
-    Generally, this will automatically be added to a newly initialized 
+    Generally, this will automatically be added to a newly initialized
     :class:`phoebe.frontend.bundle.Bundle`
-    
+
     :parameter **kwargs: defaults for the values of any of the parameters
     :return: a :class:`phoebe.parameters.parameters.ParameterSet` of all newly
         created :class:`phoebe.parameters.parameters.Parameter`s
     """
-    
+
     params = []
-    
+
     params += [BoolParameter(qualifier='log_history', value=kwargs.get('log_history', False), description='Whether to log history (undo/redo)')]
     params += [DictParameter(qualifier='dict_filter', value=kwargs.get('dict_filter', {}), description='Filters to use when using dictionary access')]
     params += [BoolParameter(qualifier='dict_set_all', value=kwargs.get('dict_set_all', False), description='Whether to set all values for dictionary access that returns more than 1 result')]
-    
-    params += [ChoiceParameter(qualifier='plotting_backend', value=kwargs.get('plotting_backend', 'mpl'), choices=['mpl', 'mpld3', 'mpl2bokeh', 'bokeh'], description='Default backend to use for plotting')]
+
+    params += [ChoiceParameter(qualifier='plotting_backend', value=kwargs.get('plotting_backend', 'mpl'), choices=['mpl', 'mpld3', 'mpl2bokeh', 'bokeh'] if _devel_enabled else ['mpl'], description='Default backend to use for plotting')]
 
     # problem with try_sympy parameter: it can't be used during initialization... so this may need to be a phoebe-level setting
     # params += [BoolParameter(qualifier='try_sympy', value=kwargs.get('try_sympy', True), description='Whether to use sympy if installed for constraints')]
