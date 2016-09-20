61f4cc94d90920b1e10acfd98c10e0b4999acd1d
gecheline <a.kochoska@gmail.com>
2016-09-20 14:34:22 -0400

    Added nosetest for dperdt.

---
+++ tests/nosetests/test_dperdt/test_dperdt.py
@@ -0,0 +1,57 @@
+"""
+"""
+
+import phoebe
+from phoebe import u
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def test_binary(plot=False):
+    b = phoebe.Bundle.default_binary()
+
+
+    b.add_dataset('lc', times=np.linspace(0,3,101))
+    b.add_compute('phoebe', compute='phoebe2')
+    b.add_compute('legacy', compute='phoebe1')
+
+    # set matching atmospheres
+    b.set_value_all('atm@phoebe2', 'extern_planckint')
+    b.set_value_all('atm@phoebe1', 'blackbody')
+
+    # set matching limb-darkening, both bolometric and passband
+    b.set_value_all('ld_func_bol', 'logarithmic')
+    b.set_value_all('ld_coeffs_bol', [0.0, 0.0])
+
+    b.set_value_all('ld_func', 'logarithmic')
+    b.set_value_all('ld_coeffs', [0.0, 0.0])
+    b.set_value_all('ecc', 0.2)
+
+    for dperdt in [-0.5, -0.25, 0.25, 0.5]:
+        b.set_value('dperdt', dperdt)
+
+
+        print "running phoebe2 model..."
+        b.run_compute(compute='phoebe2', model='phoebe2model')
+        print "running phoebe1 model..."
+        b.run_compute(compute='phoebe1', model='phoebe1model')
+
+        phoebe2_val = b.get_value('fluxes@phoebe2model')
+        phoebe1_val = b.get_value('fluxes@phoebe1model')
+
+        if plot:
+            b.plot(dataset='lc01')
+            plt.legend()
+            plt.show()
+
+            print "max (rel):", abs((phoebe2_val-phoebe1_val)/phoebe1_val).max()
+
+        assert(np.allclose(phoebe2_val, phoebe1_val, rtol=5e-3, atol=0.))
+
+    return b
+
+if __name__ == '__main__':
+    logger = phoebe.logger(clevel='INFO')
+
+
+    b = test_binary(plot=True)
