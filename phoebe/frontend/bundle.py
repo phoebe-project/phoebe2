6206cd7f5dda0745dbdbd6e773c6073bcc054911
Kyle Conroy <kyleconroy@gmail.com>
2016-09-21 14:10:29 -0400

    flip_constraint no longer in devel_mode
    required by legacy importer/exporter

--- phoebe/frontend/bundle.py
+++ phoebe/frontend/bundle.py
@@ -2059,8 +2059,8 @@ class Bundle(ParameterSet):
 
         """
 
-        if not _devel_enabled:
-            raise NotImplementedError("'flip_constraint' not officially supported for this release.  Enable developer mode to test.")
+        # if not _devel_enabled:
+        #     raise NotImplementedError("'flip_constraint' not officially supported for this release.  Enable developer mode to test.")
 
         self._kwargs_checks(kwargs)
 
