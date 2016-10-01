d067a0813a1cbf0590194276a8ecc38c9a556bdd
Kyle Conroy <kyleconroy@gmail.com>
2016-10-01 19:50:37 -0400

    have travis run, log, and plot benchmarks for each commit (#131)

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -5,7 +5,12 @@ pylint: sudo pip install pylint
 
 """
 
+import matplotlib
+matplotlib.use('Agg')
+
 from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
 import os
 import sys
 import commands
@@ -51,6 +56,10 @@ if 'doctests' in do:
 
 if 'benchmark' in do or 'benchmarks' in do:
     print "RUNNING BENCHMARKS..."
+
+    branch_name =  os.environ.get('TRAVIS_BRANCH', commands.getoutput('git rev-parse --symbolic-full-name --abbrev-ref HEAD'))
+    commit_hash = os.environ.get('TRAVIS_COMMIT', commands.getoutput('git log -n 1 --pretty=format:"%H"'))
+
     os.chdir(os.path.join(cwd, 'benchmark'))
     times = {}
     for fname in glob('./*py'):
@@ -62,6 +71,46 @@ if 'benchmark' in do or 'benchmarks' in do:
         times[f_py] = float(out.split()[-9].split('user')[0])
 
 
+
+        f_result_fname = f_py.split('.py')[0]+'.log'
+        f_result = open(f_result_fname, 'a')
+        f_result.write("{} {} {}\n".format(branch_name, commit_hash, times[f_py]))
+        f_result.close()
+
+        print "plotting benchmark history for {}...".format(f_py)
+        f_result = open(f_result_fname, 'r')
+        lines = f_result.readlines()
+        f_result.close()
+
+        branches_skip = []
+        branches = {}
+        for line in lines:
+            branch, commit, time = line.strip().split()
+            if branch not in branches.keys() and branch not in branches_skip:
+                branches[branch] = []
+
+        for line in lines:
+            branch, commit, time = line.strip().split()
+            for branch_i in branches.keys():
+                if branch_i == branch:
+                    branches[branch].append(time)
+                else:
+                    branches[branch].append(np.nan)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        x = range(len(lines))
+        for branch, benchmark_ts in branches.items():
+            ax.plot(x, benchmark_ts, 'o-', label=branch)
+        plt.legend()
+        ax.set_title(f_py.split('.py')[0])
+        ax.set_xlabel('commit')
+        ax.set_ylabel('benchmark time (s)')
+        fig.savefig(f_result_fname+'.png')
+
+
+
     print "PROFILE TIMES (see individual .profile files for details)"
     for f_py, time in times.items():
         print "{}: {}".format(f_py, time)
+
