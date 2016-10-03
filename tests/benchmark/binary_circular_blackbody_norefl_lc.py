e1a9cc07585ef456cdf737a14527545b3b3c9c6a
Kyle Conroy <kyleconroy@gmail.com>
2016-10-03 12:35:44 -0400

    add a benchmark test for reflection off

---
+++ tests/benchmark/binary_circular_blackbody_norefl_lc.py
@@ -0,0 +1,9 @@
+
+import numpy as np
+import phoebe
+
+b = phoebe.Bundle.default_binary()
+
+b.add_dataset('lc', time=np.linspace(0,3,101), dataset='lc01')
+
+b.run_compute(atm='blackbody', ld_func='logarithmic', ld_coeffs=[0,0], reflection_method='none')
