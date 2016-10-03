e4461d832ddf421878369333b64f6ceebfaad706
Kyle Conroy <kyleconroy@gmail.com>
2016-10-03 10:51:22 -0400

    update benchmarks to account for changes in defaults
    mainly that blackbody must now be specified.

--- tests/benchmark/binary_circular_atm_lc.py
+++ tests/benchmark/binary_circular_atm_lc.py
@@ -6,7 +6,4 @@ b = phoebe.Bundle.default_binary()
 
 b.add_dataset('lc', time=np.linspace(0,3,101), dataset='lc01')
 
-b.add_compute()
-b.set_value_all('atm', 'ck2004')
-
-b.run_compute()
+b.run_compute(atm='ck2004')
