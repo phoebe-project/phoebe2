ebe510c392ca09845dc694c6661eef7361df85a9
Kyle Conroy <kyleconroy@gmail.com>
2017-02-07 15:39:36 -0500

    change fti test case to have no albedo
    so we can safely turn off reflection for the comparison

--- tests/nosetests/test_fti/test_fti.py
+++ tests/nosetests/test_fti/test_fti.py
@@ -11,7 +11,7 @@ phoebe.devel_on()
 phoebe.interactive_off()
 
 def test_binary(plot=False):
-    every = 2
+    every = 10
 
     dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -20,31 +20,23 @@ def test_binary(plot=False):
     b.set_value_all('atm', 'blackbody')
     b.set_value('irrad_method', 'none')
 
-    # set matching limb-darkening, both bolometric and passband
-    b.set_value_all('ld_func_bol', 'logarithmic')
-    b.set_value_all('ld_coeffs_bol', [0.0, 0.0])
-
-    b.set_value_all('ld_func', 'logarithmic')
-    b.set_value_all('ld_coeffs', [0.0, 0.0])
-
-    # b.set_value_all('passband', 'Johnson:V')
-
     period = b.get_value('period', kind='orbit', context='component')
     times = b.to_time(np.linspace(-0.6, 0.6, 100))[::every]
     b.set_value('times', times)
-
-    # return b
 
     b.run_compute(fti_method='none')
     fluxes_legacy = np.loadtxt(os.path.join(dir, 'kic12004834.nofti.data'), unpack=True, usecols=(1,))[::every]
     fluxes = b.get_value('fluxes', context='model')
 
+
     if plot:
         print "fti off"
         print abs(fluxes_legacy-fluxes).max()
-        plt.plot(times, fluxes_legacy)
-        b.plot(show=True)
-    # assert(np.allclose(fluxes, fluxes_legacy, rtol=0, atol=1e-3))
+        plt.plot(times, fluxes_legacy, 'k-')
+        b.plot()
+        plt.legend()
+        plt.show()
+    assert(np.allclose(fluxes, fluxes_legacy, rtol=0, atol=1e-2))
 
 
     b.run_compute(fti_method='oversample', fti_oversample=5)
@@ -54,9 +46,12 @@ def test_binary(plot=False):
     if plot:
         print "fti on"
         print abs(fluxes_legacy-fluxes).max()
-        plt.plot(times, fluxes_legacy)
-        b.plot(show=True)
-    # assert(np.allclose(fluxes, fluxes_legacy, rtol=0, atol=1e-3))
+        plt.plot(times, fluxes_legacy, 'k-')
+        b.plot()
+        b.plot()
+        plt.legend()
+        plt.show()
+    assert(np.allclose(fluxes, fluxes_legacy, rtol=0, atol=1e-2))
 
 
 
