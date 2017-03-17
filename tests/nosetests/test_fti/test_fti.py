9b0a5ed30a4947fa14d1238029ab9c88b742c457
Andrej Prsa <aprsa09@gmail.com>
2017-03-16 23:19:16 -0400

    Modified the script to load test data in time space.

--- tests/nosetests/test_fti/test_fti.py
+++ tests/nosetests/test_fti/test_fti.py
@@ -13,19 +13,19 @@ phoebe.interactive_off()
 def test_binary(plot=False):
     dir = os.path.dirname(os.path.realpath(__file__))
 
-    b = phoebe.Bundle.from_legacy(os.path.join(dir, 'kic12004834.phoebe'))
+    b = phoebe.Bundle.from_legacy(os.path.join(dir, 'kic12004834.times.phoebe'))
     # this phoebe legacy file uses extern_planckint and with albedos to 0
     # and exptime already defined
     b.set_value_all('atm', 'blackbody')
     b.set_value('irrad_method', 'none')
 
-    times = b.to_time(np.linspace(-0.5, 0.5, 25)) # hardcoded to length of files
+    fluxes_legacy = np.loadtxt(os.path.join(dir, 'kic12004834.nofti.times.data'), unpack=True, usecols=(1,))
+
+    times = np.linspace(55002.04045, 55002.30277, len(fluxes_legacy))
     b.set_value('times', times)
 
     b.run_compute(fti_method='none')
-    fluxes_legacy = np.loadtxt(os.path.join(dir, 'kic12004834.nofti.data'), unpack=True, usecols=(1,))
     fluxes = b.get_value('fluxes', context='model')
-
 
     if plot:
         print "fti off"
@@ -36,9 +36,9 @@ def test_binary(plot=False):
         plt.show()
     assert(np.allclose(fluxes, fluxes_legacy, rtol=0, atol=1e-2))
 
-
-    b.run_compute(fti_method='oversample', fti_oversample=5)
-    fluxes_legacy = np.loadtxt(os.path.join(dir, 'kic12004834.fti.data'), unpack=True, usecols=(1,))
+    b['exptime'] = 1766.0
+    b.run_compute(fti_method='oversample', fti_oversample=10)
+    fluxes_legacy = np.loadtxt(os.path.join(dir, 'kic12004834.fti.times.data'), unpack=True, usecols=(1,))
     fluxes = b.get_value('fluxes', context='model')
 
     if plot:
