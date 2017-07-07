phoebe-project/phoebe2

commit 54351697341b266733a80e27837b5ad0b1a1341c

call roche_misaligned_* but hardcoded to be aligned
so this should always fallback on roche_*.  Next need to figure out how to get the misalignment vector (polar_dir) as a function of true anomaly, pitch, and yaw

phoebe/backend/universe.py
@@ -821,16 +821,6 @@ class Body(object):
         #-- Get current position/euler information
         # TODO: remove this if/else if all dynamics method now support eulerian elements
         if self.dynamics_method in ['keplerian', 'nbody', 'rebound', 'bs']:
-            # if we can't get the polar direction, assume it's in the negative Z-direction
-            try:
-                # TODO: implement get_polar_direction (see below for alpha version)
-                # this will currently ALWAYS fail and follow the except - meaning
-                # misaligned orbits are not yet supported
-                polar_dir = -self.get_polar_direction(norm=True)
-            except:
-                #logger.warning("assuming polar direction - misaligned orbits not yet supported")
-                polar_dir = default_polar_dir
-
             # TODO: get rid of this ugly _value stuff
             pos = (_value(xs[self.ind_self]), _value(ys[self.ind_self]), _value(zs[self.ind_self]))
             vel = (_value(vxs[self.ind_self]), _value(vys[self.ind_self]), _value(vzs[self.ind_self]))
@@ -839,12 +829,12 @@ class Body(object):
             raise NotImplementedError("update_position does not support dynamics_method={}".format(self.dynamics_method))
 
 
+        q, F, d, s, Phi = self._mesh_args
+
         #-- Volume Conservation
         if self.needs_volume_conservation and self.distortion_method in ['roche']:
 
             # TODO: this seems Star/Roche-specific - should it be moved to that class or can it be generalized?
-
-            q, F, d, Phi = self._mesh_args
 
             # override d to be the current value
             if ds is not None:
@@ -867,8 +857,9 @@ class Body(object):
             # print "*** libphoebe.roche_Omega_at_vol", target_volume, q, F, d, Phi, self.Phi
 
             # TODO: need to send a better guess for Omega0
-            Phi = libphoebe.roche_Omega_at_vol(target_volume,
-                                               q, F, d,
+            # TODO: replace s with s(true_anom=0, yaw, pitch)
+            Phi = libphoebe.roche_misaligned_Omega_at_vol(target_volume,
+                                               q, F, d, s,
                                                Omega0=Phi if Phi>self.Phi else self.Phi)
             # if Phi < self.Phi:
                 # then for some reason we passed the value defined at periastron...
@@ -957,7 +948,7 @@ class Body(object):
         # TODO NOW [OPTIMIZE]: get rid of the deepcopy here - but without it the mesh velocities build-up and do terrible things
         self._mesh = mesh.Mesh.from_scaledproto(scaledprotomesh.copy(),
                                                 pos, vel, euler,
-                                                polar_dir*self.freq_rot)
+                                                s*self.freq_rot)
 
         # print "time: {}\npos: {}\nvel: {}\neuler: {}".format(time, pos, vel, euler)
         # print "*** mesh.vz", self._mesh.velocities.for_computations[:,2].min(), self._mesh.velocities.for_computations[:,2].max(), self._mesh.velocities.for_computations[:,2].mean()
@@ -1534,6 +1525,10 @@ class Star(Body):
             # return self.volume_at_periastron
             return self.volume_at_periastron*self._scale**3
 
+    def get_polar_direction(self, norm=True):
+        # TODO: implement getting from pitch, yaw, and true anomaly
+        return np.array([0,0,1])
+
     def _build_mesh(self, d, mesh_method, **kwargs):
         """
         this function takes mesh_method and kwargs that came from the generic Body.intialize_mesh and returns
@@ -1550,7 +1545,10 @@ class Star(Body):
         Phi = kwargs.get('Phi', self.Phi)  # NOTE: self.Phi automatically corrects for the secondary star
         q = self.q  # NOTE: this is automatically flipped to be 1./q for secondary components
 
-        mesh_args = (q, F, d, Phi)
+        # if we can't get the polar direction, assume it's in the negative Z-direction
+        s = self.get_polar_direction(norm=True)
+
+        mesh_args = (q, F, d, s, Phi)
 
         if mesh_method == 'marching':
             ntriangles = kwargs.get('ntriangles', self.ntriangles)
@@ -1558,30 +1556,30 @@ class Star(Body):
             if self.distortion_method == 'roche':
                 # TODO: check whether roche or misaligned roche from values of incl, etc!!!!
 
-                av = libphoebe.roche_area_volume(*mesh_args,
-                                                 choice=0,
-                                                 larea=True,
-                                                 lvolume=True)
+                av = libphoebe.roche_misaligned_area_volume(*mesh_args,
+                                                            choice=0,
+                                                            larea=True,
+                                                            lvolume=True)
                 delta = np.sqrt(4./np.sqrt(3) * av['larea'] / ntriangles)
 
-                # print "*** libphoebe.roche_marching_mesh args: {}, rpole: {}, delta: {}".format(mesh_args, rpole, delta)
+                print "*** libphoebe.roche_misaligned_marching_mesh args: {}, delta: {}".format(mesh_args, delta)
 
-                new_mesh = libphoebe.roche_marching_mesh(*mesh_args,
-                                                         delta=delta,
-                                                         choice=0,
-                                                         full=True,
-                                                         max_triangles=ntriangles*2,
-                                                         vertices=True,
-                                                         triangles=True,
-                                                         centers=True,
-                                                         vnormals=True,
-                                                         tnormals=True,
-                                                         cnormals=False,
-                                                         vnormgrads=True,
-                                                         cnormgrads=False,
-                                                         areas=True,
-                                                         volume=False,
-                                                         init_phi=self.mesh_init_phi)
+                new_mesh = libphoebe.roche_misaligned_marching_mesh(*mesh_args,
+                                                                    delta=delta,
+                                                                    choice=0,
+                                                                    full=True,
+                                                                    max_triangles=ntriangles*2,
+                                                                    vertices=True,
+                                                                    triangles=True,
+                                                                    centers=True,
+                                                                    vnormals=True,
+                                                                    tnormals=True,
+                                                                    cnormals=False,
+                                                                    vnormgrads=True,
+                                                                    cnormgrads=False,
+                                                                    areas=True,
+                                                                    volume=False,
+                                                                    init_phi=self.mesh_init_phi)
 
 
                 # Now we'll get the area and volume of the Roche potential
@@ -1742,7 +1740,7 @@ class Star(Body):
                     raise NotImplementedError()
                     # TODO: need to use rpole directly?
 
-                rpole = libphoebe.roche_pole(*mesh_args)
+                rpole = libphoebe.roche_misaligned_pole(*mesh_args)
                 omega = 1./rpole
                 mesh_args = (omega,)
 
@@ -1841,8 +1839,8 @@ class Star(Body):
         """
         TODO: add documentation
         """
-        pole_func = getattr(libphoebe, '{}_pole'.format(self.distortion_method))
-        gradOmega_func = getattr(libphoebe, '{}_gradOmega_only'.format(self.distortion_method))
+        pole_func = getattr(libphoebe, '{}_pole'.format('roche_misaligned' if self.distortion_method=='roche' else self.distortion_method))
+        gradOmega_func = getattr(libphoebe, '{}_gradOmega_only'.format('roche_misaligned' if self.distortion_method=='roche' else self.distortion_method))
 
         r_pole = pole_func(*self._mesh_args)
         r_pole_ = np.array([0., 0., r_pole])

phoebe/frontend/bundle.py
@@ -1074,12 +1074,13 @@ class Bundle(ParameterSet):
                     d = 1 - parent_ps.get_value('ecc')
 
                     # TODO: this needs to be generalized once other potentials are supported
-                    critical_pots = libphoebe.roche_critical_potential(q, F, d, L1=True, L2=True)
+                    # TODO: replace s with s(true_anom=0, yaw, pitch)
+                    critical_pot = libphoebe.roche_misaligned_Omega_min(q, F, d, np.array([0,0,1]))
                     # print('q=%f, F=%f, d=%f, pot=%f, cp=%s' % (q, F, d, pot, critical_pots))
 
-                    if pot < critical_pots['L1'] or pot < critical_pots['L2']:
+                    if pot < critical_pot:
                         return False,\
-                            '{} is overflowing at periastron (L1={L1:.02f}, L2={L2:.02f})'.format(component, **critical_pots)
+                            '{} is overflowing at periastron (critical_pot={})'.format(component, critical_pot)
 
             elif kind in ['envelope']:
                 # MUST be overflowing at APASTRON (1+ecc)
@@ -1096,6 +1097,7 @@ class Bundle(ParameterSet):
                 # force OCs to be in circular orbits, in which case this test can be done at
                 # periastron as well
                 d = 1 + parent_ps.get_value('ecc')
+                # NOTE: we use the aligned case here so we can access L1 vs L2 and L3
                 critical_pots = libphoebe.roche_critical_potential(q, F, d, L1=True)
 
                 if pot > critical_pots['L1']:
@@ -1104,6 +1106,7 @@ class Bundle(ParameterSet):
 
                 # BUT MUST NOT be overflowing L2 or L3 at periastron
                 d = 1 - parent_ps.get_value('ecc')
+                # NOTE: we use the aligned case here so we can access L1 vs L2 and L3
                 critical_pots = libphoebe.roche_critical_potential(q, F, d, L2=True, L3=True)
 
                 if pot < critical_pots['L2'] or pot < critical_pots['L3']:
@@ -1124,6 +1127,13 @@ class Bundle(ParameterSet):
                 if hier.get_kind_of(starrefs[0]) != 'star' or hier.get_kind_of(starrefs[1]) != 'star':
                     # print "***", hier.get_kind_of(starrefs[0]), hier.get_kind_of(starrefs[1])
                     continue
+                if self.get_value(qualifier='pitch', component=starrefs[0])!=0.0 or \
+                        self.get_value(qualifier='pitch', component=starrefs[1])!=0.0 or \
+                        self.get_value(qualifier='yaw', component=starrefs[0])!=0.0 or \
+                        self.get_value(qualifier='yaw', component=starrefs[1])!=0.0:
+
+                    # we cannot run this test for misaligned cases
+                   continue
 
                 comp0 = hier.get_primary_or_secondary(starrefs[0], return_ind=True)
                 comp1 = hier.get_primary_or_secondary(starrefs[1], return_ind=True)
@@ -1139,6 +1149,7 @@ class Bundle(ParameterSet):
                 pot1 = self.get_value(qualifier='pot', component=starrefs[1], context='component')
                 pot1 = roche.pot_for_component(pot1, q1, comp1)
 
+                # there is no misaligned version of xrange, so here we assume the aligned case
                 xrange0 = libphoebe.roche_xrange(q0, F0, 1.0-ecc, pot0, choice=0)
                 xrange1 = libphoebe.roche_xrange(q1, F1, 1.0-ecc, pot1, choice=0)
 
@@ -1146,17 +1157,6 @@ class Bundle(ParameterSet):
                     return False,\
                         'components in {} are overlapping at periastron (change ecc@{}, syncpar@{}, or syncpar@{})'.format(orbitref, orbitref, starrefs[0], starrefs[1])
 
-        # check to make sure all stars are aligned (remove this once we support
-        # misaligned roche binaries)
-        if len(hier.get_stars()) > 1:
-            for starref in hier.get_meshables():
-                orbitref = hier.get_parent_of(starref)
-                if len(hier.get_children_of(orbitref)) == 2:
-                    incl_star = self.get_value(qualifier='incl', component=starref, context='component', unit='deg')
-                    incl_orbit = self.get_value(qualifier='incl', component=orbitref, context='component', unit='deg')
-                    if abs(incl_star - incl_orbit) > 1e-3:
-                        return False,\
-                            'misaligned orbits are not currently supported.'
 
         # check to make sure passband supports the selected atm
         for pbparam in self.filter(qualifier='passband').to_list():

tests/nosetests/test_misaligned/test_misaligned.py
@@ -1,0 +1,26 @@
+"""
+"""
+
+import phoebe
+from phoebe import u
+import numpy as np
+import matplotlib.pyplot as plt
+
+def test_binary(plot=False):
+    b = phoebe.Bundle.default_binary()
+
+    b['pitch@primary'] = 10
+    b['yaw@primary'] = 10
+
+    b.add_dataset('lc', times=np.linspace(0,1,21))
+
+    b.run_compute()
+
+    # TODO: add comparisons????
+
+    return b
+
+if __name__ == '__main__':
+    logger = phoebe.logger(clevel='INFO')
+
+    b = test_binary(plot=True)

commit 84f7b53ba618834a6544e3e8b372a38ef7986558

attempt to fix bugs introduced in misaligned support
from commit: 54351697341b266733a80e27837b5ad0b1a1341c

I'm sligtly concerned about the spin direction needing to be [0,0-1] for contacts but seemingly [0,0,1] for detached.  We really need to sort this out.

phoebe/backend/universe.py
@@ -828,8 +828,12 @@ class Body(object):
         else:
             raise NotImplementedError("update_position does not support dynamics_method={}".format(self.dynamics_method))
 
-
-        q, F, d, s, Phi = self._mesh_args
+        if isinstance(self, Envelope):
+            q, F, d, Phi = self._mesh_args
+            # envelopes MUST be aligned
+            s = np.array([0,0,-1])
+        else:
+            q, F, d, s, Phi = self._mesh_args
 
         #-- Volume Conservation
         if self.needs_volume_conservation and self.distortion_method in ['roche']:
@@ -1562,7 +1566,7 @@ class Star(Body):
                                                             lvolume=True)
                 delta = np.sqrt(4./np.sqrt(3) * av['larea'] / ntriangles)
 
-                print "*** libphoebe.roche_misaligned_marching_mesh args: {}, delta: {}".format(mesh_args, delta)
+                # print "*** libphoebe.roche_misaligned_marching_mesh args: {}, delta: {}".format(mesh_args, delta)
 
                 new_mesh = libphoebe.roche_misaligned_marching_mesh(*mesh_args,
                                                                     delta=delta,
@@ -1826,7 +1830,7 @@ class Star(Body):
 
             N = int(kwargs.get('gridsize', self.gridsize))
 
-            the_grid = potentials.discretize_wd_style(N, *mesh_args)
+            the_grid = potentials.discretize_wd_style(N, q, F, d, Phi)
             new_mesh = mesh.wd_grid_to_mesh_dict(the_grid, q, F, d)
             scale = sma
 