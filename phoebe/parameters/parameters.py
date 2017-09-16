phoebe-project/phoebe2

commit 732a530f9c5f94a8fe5a84c6fdc353a67f159fe8

forbid parameter to be constrained more than once
for example: both t0_supconj can't be constrained by BOTH t0_ref and t0_perpass or an error will be thrown

phoebe/parameters/parameters.py
@@ -5239,6 +5239,11 @@ class ConstraintParameter(Parameter):
         newly_constrained_var = self._get_var(**kwargs)
         newly_constrained_param = self.get_parameter(**kwargs)
 
+        check_kwargs = {k:v for k,v in kwargs.items()}
+        check_kwargs['context'] = 'constraint'
+        if len(self._bundle.filter(**check_kwargs)):
+            raise ValueError("'{}' is already constrained".format(newly_constrained_param.twig))
+
         currently_constrained_var = self._get_var(qualifier=self.qualifier, component=self.component)
         currently_constrained_param = currently_constrained_var.get_parameter() # or self.constrained_parameter
 

tests/nosetests/test_t0s/test_t0s.py
@@ -58,7 +58,7 @@ def test_binary(verbose=False):
     assert_t0s(p1_ref, p1_supconj, p1_perpass)
 
     # cannot flip both constraints to solve for t0_supconj
-    # assert_raises(ValueError, b.flip_constraint, 't0_perpass', solve_for='t0_supconj')
+    assert_raises(ValueError, b.flip_constraint, 't0_perpass', solve_for='t0_supconj')
 
     b.flip_constraint('t0_supconj', solve_for='t0_ref')
     b.flip_constraint('t0_perpass', solve_for='t0_supconj')

commit f9053aaf97e9a188678bb5107844f4794d8537da

fix on forbidding parameter to be constrained more than once
bug introduced in 732a530f9c5f94a8fe5a84c6fdc353a67f159fe8
In the case where constraint_func was passed for the filter, the filter falsely thought the parameter was already constrained.  The check is now much more robust.

phoebe/parameters/parameters.py
@@ -5251,7 +5251,7 @@ class ConstraintParameter(Parameter):
         newly_constrained_var = self._get_var(**kwargs)
         newly_constrained_param = self.get_parameter(**kwargs)
 
-        check_kwargs = {k:v for k,v in kwargs.items()}
+        check_kwargs = newly_constrained_param.meta
         check_kwargs['context'] = 'constraint'
         if len(self._bundle.filter(**check_kwargs)):
             raise ValueError("'{}' is already constrained".format(newly_constrained_param.twig))