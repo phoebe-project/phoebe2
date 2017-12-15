553a5152c6487ce76076cb807f24a2873d70fe0e
Martin Horvat <horvat77@gmail.com>
2017-12-15 17:46:33 +0100

    Adding numpy include dirs to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 import platform
 import os
+import numpy
 
 #
 # Setup for MS Windows
@@ -148,11 +149,15 @@ ext_modules = [
     Extension('libphoebe',
       sources = ['./phoebe/lib/libphoebe.cpp'],
       language='c++',
-      extra_compile_args = ["-std=c++11"]),
+      extra_compile_args = ["-std=c++11"],
+      include_dirs=[numpy.get_include()]
+      ),
 
     Extension('phoebe.algorithms.ceclipse',
       language='c++',
-      sources = ['phoebe/algorithms/ceclipse.cpp']),
+      sources = ['phoebe/algorithms/ceclipse.cpp'],
+      include_dirs=[numpy.get_include()]
+      ),
 ]
 
 #
