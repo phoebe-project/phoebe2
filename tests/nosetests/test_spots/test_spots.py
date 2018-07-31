2768167d5f3fcf3b50e89265871ed768a1e54b8c
Kyle Conroy <kyleconroy@gmail.com>
2018-07-31 11:04:13 -0400

    update test_spots and legacy parser to use long
    and also smarter time sampling in the comparison light curves

--- tests/nosetests/test_spots/test_spots.py
+++ tests/nosetests/test_spots/test_spots.py
@@ -11,9 +11,9 @@ phoebe.devel_on()
 def test_binary(plot=False):
     b = phoebe.Bundle.default_binary()
 
-    b.add_spot(component='primary', relteff=0.9, radius=20, colat=45, colon=90, feature='spot01')
+    b.add_spot(component='primary', relteff=0.9, radius=20, colat=45, long=90, feature='spot01')
 
-    b.add_dataset('lc', times=np.linspace(0,3,21))
+    b.add_dataset('lc', times=np.linspace(0,1,26))
     b.add_compute('phoebe', compute='phoebe2')
     b.add_compute('legacy', compute='phoebe1')
 
