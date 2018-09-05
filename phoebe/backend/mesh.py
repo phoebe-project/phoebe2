fix assigning rotation component of velocities
* for contacts need to use the velocity and euler angles of the correct component, whereas placing in orbit uses primary values
* rotation velocities need to be multiplied by the scale of the system to be in real velocity units
* synchronicity/freq/period for stars in contacts will now remain exposed, with system checks handling raising warning/errors when not-synchronous
* minor updates to rv tests, including changing the tolerance on CB RVs for now... this small discrepancy needs to be looked at more closely to see if its a problem or an "improvement" over PHOEBE 1. (NOTE: I believe rotation velocities were incorrect in PHOEBE 2.0.x, but it isn't fair to compare our tolerances now vs then since the default contact system is quite different than it used to be, but relative to the RV amplitude we actually achieve better tolerance than we used to).

phoebe/backend/mesh.py
@@ -1232,7 +1232,9 @@ class Mesh(ScaledProtoMesh):
 
     @classmethod
     def from_proto(cls, proto_mesh, scale,
-                      pos, vel, euler, rotation_vel=(0,0,0)):
+                      pos, vel, euler, euler_vel,
+                      rotation_vel=(0,0,0),
+                      envelope_com_xyz=None):
         """
         Turn a ProtoMesh into a Mesh scaled and placed in orbit.
 
@@ -1250,13 +1252,15 @@ class Mesh(ScaledProtoMesh):
 
         mesh._copy_roche_values()
         mesh._scale_mesh(scale=scale)
-        mesh._place_in_orbit(pos, vel, euler, rotation_vel)
+        mesh._place_in_orbit(pos, vel, euler, euler_vel, rotation_vel, envelope_com_xyz)
 
         return mesh
 
     @classmethod
     def from_scaledproto(cls, scaledproto_mesh,
-                         pos, vel, euler, rotation_vel=(0,0,0)):
+                         pos, vel, euler, euler_vel,
+                         rotation_vel=(0,0,0),
+                         envelope_com_xyz=None):
         """
         TODO: add documentation
         """
@@ -1264,11 +1268,11 @@ class Mesh(ScaledProtoMesh):
         mesh = cls(**scaledproto_mesh.items())
 
         # roche coordinates have already been copied
-        mesh._place_in_orbit(pos, vel, euler, rotation_vel)
+        mesh._place_in_orbit(pos, vel, euler, euler_vel, rotation_vel, envelope_com_xyz)
 
         return mesh
 
-    def _place_in_orbit(self, pos, vel, euler, rotation_vel=(0,0,0)):
+    def _place_in_orbit(self, pos, vel, euler, euler_vel, rotation_vel=(0,0,0), component_com_x=None):
         """
         TODO: add documentation
         """
@@ -1284,8 +1288,12 @@ class Mesh(ScaledProtoMesh):
         # NOTE: we do velocities first since they require the positions WRT
         # the star (not WRT the system).  Will need to keep this in mind if we
         # eventually support incremental transformations.
-        pos_array = self.vertices if self._compute_at_vertices else self.centers
-        self.update_columns_dict({k: transform_velocity_array(self[k], pos_array, vel, euler, rotation_vel) for k in vel_ks if self[k] is not None})
+        # pos_array = self.vertices if self._compute_at_vertices else self.centers
+        pos_array = self.roche_vertices if self._compute_at_vertices else self.roche_centers
+        if component_com_x is not None and component_com_x != 0.0:
+            # then we're the secondary component and need to do 1-x and then flip the rotation component vxs
+            pos_array = np.array([component_com_x, 0.0, 0.0]) - pos_array
+        self.update_columns_dict({k: transform_velocity_array(self[k], pos_array, vel, euler_vel, rotation_vel) for k in vel_ks if self[k] is not None})
         # TODO: handle velocity from mesh reprojection during volume conservation
 
         # handle rotation/displacement

phoebe/parameters/component.py
@@ -90,9 +90,9 @@ def star(component, **kwargs):
     params += [FloatParameter(qualifier='teff', value=kwargs.get('teff', 6000.), default_unit=u.K, limits=(0.0,None), description='Mean effective temperature')]
     params += [FloatParameter(qualifier='abun', visible_if='hierarchy.is_contact_binary:False', value=kwargs.get('abun', 0.), default_unit=u.dimensionless_unscaled, description='Metallicity')]   # TODO: correct units??? check if log or not? (logabun = 0)
 
-    params += [FloatParameter(qualifier='syncpar', visible_if='hierarchy.is_contact_binary:False,hierarchy.is_binary:True', value=kwargs.get('syncpar', 1.0), default_unit=u.dimensionless_unscaled, limits=(0.0,None), description='Synchronicity parameter')]
-    params += [FloatParameter(qualifier='period', visible_if='hierarchy.is_contact_binary:False', value=kwargs.get('period', 1.0), default_unit=u.d, limits=(0.0,None), description='Rotation period')]
-    params += [FloatParameter(qualifier='freq', visible_if='hierarchy.is_contact_binary:False', value=kwargs.get('freq', 2*np.pi), default_unit=u.rad/u.d, limits=(0.0,None), description='Rotation frequency')]
+    params += [FloatParameter(qualifier='syncpar', visible_if='hierarchy.is_binary:True', value=kwargs.get('syncpar', 1.0), default_unit=u.dimensionless_unscaled, limits=(0.0,None), description='Synchronicity parameter')]
+    params += [FloatParameter(qualifier='period', value=kwargs.get('period', 1.0), default_unit=u.d, limits=(0.0,None), description='Rotation period')]
+    params += [FloatParameter(qualifier='freq', value=kwargs.get('freq', 2*np.pi), default_unit=u.rad/u.d, limits=(0.0,None), description='Rotation frequency')]
 
     params += [FloatParameter(qualifier='pitch', visible_if='hierarchy.is_contact_binary:False', value=kwargs.get('pitch', 0), default_unit=u.deg, description='Pitch of the stellar rotation axis wrt the orbital rotation axis')]
     params += [FloatParameter(qualifier='yaw', visible_if='hierarchy.is_contact_binary:False', value=kwargs.get('yaw', 0), default_unit=u.deg, description='Yaw of the stellar rotation axis wrt the orbital rotation axis')]