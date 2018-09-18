phoebe-project/phoebe2

commit 49ef97fdb75f85050971dbbfe07af1585132d5a9

raise error if twig argument is not string (or None)
closes #291

phoebe/parameters/parameters.py
@@ -1368,6 +1368,9 @@ class ParameterSet(object):
             # of the Parameters hidden by this switch
             check_default = False
 
+        if twig is not None and not isinstance(twig, str):
+            raise TypeError("first argument (twig) must be of type str or None")
+
         if kwargs.get('component', None) == '_default' or\
                 kwargs.get('dataset', None) == '_default' or\
                 kwargs.get('uniqueid', None) is not None or\

commit 94904593818b6f920b8e2e53a3db7e35beecb6be

fixed bug in logic for twig type-check when using dict access
bug introduced in 49ef97fdb75f85050971dbbfe07af1585132d5a9
related to #291

phoebe/parameters/parameters.py
@@ -1203,6 +1203,10 @@ class ParameterSet(object):
                                             default={})
         else:
             kwargs = {}
+
+        if isinstance(key, int):
+            return self.filter(**kwargs).to_list()[key]
+
         return self.filter_or_get(twig=key, **kwargs)
 
     def __setitem__(self, twig, value):
@@ -1369,7 +1373,7 @@ class ParameterSet(object):
             # of the Parameters hidden by this switch
             check_default = False
 
-        if twig is not None and not isinstance(twig, str):
+        if not (twig is None or isinstance(twig, str)):
             raise TypeError("first argument (twig) must be of type str or None")
 
         if kwargs.get('component', None) == '_default' or\