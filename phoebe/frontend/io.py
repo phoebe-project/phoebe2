719fb8fa14c380da01de691a38e133c6f213141d
Kyle Conroy <kyleconroy@gmail.com>
2018-09-26 12:53:46 -0400

    remove developer mode from finesize in legacy parser

--- phoebe/frontend/io.py
+++ phoebe/frontend/io.py
@@ -948,8 +948,7 @@ def load_legacy(filename, add_compute_legacy=True, add_compute_phoebe=True):
         elif pnew == 'finesize':
                     # set gridsize
             d['value'] = val
-            if conf.devel:
-                eb.set_value_all(check_visible=False, **d)
+            eb.set_value_all(check_visible=False, **d)
             # change parameter and value to ntriangles
             val = N_to_Ntriangles(int(np.float(val)))
             d['qualifier'] = 'ntriangles'
