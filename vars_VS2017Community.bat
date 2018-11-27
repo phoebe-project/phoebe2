c08acf4453ad4e0c5fc5c009cc715093255dcd8a
Martin Horvat <horvat77@gmail.com>
2018-11-27 09:01:20 +0100

    Modifying setup.py for correctly avoiding checks in windows and win batch for setting env variables

---
+++ vars_VS2017Community.bat
@@ -0,0 +1,8 @@
+set "VSCMD_START_DIR=%CD%"
+
+set MSSdk=1
+set DISTUTILS_USE_SDK=1
+set VS90COMNTOOLS="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build"
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
