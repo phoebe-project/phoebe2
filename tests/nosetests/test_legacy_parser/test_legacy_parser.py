2fe9644aa17b56b0a285969a49ec28aec5fdf88b
Kyle Conroy <kyleconroy@gmail.com>
2019-03-21 10:08:58 -0400

    Python3 support (#323)
    * Making porting of libphoebe to python 3 more robust.
    * decode online passband dictionary to utf-8
    * skip mpi nosetests on python3 only
    * python 3.5 still giving some compatibility issues, change to 3.6
    * cleanup test_legacy_parser nosetest

--- tests/nosetests/test_legacy_parser/test_legacy_parser.py
+++ tests/nosetests/test_legacy_parser/test_legacy_parser.py
@@ -4,17 +4,12 @@ try:
 except ImportError:
     import phoebeBackend as phb1
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 
-# phb2.devel_on()
-
-def legacy_test(filename='default.phoebe', verbose=False):
+def _legacy_test(filename='default.phoebe', verbose=False):
 
     # load phoebe 1 file
-
     dir = os.path.dirname(os.path.realpath(__file__))
-
     phb1.init()
     if hasattr(phb1, 'auto_configure'):
         # then phb1 is phoebe_legacy
@@ -28,38 +23,36 @@ def legacy_test(filename='default.phoebe', verbose=False):
     b = phb2.Bundle.from_legacy(os.path.join(dir, filename), add_compute_legacy=True)
     b.rename_component('primary', 'cow')
     b.rename_component('secondary', 'pig')
-    # create time array and get datasets
 
-
-    per = b['period@orbit'].value
-
-#    time_rv = np.linspace(0, per, 4)
-#    time_lc = np.linspace(0, per, 100)
-
-
+    # NOTE: this seems to be assuming that .datasets preserves the order they're
+    # attached when later lcs[x]/rvs[x] is used?
     lcs = b.get_dataset(kind='lc').datasets
     lcs = lcs[::-1]
     rvs = b.get_dataset(kind='rv').datasets
     rvs = rvs[::-1]
-    # phb2 compute
 
     fluxes = []
     vels = []
     vels2 = []
+    lc_id = []
+    rv_id = []
 
     for x in range(len(lcs)):
         # load file
+        id = phb1.getpar('phoebe_lc_id', x)
+        lc_id.append(id)
         datafile = phb1.getpar('phoebe_lc_filename', x)
         data = np.loadtxt(os.path.join(dir, datafile))
-        time = b.filter(dataset=lcs[x], qualifier='times').get_value()
+        time = b.filter(dataset=id, qualifier='times').get_value()
         # get third column value
         err_val = phb1.getpar('phoebe_lc_indweight', x)
         print("checking time in "+str(lcs[x]))
         assert(np.all(time==data[:,0]))
-        flux = b.filter(dataset=lcs[x], qualifier='fluxes').get_value()
+        flux = b.filter(dataset=id, qualifier='fluxes').get_value()
         if verbose: print("checking flux in "+str(lcs[x]))
+        if verbose: print("x={}, datafile={}, lcs[x]={}".format(x, datafile, lcs[x]))
         assert(np.all(flux==data[:,1]))
-        sigma = b.filter(dataset=lcs[x], qualifier='sigmas').get_value()
+        sigma = b.filter(dataset=id, qualifier='sigmas').get_value()
         if verbose: print("checking sigma in "+str(lcs[x]))
 
         if err_val == 'Standard deviation':
@@ -74,8 +67,8 @@ def legacy_test(filename='default.phoebe', verbose=False):
         ldx1, ldy1 = b.filter(dataset=lcs[x], qualifier='ld_coeffs', component='cow').get_value()
 
         ld_coeffs1 =[phb1.getpar('phoebe_ld_lcx1', x), phb1.getpar('phoebe_ld_lcy1', x), phb1.getpar('phoebe_ld_lcx2', x), phb1.getpar('phoebe_ld_lcy2', x)]
-        ldx1, ldy1 = b.filter(dataset=lcs[x], qualifier='ld_coeffs', component='cow').get_value()
-        ldx2, ldy2 = b.filter(dataset=lcs[x], qualifier='ld_coeffs', component='pig').get_value()
+        ldx1, ldy1 = b.filter(dataset=id, qualifier='ld_coeffs', component='cow').get_value()
+        ldx2, ldy2 = b.filter(dataset=id, qualifier='ld_coeffs', component='pig').get_value()
         ld_coeffs2 = [ldx1, ldy1, ldx2, ldy2]
         if verbose: print("checking ld coeffs in "+str(lcs[x]))
         assert(np.all(ld_coeffs1==ld_coeffs2))
@@ -88,6 +81,7 @@ def legacy_test(filename='default.phoebe', verbose=False):
         err_val = phb1.getpar('phoebe_rv_indweight', x)
         comp = phb1.getpar('phoebe_rv_dep', x).split(' ')[0].lower()
         id = phb1.getpar('phoebe_rv_id', x)
+        rv_id.append(id)
         print("id", id)
         if comp == 'primary':
             comp_name = 'cow'
@@ -102,6 +96,7 @@ def legacy_test(filename='default.phoebe', verbose=False):
         assert(np.all(time==data[:,0]))
         rv = b.filter(dataset=id, qualifier='rvs', component=comp_name).get_value()
         if verbose: print("checking rv in "+str(rvs[a]))
+        if verbose: print("a={}, datafile={}, rvs[a]={}".format(a, datafile, rvs[a]))
         assert(np.all(rv==data[:,1]))
         sigma = b.filter(dataset=id, qualifier='sigmas', component=comp_name).get_value()
         if verbose: print("checking sigma in "+str(rvs[a]))
@@ -138,10 +133,10 @@ def legacy_test(filename='default.phoebe', verbose=False):
             sec = sec+1
 
     b.run_compute(kind='legacy')
-    for x in range(len(lcs)):
-        lc2 = b.filter('fluxes', context='model', dataset=lcs[x]).get_value()
-        time = b.filter('times', context='model', dataset=lcs[x]).get_value()
-        if verbose: print("comparing lightcurve "+str(lcs[x]))
+    for x in range(len(lc_id)):
+        lc2 = b.filter('fluxes', context='model', dataset=lc_id[x]).get_value()
+        time = b.filter('times', context='model', dataset=lc_id[x]).get_value()
+        if verbose: print("comparing lightcurve "+str(lc_id[x]))
 
         assert(np.allclose(fluxes[x], lc2, atol=1e-5))
 
@@ -173,23 +168,21 @@ def legacy_test(filename='default.phoebe', verbose=False):
     ldx2, ldy2 = b.filter(qualifier='ld_coeffs_bol', component='pig').get_value()
     ld_coeffs2 = [ldx1, ldy1, ldx2, ldy2]
     assert(np.all(ld_coeffs1==ld_coeffs2))
-#            assert(np.all(vels2[x] == rv2))
 
-#        if np.any((vels1[x]-rv2) != 0):
-#            print("lightcurve "+str(lcs[x])+" failed")
-#        else:
-#            print("lightcurve "+str(lcs[x])+" passed")
 
     return
 
+def test_default(verbose=False):
+    return _legacy_test('default.phoebe', verbose=verbose)
+
+def test_weighted(verbose=False):
+    return _legacy_test('weight.phoebe', verbose=verbose)
+
+def test_contact(verbose=False):
+    return _legacy_test('contact.phoebe', verbose=verbose)
+
 if __name__ == '__main__':
 
-#    logger= phb2.logger()
-    detached = 'default.phoebe'
-    weighted = 'weight.phoebe'
-    contact = 'contact.phoebe'
-#    print "checking detached system"
-    legacy_test(weighted, verbose=True)
-    legacy_test(detached, verbose=True)
-#    print "checking contact system"
-    legacy_test(contact, verbose=True)
+    test_default(verbose=True)
+    test_weighted(verbose=True)
+    test_contact(verbose=True)
