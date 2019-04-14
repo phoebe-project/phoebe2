phoebe-project/phoebe2

commit f25294ca1265cd97a6a30456dcfa470ec8dc91cf

cache default bundle files and fix bug in loading json w unicode
significantly speeds up loading default systems.  Use force_build to have phoebe manually build the system.

phoebe/frontend/bundle.py
@@ -19,7 +19,7 @@ from phoebe.parameters import feature as _feature
 from phoebe.backend import backends, mesh
 from phoebe.distortions import roche
 from phoebe.frontend import io
-from phoebe.atmospheres.passbands import _pbtable
+from phoebe.utils import parse_json
 import libphoebe
 
 from phoebe import u
@@ -30,6 +30,8 @@ import logging
 logger = logging.getLogger("BUNDLE")
 logger.addHandler(logging.NullHandler())
 
+
+_bundle_cache_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'default_bundles'))+'/'
 
 # Attempt imports for client requirements
 try:
@@ -193,7 +195,7 @@ class Bundle(ParameterSet):
         filename = os.path.expanduser(filename)
         logger.debug("importing from {}".format(filename))
         f = open(filename, 'r')
-        data = json.load(f)
+        data = json.load(f, object_pairs_hook=parse_json)
         f.close()
         b = cls(data)
 
@@ -391,8 +393,10 @@ class Bundle(ParameterSet):
         return io.load_legacy(filename, add_compute_legacy, add_compute_phoebe)
 
     @classmethod
-    def default_star(cls, starA='starA'):
-        """Load a bundle with a default single star as the system.
+    def default_star(cls, starA='starA', force_build=False):
+        """
+        For convenience, this function is available at the top-level as
+        <phoebe.default_star> as well as <phoebe.frontend.bundle.Bundle.default_star>.
 
         sun
 
@@ -400,9 +404,30 @@ class Bundle(ParameterSet):
 
         >>> b = Bundle.default_binary()
 
-        :return: instatiated :class`Bundle` object
+        Arguments
+        -----------
+        * `starA` (string, optional, default='starA'): the label to be set for
+            starA.
+        * `force_build` (bool, optional, default=False): whether to force building
+            the bundle from scratch.  If False, pre-cached files will be loaded
+            whenever possible to save time.
+
+        Returns
+        -----------
+        * an instantiated <phoebe.frontend.bundle.Bundle> object.
         """
+        if not force_build and not conf.devel:
+            b = cls.open(os.path.join(_bundle_cache_dir, 'default_star.bundle'))
+
+            if starA != 'starA':
+                b.rename_component('starA', starA)
+
+            return b
+
         b = cls()
+        # IMPORTANT NOTE: if changing any of the defaults for a new release,
+        # make sure to update the cached files (see frontend/default_bundles
+        # directory for script to update all cached bundles)
         b.add_star(component=starA)
         b.set_hierarchy(_hierarchy.component(b[starA]))
         b.add_compute(distortion_method='rotstar', irrad_method='none')
@@ -410,8 +435,11 @@ class Bundle(ParameterSet):
 
     @classmethod
     def default_binary(cls, starA='primary', starB='secondary', orbit='binary',
-                       contact_binary=False):
-        """Load a bundle with a default binary as the system.
+                       contact_binary=False, force_build=False):
+        """
+        For convenience, this function is available at the top-level as
+        <phoebe.default_binary> as well as
+        <phoebe.frontend.bundle.Bundle.default_binary>.
 
         primary - secondary
 
@@ -419,9 +447,48 @@ class Bundle(ParameterSet):
 
         >>> b = Bundle.default_binary()
 
-        :return: instantiated :class:`Bundle` object
+        Arguments
+        -----------
+        * `starA` (string, optional, default='primary'): the label to be set for
+            the primary component.
+        * `starB` (string, optional, default='secondary'): the label to be set for
+            the secondary component.
+        * `orbit` (string, optional, default='binary'): the label to be set for
+            the binary component.
+        * `contact_binary` (bool, optional, default=False): whether to also
+            add an envelope (with component='contact_envelope') and set the
+            hierarchy to a contact binary system.
+        * `force_build` (bool, optional, default=False): whether to force building
+            the bundle from scratch.  If False, pre-cached files will be loaded
+            whenever possible to save time.
+
+        Returns
+        -----------
+        * an instantiated <phoebe.frontend.bundle.Bundle> object.
         """
+        if not force_build and not conf.devel:
+            if contact_binary:
+                b = cls.open(os.path.join(_bundle_cache_dir, 'default_contact_binary.bundle'))
+            else:
+                b = cls.open(os.path.join(_bundle_cache_dir, 'default_binary.bundle'))
+
+            secondary = 'secondary'
+            if starA != 'primary':
+                if starA == 'secondary':
+                    secondary = 'temp_secondary'
+                    b.rename_component('secondary', secondary)
+                b.rename_component('primary', starA)
+            if starB != 'secondary':
+                b.rename_component(secondary, starB)
+            if orbit != 'binary':
+                b.rename_component('binary', 'orbit')
+
+            return b
+
         b = cls()
+        # IMPORTANT NOTE: if changing any of the defaults for a new release,
+        # make sure to update the cached files (see frontend/default_bundles
+        # directory for script to update all cached bundles)
         if contact_binary:
             orbit_defaults = {'sma': 3.35, 'period': 0.5}
             star_defaults = {'requiv': 1.5}

phoebe/frontend/default_bundles/create_default_bundles.py
@@ -1,0 +1,11 @@
+import phoebe
+
+
+b = phoebe.default_star(force_build=True)
+b.save('./default_star.bundle', compact=True)
+
+b = phoebe.default_binary(force_build=True)
+b.save('./default_binary.bundle', compact=True)
+
+b = phoebe.default_binary(contact_binary=True, force_build=True)
+b.save('./default_contact_binary.bundle', compact=True)

phoebe/frontend/default_bundles/default_star.bundle
@@ -1,0 +1,1 @@
+[{"description":"Time at which all values are provided","limits":[null,null],"default_unit":"d","value":0.0,"context":"system","copy_for":false,"Class":"FloatParameter","qualifier":"t0"},{"description":"Right ascension","limits":[null,null],"default_unit":"deg","value":0.0,"context":"system","copy_for":false,"Class":"FloatParameter","qualifier":"ra"},{"description":"Declination","limits":[null,null],"default_unit":"deg","value":0.0,"context":"system","copy_for":false,"Class":"FloatParameter","qualifier":"dec"},{"description":"Epoch of coordinates","value":"J2000","context":"system","copy_for":false,"Class":"StringParameter","qualifier":"epoch"},{"description":"Distance to the system","limits":[null,null],"default_unit":"m","value":1.0,"context":"system","copy_for":false,"Class":"FloatParameter","qualifier":"distance"},{"description":"Systemic velocity (in the direction of positive RV or negative vz)","limits":[null,null],"default_unit":"km \/ s","value":0.0,"context":"system","copy_for":false,"Class":"FloatParameter","qualifier":"vgamma"},{"description":"Hierarchy representation","value":"star:starA","context":"system","copy_for":false,"Class":"HierarchyParameter","qualifier":"hierarchy"},{"kind":"star","description":"Equivalent radius","limits":[0.0,null],"default_unit":"solRad","component":"starA","value":1.0,"context":"component","copy_for":false,"Class":"FloatParameter","qualifier":"requiv"},{"kind":"star","description":"Critical (maximum) value of the equivalent radius for the given morphology","limits":[0.0,null],"default_unit":"solRad","component":"starA","value":10.0,"context":"component","copy_for":false,"Class":"FloatParameter","qualifier":"requiv_max"},{"context":"component","kind":"star","qualifier":"requiv_min","limits":[0.0,null],"default_unit":"solRad","component":"starA","value":0.1,"visible_if":"hierarchy.is_contact_binary:True","copy_for":false,"Class":"FloatParameter","description":"Critical (minimum) value of the equivalent radius for the given morphology"},{"kind":"star","description":"Mean effective temperature","limits":[0.0,null],"default_unit":"K","component":"starA","value":6000.0,"context":"component","copy_for":false,"Class":"FloatParameter","qualifier":"teff"},{"context":"component","kind":"star","qualifier":"abun","limits":[null,null],"default_unit":"","component":"starA","value":0.0,"visible_if":"hierarchy.is_contact_binary:False","copy_for":false,"Class":"FloatParameter","description":"Metallicity"},{"context":"component","kind":"star","qualifier":"syncpar","limits":[0.0,null],"default_unit":"","component":"starA","value":1.0,"visible_if":"hierarchy.is_binary:True","copy_for":false,"Class":"FloatParameter","description":"Synchronicity parameter"},{"kind":"star","description":"Rotation period","limits":[0.0,null],"default_unit":"d","component":"starA","value":1.0,"context":"component","copy_for":false,"Class":"FloatParameter","qualifier":"period"},{"kind":"star","description":"Rotation frequency","limits":[0.0,null],"default_unit":"rad \/ d","component":"starA","value":6.283185,"context":"component","copy_for":false,"Class":"FloatParameter","qualifier":"freq"},{"kind":"star","description":"Pitch of the stellar rotation axis wrt the orbital rotation axis","limits":[null,null],"default_unit":"deg","component":"starA","value":0.0,"context":"component","copy_for":false,"Class":"FloatParameter","qualifier":"pitch"},{"kind":"star","description":"Yaw of the stellar rotation axis wrt the orbital rotation axis","limits":[null,null],"default_unit":"deg","component":"starA","value":0.0,"context":"component","copy_for":false,"Class":"FloatParameter","qualifier":"yaw"},{"kind":"star","description":"Inclination of the stellar rotation axis","limits":[null,null],"default_unit":"deg","component":"starA","value":90.0,"context":"component","copy_for":false,"Class":"FloatParameter","qualifier":"incl"},{"kind":"star","description":"Longitude of the ascending node (ie. equator) of the star","limits":[null,null],"default_unit":"deg","component":"starA","value":0.0,"context":"component","copy_for":false,"Class":"FloatParameter","qualifier":"long_an"},{"kind":"star","description":"Bolometric gravity brightening","limits":[0.0,1.0],"default_unit":"","component":"starA","value":0.32,"context":"component","copy_for":false,"Class":"FloatParameter","qualifier":"gravb_bol"},{"kind":"star","description":"ratio of incident bolometric light that is used for reflection (heating without redistribution)","limits":[0.0,1.0],"default_unit":"","component":"starA","value":0.6,"context":"component","copy_for":false,"Class":"FloatParameter","qualifier":"irrad_frac_refl_bol"},{"kind":"star","description":"ratio of incident bolometric light that is lost\/ignored","limits":[0.0,1.0],"default_unit":"","component":"starA","value":0.4,"context":"component","copy_for":false,"Class":"FloatParameter","qualifier":"irrad_frac_lost_bol"},{"kind":"star","description":"Bolometric limb darkening model","component":"starA","value":"logarithmic","choices":["linear","logarithmic","quadratic","square_root","power"],"context":"component","copy_for":false,"Class":"ChoiceParameter","qualifier":"ld_func_bol"},{"kind":"star","description":"Bolometric limb darkening coefficients","default_unit":"","component":"starA","value":[0.5,0.5],"allow_none":false,"context":"component","copy_for":false,"Class":"FloatArrayParameter","qualifier":"ld_coeffs_bol"},{"kind":"star","description":"Mass","limits":[null,null],"default_unit":"solMass","component":"starA","value":1.0,"context":"component","copy_for":false,"Class":"FloatParameter","qualifier":"mass"},{"kind":"star","qualifier":"freq","constraint_kwargs":{"component":"starA"},"in_solar_units":true,"component":"starA","value":"6.283185 \/ {period@starA@component}","constraint_func":"freq","context":"constraint","default_unit":"rad \/ d","Class":"ConstraintParameter","description":"expression that determines the constraint"},{"kind":"star","qualifier":"irrad_frac_lost_bol","constraint_kwargs":{"component":"starA"},"in_solar_units":true,"component":"starA","value":"1.000000 - {irrad_frac_refl_bol@starA@component}","constraint_func":"irrad_frac","context":"constraint","default_unit":"","Class":"ConstraintParameter","description":"expression that determines the constraint"},{"kind":"phoebe","compute":"phoebe01","description":"Whether to create synthetics in compute\/fitting run","value":true,"dataset":"_default","context":"compute","copy_for":{"context":"dataset","dataset":"*"},"Class":"BoolParameter","qualifier":"enabled"},{"kind":"phoebe","compute":"phoebe01","description":"Which method to use to determine the dynamics of components","value":"keplerian","choices":["keplerian"],"context":"compute","copy_for":false,"Class":"ChoiceParameter","qualifier":"dynamics_method"},{"kind":"phoebe","compute":"phoebe01","description":"Correct for light travel time effects","value":false,"context":"compute","copy_for":false,"Class":"BoolParameter","qualifier":"ltte"},{"kind":"phoebe","compute":"phoebe01","description":"Which method to use to handle all irradiation effects (reflection, redistribution)","value":"none","choices":["none","wilson","horvat"],"context":"compute","copy_for":false,"Class":"ChoiceParameter","qualifier":"irrad_method"},{"kind":"phoebe","compute":"phoebe01","description":"Type of boosting method","value":"none","choices":["none","linear"],"context":"compute","copy_for":false,"Class":"ChoiceParameter","qualifier":"boosting_method"},{"kind":"phoebe","compute":"phoebe01","description":"Which method to use for discretizing the surface","component":"_default","value":"marching","choices":["marching"],"context":"compute","copy_for":{"kind":["star","envelope"],"component":"*"},"Class":"ChoiceParameter","qualifier":"mesh_method"},{"context":"compute","kind":"phoebe","compute":"phoebe01","qualifier":"ntriangles","limits":[100,null],"component":"_default","value":1500,"visible_if":"mesh_method:marching","copy_for":{"kind":["star","envelope"],"component":"*"},"Class":"IntParameter","description":"Requested number of triangles (won't be exact)."},{"context":"compute","kind":"phoebe","compute":"phoebe01","qualifier":"distortion_method","component":"_default","value":"rotstar","choices":["roche","rotstar","sphere"],"visible_if":"mesh_method:marching","copy_for":{"kind":["star"],"component":"*"},"Class":"ChoiceParameter","description":"Method to use for distorting stars"},{"kind":"phoebe","compute":"phoebe01","description":"Type of eclipse algorithm","value":"native","choices":["native"],"context":"compute","copy_for":false,"Class":"ChoiceParameter","qualifier":"eclipse_method"},{"context":"compute","kind":"phoebe","compute":"phoebe01","qualifier":"horizon_method","value":"boolean","choices":["boolean"],"visible_if":"eclipse_method:native","copy_for":false,"Class":"ChoiceParameter","description":"Type of horizon method"},{"kind":"phoebe","compute":"phoebe01","description":"Atmosphere table","component":"_default","value":"ck2004","choices":["blackbody","extern_atmx","extern_planckint","ck2004"],"context":"compute","copy_for":{"kind":["star"],"component":"*"},"Class":"ChoiceParameter","qualifier":"atm"},{"kind":"phoebe","compute":"phoebe01","description":"Method to use for computing LC fluxes","value":"numerical","dataset":"_default","context":"compute","copy_for":{"kind":["lc"],"dataset":"*"},"choices":["numerical"],"Class":"ChoiceParameter","qualifier":"lc_method"},{"kind":"phoebe","compute":"phoebe01","description":"How to handle finite-time integration (when non-zero exptime)","value":"none","dataset":"_default","context":"compute","copy_for":{"kind":["lc"],"dataset":"*"},"choices":["none","oversample"],"Class":"ChoiceParameter","qualifier":"fti_method"},{"context":"compute","kind":"phoebe","compute":"phoebe01","qualifier":"fti_oversample","limits":[null,null],"value":5,"dataset":"_default","visible_if":"fti_method:oversample","copy_for":{"kind":["lc"],"dataset":"*"},"Class":"IntParameter","description":"Number of times to sample per-datapoint for finite-time integration"},{"kind":"phoebe","compute":"phoebe01","description":"Method to use for computing RVs (must be flux-weighted for Rossiter-McLaughlin effects)","component":"_default","value":"flux-weighted","dataset":"_default","context":"compute","copy_for":{"kind":["rv"],"component":"*","dataset":"*"},"choices":["flux-weighted","dynamical"],"Class":"ChoiceParameter","qualifier":"rv_method"},{"context":"compute","kind":"phoebe","compute":"phoebe01","description":"Whether gravitational redshift effects are enabled for RVs","component":"_default","value":false,"dataset":"_default","visible_if":"rv_method:flux-weighted","copy_for":{"kind":["rv"],"component":"*","dataset":"*"},"Class":"BoolParameter","qualifier":"rv_grav"},{"kind":"phoebe","compute":"phoebe01","description":"Which method to use for discretizing the surface","component":"starA","value":"marching","choices":["marching"],"context":"compute","copy_for":false,"Class":"ChoiceParameter","qualifier":"mesh_method"},{"context":"compute","kind":"phoebe","compute":"phoebe01","qualifier":"ntriangles","limits":[100,null],"component":"starA","value":1500,"visible_if":"mesh_method:marching","copy_for":false,"Class":"IntParameter","description":"Requested number of triangles (won't be exact)."},{"context":"compute","kind":"phoebe","compute":"phoebe01","qualifier":"distortion_method","component":"starA","value":"rotstar","choices":["roche","rotstar","sphere"],"visible_if":"mesh_method:marching","copy_for":false,"Class":"ChoiceParameter","description":"Method to use for distorting stars"},{"kind":"phoebe","compute":"phoebe01","description":"Atmosphere table","component":"starA","value":"ck2004","choices":["blackbody","extern_atmx","extern_planckint","ck2004"],"context":"compute","copy_for":false,"Class":"ChoiceParameter","qualifier":"atm"},{"description":"Version of PHOEBE - change with caution","value":"2.1.5","context":"setting","copy_for":false,"Class":"StringParameter","qualifier":"phoebe_version"},{"description":"Whether to log history (undo\/redo)","value":false,"context":"setting","copy_for":false,"Class":"BoolParameter","qualifier":"log_history"},{"description":"Filters to use when using dictionary access","value":{},"context":"setting","copy_for":false,"Class":"DictParameter","qualifier":"dict_filter"},{"description":"Whether to set all values for dictionary access that returns more than 1 result","value":false,"context":"setting","copy_for":false,"Class":"BoolParameter","qualifier":"dict_set_all"}]

phoebe/parameters/parameters.py
@@ -993,6 +993,8 @@ class ParameterSet(object):
         filename = os.path.expanduser(filename)
         f = open(filename, 'r')
         if _can_ujson:
+            # NOTE: this will not parse the unicode.  Bundle.open always calls
+            # json instead of ujson for this reason.
             data = ujson.load(f)
         else:
             data = json.load(f, object_pairs_hook=parse_json)

setup.py
@@ -317,6 +317,7 @@ setup (name = 'phoebe',
        packages = ['phoebe', 'phoebe.parameters', 'phoebe.frontend', 'phoebe.constraints', 'phoebe.dynamics', 'phoebe.distortions', 'phoebe.algorithms', 'phoebe.atmospheres', 'phoebe.backend', 'phoebe.utils', 'phoebe.dependencies', 'phoebe.dependencies.autofig', 'phoebe.dependencies.nparray', 'phoebe.dependencies.unitsiau2015'],
        install_requires=['numpy>=1.10','scipy>=0.17','astropy>=1.0,<3.0'],
        package_data={'phoebe.atmospheres':['tables/wd/*', 'tables/passbands/*'],
+                     'phoebe.frontend':['default_bundles/*.bundle']
                     },
        ext_modules = ext_modules,
        cmdclass = {

commit 6db5a02c1cd647b4394b95cc6ec8484a7276e11e

fix merging issue
introduced in f25294ca1265cd97a6a30456dcfa470ec8dc91cf

phoebe/frontend/bundle.py
@@ -19,6 +19,7 @@ from phoebe.parameters import feature as _feature
 from phoebe.backend import backends, mesh
 from phoebe.distortions import roche
 from phoebe.frontend import io
+from phoebe.atmospheres.passbands import _pbtable
 from phoebe.utils import parse_json
 import libphoebe
 