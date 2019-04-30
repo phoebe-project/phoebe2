dc8ee6b9c9d100264af7001feba05f1a892caff5
Kyle Conroy <kyleconroy@gmail.com>
2019-04-30 10:27:34 -0400

    remove support for pblum_mode='system flux'
    (technically just move to developer mode in case we want to re-introduce it at some point in the future)

--- phoebe/parameters/dataset.py
+++ phoebe/parameters/dataset.py
@@ -148,7 +148,7 @@ def lc_dep(is_lc=True, **kwargs):
     dep_params += [ChoiceParameter(qualifier='passband', value=kwargs.get('passband', 'Johnson:V'), choices=passbands.list_passbands(), description='Passband')]
     dep_params += [ChoiceParameter(qualifier='intens_weighting', value=kwargs.get('intens_weighting', 'energy'), choices=['energy', 'photon'], description='Whether passband intensities are weighted by energy of photons')]
     if is_lc:
-        dep_params += [ChoiceParameter(qualifier='pblum_mode', value=kwargs.get('pblum_mode', 'provided'), choices=['provided', 'color coupled', 'system flux', 'total flux', 'scale to data', 'absolute'], description='Mode for scaling passband luminosities')]
+        dep_params += [ChoiceParameter(qualifier='pblum_mode', value=kwargs.get('pblum_mode', 'provided'), choices=['provided', 'color coupled', 'system flux', 'total flux', 'scale to data', 'absolute'] if conf.devel else ['provided', 'color coupled', 'total flux', 'scale to data', 'absolute'], description='Mode for scaling passband luminosities')]
 
         dep_params += [ChoiceParameter(visible_if='[component]pblum_mode:provided', qualifier='pblum_ref', copy_for={'kind': ['star'], 'component': '*'}, component='_default', value=kwargs.get('pblum_ref', ''), choices=['self', '']+kwargs.get('starrefs', []), description='Whether to use this components pblum or to couple to that from another component in the system')]
         dep_params += [FloatParameter(qualifier='pblum', visible_if='[component]pblum_mode:provided,pblum_ref:self', copy_for={'kind': ['star'], 'component': '*'}, component='_default', value=kwargs.get('pblum', 4*np.pi), default_unit=u.W, description='Passband luminosity (defined at t0)')]
