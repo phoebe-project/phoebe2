phoebe-project/phoebe2

commit 7aa0017314432d93ac871c7fda4400d326fbdfaf

merge dataset creation functions (ie. lc, lc_syn, lc_dep)

phoebe/parameters/parameters.py
@@ -112,12 +112,7 @@ _forbidden_labels = deepcopy(_meta_fields_all)
 
 # forbid all "contexts"
 _forbidden_labels += _contexts
-_forbidden_labels += ['lc', 'lc_dep', 'lc_syn',
-                      'rv', 'rv_dep', 'rv_syn',
-                      'lp', 'lp_dep', 'lp_syn',
-                      'sp', 'sp_dep', 'sp_syn',
-                      'orb', 'orb_dep', 'orb_syn',
-                      'mesh', 'mesh_dep', 'mesh_syn']
+_forbidden_labels += ['lc', 'rv', 'lp', 'sp', 'orb', 'mesh']
 
 # forbid all "methods"
 _forbidden_labels += ['value', 'adjust', 'prior', 'posterior', 'default_unit',
@@ -1179,11 +1174,11 @@ class ParameterSet(object):
                 # means that this should exist for each component (since that has a wildcard) which
                 # has a kind in [star, disk, custombody]
                 #
-                # copy_for = {'kind': ['rv_dep'], 'component': '*', 'dataset': '*'}
+                # copy_for = {'kind': ['rv'], 'component': '*', 'dataset': '*'}
                 # or
-                # copy_for = {'component': {}, 'dataset': {'kind': 'rv_dep'}}
+                # copy_for = {'component': {}, 'dataset': {'kind': 'rv'}}
                 # means that this should exist for each component/dataset pair with the
-                # rv_dep kind
+                # rv kind
                 #
                 # copy_for = {'component': {'kind': 'star'}, 'dataset': {'kind': 'rv'}}
                 # means that this should exist for each component/dataset pair
@@ -2845,16 +2840,16 @@ class ParameterSet(object):
         else:
             dataset_ps = self.filter(dataset=dataset, context='dataset')
 
-        dataset_kind = dataset_ps.filter(kind='*dep').kind
+        dataset_kind = dataset_ps.kind
 
         if not len(self.filter(context='model').models):
             model_ps = self._bundle.get_model(model=model).filter(dataset=dataset, component=component)
         else:
             model_ps = self.filter(model=model, context='model').filter(dataset=dataset, component=component)
 
-        if dataset_kind == 'lc_dep':
+        if dataset_kind == 'lc':
             qualifier = 'fluxes'
-        elif dataset_kind == 'rv_dep':
+        elif dataset_kind == 'rv':
             qualifier = 'rvs'
         else:
             # TODO: lp compared for a given time interpolating in wavelength?
@@ -2910,7 +2905,7 @@ class ParameterSet(object):
 
         ps = self.filter(check_visible=False, **filter_kwargs).exclude(qualifier=['compute_times', 'compute_phases'])
 
-        if 'time' in kwargs.keys() and ps.kind in ['mesh', 'mesh_syn', 'lp', 'lp_syn']:
+        if 'time' in kwargs.keys() and ps.kind in ['mesh', 'lp']:
             ps = ps.filter(time=kwargs.get('time'))
 
         # If ps returns more than one dataset/model/component, then we need to
@@ -2978,7 +2973,7 @@ class ParameterSet(object):
             return return_
 
 
-        if ps.kind in ['mesh', 'mesh_syn', 'orb', 'orb_syn'] and \
+        if ps.kind in ['mesh', 'orb'] and \
                 ps.context == 'dataset':
             # nothing to plot here... at least for now
             return []
@@ -3051,7 +3046,7 @@ class ParameterSet(object):
 
             #### RETRIEVE DATA ARRAYS
             if isinstance(current_value, str):
-                if ps.kind not in ['mesh'] and direction in ['fc', 'ec']:
+                if ps.kind != 'mesh' and direction in ['fc', 'ec']:
                     logger.warning("fc and ec are not allowable for dataset={} with kind={}, ignoring {}={}".format(ps.dataset, ps.kind, direction, current_value))
                     _dump = kwargs.pop(direction)
                     return kwargs
@@ -3139,7 +3134,7 @@ class ParameterSet(object):
                     if 'residuals' in kwargs.values():
                         # then we actually need to pull the times from the dataset instead of the model since the length may not match
                         times = ps._bundle.get_value('times', dataset=ps.dataset, component=ps.component, context='dataset')
-                    elif ps.kind in ['etvs']:
+                    elif ps.kind == 'etvs':
                         times = ps.get_value('time_ecls', unit=u.d)
                     else:
                         times = ps.get_value('times', unit=u.d)
@@ -3175,7 +3170,7 @@ class ParameterSet(object):
                     # the same mesh (e.g. rvs@rv01 inside kind=mesh).  Let's
                     # check for that first.
 
-                    if ps.kind in ['mesh'] and ps._bundle is not None:
+                    if ps.kind == 'mesh' and ps._bundle is not None:
                         full_mesh_meta = {k:v for k,v in ps.meta.items() if k not in ['qualifier', 'dataset']}
                         full_mesh_ps = ps._bundle.filter(**full_mesh_meta)
                         candidate_params = full_mesh_ps.filter(current_value)
@@ -3213,7 +3208,7 @@ class ParameterSet(object):
 
         #### DIRECTION DEFAULTS
         # define defaults for directions based on ps.kind
-        if ps.kind in ['mesh', 'mesh_syn']:
+        if ps.kind == 'mesh':
             # first determine from any passed values if we're in xyz or uvw
             # (do not allow mixing between roche and POS)
             detected_qualifiers = [kwargs[af_direction] for af_direction in ['x', 'y', 'z'] if af_direction in kwargs.keys()]
@@ -3291,7 +3286,7 @@ class ParameterSet(object):
 
             sigmas_avail = []
 
-        elif ps.kind in ['orb', 'orb_syn']:
+        elif ps.kind == 'orb':
             # similar logic to meshes above, except we only have uvw
             coordinates = ['us', 'vs', 'ws']
 
@@ -3316,17 +3311,17 @@ class ParameterSet(object):
                 defaults['z'] = 0
 
             sigmas_avail = []
-        elif ps.kind in ['lc', 'lc_syn']:
+        elif ps.kind == 'lc':
             defaults = {'x': 'times',
                         'y': 'fluxes',
                         'z': 0}
             sigmas_avail = ['fluxes']
-        elif ps.kind in ['rv', 'rv_syn']:
+        elif ps.kind == 'rv':
             defaults = {'x': 'times',
                         'y': 'rvs',
                         'z': 0}
             sigmas_avail = ['rvs']
-        elif ps.kind in ['lp', 'lp_syn']:
+        elif ps.kind == 'lp':
             defaults = {'x': 'wavelengths',
                         'y': 'flux_densities',
                         'z': 0}
@@ -3343,7 +3338,7 @@ class ParameterSet(object):
             kwargs.setdefault('uncover', True)
             kwargs.setdefault('trail', 0)
 
-        elif ps.kind in ['etv', 'etv_syn']:
+        elif ps.kind == 'etv':
             defaults = {'x': 'time_ecls',
                         'y': 'etvs',
                         'z': 0}
@@ -3355,7 +3350,7 @@ class ParameterSet(object):
         #### DETERMINE AUTOFIG PLOT TYPE
         # NOTE: this must be done before calling _kwargs_fill_dimension below
         cartesian = ['xs', 'ys', 'zs', 'us', 'vs', 'ws']
-        if ps.kind in ['mesh']:
+        if ps.kind == 'mesh':
             if mesh_all_cartesian:
                 kwargs['autofig_method'] = 'mesh'
             else:
@@ -3405,7 +3400,7 @@ class ParameterSet(object):
                     kwargs['iqualifier'] = iqualifier
                 else:
                     raise NotImplementedError
-            elif ps.kind in ['etv']:
+            elif ps.kind == 'etv':
                 if iqualfier=='times':
                     kwargs['i'] = ps.get_quantity(qualifier='time_ecls')
                     kwargs['iqualifier'] = 'time_ecls'
@@ -3434,7 +3429,7 @@ class ParameterSet(object):
         if ps.context == 'dataset':
             kwargs.setdefault('linestyle', 'none')
         elif ps.context == 'model':
-            if ps.kind in ['mesh', 'mesh_syn'] and kwargs['autofig_method'] == 'plot':
+            if ps.kind == 'mesh' and kwargs['autofig_method'] == 'plot':
                 kwargs.setdefault('marker', '^')
                 kwargs.setdefault('linestyle', 'none')
             else:

phoebe/backend/backends.py
@@ -157,7 +157,7 @@ def _extract_from_bundle(b, compute, times=None, allow_oversample=False,
     for dataset in b.filter(qualifier='enabled', compute=compute, value=True).datasets:
         dataset_ps = b.filter(context='dataset', dataset=dataset)
         dataset_compute_ps = b.filter(context='compute', dataset=dataset, compute=compute, check_visible=False)
-        dataset_kind = dataset_ps.exclude(kind='*_dep').kind
+        dataset_kind = dataset_ps.kind
         time_qualifier = _timequalifier_by_kind(dataset_kind)
         if dataset_kind in ['lc']:
             # then the Parameters in the model only exist at the system-level
@@ -191,7 +191,7 @@ def _extract_from_bundle(b, compute, times=None, allow_oversample=False,
                 # mesh with this dataset in datasets@mesh
                 # for mesh_datasets_parameter in mesh_datasets_parameters:
                     # if dataset in mesh_datasets_parameter.get_value():
-                        # mesh_obs_ps = b.filter(context='dataset', dataset=mesh_datasets_parameter.dataset, component=None).exclude(kind='*_dep')
+                        # mesh_obs_ps = b.filter(context='dataset', dataset=mesh_datasets_parameter.dataset, component=None)
                         # TODO: not sure about the component=None on the next line... what will this do for rvs with different times per-component?
                         # mesh_times = _expand_mesh_times(b, mesh_obs_ps, component=None)
                         # this_times = np.unique(np.append(this_times, mesh_times))
@@ -234,7 +234,7 @@ def _extract_from_bundle(b, compute, times=None, allow_oversample=False,
                     # other (but mesh_columns does not).
                     info['mesh_columns'] = dataset_ps.get_value('columns', expand=True)
                     info['mesh_datasets'] = list(set([c.split('@')[1] for c in info['mesh_columns'] if len(c.split('@'))>1]))
-                    info['mesh_kinds'] = [b.filter(dataset=ds, context='dataset').exclude(kind='*_dep').kind for ds in info['mesh_datasets']]
+                    info['mesh_kinds'] = [b.filter(dataset=ds, context='dataset').kind for ds in info['mesh_datasets']]
 
                 if by_time:
                     for time_ in this_times:
@@ -293,7 +293,7 @@ def _create_syns(b, needed_syns):
 
             # needed_syn['columns'] = b.get_value(qualifier='columns', dataset=needed_syn['dataset'], context='dataset')
             # datasets = b.get_value(qualifier='datasets', dataset=needed_syn['dataset'], context='dataset')
-            # needed_syn['datasets'] = {ds: b.filter(datset=ds, context='dataset').exclude(kind='*_dep').kind for ds in datasets}
+            # needed_syn['datasets'] = {ds: b.filter(datset=ds, context='dataset').kind for ds in datasets}
 
         # phoebe will compute everything sorted - even if the input times array
         # is out of order, so let's make sure the exposed times array is in
@@ -303,7 +303,7 @@ def _create_syns(b, needed_syns):
 
             needed_syn['empty_arrays_len'] = len(needed_syn['times'])
 
-        these_params, these_constraints = getattr(_dataset, "{}_syn".format(syn_kind.lower()))(**needed_syn)
+        these_params, these_constraints = getattr(_dataset, syn_kind.lower())(syn=True, **needed_syn)
         # TODO: do we need to handle constraints?
         these_params = these_params.to_list()
         for param in these_params:
@@ -621,7 +621,7 @@ class PhoebeBackend(BaseBackendByTime):
         enabled_ps = b.filter(qualifier='enabled', compute=compute, value=True)
         if datasets is None:
             datasets = enabled_ps.datasets
-        # kinds = [b.get_dataset(dataset=ds).exclude(kind='*_dep').kind for ds in datasets]
+        # kinds = [b.get_dataset(dataset=ds).kind for ds in datasets]
 
         logger.debug("rank:{}/{} PhoebeBackend._create_system_and_compute_pblums: handling pblum scaling".format(mpi.myrank, mpi.nprocs))
         # NOTE: system.compute_pblum_scalings populates at t0 with ignore_effect=True (so intrinsic pblum)

phoebe/backend/universe.py
@@ -347,7 +347,7 @@ class System(object):
         pblum_scale_copy_ds = {}
         for dataset in datasets:
             ds = b.get_dataset(dataset=dataset)
-            kind = ds.exclude(kind='*_dep').kind
+            kind = ds.kind
             if kind not in ['lc']:
                 # only LCs need pblum scaling
                 continue

phoebe/frontend/bundle.py
@@ -1849,7 +1849,7 @@ class Bundle(ParameterSet):
                         return False, "ld_func_bol='{}' not supported by '{}' backend used by compute='{}'.  Use 'linear', 'logarithmic', or 'square_root'.".format(ld_func, self.get_compute(compute).kind, compute)
 
             for dataset in self.datasets:
-                if dataset=='_default' or self.get_dataset(dataset=dataset, kind='*dep').kind not in ['lc_dep', 'rv_dep']:
+                if dataset=='_default' or self.get_dataset(dataset=dataset).kind not in ['lc', 'rv']:
                     continue
                 ld_func = str(self.get_value(qualifier='ld_func', dataset=dataset, component=component, context='dataset', **kwargs))
                 ld_coeffs_source = self.get_value(qualifier='ld_coeffs_source', dataset=dataset, component=component, context='dataset', check_visible=False, **kwargs)
@@ -2893,18 +2893,18 @@ class Bundle(ParameterSet):
                        for component in components]):
             raise ValueError("'{}' not a recognized/allowable component".format(component))
 
-        obs_metawargs = {'context': 'dataset',
+        ds_metawargs = {'context': 'dataset',
                          'kind': kind,
                          'dataset': kwargs['dataset']}
 
         if kind in ['lp']:
             # then times needs to be passed now to duplicate and tag the Parameters
             # correctly
-            obs_kwargs = {'times': kwargs.pop('times', [])}
+            ds_kwargs = {'times': kwargs.pop('times', [])}
         else:
-            obs_kwargs = {}
+            ds_kwargs = {}
 
-        obs_params, constraints = func(dataset=kwargs['dataset'], component_top=self.hierarchy.get_top(), **obs_kwargs)
+        params, constraints = func(dataset=kwargs['dataset'], component_top=self.hierarchy.get_top(), **ds_kwargs)
 
         if kwargs.get('overwrite', False):
             self.remove_dataset(dataset=kwargs['dataset'])
@@ -2912,18 +2912,12 @@ class Bundle(ParameterSet):
             # something other than dataset
             self._check_label(kwargs['dataset'], allow_overwrite=False)
 
-        self._attach_params(obs_params, **obs_metawargs)
+        self._attach_params(params, **ds_metawargs)
 
         for constraint in constraints:
             # TODO: tricky thing here will be copying the constraints
             self.add_constraint(*constraint)
 
-        dep_func = _get_add_func(_dataset, "{}_dep".format(kind))
-        dep_metawargs = {'context': 'dataset',
-                         'kind': '{}_dep'.format(kind),
-                         'dataset': kwargs['dataset']}
-        dep_params = dep_func()
-        self._attach_params(dep_params, **dep_metawargs)
 
         # Now we need to apply any kwargs sent by the user.  There are a few
         # scenarios (and each kwargs could fall into different ones):
@@ -3005,7 +2999,7 @@ class Bundle(ParameterSet):
                 if kind in ['rv', 'lp'] and k in ['ld_func', 'ld_coeffs',
                                                   'passband', 'intens_weighting',
                                                   'profile_rest', 'profile_func', 'profile_sv']:
-                    # passband-dependent (ie lc_dep) parameters do not have
+                    # passband-dependent parameters do not have
                     # assigned components
                     components_ = None
                 elif k in ['compute_times', 'compute_phases']:
@@ -3120,15 +3114,9 @@ class Bundle(ParameterSet):
         # let's handle deps if kind was passed
         kind = kwargs.get('kind', None)
 
-        if kind is not None:
-            if isinstance(kind, str):
-                kind = [kind]
-            kind_deps = []
-            for kind_i in kind:
-                dep = '{}_dep'.format(kind_i)
-                if dep not in kind:
-                    kind_deps.append(dep)
-            kind = kind + kind_deps
+        if isinstance(kind, str):
+            kind = [kind]
+
         kwargs['kind'] = kind
 
 
@@ -3150,11 +3138,6 @@ class Bundle(ParameterSet):
         # parameters, etc
         kwargs.setdefault('context', ['dataset', 'model', 'constraint', 'compute'])
 
-        # ps = self.filter(**kwargs)
-        # logger.info('removing {} parameters (this is not undoable)'.\
-        #             format(len(ps)))
-
-        # print "*** kwargs", kwargs, len(ps)
         self.remove_parameters_all(**kwargs)
         # not really sure why we need to call this twice, but it seems to do
         # the trick

phoebe/parameters/component.py
@@ -67,7 +67,6 @@ def orbit(component, **kwargs):
     """
     params = []
 
-    #~ params += [ObjrefParameter(value=component)]
     params += [FloatParameter(qualifier='period', timederiv='dpdt', value=kwargs.get('period', 1.0), default_unit=u.d, limits=(0.0,None), description='Orbital period')]
     params += [FloatParameter(qualifier='freq', value=kwargs.get('freq', 2*np.pi/3.0), default_unit=u.rad/u.d, description='Orbital frequency')]
     params += [FloatParameter(qualifier='dpdt', value=kwargs.get('dpdt', 0.0), default_unit=u.s/u.yr, description='Time derivative of orbital period')]
@@ -163,7 +162,6 @@ def star(component, **kwargs):
 
     params = []
 
-    #~ params += [ObjrefParameter(value=component)]
     params += [FloatParameter(qualifier='requiv', value=kwargs.get('requiv', 1.0), default_unit=u.solRad, limits=(0.0,None), description='Equivalent radius')]
     params += [FloatParameter(qualifier='requiv_max', value=kwargs.get('requiv_max', 10.0), default_unit=u.solRad, limits=(0.0, None), description='Critical (maximum) value of the equivalent radius for the given morphology')]
     params += [FloatParameter(qualifier='requiv_min', visible_if='hierarchy.is_contact_binary:True', value=kwargs.get('requiv_min', 0.1), default_unit=u.solRad, limits=(0.0, None), description='Critical (minimum) value of the equivalent radius for the given morphology')]
@@ -193,13 +191,10 @@ def star(component, **kwargs):
     params += [FloatParameter(qualifier='irrad_frac_refl_bol', value=kwargs.get('irrad_frac_refl_bol', 0.6), default_unit=u.dimensionless_unscaled, limits=(0.0,1.0), description='ratio of incident bolometric light that is used for reflection (heating without redistribution)')]
     params += [FloatParameter(qualifier='irrad_frac_lost_bol', value=kwargs.get('irrad_frac_lost_bol', 1.0), default_unit=u.dimensionless_unscaled, limits=(0.0, 1.0), description='ratio of incident bolometric light that is lost/ignored')]
 
-    # TODO: allow for 'interp' as choice, make default, and set visible_if for ld_coeffs_bol (see ld_coeffs in dataset.py)
     params += [ChoiceParameter(qualifier='ld_func_bol', value=kwargs.get('ld_func_bol', 'logarithmic'), choices=_ld_func_choices_no_interp, description='Bolometric limb darkening model')]
     params += [FloatArrayParameter(qualifier='ld_coeffs_bol', value=kwargs.get('ld_coeffs_bol', [0.5, 0.5]), default_unit=u.dimensionless_unscaled, description='Bolometric limb darkening coefficients')]
 
     params += [FloatParameter(qualifier='mass', value=kwargs.get('mass', 1.0), default_unit=u.solMass, description='Mass')]
-
-    # TODO: add others or move to a bol_dep (in which case create the bol_dep now)?
 
     constraints = []
     # constraints handled by set_hierarchy:

commit 76ef9e0354b1855d13dc0c5bec379e5c05034a89

fix plotting bug introduced by merging of deps and syns
introduced in 7aa0017314432d93ac871c7fda4400d326fbdfaf.  Need to prevent looping over components because of the component-dependent parameters in the LC dataset (which used to be ignored because kind=='lc_dep' instead of 'lc')

phoebe/parameters/parameters.py
@@ -2930,9 +2930,6 @@ class ParameterSet(object):
                 return_ += this_return
             return return_
 
-        # For kinds, we want to ignore the deps - those won't have arrays
-        kinds = [m for m in ps.kinds if m[-3:] != 'dep']
-
         # If we are asking to plot a dataset that also shows up in columns in
         # the mesh, then remove the mesh kind.  In other words: mesh stuff
         # will only be plotted if mesh is the only kind in the filter.
@@ -2940,13 +2937,8 @@ class ParameterSet(object):
         if len(pskinds) > 1 and 'mesh' in pskinds:
             pskinds.remove('mesh')
 
-        if len(kinds) == 1 and len(pskinds) > 1:
-            # then we need to filter to exclude the dep
-            ps = ps.filter(check_visible=False, kind=kinds[0])
-            pskinds = [kinds[0]]
-
         if len(ps.kinds) > 1:
-            for kind in [m for m in pskinds if m[-3:]!='dep']:
+            for kind in pskinds:
                 this_return = ps.filter(check_visible=False, kind=kind)._unpack_plotting_kwargs(**kwargs)
                 return_ += this_return
             return return_
@@ -2965,7 +2957,8 @@ class ParameterSet(object):
                 return_ += this_return
             return return_
 
-        if len(ps.components) > 1:
+        if len(ps.components) > 1 and ps.kind not in ['lc']:
+            # lc has per-component passband-dependent parameters in the dataset which are not plottable
             return_ = []
             for component in ps.components:
                 this_return = ps.filter(check_visible=False, component=component)._unpack_plotting_kwargs(**kwargs)