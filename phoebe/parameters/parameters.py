phoebe-project/phoebe2

commit ac92a7b1758791a4b74a7761e657f23a06538680

cbs: enable pitch/yaw constraints, hide incl/long_an/pitch/yaw params
related to #345

phoebe/parameters/parameters.py
@@ -4542,7 +4542,7 @@ class FloatParameter(Parameter):
         """
         if self._is_constraint is None:
             return None
-        return self._bundle.get_parameter(context='constraint', uniqueid=self._is_constraint)
+        return self._bundle.get_parameter(context='constraint', uniqueid=self._is_constraint, check_visible=False)
 
     @property
     def constrained_by(self):

phoebe/constraints/expression.py
@@ -100,7 +100,7 @@ class ConstraintVar(object):
         """
         if self.is_param:
             # TODO: CAREFUL, this may cause infinite loops if we try to run constraints through get_value
-            return self._bundle.get_quantity(uniqueid=self.unique_label, units=units, t=t)
+            return self._bundle.get_quantity(uniqueid=self.unique_label, units=units, t=t, check_visible=False, check_default=False)
 
         else:
             # TODO: constants and methods
@@ -114,7 +114,7 @@ class ConstraintVar(object):
         """
         if self.is_param:
             # TODO: CAREFUL, this may cause infinite loops if we try to run constraints through get_value
-            return self._bundle.get_value(uniqueid=self.unique_label, units=units, t=t)
+            return self._bundle.get_value(uniqueid=self.unique_label, units=units, t=t, check_visible=False, check_default=False)
 
         else:
             # TODO: constants and methods

phoebe/frontend/bundle.py
@@ -1212,6 +1212,41 @@ class Bundle(ParameterSet):
                     self.add_constraint(constraint.rotation_period, component,
                                         constraint=self._default_label('rotation_period', context='constraint'))
 
+                logger.debug('re-creating pitch constraint for {}'.format(component))
+                # TODO: will this cause problems if the constraint has been flipped?
+                # TODO: what if the user disabled/removed this constraint?
+                if len(self.filter(context='constraint',
+                                   constraint_func='pitch',
+                                   component=component)):
+                    constraint_param = self.get_constraint(constraint_func='pitch',
+                                                           component=component)
+                    self.remove_constraint(constraint_func='pitch',
+                                           component=component)
+                    self.add_constraint(constraint.pitch, component,
+                                        solve_for=constraint_param.constrained_parameter.uniquetwig,
+                                        constraint=constraint_param.constraint)
+                else:
+                    self.add_constraint(constraint.pitch, component,
+                                        constraint=self._default_label('pitch', context='constraint'))
+
+                logger.debug('re-creating yaw constraint for {}'.format(component))
+                # TODO: will this cause problems if the constraint has been flipped?
+                # TODO: what if the user disabled/removed this constraint?
+                if len(self.filter(context='constraint',
+                                   constraint_func='yaw',
+                                component=component)):
+                    constraint_param = self.get_constraint(constraint_func='yaw',
+                                                           component=component)
+                    self.remove_constraint(constraint_func='yaw',
+                                           component=component)
+                    self.add_constraint(constraint.yaw, component,
+                                        solve_for=constraint_param.constrained_parameter.uniquetwig,
+                                        constraint=constraint_param.constraint)
+                else:
+                    self.add_constraint(constraint.yaw, component,
+                                        constraint=self._default_label('yaw', context='constraint'))
+
+
                 if self.hierarchy.is_contact_binary(component):
                     # then we're in a contact binary and need to create pot<->requiv constraints
                     # NOTE: pot_min and pot_max are handled above at the envelope level
@@ -1282,39 +1317,6 @@ class Bundle(ParameterSet):
                         self.add_constraint(constraint.requiv_detached_max, component,
                                             constraint=self._default_label('requiv_max', context='constraint'))
 
-                    logger.debug('re-creating pitch constraint for {}'.format(component))
-                    # TODO: will this cause problems if the constraint has been flipped?
-                    # TODO: what if the user disabled/removed this constraint?
-                    if len(self.filter(context='constraint',
-                                       constraint_func='pitch',
-                                       component=component)):
-                        constraint_param = self.get_constraint(constraint_func='pitch',
-                                                               component=component)
-                        self.remove_constraint(constraint_func='pitch',
-                                               component=component)
-                        self.add_constraint(constraint.pitch, component,
-                                            solve_for=constraint_param.constrained_parameter.uniquetwig,
-                                            constraint=constraint_param.constraint)
-                    else:
-                        self.add_constraint(constraint.pitch, component,
-                                            constraint=self._default_label('pitch', context='constraint'))
-
-                    logger.debug('re-creating yaw constraint for {}'.format(component))
-                    # TODO: will this cause problems if the constraint has been flipped?
-                    # TODO: what if the user disabled/removed this constraint?
-                    if len(self.filter(context='constraint',
-                                       constraint_func='yaw',
-                                    component=component)):
-                        constraint_param = self.get_constraint(constraint_func='yaw',
-                                                               component=component)
-                        self.remove_constraint(constraint_func='yaw',
-                                               component=component)
-                        self.add_constraint(constraint.yaw, component,
-                                            solve_for=constraint_param.constrained_parameter.uniquetwig,
-                                            constraint=constraint_param.constraint)
-                    else:
-                        self.add_constraint(constraint.yaw, component,
-                                            constraint=self._default_label('yaw', context='constraint'))
 
         # if user_interactive_constraints:
             # conf.interactive_constraints_on()
@@ -1493,13 +1495,13 @@ class Bundle(ParameterSet):
                             return False,\
                                 'contact binaries must by circular, but ecc@{}!=0'.format(component)
 
-                        if self.get_value(qualifier='pitch', component=component, context='component', **kwargs) != 0.0:
+                        if self.get_value(qualifier='pitch', component=component, context='component', check_visible=False, **kwargs) != 0.0:
                             return False,\
-                                'contact binaries must be aligned, but pitch@{}!=0'.format(component)
+                                'contact binaries must be aligned, but pitch@{}!=0.  Try b.set_value(qualifier=\'pitch\', component=\'{}\' value=0.0, check_visible=False) to align.'.format(component, component)
 
-                        if self.get_value(qualifier='yaw', component=component, context='component', **kwargs) != 0.0:
+                        if self.get_value(qualifier='yaw', component=component, context='component', check_visible=False, **kwargs) != 0.0:
                             return False,\
-                                'contact binaries must be aligned, but yaw@{}!=0'.format(component)
+                                'contact binaries must be aligned, but yaw@{}!=0.  Try b.set_value(qualifier=\'yaw\', component=\'{}\', value=0.0, check_visible=False) to align.'.format(component, component)
 
                     # MUST NOT be overflowing at PERIASTRON (d=1-ecc, etheta=0)
 

phoebe/parameters/component.py
@@ -94,11 +94,11 @@ def star(component, **kwargs):
     params += [FloatParameter(qualifier='period', value=kwargs.get('period', 1.0), default_unit=u.d, limits=(0.0,None), description='Rotation period')]
     params += [FloatParameter(qualifier='freq', value=kwargs.get('freq', 2*np.pi), default_unit=u.rad/u.d, limits=(0.0,None), description='Rotation frequency')]
 
-    params += [FloatParameter(qualifier='pitch', value=kwargs.get('pitch', 0), default_unit=u.deg, description='Pitch of the stellar rotation axis wrt the orbital rotation axis')]
-    params += [FloatParameter(qualifier='yaw', value=kwargs.get('yaw', 0), default_unit=u.deg, description='Yaw of the stellar rotation axis wrt the orbital rotation axis')]
+    params += [FloatParameter(qualifier='pitch', visible_if='hierarchy.is_contact_binary:False', value=kwargs.get('pitch', 0), default_unit=u.deg, description='Pitch of the stellar rotation axis wrt the orbital rotation axis')]
+    params += [FloatParameter(qualifier='yaw', visible_if='hierarchy.is_contact_binary:False', value=kwargs.get('yaw', 0), default_unit=u.deg, description='Yaw of the stellar rotation axis wrt the orbital rotation axis')]
 
-    params += [FloatParameter(qualifier='incl', value=kwargs.get('incl', 90), default_unit=u.deg, description='Inclination of the stellar rotation axis')]
-    params += [FloatParameter(qualifier='long_an', value=kwargs.get('long_an', 0.0), default_unit=u.deg, description='Longitude of the ascending node (ie. equator) of the star')]
+    params += [FloatParameter(qualifier='incl', visible_if='hierarchy.is_contact_binary:False', value=kwargs.get('incl', 90), default_unit=u.deg, description='Inclination of the stellar rotation axis')]
+    params += [FloatParameter(qualifier='long_an', visible_if='hierarchy.is_contact_binary:False', value=kwargs.get('long_an', 0.0), default_unit=u.deg, description='Longitude of the ascending node (ie. equator) of the star')]
 
     # params += [FloatParameter(qualifier='vsini', value=kwargs.get('vsini', 1), default_unit=u.km/u.s, description='Projected maximum rotational velocity')]
 

phoebe/parameters/constraint.py
@@ -1602,9 +1602,9 @@ def pitch(b, component, solve_for=None, **kwargs):
     parentorbit = hier.get_parent_of(component)
     parentorbit_ps = _get_system_ps(b, parentorbit)
 
-    incl_comp = component_ps.get_parameter(qualifier='incl')
-    pitch_comp = component_ps.get_parameter(qualifier='pitch')
-    incl_orb = parentorbit_ps.get_parameter(qualifier='incl')
+    incl_comp = component_ps.get_parameter(qualifier='incl', check_visible=False)
+    pitch_comp = component_ps.get_parameter(qualifier='pitch', check_visible=False)
+    incl_orb = parentorbit_ps.get_parameter(qualifier='incl', check_visible=False)
 
     if solve_for in [None, incl_comp]:
         lhs = incl_comp
@@ -1648,9 +1648,9 @@ def yaw(b, component, solve_for=None, **kwargs):
     parentorbit = hier.get_parent_of(component)
     parentorbit_ps = _get_system_ps(b, parentorbit)
 
-    long_an_comp = component_ps.get_parameter(qualifier='long_an')
-    yaw_comp = component_ps.get_parameter(qualifier='yaw')
-    long_an_orb = parentorbit_ps.get_parameter(qualifier='long_an')
+    long_an_comp = component_ps.get_parameter(qualifier='long_an', check_visible=False)
+    yaw_comp = component_ps.get_parameter(qualifier='yaw', check_visible=False)
+    long_an_orb = parentorbit_ps.get_parameter(qualifier='long_an', check_visible=False)
 
     if solve_for in [None, long_an_comp]:
         lhs = long_an_comp

commit 5305132c0e9b86fb36781449bdda350186f4fd8e

fixed additional visibility issues related to pitch/yaw in contacts
related to #345
caused by ac92a7b1758791a4b74a7761e657f23a06538680

phoebe/parameters/parameters.py
@@ -4576,7 +4576,7 @@ class FloatParameter(Parameter):
         """
         expressions = []
         for uniqueid in self._in_constraints:
-            expressions.append(self._bundle.get_parameter(context='constraint', uniqueid=uniqueid))
+            expressions.append(self._bundle.get_parameter(context='constraint', uniqueid=uniqueid, check_visible=False))
         return expressions
 
     @property