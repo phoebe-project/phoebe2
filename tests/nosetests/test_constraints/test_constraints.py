afec2d845cd2305e54fd6bf34f1e64fb5e9cd0ef
Kyle Conroy <kyleconroy@gmail.com>
2019-08-13 10:31:29 -0400

    rewrite run_checks infrastructure to report multiple warnings/errors
    b.run_checks() now returns an object with a .passed attribute and multiple entries for any warning/errors.
    
    passed, msg = b.run_checks()
    is now replaced with
    report = b.run_checks()
    report.passed
    [item.message for item in report.items]

--- tests/nosetests/test_constraints/test_constraints.py
+++ tests/nosetests/test_constraints/test_constraints.py
@@ -15,14 +15,14 @@ def test_esinw_ecosw(verbose=False):
 
     assert(b.get_value('ecosw', context='component')==0.5)
     assert(b.get_value('esinw', context='component')==0.0)
-    assert(b.run_checks()[0])
+    assert(b.run_checks().passed)
 
     if verbose: print("b.flip_constraint('esinw', solve_for='ecc')")
     b.flip_constraint('esinw', solve_for='ecc')
 
     if verbose: print(b.filter(qualifier=['ecosw', 'esinw', 'ecc', 'per0']))
 
-    assert(b.run_checks()[0]==False)
+    assert(b.run_checks().passed==False)
     assert_raises(ValueError, b.run_compute)
 
     if verbose: print("b.set_value('per0', 10)")
@@ -30,7 +30,7 @@ def test_esinw_ecosw(verbose=False):
 
     if verbose: print(b.filter(qualifier=['ecosw', 'esinw', 'ecc', 'per0']))
 
-    assert(b.run_checks()[0])
+    assert(b.run_checks().passed)
 
     assert(b.get_value('ecc', context='component')==0)
 
@@ -39,21 +39,21 @@ def test_esinw_ecosw(verbose=False):
 
     if verbose: print(b.filter(qualifier=['ecosw', 'esinw', 'ecc', 'per0']))
 
-    assert(b.run_checks()[0]==False)
+    assert(b.run_checks().passed==False)
 
     if verbose: print("b.flip_constraint('ecosw', solve_for='per0')")
     b.flip_constraint('ecosw', solve_for='per0')
 
     if verbose: print(b.filter(qualifier=['ecosw', 'esinw', 'ecc', 'per0']))
 
-    assert(b.run_checks()[0])
+    assert(b.run_checks().passed)
     assert(b.get_value('ecc', context='component')==0.0)
     assert(b.get_value('per0', context='component')==0.0)
 
     if verbose: print("b.set_value('ecosw', 0.5)")
     b.set_value('ecosw', 0.5)
 
-    assert(b.run_checks()[0])
+    assert(b.run_checks().passed)
 
     if verbose: print(b.filter(qualifier=['ecosw', 'esinw', 'ecc', 'per0']))
 
@@ -73,7 +73,7 @@ def test_pot_filloutfactor(verbose=False):
     b.set_value('fillout_factor', 0.45)
     b.set_value('q', 0.25)
 
-    assert(b.run_checks()[0])
+    assert(b.run_checks().passed)
 
 if __name__ == '__main__':
     logger = phoebe.logger(clevel='WARNING')
