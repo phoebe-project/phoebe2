43bf8460e620dc679c64de1aa8cf7e504a948aae
Kyle Conroy <kyleconroy@gmail.com>
2019-08-14 14:35:11 -0400

    run_checks_compute is no longer considered an advanced parameter

--- phoebe/parameters/setting.py
+++ phoebe/parameters/setting.py
@@ -21,7 +21,7 @@ def settings(**kwargs):
     params += [DictParameter(qualifier='dict_filter', value=kwargs.get('dict_filter', {}), advanced=True, description='Filters to use when using dictionary access')]
     params += [BoolParameter(qualifier='dict_set_all', value=kwargs.get('dict_set_all', False), advanced=True, description='Whether to set all values for dictionary access that returns more than 1 result')]
 
-    params += [SelectParameter(qualifier='run_checks_compute', value=kwargs.get('run_checks_compute', '*'), choices=[''], description='Compute options to use when calling run_checks or within interactive checks.')]
+    params += [SelectParameter(qualifier='run_checks_compute', value=kwargs.get('run_checks_compute', '*'), choices=[''], advanced=False, description='Compute options to use when calling run_checks or within interactive checks.')]
 
     # params += [ChoiceParameter(qualifier='plotting_backend', value=kwargs.get('plotting_backend', 'mpl'), choices=['mpl', 'mpld3', 'mpl2bokeh', 'bokeh'] if conf.devel else ['mpl'], description='Default backend to use for plotting')]
 
