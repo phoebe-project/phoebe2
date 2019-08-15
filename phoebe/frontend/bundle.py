phoebe-project/phoebe2

commit 1fa5adfe1662f15fadbcd7a344fa9b22d88c1977

add ecc to overflow warning if non-zero

phoebe/frontend/bundle.py
@@ -2153,12 +2153,19 @@ class Bundle(ParameterSet):
 
                     else:
                         if requiv > requiv_max:
+                            params = [comp_ps.get_parameter(qualifier='requiv', **kwargs),
+                                     comp_ps.get_parameter(qualifier='requiv_max', **kwargs),
+                                     parent_ps.get_parameter(qualifier='sma', **kwargs)]
+
+                            if parent_ps.get_value(qualifier='ecc', **kwargs) > 0.0:
+                                params += [parent_ps.get_parameter(qualifier='ecc', **kwargs)]
+
+                            if len(b.filter(kind='envelope', context='component', **_skip_filter_checks)):
+                                params += [hier]
+
                             report.add_item(self,
                                             '{} is overflowing at periastron (requiv={}, requiv_max={}).  Use contact model if overflowing is desired.'.format(component, requiv, requiv_max),
-                                            [comp_ps.get_parameter(qualifier='requiv', **kwargs),
-                                             comp_ps.get_parameter(qualifier='requiv_max', **kwargs),
-                                             parent_ps.get_parameter(qualifier='sma', **kwargs),
-                                             hier],
+                                            params,
                                             True)
 
             else:

commit 042602b256348b3d97a3a1151f585f3b89680fe5

fixed typo in run_checks
introduced in 1fa5adfe1662f15fadbcd7a344fa9b22d88c1977

phoebe/frontend/bundle.py
@@ -2160,7 +2160,7 @@ class Bundle(ParameterSet):
                             if parent_ps.get_value(qualifier='ecc', **kwargs) > 0.0:
                                 params += [parent_ps.get_parameter(qualifier='ecc', **kwargs)]
 
-                            if len(b.filter(kind='envelope', context='component', **_skip_filter_checks)):
+                            if len(self.filter(kind='envelope', context='component', **_skip_filter_checks)):
                                 params += [hier]
 
                             report.add_item(self,