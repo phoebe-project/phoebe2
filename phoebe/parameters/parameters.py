81d4cc0bd4effab7ebadbcd51edda5c0458eb754
Kyle Conroy <kyleconroy@gmail.com>
2019-09-25 14:33:37 -0400

    api docs cleanup

--- phoebe/parameters/parameters.py
+++ phoebe/parameters/parameters.py
@@ -1936,27 +1936,27 @@ class ParameterSet(object):
 
         Arguments
         ------------
-        `fname` (string or file object): passed to np.savetxt.
+        * `fname` (string or file object): passed to np.savetxt.
             If the filename ends in .gz, the file is automatically saved in
             compressed gzip format. loadtxt understands gzipped files
             transparently.
-        `delimiter` (string, optional, default=' '): passed to np.savetxt.
+        * `delimiter` (string, optional, default=' '): passed to np.savetxt.
             String or character separating columns.
-        `header` (string, optional): The header will automatically be appended
+        * `header` (string, optional): The header will automatically be appended
             with the twigs of the parameters making up the columns and then
             passed to np.savetxt.
             String that will be written at the beginning of the file.
-        `footer` (string, optional): passed to np.savetxt.
+        * `footer` (string, optional): passed to np.savetxt.
             String that will be written at the end of the file.
-        `comments` (string, optional, default='#'): passed to np.savetxt.
+        * `comments` (string, optional, default='#'): passed to np.savetxt.
             String that will be prepended to the `header` and `footer` strings,
             to mark them as comments.
-        `encoding` (None or string, optional, default=None): passed to np.savetxt.
+        * `encoding` (None or string, optional, default=None): passed to np.savetxt.
             Encoding used to encode the outputfile. Does not apply to output
             streams. If the encoding is something other than ‘bytes’ or ‘latin1’
             you will not be able to load the file in NumPy versions < 1.14.
             Default is ‘latin1’.
-        `**kwargs`: all additional keyword arguments will be sent to
+        * `**kwargs`: all additional keyword arguments will be sent to
             <phoebe.parameters.ParameterSet.filter>.  The filter must result
             in all <phoebe.parameters.Parameter.FloatArrayParameter> objects
             with the same length, otherwise an error will be raised.
