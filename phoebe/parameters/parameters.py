53f26f386738078ac81c7fb368e01250519ca5ee
Kyle Conroy <kyleconroy@gmail.com>
2019-12-11 14:52:11 -0500

    add additional information in parameter not found error

--- phoebe/parameters/parameters.py
+++ phoebe/parameters/parameters.py
@@ -9067,7 +9067,7 @@ class ConstraintParameter(Parameter):
                 logger.debug("ConstraintParameter.get_parameter: reverting to filtering on bundle, could not {} find in {}".format(kwargs, vars.twigs))
                 kwargs['context'] = [c for c in self._bundle.contexts if c!='constraint']
                 return self._bundle.get_parameter(**kwargs)
-            raise KeyError("no result found")
+            raise KeyError("no result found for {}".format(kwargs))
 
     @property
     def default_unit(self):
