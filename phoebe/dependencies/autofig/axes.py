dfc07427f74c7f6590d1d9dfa892115547c8c4c6
Kyle Conroy <kyleconroy@gmail.com>
2019-12-12 09:52:28 -0500

    update autofig to ignore equal_aspect for 3d axes

--- phoebe/dependencies/autofig/axes.py
+++ phoebe/dependencies/autofig/axes.py
@@ -1224,7 +1224,11 @@ class Axes(object):
             aspect = 'auto'
             adjustable = 'box'
 
-        ax.set_aspect(aspect=aspect, adjustable=adjustable)
+        axes_3d = isinstance(ax, Axes3D)
+        if not axes_3d:
+            ax.set_aspect(aspect=aspect, adjustable=adjustable)
+        elif self.equal_aspect:
+            print("WARNING: equal_aspect not supported for 3d axes, ignoring")
 
         # return_calls = []
         self._colorcycler.clear_tmp()
@@ -1245,7 +1249,6 @@ class Axes(object):
         if draw_title and self.title is not None:
             ax.set_title(self.title)
 
-        axes_3d = isinstance(ax, Axes3D)
 
         ax.set_xlabel(self.x.label_with_units)
         ax.set_ylabel(self.y.label_with_units)
