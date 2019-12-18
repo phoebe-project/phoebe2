9c92f49f939b7763f7f38cb1a81b164a32cb3673
Kyle Conroy <kyleconroy@gmail.com>
2019-12-18 13:13:17 -0500

    fix broken links in API docs for set/get_download_passband_defaults

--- phoebe/__init__.py
+++ phoebe/__init__.py
@@ -651,7 +651,7 @@ def devel_off():
 
 def set_download_passband_defaults(**kwargs):
     """
-    Set default options to use for <phoebe.passbands.atmospheres.download_passband>.
+    Set default options to use for <phoebe.atmospheres.passbands.download_passband>.
 
     These can also be set at import time via the following environment variables:
     * PHOEBE_DOWNLOAD_PASSBAND_DEFAULTS_CONTENT (defaults to 'all')
@@ -659,9 +659,9 @@ def set_download_passband_defaults(**kwargs):
 
     See also:
     * <phoebe.get_download_passband_defaults>
-    * <phoebe.passbands.atmospheres.download_passband>
-    * <phoebe.passbands.atmospheres.update_passband>
-    * <phoebe.passbands.atmospheres.get_passband>
+    * <phoebe.atmospheres.passbands.download_passband>
+    * <phoebe.atmospheres.passbands.update_passband>
+    * <phoebe.atmospheres.passbands.get_passband>
 
     Arguments
     ------------
@@ -675,13 +675,13 @@ def set_download_passband_defaults(**kwargs):
 
 def get_download_passband_defaults():
     """
-    Access default options to use for <phoebe.passbands.atmospheres.download_passband>.
+    Access default options to use for <phoebe.atmospheres.passbands.download_passband>.
 
     See also:
     * <phoebe.set_download_passband_defaults>
-    * <phoebe.passbands.atmospheres.download_passband>
-    * <phoebe.passbands.atmospheres.update_passband>
-    * <phoebe.passbands.atmospheres.get_passband>
+    * <phoebe.atmospheres.passbands.download_passband>
+    * <phoebe.atmospheres.passbands.update_passband>
+    * <phoebe.atmospheres.passbands.get_passband>
 
     Returns
     ---------
