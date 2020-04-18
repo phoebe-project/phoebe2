691d0c63a056cd8faa97dab060203e9e015c67df
Kyle Conroy <kyleconroy@gmail.com>
2020-04-18 13:56:36 -0400

    remove solver figures and improve distribution figures
    * also no longer include distribution figures in auto_add_figure

--- phoebe/parameters/figure/distribution.py
+++ phoebe/parameters/figure/distribution.py
@@ -7,11 +7,12 @@ from phoebe.parameters import *
 def distribution_collection(b, **kwargs):
     params = []
 
-    params += [SelectParameter(qualifier='distributions', value=kwargs.get('distributions', []), choices=[''], description='Distributions to include in the plot')]
-    params += [ChoiceParameter(qualifier='combine', value=kwargs.get('combine', 'and'), choices=['and', 'or', 'first'], description='How to combine multiple distributions for the same parameter.')]
-    params += [BoolParameter(qualifier='include_constrained', value=kwargs.get('include_constrained', False), description='Whether to include constrained parameters')]
-    params += [BoolParameter(qualifier='to_uniforms', value=kwargs.get('to_uniforms', False), description='Wheter to convert all distribution to univariate uniforms')]
-    params += [IntParameter(visible_if='to_uniforms:True', qualifier='to_uniforms_sigma', value=kwargs.get('to_uniforms_sigma', 1), description='Sigma to adopt for non-uniform distributions')]
-    params += [BoolParameter(visible_if='to_uniforms:False', qualifier='to_univariates', value=kwargs.get('to_univariates', False), description='Whether to convert any multivariate distribution to univaritates before adding to the collection')]
+    params += [ChoiceParameter(qualifier='distribution_set', value=kwargs.get('distribution_set', 'manual'), choices=['manual'], description='Set of distributions to include in the plot.  Use \'manual\' to explicitly select distributions and combining logic')]
+    params += [SelectParameter(visible_if='distribution_set:manual', qualifier='distributions', value=kwargs.get('distributions', []), choices=[''], description='Distributions to include in the plot')]
+    params += [ChoiceParameter(visible_if='distribution_set:manual', qualifier='combine', value=kwargs.get('combine', 'and'), choices=['and', 'or', 'first'], description='How to combine multiple distributions for the same parameter.')]
+    params += [BoolParameter(visible_if='distribution_set:manual', qualifier='include_constrained', value=kwargs.get('include_constrained', False), description='Whether to include constrained parameters')]
+    params += [BoolParameter(visible_if='distribution_set:manual', qualifier='to_uniforms', value=kwargs.get('to_uniforms', False), description='Wheter to convert all distribution to univariate uniforms')]
+    params += [IntParameter(visible_if='distribution_set:manual,to_uniforms:True', qualifier='to_uniforms_sigma', value=kwargs.get('to_uniforms_sigma', 1), description='Sigma to adopt for non-uniform distributions')]
+    params += [BoolParameter(visible_if='distribution_set:manual,to_uniforms:False', qualifier='to_univariates', value=kwargs.get('to_univariates', False), description='Whether to convert any multivariate distribution to univaritates before adding to the collection')]
 
     return ParameterSet(params)
