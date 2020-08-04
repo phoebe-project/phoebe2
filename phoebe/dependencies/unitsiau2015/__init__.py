phoebe-project/phoebe2

commit 6b5da737eaf77d034aa0e7f5cb7d90dd1a925f0b

estimator.bls: duration in days (not phase)
* and handle any necessary unit conversions
* FloatParameter.get_value/quantity now accepts unit='solar'/'SI'
* duration defaults to [0.1] (days)

phoebe/dependencies/unitsiau2015/__init__.py
@@ -137,6 +137,16 @@ _physical_types_to_solar = {'length': 'solRad',
                             'angular speed': 'rad/d',
                             'dimensionless': ''}
 
+_physical_types_to_si = {'length': 'm',
+                            'mass': 'kg',
+                            'temperature': 'K',
+                            'power': 'W',
+                            'time': 's',
+                            'speed': 'm/s',
+                            'angle': 'rad',
+                            'angular speed': 'rad/s',
+                            'dimensionless': ''}
+
 def _get_physical_type(object):
 
     if hasattr(object, 'physical_type'):

phoebe/parameters/parameters.py
@@ -3036,7 +3036,9 @@ class ParameterSet(object):
         * `twig` (string, optional, default=None): twig to be used to access
             the Parameter.  See <phoebe.parameters.ParameterSet.get_parameter>.
         * `unit` (string or unit, optional, default=None): unit to convert the
-            quantity.  If not provided or None, will use the default unit.  See
+            quantity.  If not provided or None, will use the default unit.
+            'SI' or 'solar' are also allowed values which will then be determined
+            based on the physical type of the default unit.  See
             <phoebe.parameters.ParameterSet.get_default_unit>.
         * `default` (quantity, optional, default=None): value to return if
             no results are returned by <phoebe.parameters.ParameterSet.get_parameter>
@@ -3122,7 +3124,9 @@ class ParameterSet(object):
         * `twig` (string, optional, default=None): twig to be used to access
             the Parameter.  See <phoebe.parameters.ParameterSet.get_parameter>.
         * `unit` (string or unit, optional, default=None): unit to convert the
-            value.  If not provided or None, will use the default unit.  See
+            value.  If not provided or None, will use the default unit.
+            'SI' or 'solar' are also allowed values which will then be determined
+            based on the physical type of the default unit. See
             <phoebe.parameters.ParameterSet.get_default_unit>. `unit` will
             be ignored for Parameters that do not store quantities.
         * `draw_from` (string, optional, default=None): distribution-tag to
@@ -9089,9 +9093,18 @@ class FloatParameter(Parameter):
 
         # TODO: check to see if this is still necessary
         if isinstance(unit, str):
-            # we need to do this to make sure we get PHOEBE's version of
-            # the unit instead of astropy's
-            unit = u.Unit(unit)
+
+                                    v = np.float64(u.to_solar(quantity).value)
+                                else:
+                                    v = np.float64(quantity.si.value)
+            if unit == 'solar':
+                unit = u._physical_types_to_solar.get(u._get_physical_type(self.default_unit))
+            elif unit in ['si', 'SI']:
+                unit = u._physical_types_to_si.get(u._get_physical_type(self.default_unit))
+            else:
+                # we need to do this to make sure we get PHOEBE's version of
+                # the unit instead of astropy's
+                unit = u.Unit(unit)
 
         # TODO: catch astropy units and convert to PHOEBE's?
 

phoebe/parameters/solver/estimator.py
@@ -112,7 +112,7 @@ def lc_periodogram(**kwargs):
     params += [FloatArrayParameter(visible_if='sample_mode:manual', qualifier='sample_periods', value=kwargs.get('sample_periods', []), default_unit=u.d, description='Manual period grid for sampling the periodogram.  Note: if algorithm is \'ls\', these will be converted to frequencies and will be more efficient if sampled evenly in frequency space (consider using phoebe.invspace instead of phoebe.linspace).')]
 
     # BLS-specific options
-    params += [FloatArrayParameter(visible_if='algorithm:bls', qualifier='duration', value=kwargs.get('duration', _geomspace(0.01,0.3,5) if StrictVersion(np.__version__) >= StrictVersion("1.13") else _linspace(0.01, 0.3, 5)), default_unit=u.dimensionless_unscaled, description='The set of durations (in phase-space) that will be considered.  See https://docs.astropy.org/en/stable/api/astropy.timeseries.BoxLeastSquares.html')]
+    params += [FloatArrayParameter(visible_if='algorithm:bls', qualifier='duration', value=kwargs.get('duration', [0.1]), default_unit=u.d, description='The set of durations (in time-space) that will be considered.  See https://docs.astropy.org/en/stable/api/astropy.timeseries.BoxLeastSquares.html')]
     params += [ChoiceParameter(visible_if='algorithm:bls', qualifier='objective', value=kwargs.get('objective', 'likelihood'), choices=['likelihood', 'snr'], description='Objective to use for the periodogram.  See https://docs.astropy.org/en/stable/timeseries/bls.html#objectives')]
     params += [IntParameter(visible_if='sample_mode:auto,algorithm:bls', qualifier='minimum_n_cycles', value=kwargs.get('minimum_n_cycles', 3), advanced=True, limits=(1,None), description='Minimum number of cycles/eclipses.  This is passed directly to autopower as \'minimum_n_transit\'. See https://docs.astropy.org/en/stable/api/astropy.timeseries.BoxLeastSquares.html#astropy.timeseries.BoxLeastSquares.autopower')]
 

phoebe/solverbackends/solverbackends.py
@@ -481,7 +481,7 @@ class BaseSolverBackend(object):
         solver_ps = b.get_solver(solver=solver, **_skip_filter_checks)
         for param in solver_ps.to_list():
             # we need to make sure SelectParameters are expanded correctly if sent through kwargs
-            kwargs[param.qualifier] = param.get_value(expand=True, **{param.qualifier: kwargs.get(param.qualifier, None)})
+            kwargs[param.qualifier] = param.get_value(expand=True, unit='solar', **{param.qualifier: kwargs.get(param.qualifier, None)})
 
         packet, solution_ps = self._get_packet_and_solution(b, solver, **kwargs)
 
@@ -903,12 +903,11 @@ class _PeriodogramBaseBackend(BaseSolverBackend):
 
         sample_mode = kwargs.get('sample_mode')
 
-        # TODO: options for duration to autopower/power (not sure what it does from the astropy docs)
         if algorithm == 'bls':
             model = _BoxLeastSquares(times, y, dy=sigmas)
-            # TODO: expose duration to solver options
             # https://docs.astropy.org/en/stable/api/astropy.timeseries.BoxLeastSquares.html#astropy.timeseries.BoxLeastSquares.autoperiod
             # https://docs.astropy.org/en/stable/api/astropy.timeseries.BoxLeastSquares.html#astropy.timeseries.BoxLeastSquares.period
+            # NOTE: duration will be in days (solar units)
             power_kwargs = {'duration': kwargs.get('duration'), 'objective': kwargs.get('objective')}
             autopower_kwargs = _deepcopy(power_kwargs)
             autopower_kwargs['minimum_n_transit'] = kwargs.get('minimum_n_cycles')

commit 3366ec458ec2be334945a657460035875d21c944

update unitsiau2015
* fixes issue when accessing units=solar from within periodogram introduced in 6b5da737eaf77d034aa0e7f5cb7d90dd1a925f0b

phoebe/dependencies/unitsiau2015/__init__.py
@@ -184,6 +184,9 @@ def to_solar(object):
 
 u.can_convert_to_solar = can_convert_to_solar
 u.to_solar = to_solar
+u._get_physical_type = _get_physical_type
+u._physical_types_to_si  = _physical_types_to_si
+u._physical_types_to_solar = _physical_types_to_solar
 
 
 