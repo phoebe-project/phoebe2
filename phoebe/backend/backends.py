phoebe-project/phoebe2

commit 750e2aa7683b49124d8eec52e378e527744e503f

implement period_anom for apsidal motion (#382)
* ellc/jktebop: handle time-derivatives and translate from t0@system to respective t0@component before passing
* jktebop dpdt/dperdt and ellc dpdt: raise warning in run_checks_compute that backend does not support the respective time-derivative
* run_checks_system: add warning when t0 not near t0_supconj (time dependent systems only)
* legacy wrapper: cleanup bundle calls
* handle spot co-rotation for dperdt!=0.0 case
* improve visible_if support for excluding multiple tags
* fix b.to_phase,to_time for dpdt != 0
* support for passing period and t0 to plotting via figures

phoebe/backend/backends.py
@@ -2349,6 +2349,7 @@ class EllcBackend(BaseBackendByDataset):
         logger.debug("rank:{}/{} EllcBackend._worker_setup".format(mpi.myrank, mpi.nprocs))
 
         computeparams = b.get_compute(compute, force_ps=True, **_skip_filter_checks)
+        t0_system = b.get_value(qualifier='t0', context='system', unit=u.d, **_skip_filter_checks)
 
         hier = b.get_hierarchy()
 
@@ -2368,26 +2369,42 @@ class EllcBackend(BaseBackendByDataset):
 
         exact_grav = computeparams.get_value(qualifier='exact_grav', exact_grav=kwargs.get('grav', None), **_skip_filter_checks)
 
-        comp_ps = b.filter(context='component')
+        comp_ps = b.filter(context='component', **_skip_filter_checks)
 
         a = comp_ps.get_value(qualifier='sma', component=orbitref, unit=u.solRad, **_skip_filter_checks)
         radius_1 = comp_ps.get_value(qualifier='requiv', component=starrefs[0], unit=u.solRad, **_skip_filter_checks) / a
         radius_2 = comp_ps.get_value(qualifier='requiv', component=starrefs[1], unit=u.solRad, **_skip_filter_checks) / a
 
-        period = comp_ps.get_value(qualifier='period', component=orbitref, unit=u.d, **_skip_filter_checks)
+        period_anom = comp_ps.get_value(qualifier='period_anom', component=orbitref, unit=u.d, **_skip_filter_checks)
         q = comp_ps.get_value(qualifier='q', component=orbitref, **_skip_filter_checks)
 
-        # TODO: there seems to be a convention flip between primary and secondary star in ellc... maybe we can just address via t_zero?
         t_zero = comp_ps.get_value(qualifier='t0_supconj', component=orbitref, unit=u.d, **_skip_filter_checks)
 
         incl = comp_ps.get_value(qualifier='incl', component=orbitref, unit=u.deg, **_skip_filter_checks)
         didt = 0.0
         # didt = b.get_value(qualifier='dincldt', component=orbitref, context='component', unit=u.deg/u.d) * period
+        # incl += didt * (t_zero - t0_system)
 
         ecc = comp_ps.get_value(qualifier='ecc', component=orbitref, **_skip_filter_checks)
         w = comp_ps.get_value(qualifier='per0', component=orbitref, unit=u.rad, **_skip_filter_checks)
 
-        domdt = comp_ps.get_value(qualifier='dperdt', component=orbitref, unit=u.deg/u.d, **_skip_filter_checks) * period
+        # need to correct w (per0) to be at t_zero (t0_supconj) instead of t0@system as defined in PHOEBE
+        logger.debug("per0(t0@system): {}".format(w))
+        domdt_rad = comp_ps.get_value(qualifier='dperdt', component=orbitref, unit=u.rad/u.d, **_skip_filter_checks)
+        w += domdt_rad * (t_zero - t0_system)
+        logger.debug("per0(t0_supconj): {}".format(w))
+
+        # NOTE: domdt is listed in ellc as deg/anomalistic period, but as deg/sidereal period in the fortran source (which agrees with comparisons)
+        # NOTE: this does NOT need to be iterative, because the original dperdt is in deg/d and independent of period
+        logger.debug("dperdt (rad/d): ", domdt_rad)
+        period_sid = comp_ps.get_value(qualifier='period', component=orbitref, unit=u.d, **_skip_filter_checks)
+        # NOTE: period_sidereal does not need to be corrected from t0@system -> t0_supconj because ellc does not support dpdt
+        logger.debug("period_sidereal(t0@system,t0_ref,dpdt=0): ", period_sid)
+        domdt = comp_ps.get_value(qualifier='dperdt', component=orbitref, unit=u.deg/u.d, **_skip_filter_checks) * period_sid
+        logger.debug("dperdt (deg/d * period_sidereal): ", domdt)
+
+        f_c = np.sqrt(ecc) * np.cos(w)
+        f_s = np.sqrt(ecc) * np.sin(w)
 
         gdc_1 = comp_ps.get_value(qualifier='gravb_bol', component=starrefs[0], **_skip_filter_checks)
         gdc_2 = comp_ps.get_value(qualifier='gravb_bol', component=starrefs[1], **_skip_filter_checks)
@@ -2403,6 +2420,8 @@ class EllcBackend(BaseBackendByDataset):
             # Parameters of the spots on star 1. For each spot the parameters, in order,
             # are longitude, latitude, size and brightness factor. All three angles are
             # in degrees.
+
+            # TODO: do we need to correct these from t0_system to t_zero (if rotfac!=1)?
 
             spots_1 = []
             spots_2 = []
@@ -2436,8 +2455,8 @@ class EllcBackend(BaseBackendByDataset):
         #     A_g/2, where A_g is the geometric albedo.
         irrad_method = computeparams.get_value(qualifier='irrad_method', irrad_method=kwargs.get('irrad_method', None), **_skip_filter_checks)
         if irrad_method == 'lambert':
-            heat_1 = b.get_value(qualifier='irrad_frac_refl_bol', component=starrefs[0], context='component') / 2.
-            heat_2 = b.get_value(qualifier='irrad_frac_refl_bol', component=starrefs[1], context='component') / 2.
+            heat_1 = b.get_value(qualifier='irrad_frac_refl_bol', component=starrefs[0], context='component', **_skip_filter_checks) / 2.
+            heat_2 = b.get_value(qualifier='irrad_frac_refl_bol', component=starrefs[1], context='component', **_skip_filter_checks) / 2.
             # let's save ourselves, and also allow for flux-weighted RVs
             if heat_1 == 0 and heat_2 == 0:
                 heat_1 = None
@@ -2447,9 +2466,6 @@ class EllcBackend(BaseBackendByDataset):
             heat_2 = None
         else:
             raise NotImplementedError("irrad_method='{}' not supported".format(irrad_method))
-
-        f_c = np.sqrt(ecc) * np.cos(w)
-        f_s = np.sqrt(ecc) * np.sin(w)
 
         # lambda_1/2 : {None, float},  optional
          # Sky-projected angle between orbital and rotation axes, star 1/2 [degrees]
@@ -2466,7 +2482,7 @@ class EllcBackend(BaseBackendByDataset):
                     radius_1=radius_1, radius_2=radius_2,
                     incl=incl,
                     t_zero=t_zero,
-                    period=period,
+                    period_anom=period_anom,
                     q=q,
                     a=a,
                     f_c=f_c, f_s=f_s,
@@ -2502,7 +2518,7 @@ class EllcBackend(BaseBackendByDataset):
         incl = kwargs.get('incl')
 
         t_zero = kwargs.get('t_zero')
-        period = kwargs.get('period')
+        period_anom = kwargs.get('period_anom')
         a = kwargs.get('a')
         q = kwargs.get('q')
 
@@ -2540,7 +2556,7 @@ class EllcBackend(BaseBackendByDataset):
         ldc_2 = ds_ps.get_value(qualifier='ld_coeffs', component=starrefs[1], **_skip_filter_checks)
 
         pblums = kwargs.get('pblums').get(info['dataset'])
-        sbratio = (pblums.get(starrefs[1])/b.get_value(qualifier='requiv', component=starrefs[1], context='component', unit=u.solRad)**2)/(pblums.get(starrefs[0])/b.get_value(qualifier='requiv', component=starrefs[0], context='component', unit=u.solRad)**2)
+        sbratio = (pblums.get(starrefs[1])/b.get_value(qualifier='requiv', component=starrefs[1], context='component', unit=u.solRad, **_skip_filter_checks)**2)/(pblums.get(starrefs[0])/b.get_value(qualifier='requiv', component=starrefs[0], context='component', unit=u.solRad, **_skip_filter_checks)**2)
 
         if info['kind'] == 'lc':
             # third light handled by run_compute
@@ -2560,7 +2576,7 @@ class EllcBackend(BaseBackendByDataset):
                              incl=incl,
                              light_3=light_3,
                              t_zero=t_zero,
-                             period=period,
+                             period=period_anom,
                              a=a,
                              q=q,
                              f_c=f_c, f_s=f_s,

phoebe/backend/universe.py
@@ -3092,9 +3092,14 @@ class Spot(Feature):
         if len(b.hierarchy.get_stars())>=2:
             star_ps = b.get_component(component=feature_ps.component, **_skip_filter_checks)
             orbit_ps = b.get_component(component=b.hierarchy.get_parent_of(feature_ps.component), **_skip_filter_checks)
-            syncpar = star_ps.get_value(qualifier='syncpar', **_skip_filter_checks)
-            period = orbit_ps.get_value(qualifier='period', **_skip_filter_checks)
-            dlongdt = (syncpar - 1) / period * 2 * np.pi
+            # TODO: how should this handle dpdt?
+
+            # we won't use syncpar directly because that is defined wrt sidereal period and we want to make sure
+            # this translated to roche longitude correctly.  In the non-apsidal motion case
+            # syncpar = period_anom_orb / period_star
+            period_anom_orb = orbit_ps.get_value(qualifier='period_anom', unit=u.d, **_skip_filter_checks)
+            period_star = star_ps.get_value(qualifier='period', unit=u.d, **_skip_filter_checks)
+            dlongdt = 2*pi * (period_anom_orb/period_star - 1) / period_anom_orb
         else:
             star_ps = b.get_component(component=feature_ps.component, **_skip_filter_checks)
             dlongdt = star_ps.get_value(qualifier='freq', unit=u.rad/u.d, **_skip_filter_checks)

phoebe/constraints/builtin.py
@@ -12,31 +12,54 @@ logger.addHandler(logging.NullHandler())
 # expose these at top-level so they're available to constraints
 from numpy import sin, cos, tan, arcsin, arccos, arctan, arctan2, sqrt, log10
 
-def phases_to_times(phase, period, dpdt, t0, t0_supconj, t0_perpass, t0_ref):
-    if t0 == 't0_supconj':
+def phases_to_times(phase, period_choice, period, period_anom, dpdt, t0_choice, t0_supconj, t0_perpass, t0_ref):
+    # print("*** builtin.phases_to_times period_choice: {}, t0_choice: {}".format(period_choice, t0_choice))
+    if period_choice == 'period':
+        period = period
+    elif period_choice == 'period_anom':
+        period = period_anom
+    else:
+        raise NotImplementedError()
+
+    if t0_choice == 't0_supconj':
         t0 = t0_supconj
-    elif t0 == 't0_perpass':
+    elif t0_choice == 't0_perpass':
         t0 = t0_perpass
-    elif t0 == 't0_ref':
+    elif t0_choice == 't0_ref':
         t0 = t0_ref
+    else:
+        raise NotImplementedError()
+
+    # print("*** builtin.phases_to_times period: {}, t0: {}".format(period, t0))
 
     if isinstance(phase, list):
         phase = np.asarray(phase)
 
     if dpdt != 0:
+        # NOTE: this seems to be incorrect and giving ridiculous answers
         time = t0 + 1./dpdt*(np.exp(dpdt*(phase))-period)
     else:
         time = t0 + (phase)*period
 
     return time
 
-def times_to_phases(time, period, dpdt, t0, t0_supconj, t0_perpass, t0_ref):
-    if t0 == 't0_supconj':
+def times_to_phases(time, period_choice, period, period_anom, dpdt, t0_choice, t0_supconj, t0_perpass, t0_ref):
+    # print("*** builtin.times_to_phases", period_choice, t0_choice)
+    if period_choice == 'period':
+        period = period
+    elif period_choice == 'period_anom':
+        period = period_anom
+    else:
+        raise NotImplementedError()
+
+    if t0_choice == 't0_supconj':
         t0 = t0_supconj
-    elif t0 == 't0_perpass':
+    elif t0_choice == 't0_perpass':
         t0 = t0_perpass
-    elif t0 == 't0_ref':
+    elif t0_choice == 't0_ref':
         t0 = t0_ref
+    else:
+        raise NotImplementedError()
 
     if isinstance(time, list):
         time = np.asarray(time)

phoebe/dynamics/keplerian.py
@@ -75,12 +75,13 @@ def dynamics_from_bundle(b, times, compute=None, return_euler=False, **kwargs):
 
         #print "***", component, ancestororbits
 
-        periods.append([s.get_value(qualifier='period', unit=u.d, component=orbit, **_skip_filter_checks) for orbit in ancestororbits])
+        periods.append([s.get_value(qualifier='period_anom', unit=u.d, component=orbit, **_skip_filter_checks) for orbit in ancestororbits])
         eccs.append([s.get_value(qualifier='ecc', component=orbit, **_skip_filter_checks) for orbit in ancestororbits])
         t0_perpasses.append([s.get_value(qualifier='t0_perpass', unit=u.d, component=orbit, **_skip_filter_checks) for orbit in ancestororbits])
         per0s.append([s.get_value(qualifier='per0', unit=u.rad, component=orbit, **_skip_filter_checks) for orbit in ancestororbits])
         long_ans.append([s.get_value(qualifier='long_an', unit=u.rad, component=orbit, **_skip_filter_checks) for orbit in ancestororbits])
         incls.append([s.get_value(qualifier='incl', unit=u.rad, component=orbit, **_skip_filter_checks) for orbit in ancestororbits])
+        # TODO: do we need to convert from anomalistic to sidereal?
         dpdts.append([s.get_value(qualifier='dpdt', unit=u.d/u.d, component=orbit, **_skip_filter_checks) for orbit in ancestororbits])
         if conf.devel:
             try:
@@ -141,7 +142,7 @@ def dynamics(times, periods, eccs, smas, t0_perpasses, per0s, long_ans, incls,
     Args:
         times: (iterable) times at which to compute positions and
             velocities for each star
-        periods: (iterable) period of the parent orbit for each star
+        periods: (iterable) anomalistic period of the parent orbit for each star
             [days]
         eccs: (iterable) eccentricity of the parent orbit for each star
         smas: (iterable) semi-major axis of the parent orbit for each

commit 0ee9341b55d595b5dae60ca90698ebac94f3e33b

ellc: fix passing period_anom as period
fixed bug introduced in 750e2aa7683b49124d8eec52e378e527744e503f

phoebe/backend/backends.py
@@ -2644,7 +2644,7 @@ class EllcBackend(BaseBackendByDataset):
                              sbratio=sbratio,
                              incl=incl,
                              t_zero=t_zero,
-                             period=period,
+                             period=period_anom,
                              a=a,
                              q=q,
                              f_c=f_c, f_s=f_s,