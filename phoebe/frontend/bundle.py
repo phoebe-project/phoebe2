phoebe-project/phoebe2

commit 533bcdf4867d1f289ce7c70d12d8c69021629faa

fix constrained parameters in dataset context

phoebe/frontend/bundle.py
@@ -8838,7 +8838,7 @@ class Bundle(ParameterSet):
             if ld_mode == 'interp':
                 logger.debug("skipping computing ld_coeffs{} for {}@{} because ld_mode{}='interp'".format(bol_suffix, ldcs_param.dataset, ldcs_param.component, bol_suffix))
             elif ld_mode == 'manual':
-                ld_coeffs_manual = self.get_value(qualifier='ld_coeffs{}'.format(bol_suffix), dataset=ldcs_param.dataset, component=ldcs_param.component, **_skip_filter_checks)
+                ld_coeffs_manual = self.get_value(qualifier='ld_coeffs{}'.format(bol_suffix), dataset=ldcs_param.dataset, component=ldcs_param.component, context='dataset', **_skip_filter_checks)
                 ld_coeffs_ret["{}@{}@{}".format('ld_coeffs{}'.format(bol_suffix), ldcs_param.component, 'component' if is_bol else ldcs_param.dataset)] = ld_coeffs_manual
                 continue
             elif ld_mode == 'lookup':
@@ -8883,7 +8883,7 @@ class Bundle(ParameterSet):
 
                 ld_coeffs_ret["ld_coeffs{}@{}@{}".format(bol_suffix, ldcs_param.component, 'component' if is_bol else ldcs_param.dataset)] = ld_coeffs
                 if set_value:
-                    self.set_value(qualifier='ld_coeffs{}'.format(bol_suffix), component=ldcs_param.component, dataset=ldcs_param.dataset, check_visible=False, value=ld_coeffs)
+                    self.set_value(qualifier='ld_coeffs{}'.format(bol_suffix), component=ldcs_param.component, dataset=ldcs_param.dataset, context='dataset', check_visible=False, value=ld_coeffs)
             else:
                 raise NotImplementedError("compute_ld_coeffs not implemented for ld_mode{}='{}'".format(bol_suffix, ld_mode))
 

phoebe/backend/universe.py
@@ -1276,7 +1276,7 @@ class Star(Body):
         ld_func_override = kwargs.pop('ld_func', None)
         ld_func = {ds: b.get_value(qualifier='ld_func', dataset=ds, component=component, ld_func=ld_func_override, **_skip_filter_checks) for ds in datasets_intens}
         ld_coeffs_override = kwargs.pop('ld_coeffs', None)
-        ld_coeffs = {ds: b.get_value(qualifier='ld_coeffs', dataset=ds, component=component, ld_coeffs=ld_coeffs_override, **_skip_filter_checks) for ds in datasets_intens}
+        ld_coeffs = {ds: b.get_value(qualifier='ld_coeffs', dataset=ds, component=component, context='dataset', ld_coeffs=ld_coeffs_override, **_skip_filter_checks) for ds in datasets_intens}
         ld_coeffs_source_override = kwargs.pop('ld_coeffs_source', None)
         ld_coeffs_source = {ds: b.get_value(qualifier='ld_coeffs_source', dataset=ds, component=component, ld_coeffs_source=ld_coeffs_source_override, **_skip_filter_checks) for ds in datasets_intens}
         ld_func_bol_override = kwargs.pop('ld_func_bol', None)

phoebe/parameters/parameters.py
@@ -11288,7 +11288,7 @@ class ConstraintParameter(Parameter):
         self._remove_bookkeeping()
         # logger.debug("ConstraintParameter {} _update_bookkeeping".format(self.twig))
         for param in self.vars.to_list():
-            if param.qualifier == self.qualifier and param.component == self.component:
+            if param.qualifier == self.qualifier and param.component == self.component and param.dataset == self.dataset:
                 # then this is the currently constrained parameter
                 param._is_constraint = self.uniqueid
                 if self.uniqueid in param._in_constraints:
@@ -11299,7 +11299,7 @@ class ConstraintParameter(Parameter):
                     param._in_constraints.append(self.uniqueid)
 
         for param in self.addl_vars.to_list():
-            if param.qualifier == self.qualifier and param.component == self.component:
+            if param.qualifier == self.qualifier and param.component == self.component and param.dataset == self.dataset:
                 # then this is the currently constrained parameter
                 param._is_constraint = self.uniqueid
 

commit fd454c6f0a2a00e0907c66dcda9bf456661ac4ca

fix ld_coeffs_bol lookup error introduced in previous commits
introduced in 533bcdf4867d1f289ce7c70d12d8c69021629faa

phoebe/frontend/bundle.py
@@ -8883,7 +8883,7 @@ class Bundle(ParameterSet):
 
                 ld_coeffs_ret["ld_coeffs{}@{}@{}".format(bol_suffix, ldcs_param.component, 'component' if is_bol else ldcs_param.dataset)] = ld_coeffs
                 if set_value:
-                    self.set_value(qualifier='ld_coeffs{}'.format(bol_suffix), component=ldcs_param.component, dataset=ldcs_param.dataset, context='dataset', check_visible=False, value=ld_coeffs)
+                    self.set_value(qualifier='ld_coeffs{}'.format(bol_suffix), component=ldcs_param.component, dataset=ldcs_param.dataset, context='component' if bol_suffix else 'dataset', check_visible=False, value=ld_coeffs)
             else:
                 raise NotImplementedError("compute_ld_coeffs not implemented for ld_mode{}='{}'".format(bol_suffix, ld_mode))
 