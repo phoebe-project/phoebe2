4b71bf2df3d8c603e6388e3775ce7b50a9b85d7f
Angela <a.kochoska@gmail.com>
2022-04-11 20:12:12 -0400

    Implemented celerite2 and sklearn GPs as separate features

--- phoebe/frontend/bundle.py
+++ phoebe/frontend/bundle.py
@@ -12003,36 +12003,20 @@ class Bundle(ParameterSet):
                 enabled_features = self.filter(qualifier='enabled', compute=compute, context='compute', value=True, **_skip_filter_checks).features
                 
                 for ds in model_ps.datasets:
-                    gp_features = self.filter(feature=enabled_features, dataset=ds, kind='gaussian_process', **_skip_filter_checks).features
-                    if len(gp_features) > 0:
-                        
-                        # NOTE: this is already in run_checks_compute, so this error
-                        # should never be raised
-                        # TODO: update checks to check for celerite2
-                        # if not _use_sklearn:
-                        #     raise ImportError("gaussian processes require scikit-learn to be installed.  Install (pip install scikit-learn) and restart phoebe.")
-
-                        # NOTE: only those exposed in feature.gaussian_process
-                        # will be available to the user (we don't allow jitter, for example)
-                        gp_kernel_classes = {'constant': _sklearn.gaussian_process.kernels.ConstantKernel,
-                                             'white': _sklearn.gaussian_process.kernels.WhiteKernel,
-                                             'rbf': _sklearn.gaussian_process.kernels.RBF,
-                                             'matern': _sklearn.gaussian_process.kernels.Matern,
-                                             'rational_quadratic': _sklearn.gaussian_process.kernels.RationalQuadratic,
-                                             'exp_sine_squared': _sklearn.gaussian_process.kernels.ExpSineSquared,
-                                             'dot_product': _sklearn.gaussian_process.kernels.DotProduct,
-                                             'sho': _celerite2.terms.SHOTerm,
-                                             'rotation': _celerite2.terms.RotationTerm,
-                                             'matern32': _celerite2.terms.Matern32Term
-                                             }
-
+                    gp_sklearn_features = self.filter(feature=enabled_features, dataset=ds, kind='gp_sklearn', **_skip_filter_checks).features
+                    gp_celerite2_features = self.filter(feature=enabled_features, dataset=ds, kind='gp_celerite2', **_skip_filter_checks).features
+                    
+                    if len(gp_sklearn_features)!=0 and len(gp_celerite2_features)!=0:
+                        raise NotImplementedError('Combining GPs from scikit-learn and celerite2 is not supported yet. Please remove one with .remove_feature()')
+                    
+                    elif len(gp_sklearn_features)!=0 or len(gp_celerite2_features)!=0:
+                        # we'll loop over components (for RVs or LPs, for example)
                         # get the data we need to fit the GP model
                         ds_ps = self.get_dataset(dataset=ds, **_skip_filter_checks)
                         xqualifier = {'lp': 'wavelength'}.get(ds_ps.kind, 'times')
                         yqualifier = {'lp': 'flux_densities', 'rv': 'rvs', 'lc': 'fluxes'}.get(ds_ps.kind)
                         yerrqualifier = {'lp': 'wavelength'}.get(ds_ps.kind, 'sigmas')
                         
-                        # we'll loop over components (for RVs or LPs, for example)
                         if ds_ps.kind in ['lc']:
                             ds_comps = [None]
                         else:
@@ -12053,95 +12037,95 @@ class Bundle(ParameterSet):
                                                         return_interp_model=True, 
                                                         as_quantity=False, 
                                                         consider_gaussian_process=False)
-                        
-                        # build the celerite GP object from the enabled GP features attached to this dataset
-                        sklearn_gp_kernels = []
-                        sklearn_alg_operations = []
-                        celerite2_gp_kernels = [] 
-                        celerite2_alg_operations = []
-                        
-                        for gp in gp_features:
-                            gp_ps = self.filter(feature=gp, context='feature', **_skip_filter_checks)
-                            kind = gp_ps.get_value(qualifier='kernel', **_skip_filter_checks)
+                        model_y = model_ps.get_quantity(qualifier=yqualifier, dataset=ds, component=ds_comp, **_skip_filter_checks)
 
-                            kwargs = {p.qualifier: p.value for p in gp_ps.exclude(qualifier=['kernel', 'enabled']).to_list() if p.is_visible}
-                            
-                            exclude_phase_ranges = kwargs.pop('exclude_phase_ranges')
-                            if kind in ['sho', 'rotation', 'matern32']:
-                                celerite2_alg_operations.append(kwargs.pop('alg_operation'))
-                                celerite2_gp_kernels.append(gp_kernel_classes.get(kind)(**kwargs))
-                            else:
-                                sklearn_alg_operations.append(kwargs.pop('alg_operation'))
-                                sklearn_gp_kernels.append(gp_kernel_classes.get(kind)(**kwargs))
-
-                            # TODO: figure out how to handle this per gp_feature or as an external parameter visible only if gp features present
-                            # this way only takes the value from the last feature
-                            
+                        gp_kernels = []
+                        alg_operations = []
                         
-                        if len(exclude_phase_ranges) != 0:
-                            # get t0, period and mask_phases
-                            ephem = self.get_ephemeris(component='binary', period='period', t0='t0_supconj')
-                            t0 = ephem.get('t0', 0.0)
-                            period = ephem.get('period', 1.0)
-                            
-                            phases = np.array(exclude_phase_ranges)
-                            
-                            # determine extent of data wrt t0
-                            i0 = int((t0 - min(ds_x))/period)-1
-                            i1 = int((max(ds_x-t0))/period)+1
-                            
-                            x_new = ds_x
-                            residuals_new = residuals
-                            sigmas_new = ds_sigmas
-                            for i in range(i0,i1+1,1):
-                                for j in range(phases.shape[0]):
-                                    condition = (x_new < t0+(i+phases[j][0])*period) | (x_new > t0+(i+phases[j][1])*period)
-                                    x_new = x_new[condition]
-                                    residuals_new = residuals_new[condition]
-                                    sigmas_new = sigmas_new[condition]
+                        def _load_gps(gp_kernel_classes, gp_features):
                                 
-                            gp_x = x_new
-                            gp_y = residuals_new
-                            gp_yerr = sigmas_new
+                            for gp in gp_features:
+                                gp_ps = self.filter(feature=gp, context='feature', **_skip_filter_checks)
+                                kind = gp_ps.get_value(qualifier='kernel', **_skip_filter_checks)
 
-                        else:
-                            gp_x = ds_x
-                            gp_y = residuals
-                            gp_yerr = ds_sigmas
-                            
-                        # gp_y = np.zeros(len(ds_x))
-                        if len(sklearn_gp_kernels) > 0:
-                            sklearn_gp_kernel = sklearn_gp_kernels[0]
-                            if len(sklearn_gp_kernels) > 1:
-                                for i in range(1, len(sklearn_gp_kernels)):
-                                    if sklearn_alg_operations[i] == 'product':
-                                        sklearn_gp_kernel *= sklearn_gp_kernels[i]
+                                kwargs = {p.qualifier: p.value for p in gp_ps.exclude(qualifier=['kernel', 'enabled']).to_list() if p.is_visible}
+                                # TODO: replace this with getting the parameter from compute options
+                                exclude_phase_ranges = kwargs.pop('exclude_phase_ranges')
+                                
+                                alg_operations.append(kwargs.pop('alg_operation'))
+                                gp_kernels.append(gp_kernel_classes.get(kind)(**kwargs))
+                                
+                                gp_kernel = gp_kernels[0]
+                                for i in range(1, len(gp_kernels)):
+                                    if alg_operations[i] == 'product':
+                                        gp_kernel *= gp_kernels[i]
                                         # print(gp_kernel)
                                     else:
-                                        sklearn_gp_kernel += sklearn_gp_kernels[i]
+                                        gp_kernel += gp_kernels[i]
                                         # print(gp_kernel)
+                                        
+                                        
+                                if len(exclude_phase_ranges) != 0:
+                                        # get t0, period and mask_phases
+                                    ephem = self.get_ephemeris(component='binary', period='period', t0='t0_supconj')
+                                    t0 = ephem.get('t0', 0.0)
+                                    period = ephem.get('period', 1.0)
+                                    
+                                    phases = np.array(exclude_phase_ranges)
+                                    
+                                    # determine extent of data wrt t0
+                                    i0 = int((t0 - min(ds_x))/period)-1
+                                    i1 = int((max(ds_x-t0))/period)+1
+                                    
+                                    x_new = ds_x
+                                    residuals_new = residuals
+                                    sigmas_new = ds_sigmas
+                                    for i in range(i0,i1+1,1):
+                                        for j in range(phases.shape[0]):
+                                            condition = (x_new < t0+(i+phases[j][0])*period) | (x_new > t0+(i+phases[j][1])*period)
+                                            x_new = x_new[condition]
+                                            residuals_new = residuals_new[condition]
+                                            sigmas_new = sigmas_new[condition]
+                                        
+                                    gp_x = x_new
+                                    gp_y = residuals_new
+                                    gp_yerr = sigmas_new
 
-                            gp_regressor = GaussianProcessRegressor(kernel=sklearn_gp_kernel)
+                                else:
+                                    gp_x = ds_x
+                                    gp_y = residuals
+                                    gp_yerr = ds_sigmas
+                                
+                            return gp_kernel, gp_x, gp_y, gp_yerr
+                                            
+                        if len(gp_sklearn_features) > 0:
+                            gp_kernel_classes = {'constant': _sklearn.gaussian_process.kernels.ConstantKernel,
+                                                'white': _sklearn.gaussian_process.kernels.WhiteKernel,
+                                                'rbf': _sklearn.gaussian_process.kernels.RBF,
+                                                'matern': _sklearn.gaussian_process.kernels.Matern,
+                                                'rational_quadratic': _sklearn.gaussian_process.kernels.RationalQuadratic,
+                                                'exp_sine_squared': _sklearn.gaussian_process.kernels.ExpSineSquared,
+                                                'dot_product': _sklearn.gaussian_process.kernels.DotProduct}
+                            
+                            gp_kernel, gp_x, gp_y, gp_yerr = _load_gps(gp_kernel_classes, gp_sklearn_features)
+                            
+                            gp_regressor = GaussianProcessRegressor(kernel=gp_kernel)
                             gp_regressor.fit(gp_x.reshape(-1,1), gp_y)
                             
                             # NOTE: .predict can also be called directly to the model times if we want to avoid interpolation altogether 
-                            gp_y = gp_regressor.predict(ds_x.reshape(-1,1), return_std=False)                              
-                        
-                        if len(celerite2_gp_kernels) > 0:
-                            celerite2_gp_kernel = celerite2_gp_kernels[0]
-                            if len(celerite2_gp_kernels) > 1:
-                                for i in range(1, len(celerite2_gp_kernels)):
-                                    if celerite2_alg_operations[i] == 'product':
-                                        celerite2_gp_kernel *= celerite2_gp_kernels[i]
-                                        # print(gp_kernel)
-                                    else:
-                                        celerite2_gp_kernel += celerite2_gp_kernels[i]
-                                        # print(gp_kernel)
-                            gp = _celerite2.GaussianProcess(celerite2_gp_kernel, mean=0.0)
+                            gp_y = gp_regressor.predict(ds_x.reshape(-1,1), return_std=False) 
+                            
+                        if len(gp_celerite2_features) > 0:
+                            gp_kernel_classes = {'sho': _celerite2.terms.SHOTerm,
+                                                'rotation': _celerite2.terms.RotationTerm,
+                                                'matern32': _celerite2.terms.Matern32Term
+                                                }
+                            gp_kernel, gp_x, gp_y, gp_yerr = _load_gps(gp_kernel_classes, gp_celerite2_features)
+                            
+                            gp = _celerite2.GaussianProcess(gp_kernel, mean=0.0)
                             gp.compute(gp_x, yerr=gp_yerr) 
                             gp_y = gp.predict(gp_y, t=ds_x, return_var=False)
-                            
-                        model_y = model_ps.get_quantity(qualifier=yqualifier, dataset=ds, component=ds_comp, **_skip_filter_checks)
+                                                        
 
                         # store just the GP component in the model PS as well
                         gp_param = FloatArrayParameter(qualifier='gps', value=gp_y, default_unit=model_y.unit, readonly=True, description='GP contribution to the model {}'.format(yqualifier))
