9e8cb003e370f4a3ed0c131753e77ccda0cd90e0
Angela <a.kochoska@gmail.com>
2022-05-03 12:06:42 -0400

    ebai_model --> ebai_method in forbidden labels (◔_◔)

--- phoebe/parameters/parameters.py
+++ phoebe/parameters/parameters.py
@@ -256,7 +256,7 @@ _forbidden_labels += ['enabled', 'dynamics_method', 'ltte', 'comments',
 # from solver:
 _forbidden_labels += ['nwalkers', 'niters', 'priors', 'priors_requires', 'init_from', 'init_from_requires',
                       'lc_datasets', 'rv_datasets', 'lc_combine',
-                      'phase_bin', 'phase_nbins', 'ebai_model',
+                      'phase_bin', 'phase_nbins', 'ebai_method',
                       'algorithm', 'duration', 'minimum_n_cycles', 'frequency_factor',
                       'samples_per_peak', 'nyquist_factor',
                       't0_near_times', 'sample_periods', 'sample_frequencies', 'objective',
