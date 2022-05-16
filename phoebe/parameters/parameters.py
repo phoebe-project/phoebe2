Final edits to the MLE fix. To be strictly correct, the MLE is a _method_ maximases log-likelihood (lnf), thus we are computing lnf and not MLE. Because of that I replaced 'mle' with 'lnf' throughout and I also fixed a small bug and removed a superfluous argument.

phoebe/parameters/parameters.py
@@ -3651,7 +3651,7 @@ class ParameterSet(object):
 
         See also:
         * <phoebe.parameters.ParameterSet.calculate_chi2>
-        * <phoebe.parameters.ParameterSet.calculate_mle>
+        * <phoebe.parameters.ParameterSet.calculate_lnf>
         * <phoebe.parameters.ParameterSet.calculate_lnlikelihood>
 
         Arguments
@@ -3802,9 +3802,9 @@ class ParameterSet(object):
 
     def _calculate_cf(self, model=None, dataset=None, component=None,
                        consider_gaussian_process=True, mask_enabled=None,
-                       mask_phases=None, cf='mle'):
-        if cf not in ['mle', 'chi2']:
-            raise ValueError("cf must be either 'mle' or 'chi2'")
+                       mask_phases=None, cf='lnf'):
+        if cf not in ['lnf', 'chi2']:
+            raise ValueError("cf must be either 'lnf' or 'chi2'")
 
         ret = 0.
 
@@ -3852,10 +3852,10 @@ class ParameterSet(object):
 
                 if len(sigmas):
                     sigmas2 = sigmas**2
-                    if mle and sigmas_lnf != -np.inf:
+                    if cf == 'lnf' and sigmas_lnf != -np.inf:
                         sigmas2 += model_interp.value**2 * np.exp(2 * sigmas_lnf)
 
-                    if cf=='mle':
+                    if cf == 'lnf':
                         ret += np.sum((residuals.value**2 / sigmas2) + np.log(2*np.pi*sigmas2))
                     else:
                         ret += np.sum(residuals.value**2 / sigmas2)
@@ -3866,7 +3866,7 @@ class ParameterSet(object):
 
     def calculate_chi2(self, model=None, dataset=None, component=None,
                        consider_gaussian_process=True, mask_enabled=None,
-                       mask_phases=None, mle=False):
+                       mask_phases=None):
         """
         Compute the chi2 between a model and the observed values in the dataset(s).
 
@@ -3889,7 +3889,7 @@ class ParameterSet(object):
 
         See also:
         * <phoebe.parameters.ParameterSet.calculate_residuals>
-        * <phoebe.parameters.ParameterSet.calculate_mle>
+        * <phoebe.parameters.ParameterSet.calculate_lnf>
         * <phoebe.parameters.ParameterSet.calculate_lnlikelihood>
         * <phoebe.frontend.bundle.Bundle.calculate_lnp>
 
@@ -3926,16 +3926,15 @@ class ParameterSet(object):
                                   mask_enabled=mask_enabled, mask_phases=mask_phases,
                                   cf='chi2')
 
-    def calculate_mle(self, model=None, dataset=None, component=None,
+    def calculate_lnf(self, model=None, dataset=None, component=None,
                       consider_gaussian_process=True, mask_enabled=None,
                       mask_phases=None):
         """
-        Compute the mle (maximum likelihood estimation) between a model and the
-        observed values in the dataset(s).
+        Compute the lnf (log-likelihood under the maximum likelihood
+        estimation) between a model and the observed values in the dataset(s).
 
-        Currently supports the following datasets:
-        * <phoebe.parameters.dataset.lc>
-        * <phoebe.parameters.dataset.rv>
+        Currently supports the following datasets: *
+        <phoebe.parameters.dataset.lc> * <phoebe.parameters.dataset.rv>
 
         If necessary (due to the `compute_times`/`compute_phases` parameters
         or a change in the dataset `times` since the model was computed),
@@ -3945,8 +3944,8 @@ class ParameterSet(object):
 
         Residuals per-dataset for the given model are computed by
         <phoebe.parameters.ParameterSet.calculate_residuals>.  The returned
-        mle value is then the sum over the mle of each dataset, where each
-        dataset's mle value is computed as the sum of squares of residuals
+        lnf value is then the sum over the lnf of each dataset, where each
+        dataset's lnf value is computed as the sum of squares of residuals
         over the squares of sigmas (if available) plus log(2*pi*sigmas**2).
 
         If `sigmas_lnf` is not -inf (default value), then the following term
@@ -3955,28 +3954,30 @@ class ParameterSet(object):
         `interpolated_model**2 * np.exp(2 * sigmas_lnf)`
 
 
-        See also:
-        * <phoebe.parameters.ParameterSet.calculate_chi2>
-        * <phoebe.parameters.ParameterSet.calculate_residuals>
-        * <phoebe.parameters.ParameterSet.calculate_lnlikelihood>
-        * <phoebe.frontend.bundle.Bundle.calculate_lnp>
+        See also: * <phoebe.parameters.ParameterSet.calculate_chi2> *
+        <phoebe.parameters.ParameterSet.calculate_residuals> *
+        <phoebe.parameters.ParameterSet.calculate_lnlikelihood> *
+        <phoebe.frontend.bundle.Bundle.calculate_lnp>
 
         Arguments
         -----------
         * `model` (string, optional, default=None): model to compare against
             observations.  Required if more than one model exist.
-        * `dataset` (string or list, optional, default=None): dataset(s) for comparison.
-            Will sum over chi2 values of all datasets that match the filter.  So
-            if not provided, will default to all datasets exposed in the model.
-        * `component` (string or list, optional, default=None): component(s) for
+        * `dataset` (string or list, optional, default=None): dataset(s) for
+          comparison.
+            Will sum over chi2 values of all datasets that match the filter.
+            So if not provided, will default to all datasets exposed in the
+            model.
+        * `component` (string or list, optional, default=None): component(s)
+          for
             comparison.  Required only if more than one component exist in the
             dataset (for RVs, for example) and not all should be included in
             the chi2
         * `consider_gaussian_process` (bool, optional, default=True): whether
             to consider a system with gaussian process(es) as time-dependent
         * `mask_enabled` (bool, optional, default=None): whether to enable
-            masking on the dataset(s).  If None or not provided, will default to
-            the values set in the dataset(s).
+            masking on the dataset(s).  If None or not provided, will default
+            to the values set in the dataset(s).
         * `mask_phases` (list of tuples, optional, default=None): phase masks
             to apply if `mask_enabled = True`.  If None or not provided, will
             default to the values set in the dataset(s).
@@ -3984,16 +3985,17 @@ class ParameterSet(object):
 
         Returns
         -----------
-        * (float) mle value
+        * (float) lnf value
 
         Raises
         ----------
-        * NotImplementedError: if the dataset kind is not supported for residuals.
+        * NotImplementedError: if the dataset kind is not supported for
+          residuals.
         """
         return self._calculate_cf(model=model, dataset=dataset, component=component,
                                   consider_gaussian_process=consider_gaussian_process,
                                   mask_enabled=mask_enabled, mask_phases=mask_phases,
-                                  cf='mle')
+                                  cf='lnf')
 
     def calculate_lnlikelihood(self, model=None, dataset=None, component=None, consider_gaussian_process=True):
         """
@@ -4008,7 +4010,7 @@ class ParameterSet(object):
         See also:
         * <phoebe.parameters.ParameterSet.calculate_residuals>
         * <phoebe.parameters.ParameterSet.calculate_chi2>
-        * <phoebe.parameters.ParameterSet.calculate_mle>
+        * <phoebe.parameters.ParameterSet.calculate_lnf>
         * <phoebe.frontend.bundle.Bundle.calculate_lnp>
 
         Arguments
@@ -4034,7 +4036,7 @@ class ParameterSet(object):
         * NotImplementedError: if the dataset kind is not supported for residuals.
         """
 
-        return -0.5 * self.calculate_mle(model, dataset, component, consider_gaussian_process=consider_gaussian_process)
+        return -0.5 * self.calculate_lnf(model, dataset, component, consider_gaussian_process=consider_gaussian_process)
 
     def _unpack_plotting_kwargs(self, animate=False, **kwargs):
 