1cd4ed2b1e9a88ee12beb0d693322df08971a5b0
Kyle <kyleconroy@gmail.com>
2022-06-24 15:09:42 -0400

    update branch tag in version

--- phoebe/__init__.py
+++ phoebe/__init__.py
@@ -17,7 +17,7 @@ Available environment variables:
 
 """
 
-__version__ = '2.4.3'
+__version__ = '2.4.3.dev+release-2.5'
 
 import os as _os
 import sys as _sys
--- setup.py
+++ setup.py
@@ -405,8 +405,8 @@ else:
     long_description = "\n".join(long_description_s[long_description_s.index("INTRODUCTION"):])
 
 setup (name = 'phoebe',
-       version = '2.4.3',
-       description = 'PHOEBE devel version',
+       version = '2.4.3.dev+release-2.5',
+       description = 'PHOEBE 2.4',
        long_description=long_description,
        author = 'PHOEBE development team',
        author_email = 'phoebe-devel@lists.sourceforge.net',
