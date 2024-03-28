6fc6cb4ef5c16ac751e84cfd8e6457e2b250e1b2
Andrej Prsa <aprsa09@gmail.com>
2024-03-28 09:53:39 -0400

    Fixing the if statement per @kecnry's instructions.

--- phoebe/frontend/bundle.py
+++ phoebe/frontend/bundle.py
@@ -1,7 +1,6 @@
 import sys
 import subprocess
 import os
-import numpy as np
 
 try:
     from subprocess import DEVNULL
@@ -3783,8 +3782,8 @@ class Bundle(ParameterSet):
             # NOTE: atms are not attached to datasets, but per-compute and per-component
             # NOTE: atmparam includes a '_default' compute pset, which depends on the
             # ck2004 atmospheres; the checks should not include it. This is achieved
-            # by filtering check_visible=True in the line below:
-            for atmparam in self.filter(qualifier='atm', kind='phoebe', compute=computes, check_visible=True, check_default=False).to_list() + self.filter(qualifier='ld_coeffs_source').to_list():
+            # by filtering check_visible=False and check_default=True in the line below:
+            for atmparam in self.filter(qualifier='atm', kind='phoebe', compute=computes, check_visible=False, check_default=True).to_list() + self.filter(qualifier='ld_coeffs_source').to_list():
                 # check to make sure passband supports the selected atm
                 atm = atmparam.get_value(**_skip_filter_checks)
                 if atmparam.qualifier == 'ld_coeffs_source' and atm == 'auto':
