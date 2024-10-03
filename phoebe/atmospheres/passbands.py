9630efb363dbb9a6afa408c50cfe63b5d929dcae
Andrej Prsa <aprsa@villanova.edu>
2024-10-03 16:04:34 +0200

    Fixes invalid escape sequences (#948)
    Strings with invalid escape sequences raised a deprecation syntax warning in python 3.11 but a syntax error in 3.12+. All instances (at least the ones that present themselves at import time) have been fixed.

--- phoebe/atmospheres/passbands.py
+++ phoebe/atmospheres/passbands.py
@@ -585,7 +585,7 @@ class Passband:
         return hclkt * expterm/(expterm-1)
 
     def _bb_intensity(self, Teff, photon_weighted=False):
-        """
+        r"""
         Computes mean passband intensity using blackbody atmosphere:
 
         I_pb^E = \int_\lambda I(\lambda) P(\lambda) d\lambda / \int_\lambda P(\lambda) d\lambda
@@ -611,7 +611,7 @@ class Passband:
             return integrate.quad(pb, self.wl[0], self.wl[-1])[0]/self.ptf_area
 
     def _bindex_blackbody(self, Teff, photon_weighted=False):
-        """
+        r"""
         Computes the mean boosting index using blackbody atmosphere:
 
         B_pb^E = \int_\lambda I(\lambda) P(\lambda) B(\lambda) d\lambda / \int_\lambda I(\lambda) P(\lambda) d\lambda
@@ -1691,7 +1691,7 @@ class Passband:
             f.close()
 
     def compute_ck2004_ldints(self):
-        """
+        r"""
         Computes integrated limb darkening profiles for ck2004 atmospheres.
         These are used for intensity-to-flux transformations. The evaluated
         integral is:
@@ -1738,7 +1738,7 @@ class Passband:
             self.content.append('ck2004:ldint')
 
     def compute_phoenix_ldints(self):
-        """
+        r"""
         Computes integrated limb darkening profiles for PHOENIX atmospheres.
         These are used for intensity-to-flux transformations. The evaluated
         integral is:
@@ -2093,7 +2093,7 @@ class Passband:
         return 10**Imu
 
     def Inorm(self, Teff=5772., logg=4.43, abun=0.0, atm='ck2004', ldatm='ck2004', ldint=None, ld_func='interp', ld_coeffs=None, photon_weighted=False):
-        """
+        r"""
 
         Arguments
         ----------
@@ -2173,7 +2173,7 @@ class Passband:
         return retval
 
     def Imu(self, Teff=5772., logg=4.43, abun=0.0, mu=1.0, atm='ck2004', ldatm='ck2004', ldint=None, ld_func='interp', ld_coeffs=None, photon_weighted=False):
-        """
+        r"""
         Arguments
         ----------
         * `Teff`
@@ -3244,7 +3244,7 @@ def get_passband(passband, content=None, reload=False, update_if_necessary=False
     return _pbtable[passband]['pb']
 
 def Inorm_bol_bb(Teff=5772., logg=4.43, abun=0.0, atm='blackbody', photon_weighted=False):
-    """
+    r"""
     Computes normal bolometric intensity using the Stefan-Boltzmann law,
     Inorm_bol_bb = 1/\pi \sigma T^4. If photon-weighted intensity is
     requested, Inorm_bol_bb is multiplied by a conversion factor that
