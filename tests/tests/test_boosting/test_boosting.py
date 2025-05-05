d36bc2f24918a905de0f4e5e27a2bc4cbbb98b08
David Jones <davidjones.pne@gmail.com>
2025-05-05 11:23:20 +0100

    Simple test for boosting
    Simple test that checks boosting can be set for one component and that it actually boosts the light curve.

---
+++ tests/tests/test_boosting/test_boosting.py
@@ -0,0 +1,22 @@
+import phoebe
+
+
+
+
+def test_boosting(verbose=False):
+    b=phoebe.default_binary()
+    b.add_dataset('lc', times=0.25)
+    b.run_compute()
+    noboost=b['fluxes@lc@model'].value
+    b.set_value_all('boosting_method','manual')
+    b['boosting_index@primary']=2.
+    b.run_compute()
+    boost=b['fluxes@lc@model'].value
+    
+    assert boost > noboost
+
+
+
+if __name__ == '__main__':
+    test_boosting()
+
