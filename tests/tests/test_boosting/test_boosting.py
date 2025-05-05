ec95dd05cd6235c5caccf0f3a128003217c9e781
David Jones <davidjones.pne@gmail.com>
2025-05-05 17:14:01 +0100

    Update test_boosting.py

--- tests/tests/test_boosting/test_boosting.py
+++ tests/tests/test_boosting/test_boosting.py
@@ -7,7 +7,7 @@ def test_boosting(verbose=False):
     b=phoebe.default_binary()
     b.add_dataset('lc', times=0.25)
     b.run_compute(irrad_method='none')
-    noboost=b['fluxes@lc@model'].value
+    noboost = b.get_value(qualifier='fluxes', kind='lc', context='model')
     b.set_value_all('boosting_method','manual')
     b.set_value(qualifier='boosting_index', component='primary', value=2)
     b.run_compute(irrad_method='none')
