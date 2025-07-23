06c5c0488f11bb60671705969f1bd8e4f95d3135
Andrej Prsa <andrej@prsa.us>
2025-07-23 09:57:33 +0200

    Support for non-standard openmpi paths This commit adds openmpi_path and openmpi_ld_library_path to the Server class (and derivatives).
    Example:
    ```
    from phoebe.dependencies import crimpl
    s = crimpl.RemoteSlurmServer(host='terra', directory='~/workshop/crimpl', openmpi_path='/opt/openmpi-4.1.6/bin', openmpi_ld_library_path='/opt/openmpi-4.1.6/lib')
    s.save('terra', overwrite=True)
    ```

--- phoebe/dependencies/crimpl/common.py
+++ phoebe/dependencies/crimpl/common.py
@@ -36,11 +36,13 @@ def _run_cmd(cmd, detach=False, log_cmd=True, allow_retries=True):
             return ret
 
 class Server(object):
-    def __init__(self, directory=None):
+    def __init__(self, directory=None, openmpi_path=None, openmpi_ld_library_path=None):
         self._directory = directory
         self._directory_exists = False
+        self._openmpi_path = openmpi_path
+        self._openmpi_ld_library_path = openmpi_ld_library_path
 
-        self._dict_keys = ['directory']
+        self._dict_keys = ['directory', 'openmpi_path', 'openmpi_ld_library_path']
 
     def __repr__(self):
         def _format_val(v):
@@ -69,6 +71,14 @@ class Server(object):
         if "~" not in self._directory:
             return _os.path.join("~", self._directory)
         return self._directory
+
+    @property
+    def openmpi_path(self):
+        return self._openmpi_path
+
+    @property
+    def openmpi_ld_library_path(self):
+        return self._openmpi_ld_library_path
 
     @property
     def existing_jobs(self):
@@ -183,7 +193,7 @@ class Server(object):
         if conda_env is False:
             return None, None
 
-        default_deps = "pip numpy"
+        default_deps = "pip"
 
         if not (isinstance(conda_env, str) or conda_env is None):
             raise TypeError("conda_env must be a string or None")
@@ -220,13 +230,12 @@ class Server(object):
 
             # create the environment at the server level
             envpath = _os.path.join(self.directory, "crimpl-envs", conda_env)
-            cmd = "conda create -p {envpath} -y {default_deps} python>={python_version}".format(envpath=envpath, default_deps=default_deps, python_version=python_version)
+            cmd = "conda create -p {envpath} -y {default_deps} python={python_version}".format(envpath=envpath, default_deps=default_deps, python_version=python_version)
 
         if run_cmd:
             return self._run_server_cmd(cmd), envpath
         else:
             return self.ssh_cmd.format(cmd), envpath
-
 
     def _create_crimpl_directory(self):
 
@@ -240,14 +249,17 @@ class Server(object):
         else:
             # TODO: use temporary file
             f = open('exportpath.sh', 'w')
-            f.write('export PATH="{}/crimpl-bin:$PATH"'.format(self.directory.replace("~", "$HOME")))
+            f.write(f'export PATH="{self.directory.replace("~", "$HOME")}/crimpl-bin:$PATH"\n')
+            if self.openmpi_path:
+                f.write(f'export PATH="{self.openmpi_path}:$PATH"\n')
+            if self.openmpi_ld_library_path:
+                f.write(f'export LD_LIBRARY_PATH="{self.openmpi_ld_library_path}:$LD_LIBRARY_PATH"\n')
             f.close()
             scp_cmd = self.scp_cmd_to.format(local_path='exportpath.sh', server_path=self.directory+"/")
             _run_cmd(scp_cmd)
 
         self._directory_exists = True
         return True
-
 
     def install_conda(self, in_server_directory=False):
         """
