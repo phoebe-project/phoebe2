02f32448d777824baa3bcc7b766eb709aebee1ca
Kyle Conroy <kyleconroy@gmail.com>
2026-07-07 08:29:21 -0400

    remove redundant definitions of allowed_components/datasets
    these are now stored in the classes themselves

--- phoebe/parameters/feature.py
+++ phoebe/parameters/feature.py
@@ -10,27 +10,19 @@ logger.addHandler(logging.NullHandler())
 
 ### NOTE: if creating new parameters, add to the _forbidden_labels list in parameters.py
 
-_allowed_components = {'gp_sklearn': [None],
-                       'gp_celerite2': [None]}
-
-_allowed_datasets = {'gp_sklearn': ['lc', 'rv', 'lp'],
-                     'gp_celerite2': ['lc', 'rv', 'lp']} 
-
 _feature_classes = {}
 
 
 def _component_allowed_for_feature(feature_kind, component_kind):
-    return component_kind in getattr(feature_kind, 'allowed_component_kinds', _allowed_components.get(feature_kind, []))
+    return component_kind in getattr(feature_kind, 'allowed_component_kinds', [])
 
 
 def _dataset_allowed_for_feature(feature_kind, dataset_kind):
-    return dataset_kind in getattr(feature_kind, 'allowed_dataset_kinds', _allowed_datasets.get(feature_kind, []))
+    return dataset_kind in getattr(feature_kind, 'allowed_dataset_kinds', [])
 
 
 def _register_feature(feature_cls, name):
     globals()[name] = feature_cls.create_feature_parameters
-    _allowed_components[name] = getattr(feature_cls, 'allowed_component_kinds', [])
-    _allowed_datasets[name] = getattr(feature_cls, 'allowed_dataset_kinds', [])
     _feature_classes[name] = feature_cls
 
 
