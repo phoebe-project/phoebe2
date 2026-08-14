e6865817990ccc925cf8ccf1a0494ba07230e6d0
Kyle Conroy <kyleconroy@gmail.com>
2026-08-14 13:49:20 -0400

    fix calculate_lnlikelihood with meshes in model (#1172)

--- tests/tests/test_solver_infra/test_solver_times.py
+++ tests/tests/test_solver_infra/test_solver_times.py
@@ -1,3 +1,4 @@
+import numpy as np
 import phoebe
 
 
@@ -10,6 +11,33 @@ def test_parse_solver_times():
     b.add_solver('optimizer.nelder_mead')
     b.set_value('solver_times', 'times')
     b.parse_solver_times()
+
+
+def test_calculate_lnlikelihood_with_mesh_dataset_present():
+    b = phoebe.default_binary()
+    times = phoebe.linspace(0, 1, 5)
+    synthetic_fluxes = np.ones(5)
+    synthetic_sigmas = np.full(5, 1e-3)
+
+    b.add_dataset(
+        'lc',
+        dataset='lc01',
+        times=times,
+        fluxes=synthetic_fluxes,
+        sigmas=synthetic_sigmas,
+        passband='Johnson:V',
+    )
+    b.add_dataset('mesh', dataset='mesh01', compute_times=[0.0])
+
+    b.add_compute(compute='fast_compute')
+    b.set_value('irrad_method', compute='fast_compute', value='none')
+    b.set_value_all('distortion_method', compute='fast_compute', value='sphere')
+    b.set_value_all('ntriangles', compute='fast_compute', value=100)
+
+    b.run_compute(compute='fast_compute', model='fit_model')
+    lnlike = b.calculate_lnlikelihood(model='fit_model')
+
+    assert np.isfinite(lnlike)
 
 
 if __name__ == '__main__':
